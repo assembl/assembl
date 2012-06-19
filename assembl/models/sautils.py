ca5de765784bed9bd1c3618da8da3c6d42988123
Nicolas Cadou <ncadou@cadou.ca>
2012-06-19 17:19:12 -0400

    Added a crude data storage abstraction layer

--- assembl/models/sautils.py
+++ assembl/models/sautils.py
@@ -7,13 +7,47 @@ from sqlalchemy import Column, DateTime
 _DB = None
 
 
+def db():
+    """ Grab the DBSession object and avoid circular dependency. """
+    global _DB
+    if _DB is None:
+        from . import DBSession as _DB
+    return _DB
+
+
 class BaseOps(object):
-    """ Basic database operations are abstracted away in this class. """
+    """ Basic database operations are abstracted away in this class.
+
+    The idea is to have the API as independent as practically possible from
+    both data storage- and web- specific stuff.
+
+    """
+    def delete(self):
+        db().delete(self)
+
+    @classmethod
+    def get(cls, **criteria):
+        return db().query(cls).filter_by(**criteria).one()
+
+    @classmethod
+    def list(cls, **criteria):
+        return db().query(cls).filter_by(**criteria).all()
+
+    @property
+    def is_new(self):
+        return True or False  # TODO
+
+    def save(self, flush=True):
+        if self.is_new:
+            db().add(self)
+            if flush:
+                db().flush()
 
     def iteritems(self, include=None, exclude=None):
         """ Return a generator that iterates through model columns. """
-        if include and exclude:
-            raise ValueError('Args include and exclude are mutually exclusive')
+        if include is not None and exclude is not None:
+            include = set(include) - set(exclude)
+            exclude = None
         for c in self.__table__.columns:
             if ((not include or c.name in include)
             and (not exclude or c.name not in exclude)):
