afe2b54ca402d037b9d5fb49db56dc222a16ddfd
Janina Szkut <jszkut@caravan.coop>
2012-10-22 17:12:28 -0400

    JS - add a few attributes to Post model, create Node model

--- assembl/static/js/assembl.js
+++ assembl/static/js/assembl.js
@@ -10,7 +10,10 @@ $(function(){
 	        subject: null,
 	        body: null,
 	        parent: null,
-	        children: null
+	        children: null,
+            read: false,
+            caught: false,
+            synthesis: false
 	      };
 	    },
 
@@ -23,6 +26,22 @@ $(function(){
     	// Reference to this collection's model.
     	model: Post,
     	url: '/api/posts?start=17&levels=6'
+    });
+
+    /* A node of the full debate summary tree */
+    /* Can also be a ToC node or a Synthesis node */
+    var Node = Backbone.Model.extend({
+        defaults: function() {
+            return {
+                title: null,
+                description: null,
+                extracts: [], // Contains Open Annotations - to be determined how to model
+                children: [],
+                parent: null,
+                isTOC: false,
+                isSynthesis: false
+            }
+        }
     });
 
     var PostView = Backbone.View.extend({
@@ -41,10 +60,7 @@ $(function(){
         
 
         render: function() {
-            //console.log(this.model.toJSON());
             this.$el.html(ich.messages(this.model.toJSON()));
-            //xthis.$el.html(this.model.get('body'));
-            //this.$el.html('Testing, testing, 1 2 3');
             return this;
         }
     });
@@ -104,17 +120,7 @@ $(function(){
                 }
 
                 prev = post.id;
-                //for (var i = parent_stack.length - 1; i >= 0; i--) {
-                //    message_html += '*';
-                //};
                 message_html += ich.messages(post.toJSON(), true);
-                /*message_html += '<li>';
-                message_html += post.get('subject');
-                message_html += ' My ID ';
-                message_html += post.get('id');
-                message_html += ' Parent ID ';
-                message_html += post.get('parent_id');
-                message_html += '</li>';*/
             });
             
             $(this.el).html(message_html)
