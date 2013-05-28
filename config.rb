4a32770f207be00478f09a3a4d04b789dd1fbab6
André Farzat <andrefarzat@gmail.com>
2013-05-28 15:57:45 -0400

    Adding icons to styleguide

--- config.rb
+++ config.rb
@@ -6,11 +6,12 @@ css_dir = "assembl/static/css"
 sass_dir = "assembl/static/css"
 
 images_dir = 'assembl/static/img'
-images_path = images_dir
-sprite_load_path = images_dir
-generated_images_dir = images_dir + '/sprites'
-
-http_images_path = "/static/img"
+images_path = 'assembl/static/img'
+generated_images_path = 'assembl/static/img/sprites'
+sprite_load_path = 'assembl/static/img'
+generated_images_dir = 'assembl/static/img/sprites'
+http_images_path = '/static/img'
+http_generated_images_path = '/static/img/sprites'
 
 javascripts_dir = "assembl/static/js"
 
