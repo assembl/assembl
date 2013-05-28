e8f1baa8d246a98e00806f51779f181db8bd8e6d
André Farzat <andrefarzat@gmail.com>
2013-05-28 15:04:22 -0400

    Updating config.rb for sprites to be conform the paths

--- config.rb
+++ config.rb
@@ -1,10 +1,19 @@
 # Compass Config file
 
 http_path = "/"
+
 css_dir = "assembl/static/css"
 sass_dir = "assembl/static/css"
-images_path = 'assembl/static/img'
-javascripts_dir = "assembl/static/js"
+
+images_dir = 'assembl/static/img'
+images_path = images_dir
+sprite_load_path = images_dir
+generated_images_dir = images_dir + '/sprites'
+
 http_images_path = "/static/img"
+
+javascripts_dir = "assembl/static/js"
+
 additional_import_paths = [ ]
+
 sass_options = { :debug_info => true } # activing source maps
