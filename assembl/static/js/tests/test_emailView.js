eb257a8d5b70fcc76a5555bc7bc593d15bd7d11c
André Farzat <andrefarzat@gmail.com>
2013-06-04 20:19:25 -0400

    Creating several test cases for emailView

--- assembl/static/js/tests/test_emailView.js
+++ assembl/static/js/tests/test_emailView.js
@@ -1,16 +1,17 @@
 define(['jasmine', 'underscore', 'app', 'views/email'],
 function(jasmine, _, app, EmailView){
 
-
-	return describe('Email view', function(){
+    return describe('Email view', function(){
         var view,
-            fixEmails;
+            fixEmails,
+            DATA_LEVEL = 'data-emaillist-level';
 
         beforeEach(function(){
             view = new EmailView();
             view.model = new Backbone.Model({
                 subject: 'Default Suject',
-                total: 0,
+                total: 10,
+                level: 1,
                 hasChildren: false
             });
 
@@ -20,22 +21,83 @@ function(jasmine, _, app, EmailView){
             fixEmails.append( view.render().el );
         });
 
-		it('uses the right template', function(){
-			var tmpl = app.loadTemplate('email');
-            var email = (new Email.Model()).toJSON();
+        it('uses the right template', function(){
+            var tmpl = app.loadTemplate('email');
 
-			expect(tmpl(email)).toBe(view.template(email));
-		});
-
+            expect(tmpl(view.model.toJSON())).toBe(view.template(view.model.toJSON()));
+        });
 
         it('must change the state to .is-selected when checkbox is selected', function(){
             var chk = view.$('input').first();
             chk.trigger('click');
-            
+
             expect(view.el).toHaveClass('is-selected');
         });
 
+        it('should have the arrow if hasChildren is true', function(){
+            expect(view.el).not.toContain('span.emaillist-label-arrow');
 
-	});
+            view.model.set('hasChildren', true);
+            fixEmails.empty().append( view.render().el );
+
+            expect(view.el).toContain('span.emaillist-label-arrow');
+        });
+
+        it('should show the options when it swipes to left', function(){
+            var label = view.$('.emaillist-label');
+            label.trigger('swipeLeft');
+
+            expect(label.get(0)).toHaveClass('is-optioned');
+        });
+
+        it('should hide the options when it swipes to right', function(){
+            var label = view.$('.emaillist-label');
+
+            label.addClass('is-optioned');
+            label.trigger('swipeRight');
+
+            expect(label.get(0)).not.toHaveClass('is-optioned');
+        });
+
+        it('should have the data-emaillist-level the same of level attribute', function(){
+            var level = ~~view.el.getAttribute(DATA_LEVEL);
+
+            expect( level ).toBe( view.model.get('level') );
+        });
+
+        it('should be hidden if the level is bigger than 1', function(){
+            view.model.set('level', 2);
+            fixEmails.empty().append( view.render().el );
+
+            expect( view.el ).toHaveClass('is-hidden');
+        });
+
+        it('should have the counter the same of the total property', function(){
+            var counter = ~~ view.$('.fixedcounter').text();
+            expect( counter ).toBe(view.model.get('total'));
+        });
+
+        it('should trigger toggle method when click in the arrow', function(){
+            spyOn(EmailView.prototype, 'toggle').andCallThrough();
+            spyOn(EmailView.prototype, 'showItemInCascade').andCallThrough();
+            spyOn(EmailView.prototype, 'closeItemInCascade').andCallThrough();
+            view = new EmailView({model:view.model});
+
+            view.model.set('hasChildren', true);
+            fixEmails.empty().append( view.render().el );
+
+            var arrow = view.$('.emaillist-label-arrow');
+            arrow.trigger('click');
+
+            expect(view.toggle).toHaveBeenCalled();
+            expect(view.showItemInCascade).toHaveBeenCalled();
+            expect(view.el).toHaveClass('is-open');
+
+            arrow.trigger('click');
+            expect(view.toggle.callCount).toBe(2);
+            expect(view.closeItemInCascade).toHaveBeenCalled();                        
+        });
+
+    });
 
 });
--- assembl/static/js/views/email.js
+++ assembl/static/js/views/email.js
@@ -1,37 +1,72 @@
 define(['backbone', 'underscore', 'jquery', 'app'],
 function(Backbone, _, $, app){
 
-    return Backbone.View.extend({
+    var DATA_LEVEL = 'data-emaillist-level';
+
+    var EmailView = Backbone.View.extend({
         tagName: 'li',
         template: app.loadTemplate('email'),
         events: {
-            'click .fixedcounter': 'sim',
-            'click [type=checkbox]': 'onCheckboxClick'
+            'click [type=checkbox]': 'onCheckboxClick',
+            'swipeLeft .emaillist-label': 'showOptions',
+            'swipeRight .emaillist-label': 'hideOptions',
+            'click .emaillist-label-arrow': 'toggle'
         },
         render: function(){
             var data = this.model.toJSON();
-            this.el.setAttribute('data-emaillist-level', data.level);
+            this.el.setAttribute(DATA_LEVEL, data.level);
             this.$el.addClass('emaillist-item');
 
             if( data.level > 1 ){
                 this.$el.addClass('is-hidden');                
             }
 
-            if( data.hasChildren ){
-                this.$el.addClass('emaillist-item--toplevel');
-            }
-
             this.$el.html(this.template(data));
             return this;
         },
+        // Methods
+        showItemInCascade: function(item, parentLevel){
+            if( item.length === 0 ){
+                return;
+            }
 
-        // Events
-        sim: function(){
-            alert('You just click on .fixedcounter');
+            var currentLevel = ~~item.attr(DATA_LEVEL);
+
+            if( currentLevel === (parentLevel+1) ){
+                item.removeClass("is-hidden");
+                this.showItemInCascade(item.next(), parentLevel);
+            }
+        },
+        closeItemInCascade: function (item, parentLevel){
+            if( item.length === 0 ){
+                return;
+            }
+
+            var currentLevel = ~~item.attr('data-emaillist-level');
+
+            if( currentLevel > parentLevel ){
+                item.addClass("is-hidden").removeClass('is-open');
+                this.closeItemInCascade(item.next(), parentLevel);
+            }
+        },
+        // Events,
+        showOptions: function(ev){
+            $(ev.currentTarget).addClass('is-optioned');
+        },
+        hideOptions: function(ev){
+            $(ev.currentTarget).removeClass('is-optioned');
+        },
+        toggle: function(ev){
+            if( this.$el.hasClass('is-open') ){
+                this.$el.removeClass('is-open');
+                this.closeItemInCascade( this.$el.next(), ~~this.$el.attr(DATA_LEVEL) );
+            } else {
+                this.$el.addClass('is-open');
+                this.showItemInCascade( this.$el.next(), ~~this.$el.attr(DATA_LEVEL) );
+            }
         },
         onCheckboxClick: function(ev){
             var chk = ev.currentTarget;
-            alert( chk );
 
             if( chk.checked ){
                 this.$el.addClass('is-selected');
@@ -41,4 +76,15 @@ function(Backbone, _, $, app){
         }
     });
 
+    /**
+     * States
+     */
+    EmailView.prototype.states = {
+        hidden: 'is-hidden',
+        optioned: 'is-optioned',
+        selected: 'is-selected',
+        open: 'is-open'
+    };
+
+    return EmailView;
 });
