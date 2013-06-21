b4f9ce73d7eb4b487fb5a3dd12ec57148c3764a8
André Farzat <andrefarzat@gmail.com>
2013-06-21 18:01:02 -0400

    interface 0.2

--- assembl/static/js/main.js
+++ assembl/static/js/main.js
@@ -2,8 +2,10 @@ define([
     "app",
     "zepto",
     "views/lateralMenu",
-    "views/ideaList"
-], function(app, $, LateralMenu, IdeaList){
+    "views/ideaList",
+    "views/segmentList",
+    "views/message"
+], function(app, $, LateralMenu, IdeaList, SegmentList, Message){
     'use strict';
 
     app.init();
@@ -12,32 +14,24 @@ define([
     app.lateralMenu = new LateralMenu({el: '#lateralMenu'}).render();
     $('#assembl-mainbutton').on('click', app.lateralMenu.trigger.bind(app.lateralMenu, 'toggle'));
 
-    // Table of contents
-    app.tableOfContents = new IdeaList({ el: '#table-of-contents' });
-    app.tableOfContents.ideas.fetch({reset: true});
+    // Idea list
+    app.ideaList = new IdeaList({ el: '#idealist' });
+    app.ideaList.ideas.fetch({reset: true});
 
-    // var inboxEmails = new Email.Collection();
-    // app.inbox = new Inbox({collection: inboxEmails}).render();
-    // app.inbox.collection.fetch({reset: true});
-
-    // Lateral menu
-    // var lateralMenuEmails = new Email.Collection();
-    // app.lateralMenu = new LateralMenu({el: '#lateralMenu', emails: lateralMenuEmails});
-    // app.lateralMenu.emails.fetch({reset: true});
-
-    // SegmentList
-    // app.segmentList = new SegmentList({el: '#segmentlist'}).render();
+    // Segment List
+    app.segmentList = new SegmentList({el: '#segmentlist'});
+    app.segmentList.render();
 
     // Message
-    // app.message = new Message({el: '#message'}).render();
-    // app.on('lateralmenu.open', function(){
-    //     app.message.hideTooltip();
-    // });
-    // app.selectionTooltip.on('click', function(){
-    //     app.selectionTooltip.hide();
-    //     var segment = app.selectionTooltip.attr('data-segment');
-    //     app.segmentList.addSegment(segment);
-    // });
+    app.message = new Message({el: '#message'}).render();
+    app.on('lateralmenu.open', function(){
+        app.message.hideTooltip();
+    });
+    app.selectionTooltip.on('click', function(){
+        app.selectionTooltip.hide();
+        var segment = app.selectionTooltip.attr('data-segment');
+        app.segmentList.addSegment(segment);
+    });
 
 
 });
