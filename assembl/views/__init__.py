719c88359fc9bb158281e686037c09abdfd6484e
André Farzat <andrefarzat@gmail.com>
2013-07-17 13:17:54 -0400

    Creating the login page

--- assembl/views/__init__.py
+++ assembl/views/__init__.py
@@ -17,6 +17,7 @@ def includeme(config):
 
     config.add_renderer('json', json_renderer_factory)
     config.add_route('home', '/')
+    config.add_route('login', '/login')
     config.add_route('toc', '/toc')
     config.add_route('nodetest', '/nodetest')
     config.add_route('styleguide', '/styleguide')
