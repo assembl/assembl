1e0c979eb1e4284d151a9b7f7a4603ae129c0eae
Jeff Marshall <jeff@classy.co>
2013-07-17 14:41:58 -0400

    Configuring the SQLA scoped session the old-fashioned way.
    It doesn't look like `lib/sqla.py` is used anywhere anymore. Leaving it where
    it is just in case.

--- assembl/__init__.py
+++ assembl/__init__.py
@@ -1,16 +1,23 @@
 """ Pyramid add start-up module. """
 
 from pyramid.config import Configurator
+from sqlalchemy import engine_from_config
+
+from .db import DBSession
 
 
 def main(global_config, **settings):
     """ Return a Pyramid WSGI application. """
     settings['config_uri'] = global_config['__file__']
 
+    # here we create the engine and bind it to the (not really a) session
+    # factory called DBSession.
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
+
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.include('cornice')  # REST services library.
-    config.include('.lib.sqla')
     config.include('.lib.alembic')
     config.include('.lib.email')
     config.include('.views')
