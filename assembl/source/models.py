1ef97b3bf76b09f1c4c5494fdb3e09e2d546ef5f
Jeff Marshall <jeff@classy.co>
2013-07-23 17:46:26 -0400

    Adding 'ancestry' field and methods to Post.

--- assembl/source/models.py
+++ assembl/source/models.py
@@ -10,6 +10,7 @@ from sqlalchemy import (
     Boolean,
     Integer, 
     String, 
+    Text,
     Unicode, 
     UnicodeText, 
     DateTime,
@@ -289,6 +290,7 @@ class Post(SQLAlchemyBaseModel):
     id = Column(Integer, primary_key=True)
     creation_date = Column(DateTime, nullable=False, default=datetime.utcnow)
 
+    ancestry = Column(Text)
     parent_id = Column(Integer, ForeignKey('post.id'))
     children = relationship(
         "Post",
@@ -298,6 +300,41 @@ class Post(SQLAlchemyBaseModel):
 
     content_id = Column(Integer, ForeignKey('content.id', ondelete='CASCADE'))
     content = relationship('Content', uselist=False)
+
+    def get_descendants(self):
+        ancestry_query_string = "%s%d,%%" % (self.ancestry or '', self.id)
+
+        descendants = DBSession.query(Post).filter(
+            Post.ancestry.like(ancestry_query_string)
+        ).all()
+
+        return descendants
+
+    def set_ancestry(self, new_ancestry):
+        descendants = self.get_descendants()
+        old_ancestry = self.ancestry or ''
+        self.ancestry = new_ancestry
+        DBSession.add(self)
+
+        for descendant in descendants:
+            updated_ancestry = descendant.ancestry.replace(
+                "%s%d," % (old_ancestry, self.id),
+                "%s%d," % (new_ancestry, self.id),
+                1
+            )
+
+            descendant.ancestry = updated_ancestry
+            DBSession.add(descendant)
+            
+    def set_parent(self, parent):
+        self.parent = parent
+        DBSession.add(self)
+        DBSession.add(parent)
+
+        self.set_ancestry("%s%d," % (
+            parent.ancestry or '',
+            parent.id
+        ))
 
     def __repr__(self):
         return "<Post '%s'>" % self.content
