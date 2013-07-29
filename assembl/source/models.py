24faf0b1474cadb3ed29b629602b6ebf52e0f045
Jeff Marshall <jeff@classy.co>
2013-07-28 21:09:36 -0400

    Adding method Post.number_of_unread_descendants()

--- assembl/source/models.py
+++ assembl/source/models.py
@@ -341,5 +341,13 @@ class Post(SQLAlchemyBaseModel):
             parent.id
         ))
 
+    def number_of_unread_descendants(self):
+        ancestry_query_string = "%s%d,%%" % (self.ancestry or '', self.id)
+        return DBSession.query(Post).filter(
+                Post.ancestry.like(ancestry_query_string)
+            ).filter_by(
+                is_read=False
+            ).count()
+
     def __repr__(self):
         return "<Post '%s'>" % self.content
