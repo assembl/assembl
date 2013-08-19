b5c4d1b59620b2148c01ba722ec2c875fbb75618
André Farzat <andrefarzat@gmail.com>
2013-08-19 10:07:13 -0300

    Removing the generation of uuid segment creation

--- assembl/static/js/models/segment.js
+++ assembl/static/js/models/segment.js
@@ -11,10 +11,10 @@ function(Backbone, app, moment, User){
          * @init
          */
         initialize: function(){
-            if( !this.id ){
-                this.id = app.createUUID();
-                this.attributes.id = this.id;
-            }
+            // if( !this.id ){
+            //     this.id = app.createUUID();
+            //     this.attributes.id = this.id;
+            // }
 
             if( !this.get('creationDate') ){
                 this.set( 'creationDate', app.getCurrentTime() );
