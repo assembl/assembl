d2d55ef4b5bf17f9a7e3b24ff1320cc667f92ac0
Marc-Antoine Parent <maparent@acm.org>
2013-09-04 15:52:33 -0400

    two authentication bugs

--- assembl/views/auth/views.py
+++ assembl/views/auth/views.py
@@ -108,6 +108,7 @@ def get_profile(request):
 @view_config(route_name='profile')
 def assembl_profile(request):
     user, profile = get_profile(request)
+    id_type = request.matchdict.get('type').strip()
     logged_in = authenticated_userid(request)
     save = request.method == 'POST'
     if logged_in and not user:
@@ -121,7 +122,7 @@ def assembl_profile(request):
             'assembl:templates/view_profile.jinja2',
             dict(default_context,
                  profile=profile,
-                 user=DBSession.query(User).get(logged_in)))
+                 user=logged_in and DBSession.query(User).get(logged_in)))
 
     errors = []
     if save:
