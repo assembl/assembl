18ca628513beea7251e1aeb71b7138a76d94fafb
Jeff Marshall <jeff@classy.co>
2013-09-16 15:10:24 -0400

    Adding an API endpoint for getting sources.

---
+++ assembl/views/api/sources.py
@@ -0,0 +1,33 @@
+import json
+
+from pyramid.httpexceptions import HTTPNotFound
+# from . import acls
+
+from cornice import Service
+
+from assembl.views.api import API_DISCUSSION_PREFIX
+from assembl.db import DBSession
+
+from assembl.synthesis.models import Discussion
+
+
+
+sources = Service(
+    name='sources',
+    path=API_DISCUSSION_PREFIX + '/sources/',
+    description="Manipulate a discussion's sources.",
+    renderer='json',
+)
+
+
+@sources.get()
+def get_sources(request):
+    discussion_id = int(request.matchdict['discussion_id'])
+    discussion = DBSession.query(Discussion).get(discussion_id)
+
+    if not discussion:
+        raise HTTPNotFound(
+            "Discussion with id '%s' not found." % discussion_id
+        )
+
+    return [source.serializable() for source in discussion.sources]
