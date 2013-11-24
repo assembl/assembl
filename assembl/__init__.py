9cbd0ccd7f22025dcc035dcd2a6b0e85f17125db
Marc-Antoine Parent <maparent@acm.org>
2013-11-23 20:02:31 -0500

    ensure default roles and permissions at startup, they're not worth alembic revisions.

--- assembl/__init__.py
+++ assembl/__init__.py
@@ -1,5 +1,6 @@
 """ Pyramid add start-up module. """
 
+import transaction
 from pyramid.config import Configurator
 from pyramid.authentication import SessionAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
@@ -7,8 +8,8 @@ from pyramid_beaker import session_factory_from_settings
 
 from .lib.sqla import configure_engine
 
-# Do not import models here, it will break tests.
 
+# Do not import models here, it will break tests.
 def main(global_config, **settings):
     """ Return a Pyramid WSGI application. """
     settings['config_uri'] = global_config['__file__']
@@ -18,15 +19,22 @@ def main(global_config, **settings):
     configure_engine(settings)
 
     config = Configurator(settings=settings)
-    session_factory = session_factory_from_settings(
-            settings
-            )
+    session_factory = session_factory_from_settings(settings)
     config.set_session_factory(session_factory)
     # import after session to delay loading of BaseOps
     from auth import authentication_callback
-    auth_policy = SessionAuthenticationPolicy(callback=authentication_callback)
+    auth_policy = SessionAuthenticationPolicy(
+        callback=authentication_callback)
     config.set_authentication_policy(auth_policy)
     config.set_authorization_policy(ACLAuthorizationPolicy())
+    # ensure default roles and permissions at startup
+    from models import get_session_maker
+    from auth.models import (
+        populate_default_roles, populate_default_permissions)
+    session = get_session_maker()
+    populate_default_roles(session)
+    populate_default_permissions(session)
+    session.close()
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.include('cornice')  # REST services library.
     # config.include('.lib.alembic')
@@ -42,5 +50,4 @@ def main(global_config, **settings):
     # Mailer
     config.include('pyramid_mailer')
 
-    
     return config.make_wsgi_app()
