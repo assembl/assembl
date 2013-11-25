19971205e881ba88afc233f096182401aa1a3ead
Marc-Antoine Parent <maparent@acm.org>
2013-11-25 12:09:22 -0500

    XPUB is not helpful here. Also subscribe.

--- assembl/lib/zmqlib.py
+++ assembl/lib/zmqlib.py
@@ -3,11 +3,11 @@ import zmq.devices
 
 context = zmq.Context.instance()
 
-td = zmq.devices.ThreadDevice(zmq.FORWARDER, zmq.XSUB, zmq.XPUB)
+td = zmq.devices.ThreadDevice(zmq.FORWARDER, zmq.SUB, zmq.PUB)
 td.bind_in('inproc://assemblchanges')
 td.bind_out('ipc:///tmp/assembl_changes/0')
 td.setsockopt_in(zmq.IDENTITY, 'SUB')
-#td.setsockopt_in(zmq.SUBSCRIBE, "")
+td.setsockopt_in(zmq.SUBSCRIBE, "")
 td.setsockopt_out(zmq.IDENTITY, 'PUB')
 td.start()
 print "td:Started!"
