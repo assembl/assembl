478b0dc3116b855bf25266989b1266c354804fa1
Marc-Antoine Parent <maparent@acm.org>
2013-11-29 14:55:55 -0500

    Revert "pshell with setup, calling the db comes too early"
    This reverts commit f5d781a5b07a013b1f07902ebe9fb6465a886ff5.
    There is a better way.

--- assembl/lib/pshell_session.py
+++ assembl/lib/pshell_session.py
@@ -1,11 +1,3 @@
-import transaction
 from sqla import get_session_maker
+# an implicit session for pshell
 db = get_session_maker(False)
-
-def setup(env):
-    # Wait until all is done
-    from assembl import models
-    print env
-    env['db'] = db
-    env['transaction'] = transaction
-    env['models'] = models
