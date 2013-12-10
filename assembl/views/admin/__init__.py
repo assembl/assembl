51d94ee9f8fd5094f67af62049acd61764f9cbbd
Marc-Antoine Parent <maparent@acm.org>
2013-12-10 18:53:52 -0500

    permission interface

--- assembl/views/admin/__init__.py
+++ assembl/views/admin/__init__.py
@@ -1,3 +1,6 @@
 
 def includeme(config):
     config.add_route('discussion_admin', '/admin/discussions/')
+    config.add_route('general_permissions', '/admin/permissions')
+    config.add_route('discussion_permissions',
+                     '/admin/permissions/discussion/{discussion_id:\d+}')
