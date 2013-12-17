c8f752127cb574fbbab8bd2044e880a007117e87
Marc-Antoine Parent <maparent@acm.org>
2013-12-17 15:31:36 -0500

    corrections and updates

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ install_reqs = parse_requirements('requirements.txt')
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
 requires = [str(ir.req) for ir in install_reqs]
 
-tests_require = ['webtest']
+tests_require = ['WebTest']
 
 
 
