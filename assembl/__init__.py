19fcef0066dec4466782642255e7fd4219973cbe
Marc-Antoine Parent <maparent@acm.org>
2014-01-12 12:55:44 -0500

    i18n first pass. Not quite done translating.

--- assembl/__init__.py
+++ assembl/__init__.py
@@ -1,6 +1,7 @@
 """ Pyramid add start-up module. """
 
 import transaction
+
 from pyramid.config import Configurator
 from pyramid.authentication import SessionAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
@@ -8,6 +9,13 @@ from pyramid_beaker import session_factory_from_settings
 
 from .lib.sqla import configure_engine
 
+
+def my_locale_negotiator(request):
+    if not hasattr(request, '_LOCALE_'):
+        request._LOCALE_ = request.accept_language.best_match(
+            ('en', 'fr'), 'en')
+
+    return request._LOCALE_
 
 # Do not import models here, it will break tests.
 def main(global_config, **settings):
@@ -20,6 +28,9 @@ def main(global_config, **settings):
 
     from views import root_factory
     config = Configurator(settings=settings, root_factory=root_factory)
+    config.add_translation_dirs('assembl:locale/')
+    config.set_locale_negotiator(my_locale_negotiator)
+
     config.include('.lib.zmqlib')
     session_factory = session_factory_from_settings(settings)
     config.set_session_factory(session_factory)
@@ -49,6 +60,7 @@ def main(global_config, **settings):
 
     # jinja2
     config.include('pyramid_jinja2')
+    config.add_jinja2_extension('jinja2.ext.i18n')
 
     # Mailer
     config.include('pyramid_mailer')
