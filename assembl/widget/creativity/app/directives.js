cf26f07174b4179bdf09af55b39e9ae741b56b75
hellsingblack <hellsingblack@gmail.com>
2014-05-01 12:44:26 +0200

    include light bulb icon

--- assembl/widget/creativity/app/directives.js
+++ assembl/widget/creativity/app/directives.js
@@ -3,9 +3,24 @@
 creativityApp.directive('rate', function(){
     return{
         restrict:'E',
-        template:'<a class="glyphicon glyphicon-chevron-down text-danger"></a><input class="rate" type="text" name="rate" value="1" disabled><a class="glyphicon glyphicon-chevron-up text-success"></a>',
+        template:'<a class="glyphicon glyphicon-chevron-down text-danger" ng-click="rateDown()"></a><input class="rate" type="text" name="rate" ng-model="rate" value="{{rate}}" disabled><a class="glyphicon glyphicon-chevron-up text-success" ng-click="rateUp()"></a>',
         link: function(scope, elements, attrs){
 
+            scope.rate = 0;
+
+            scope.rateUp = function(){
+               if(scope.rate > 4){
+                   return;
+               }
+               scope.rate += 1;
+            }
+
+            scope.rateDown = function(){
+                if(scope.rate <= 0){
+                    return;
+                }
+                scope.rate -= 1;
+            }
 
         }
     }
