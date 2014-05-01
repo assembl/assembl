56c1fb51ba7bb419da7cdda82e9d8c6ca2b394f7
hellsingblack <hellsingblack@gmail.com>
2014-05-01 11:57:07 +0200

    Add module vote and behavior

--- assembl/widget/creativity/app/directives.js
+++ assembl/widget/creativity/app/directives.js
@@ -1,37 +1,12 @@
 "use strict";
 
-creativityApp.directive('rate', function($compile){
+creativityApp.directive('rate', function(){
     return{
         restrict:'E',
-        replace: true,
-        template:'<button href="#" type="button" class="btn btn-xs btn-primary vote">{{"vote"| translate }}</button>',
-        link: function(scope, elements){
+        template:'<a class="glyphicon glyphicon-chevron-down text-danger"></a><input class="rate" type="text" name="rate" value="1" disabled><a class="glyphicon glyphicon-chevron-up text-success"></a>',
+        link: function(scope, elements, attrs){
 
-           $(elements).popover({
-                container:'.session-comment .caption',
-                html:true,
-                placement:'left',
-                trigger:'click',
-                content:function(){
-                    return $compile(
-                        '<span class="label label-default rating-1" ng-click="rate(1)">1</span>' +
-                        '<span class="label label-default rating-2" ng-click="rate(2)">2</span>' +
-                        '<span class="label label-default rating-3" ng-click="rate(3)">3</span>' +
-                        '<span class="label label-default rating-4" ng-click="rate(4)">4</span>' +
-                        '<span class="label label-default rating-5" ng-click="rate(5)">5</span>')(scope);
-                }
 
-            });
-
-            scope.rate = function(rate){
-
-               $(elements).popover('destroy');
-
-               elements.parent().html('<button href="#" type="button" class="btn btn-xs btn-success vote"><span class="glyphicon glyphicon-ok"></span></button>');
-
-               $compile(elements.contents())(scope);
-
-            }
         }
     }
 })
