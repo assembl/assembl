3b0220a037688f1c33163a1b0a5f9a5020c80988
swergas <swergas@yahoo.fr>
2014-05-19 12:14:47 +0200

    vote widget: make the UI use the dynamically loaded configuration data

--- assembl/widget/vote/README.txt
+++ assembl/widget/vote/README.txt
@@ -1,7 +1,34 @@
 
+General information
+===================
+
 Consider "." the directory which contains this Readme file.
 
 The ./.bowerrc file which contains an empty JSON is necessary: it overrides the general bower configuration of the Assembl project, so that the files which bower will install will go in a ./bower_components folder, instead of ./assembl/static/js/bower/ .
+
+Axes = criteria.
+
+
+How to use the vote widget
+==========================
+
+1) Create an instance of the vote widget, and give it a configuration (the criteria the user will vote on, etc)
+
+You have to POST to http://localhost:6543/data/Discussion/1/widgets using the following variables:
+- variable name: "widget_type", variable value: "vote"
+- variable name: "settings", variable example value:
+{"axes":[{"name":"Rentabilite (%)","description":"Description de l'axe rentabilite","valueMin":0,"valueMax":100,"valueDefault":10,"ticks":6},{"name":"Risque (%)","description":"Description de l'axe risque","valueMin":0,"valueMax":100,"valueDefault":0,"ticks":6},{"name":"Investissement (Euros)","description":"Description de l'axe investissement","valueMin":0,"valueMax":50000,"valueDefault":22222,"ticks":5},{"name":"Difficulte de mise en oeuvre (note)","description":"Description de l'axe difficulte de mise en oeuvre","valueMin":0,"valueMax":100,"valueDefault":50,"ticks":12,"descriptionMin":"Tres facile","descriptionMax":"Tres difficile","colorMin":"#00ff00","colorMax":"#ff0000","colorAverage":"#ffff00","colorCursor":"#000000"}],"width":900,"height":300,"padding":60,"axisWidthDefault":100}
+
+2) Use the response to build the URL which users will access
+
+The response contains a "location" field, which gives the id of this new instance, for example "local:Widget/22".
+Then you can translate "local:" to "http://localhost:6543/data/" and obtain http://localhost:6543/data/Widget/22
+This URL is the configuration parameter which you can now give when you access the vote widget with your browser.
+So an example of widget URL is http://localhost:6543/widget/vote/?config=http://localhost:6543/data/Widget/22#/
+
+
+
+
 
 
 
--- assembl/widget/vote/app/controllers.js
+++ assembl/widget/vote/app/controllers.js
@@ -12,6 +12,202 @@ voteApp.controller('indexCtl',
       $scope.settings = configService.settings;
       console.log("settings:");
       console.log($scope.settings);
-    }
+      $scope.drawUI();
+    };
+
+    $scope.drawUI = function(){
+      var config = $scope.settings;
+
+
+      var holder;
+
+      function getSelectorIndexByX(x)
+      {
+        // find the nearest selector, but return -1 if the position clicked is too far
+        var inv = xScale.invert(x);
+        if ( (inv % 1.0) < 0.6 && (inv % 1.0) > 0.4 )
+          return -1;
+        return Math.round(xScale.invert(x));
+      }
+
+      function click()
+      {
+        // Ignore the click event if it was suppressed
+        if (d3.event.defaultPrevented) return;
+
+        // Extract the click location
+        var point = d3.mouse(this);
+        var p = {x: point[0], y: point[1] };
+
+        // find on which slider the user is clicking
+
+        var indexSelector = getSelectorIndexByX(p.x);
+
+        if ( indexSelector >= 0 && indexSelector < config.axes.length )
+          d3.select("#circle"+indexSelector).attr("cy", scales[indexSelector](scales[indexSelector].invert(p.y)));
+      }
+
+      function dragmove(d) {
+        var x = d3.event.x;
+        var y = d3.event.y;
+
+        var indexSelector = getSelectorIndexByX(x);
+
+        if ( indexSelector >= 0 && indexSelector < config.axes.length )
+          d3.select(this).attr("cy", scales[indexSelector](scales[indexSelector].invert(y)));
+      }
+
+      // create scales
+
+      var scales = [];
+      var axes = [];
+      for ( var i = 0; i < config.axes.length; ++i )
+      {
+        // vertical scales
+        var scale = d3.scale.linear()
+          .domain([config.axes[i].valueMin, config.axes[i].valueMax])
+          .range([config.height - config.padding, config.padding])
+          .clamp(true);
+        scales.push(scale);
+
+        var ticks = 5;
+        if ( config.axes[i].ticks )
+          ticks = config.axes[i].ticks;
+        var axis = d3.svg.axis()
+          .scale(scale)
+          .orient("left")
+          .ticks(ticks);
+        axes.push(axis);
+      }
+
+      var xScale = d3.scale.linear()
+        .domain([0, config.axes.length])
+        .range([config.padding, config.width - config.padding])
+        .clamp(true);
+
+
+      // create (hidden) X axis using its scale
+      var xAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient("bottom");
+
+      // define drag beavior
+      var drag = d3.behavior.drag()
+        .on("drag", dragmove);
+
+      // create the graph
+      var holder = d3.select("#d3_container")
+        .append("svg")
+        .attr("width", config.width)    
+        .attr("height", config.height)
+        .on("click", click);
+
+      var svg = d3.select("#d3_container svg");
+
+
+      // show vertical axes, their label, and their gradient
+      for ( var i = 0; i < config.axes.length; ++i )
+      {
+        if ( config.axes[i].colorMin && config.axes[i].colorMax )
+        {
+          // define a gradient in the SVG (using a "linearGradient" tag inside the "defs" tag). 0% = top; 100% = bottom
+          var defs = svg.append("defs");
+          var lg = defs.append("linearGradient")
+            .attr("id","gradient"+i)
+            .attr("x1","0%")
+            .attr("y1","0%")
+            .attr("x2","0%")
+            .attr("y2","100%");
+          lg.append("stop")
+            .attr("offset","0%")
+            .style("stop-color",config.axes[i].colorMax)
+            .style("stop-opacity","1");
+          if ( config.axes[i].colorAverage )
+          {
+            lg.append("stop")
+              .attr("offset","50%")
+              .style("stop-color",config.axes[i].colorAverage)
+              .style("stop-opacity","1");
+          }
+          lg.append("stop")
+            .attr("offset","100%")
+            .style("stop-color",config.axes[i].colorMin)
+            .style("stop-opacity","1");
+        }
+
+
+
+        var g = svg.append("g");
+        
+        // show color gradient if set
+        var axisBonusClasses = "";
+        if ( config.axes[i].colorMin && config.axes[i].colorMax )
+        {
+          axisBonusClasses = "gradient"
+          var gradientWidth = 10;
+          g.append("rect")
+            .attr("x", xScale(i) -gradientWidth/2 )
+            .attr("y", config.padding-1 )
+            //.attr("y", scales[i](config.axes[i].valueMax) )
+            .attr("width",gradientWidth)
+            .attr("height",scales[i](config.axes[i].valueMin) - config.padding +1)
+            .attr("fill","url(#gradient"+i+")");
+        }
+
+        // show vertical axis
+        g.append("g")
+          .attr("class", "axis "+axisBonusClasses)
+          .attr("transform", "translate(" + xScale(i) + ",0)")
+          .call(axes[i]);
+
+        // if there is a gradient, edit the axis (which we just created) to show ticks differently
+        if ( config.axes[i].colorMin && config.axes[i].colorMax )
+        {
+          g.selectAll("line")
+            .attr("x1","-2")
+            .attr("x2","2");
+        }
+
+        // show axis label
+        g.append("text")
+          .attr("y", config.height - config.padding*0.3 )
+          .attr("x", xScale(i) )
+          .style("text-anchor", "middle")
+          .text(config.axes[i].name);
+
+        
+
+        // show descriptions of the minimum and maximum values
+        if ( config.axes[i].descriptionMin )
+        {
+          g.append("text")
+            .attr("y", config.height - config.padding*0.7 )
+            .attr("x", xScale(i) )
+            .style("text-anchor", "middle")
+            .text(config.axes[i].descriptionMin);
+        }
+        if ( config.axes[i].descriptionMax )
+        {
+          g.append("text")
+            .attr("y", config.padding*0.7 )
+            .attr("x", xScale(i) )
+            .style("text-anchor", "middle")
+            .text(config.axes[i].descriptionMax);
+        }
+        
+      }
+
+
+      // draw the cursors
+      svg.selectAll("circle").data(config.axes).enter().append("circle")
+        .attr("id", function(d, i){ return "circle"+i; })
+        .attr("cx", function(d, i){ return xScale(i); })
+        .attr("cy", function(d, i){ return scales[i](d.valueDefault); })
+        .attr("r", 8)
+        .style("fill", function(d,i){ if ( config.axes[i].colorCursor ) return config.axes[i].colorCursor; return "blue"; })
+        .style("cursor", "pointer")
+        .call(drag);
+
+    };
 
 }]);
