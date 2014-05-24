1be93239cb8d27f9ecb14b4ac3ef9e009d6cc5ee
Marc-Antoine Parent <maparent@acm.org>
2014-05-24 15:35:45 -0400

    try to correct the missing parent from the other end

--- assembl/static/js/app/models/ideaLink.js
+++ assembl/static/js/app/models/ideaLink.js
@@ -23,6 +23,16 @@ function(Base, _, Segment, app, i18n, Types, Permissions){
             target: '',
             order: 1
         },
+
+        correctParentBug: function() {
+            var child = app.ideaList.ideas.get(this.get('target'));
+            if (child.get('parentId') === null) {
+                console.log("correct parent bug");
+                child.set('parentId', this.get('source'));
+                child.get('parents').push(this.get('source'));
+            }
+        }
+
     });
 
     /**
@@ -35,6 +45,20 @@ function(Base, _, Segment, app, i18n, Types, Permissions){
          * @type {IdeaModel}
          */
         model: IdeaLinkModel,
+
+        /**
+         * add function
+         * @type {IdeaModel}
+         */
+        add: function(models, options) {
+            models = Backbone.Collection.prototype.set.call(this, models, options);
+            if (_.isArray(models)) {
+                _.each(models, function(m) {m.correctParentBug();})
+            } else {
+                models.correctParentBug();
+            }
+            return models;
+        },
     });
 
     return {
