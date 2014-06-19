b5c9372ff6e509e46a0a22faeb469dfd81aa7dab
hellsingblack <hellsingblack@gmail.com>
2014-06-19 12:54:50 +0200

    widget :  add favincon

--- assembl/widget/creativity/test/spec/controllers.spec.js
+++ assembl/widget/creativity/test/spec/controllers.spec.js
@@ -2,19 +2,80 @@
 
 describe('creativityApp', function(){
 
-    beforeEach(angular.mock.module("creativityApp"));
+    var scope, ratingCtl, $httpBackend, sessionCtl, configService;
 
-    describe('Controller: rating', function(){
+    beforeEach(module("creativityApp"));
+    beforeEach(inject(function(_$httpBackend_, $controller, $rootScope, _configService_){
 
-        it('Should be defined', angular.mock.inject(function($rootScope, $controller) {
+        $rootScope = $rootScope;
+        scope = $rootScope.$new();
+        $httpBackend = _$httpBackend_;
+        configService = _configService_;
 
-            this.scope = $rootScope.$new();
-            $controller('MyController', {
-                $scope: this.scope
+        $httpBackend.when('GET', 'app/locales/fr.json')
+            .respond({
+                comment: "Blablabla",
+                acomment: "new comment"
             });
 
-        }));
+        var $controller = $controller;
 
+        ratingCtl = function(){
+            return $controller('ratingCtl', {
+                '$scope': scope
+            })
+        }
+
+        sessionCtl = function(){
+            return $controller('sessionCtl', {
+                '$scope': scope
+            })
+        }
+
+    }));
+
+    afterEach(function(){
+       $httpBackend.verifyNoOutstandingExpectation();
+       $httpBackend.verifyNoOutstandingRequest();
+
+    });
+
+    it('Should set local', function(){
+        $httpBackend.expectGET('app/locales/fr.json');
+        $httpBackend.flush();
+    });
+
+    describe('rating controller : ', function(){
+
+        expect(angular.isFunction(configService.getWidget())).toBe(true);
+
+        it('Should return data', function(){
+
+            //$httpBackend.when('GET', '/data/Discussion/1/widgets/1/base_idea/-/children')
+              //  .respond(200, [{}]);
+
+            //$httpBackend.expect('GET', '/data/Discussion/1/widgets/1/base_idea/-/children');
+            //$httpBackend.flush();
+
+            /*scope.$apply(function(){
+                scope.runTest();
+            });*/
+
+        })
+
+    });
+
+    describe('session controller : ', function(){
+        sessionCtl = function(){
+            return $controller('sessionCtl', {
+                '$scope': scope
+            })
+        }
+
+        it('Variable should defined', function(){
+
+
+        });
     })
 
 });
