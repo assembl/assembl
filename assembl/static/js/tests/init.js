e492f864a3b464aa32a86153582641feb10f04b7
Marc-Antoine Parent <maparent@acm.org>
2014-07-12 20:56:25 -0400

    run jasmine as part of testsuite
    Conflicts:
    	assembl/static/js/main.js

--- assembl/static/js/tests/init.js
+++ assembl/static/js/tests/init.js
@@ -1,153 +1,20 @@
-requirejs.config({
-  baseUrl: "/static/js/",
-  urlArgs: urlArgs,
-  waitSeconds: 20,
-  packages: [
-    'text',{
-      name:'text',
-      location:'lib/',
-      main:'text'
-    }
-  ],
-  paths: {
-    'app': 'app/app',
-    'router': 'app/router',
-    'views':'app/views',
-    'models':'app/models',
-    'i18n': 'app/utils/i18n',
-    'socket': 'app/utils/socket',
-    'types': 'app/utils/types',
-    'permissions': 'app/utils/permissions',
+requirejs.config(requirejs_config);
 
-    'jquery': "bower/jquery/jquery",
-    'tipsy': 'bower/tipsy/src/javascripts/jquery.tipsy',
-    'jquery-highlight': 'lib/jquery-highlight/jquery.highlight',
-
-    'backbone': 'bower/backbone/backbone',
-    'underscore': 'bower/underscore/underscore-min',
-
-    'annotator': 'lib/annotator/annotator-full.min',
-
-    'jasmine': 'bower/jasmine/lib/jasmine-core/jasmine',
-    'jasmine-html': 'bower/jasmine/lib/jasmine-core/jasmine-html',
-    'jasmine-jquery': 'bower/jasmine-jquery/lib/jasmine-jquery',
-
-    'ckeditor': 'bower/ckeditor/ckeditor',
-    'ckeditor-sharedspace': 'lib/ckeditor-sharedcontainer/plugin',
-
-    'moment': 'bower/momentjs/moment',
-    'moment_lang': 'bower/momentjs/lang/'+((assembl_locale=='en')?'fr':assembl_locale),
-    'zeroclipboard': 'bower/zeroclipboard/ZeroClipboard',
-    'sockjs': 'bower/sockjs/sockjs',
-    'cytoscape': 'bower/cytoscape/cytoscape',
-    'jit': 'bower/jit/Jit/jit',
-    'sprintf': 'bower/sprintf/src/sprintf',
-    'backboneModal':'lib/backbone-modal/backbone.modal'
-  },
-  shim: {
-    backbone: {
-        deps: ['underscore', 'jquery'],
-        exports: 'Backbone'
-    },
-    'underscore': {
-        exports: '_'
-    },
-    'jquery': {
-        exports: 'jQuery'
-    },
-    'jquery-highlight': {
-        deps: ['jquery'],
-        exports: 'jQuery'
-    },
-    'app': {
-        deps: ['annotator', 'ckeditor', 'tipsy', 'i18n', 'jquery-highlight', 'zeroclipboard'],
-        exports: 'app'
-    },
-    'i18n': {
-        exports: 'i18n',
-        init: function(i18n) {
-            this.i18n(json);
-            return this.i18n;
-        }
-    },
-    'socket': {
-        deps: ['sockjs']
-    },
-    'jasmine': {
-        exports: 'jasmine'
-    },
-    'jasmine-html': {
-        deps: ['jasmine'],
-        exports: 'jasmine-html'
-    },
-    'ckeditor': {
-        exports: 'CKEDITOR'
-    },
-    'ckeditor-sharedspace': {
-        deps: ['ckeditor'],
-        exports: 'CKEDITOR'
-    },
-    'tipsy': {
-        deps: ['jquery']
-    },
-    'zeroclipboard' : {
-        exports: 'ZeroClipboard'
-    },
-    'sockjs': {
-        deps: ['jquery'],
-        exports: 'SockJS'
-    },
-    'cytoscape': {
-        deps: ['jquery'],
-        exports: 'cytoscape'
-    },
-    'jit': {
-        deps: [],
-        exports: '$jit'
-    },
-    'annotator' : {
-        deps: ['jquery'],
-        exports: 'Annotator'
-    },
-    'sprintf' : {
-        deps: [],
-        exports: 'sprintf'
-    },
-    'moment_lang': {
-        deps: ['moment'],
-        exports: 'moment_lang'
-    },
-    'backboneModal': {
-        deps:['backbone'],
-        exports: 'BackboneModal'
-    }
-  }
-});
-
-require(['jquery'], function($){
+require(['jquery', 'jasmine-boot'],
+function($, jasmine_boot) {
+    'use strict';
     var jasmineEnv = jasmine.getEnv();
     jasmineEnv.updateInterval = 1000;
-
-    // jasmineEnv.specFilter = function(spec) {
-    //     return htmlReporter.specFilter(spec);
-    // };
-
-    var specs = [
+    $('#wrapper').hide();
+    require([
         'tests/test_app',
-        'tests/test_ideaView',
-        'tests/test_ideaPanelView',
-        'tests/test_ideaList',
-        'tests/test_ideaModel',
-        'tests/test_lateralMenu',
-        'tests/test_segmentList'
-    ];
-
-    $(function(){
-        requirejs(specs, function(){
-            $('#wrapper').hide();
-
-            jasmineEnv.execute();
-        });
+        // 'tests/test_ideaView',
+        // 'tests/test_ideaPanelView',
+        // 'tests/test_ideaList',
+        // 'tests/test_ideaModel',
+        // 'tests/test_segmentList'
+    ], function () {
+      // Initialize the HTML Reporter and execute the environment (setup by `boot.js`)
+      window.onload();
     });
-
 });
--- assembl/tests/test_browser.py
+++ assembl/tests/test_browser.py
@@ -1,8 +1,16 @@
 from splinter import browser
 
+from jasmine_runner.commands import run_specs_with_browser
+
 def test_some_browser_stuff(browser, test_server):
     """Test using real browser."""
-    print test_server.url
     browser.visit(test_server.url)
-    print browser.html
     assert browser.is_text_present('Assembl')
+
+
+def test_jasmine(browser, test_server, discussion, test_session):
+    """Test using real browser."""
+    url = "%s/%s/test" % (test_server.url, discussion.slug)
+    test_session.commit()
+    num_failures = run_specs_with_browser(url, browser)
+    assert not num_failures
