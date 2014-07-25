e9e848ac526bf225b108bd0364d3039cab9dc7ef
Marc-Antoine Parent <maparent@acm.org>
2014-07-25 10:38:53 -0400

    dead code

--- assembl/views/__init__.py
+++ assembl/views/__init__.py
@@ -30,20 +30,6 @@ def backbone_include(config):
     config.add_route('graph_view', '/graph')
 
 
-"""
-TODO:  Rewrite this as a command line util and integrate in build system
-
-def js_message_ids():
-    from babel.messages.pofile import read_po
-    pot = read_po(open(os.path.join(os.path.dirname(__file__), '..', 'locale', 'assembl.pot')))
-    def is_js(msg):
-        for (filename, lineno) in msg.locations:
-            if filename.endswith('.js'):
-                return True
-    return [m.id for m in pot if is_js(m)]
-
-JS_MESSAGE_IDS = js_message_ids()"""
-
 def get_default_context(request):
     localizer = request.localizer
     _ = TranslationStringFactory('assembl')
