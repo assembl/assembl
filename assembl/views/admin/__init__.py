a79e61581114fc11073cb5b854f3d151e0087694
Swergas <swergas@yahoo.fr>
2014-08-04 16:50:54 -0400

    add an admin panel to edit some discussion fields

--- assembl/views/admin/__init__.py
+++ assembl/views/admin/__init__.py
@@ -4,3 +4,5 @@ def includeme(config):
     config.add_route('general_permissions', '/admin/permissions')
     config.add_route('discussion_permissions',
                      '/admin/permissions/discussion/{discussion_id:\d+}')
+    config.add_route('discussion_edit',
+                     '/admin/discussion/edit/{discussion_id:\d+}')
