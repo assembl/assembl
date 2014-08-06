202b7e8cbeb72a8ce0460aef70ad70696b0404f2
Swergas <swergas@yahoo.fr>
2014-08-06 18:17:06 -0400

    context page: add pie chart of messages (first pass)

--- assembl/static/js/app/views/contextPage.js
+++ assembl/static/js/app/views/contextPage.js
@@ -142,8 +142,8 @@ define(function(require){
 
                 messages_sorted_by_date = messages_sorted_by_date.toJSON();
 
-                //console.log("messages_sorted_by_date2:");
-                //console.log(messages_sorted_by_date);
+                console.log("messages_sorted_by_date2:");
+                console.log(messages_sorted_by_date);
 
                 var messages_total = messages_sorted_by_date.length;
                 console.log("messages_total: " + messages_total);
@@ -243,7 +243,7 @@ define(function(require){
                 });
 
                 
-                // accumulate messages. maybe we should not start à 0 but at the accumulated value on start date (accumulated from the beginning of the debate)
+                // accumulate messages. maybe we should not start at 0 but at the accumulated value on start date (accumulated from the beginning of the debate)
                 var messages_in_period_total = 0;
                 _.each(messages_in_period, function(msg){
                     messages_in_period_total += msg.value;
@@ -251,12 +251,68 @@ define(function(require){
                 });
 
 
-                $("#statistics").html("<h2>Statistics since " + statsPeriodName + "</h2><div id='stats_messages'>Messages posted: " + messages_in_period_total + " (" + messages_total + " since the beginning of the debate)<div id='chart'></div></div>");
+
+                // -----
+                // compute messages authors for 2 periods: current period and since the beginning of the debate
+                // -----
+
+                var messages_authors = _.map(messages_sorted_by_date, function(msg){return msg.idCreator;});
+                console.log("messages_authors:");
+                console.log(messages_authors);
+                messages_authors = _.uniq(messages_authors);
+                console.log("messages_authors:");
+                console.log(messages_authors);
+                messages_authors_total = messages_authors.length;
+
+
+                // -----
+                // show results
+                // -----
+
+                $("#statistics").html("<h2>Statistics since " + statsPeriodName + "</h2><div id='stats_messages'>Messages posted: " + messages_in_period_total + " (" + messages_total + " since the beginning of the debate)<div id='chart'></div>");
                 
                 
                 //that.drawLineGraph(messages_per_day_totals_array);
                 that.drawLineGraph(messages_in_period);
                 //that.drawLineGraph(messages_per_day_totals_filled_array);
+
+                var messages_from_new_authors_in_current_period = messages_in_period_total - 1; // TODO: real value
+
+                var pie_chart_data = [
+                    "Messages since the beginning of the debate",
+                    messages_total,
+                    0,
+                    {
+                        "Messages posted during the current period": [
+                            "Messages posted during the current period",
+                            messages_in_period_total,
+                            0,
+                            {
+                                "Messages from new authors": [
+                                    "Messages from new authors",
+                                    messages_from_new_authors_in_current_period,
+                                    0,
+                                    {}
+                                ],
+                                "Messages from old authors": [
+                                    "Messages from old authors",
+                                    messages_in_period_total-messages_from_new_authors_in_current_period,
+                                    0,
+                                    {}
+                                ]
+                            }
+                        ],
+                        "Messages posted before current period": [
+                            "Messages posted before current period",
+                            messages_total-messages_in_period_total,
+                            0,
+                            {}
+                        ]
+                    }
+                ];
+
+
+                that.drawPieChart(pie_chart_data);
             });
         },
 
@@ -445,6 +501,251 @@ define(function(require){
 
             draw();
 
+        },
+
+        drawPieChart: function(code_hierarchy_data){
+            /*
+            taken from:
+            http://bl.ocks.org/adewes/4710330/94a7c0aeb6f09d681dbfdd0e5150578e4935c6ae
+            http://www.andreas-dewes.de/code_is_beautiful/
+            */
+            function init_code_hierarchy_plot(element_id,data)
+            {
+                var plot = document.getElementById(element_id);
+
+                while (plot.hasChildNodes())
+                {
+                    plot.removeChild(plot.firstChild);
+                }
+
+                var width = plot.offsetWidth;
+                var height = width;
+                var x_margin = 40;
+                var y_margin = 40;
+                var name_index = 0;
+                var count_index = 1;
+                var children_index = 3;
+                
+                var max_depth=3;
+                
+                var data_slices = [];
+                var max_level = 4;
+                var color = d3.scale.category20c();
+
+                var svg = d3.select("#"+element_id).append("svg")
+                    .attr("width", width)
+                    .attr("height", height)
+                    .append("g")
+                    .attr("transform", "translate(" + width / 2 + "," + height * .52 + ")");
+                      
+                function process_data(data,level,start_deg,stop_deg)
+                {
+                    var name = data[0];
+                    var total = data[1];
+                    var children = data[3];
+                    var current_deg = start_deg;
+                    if (level > max_level)
+                    {
+                        return;
+                    }
+                    if (start_deg == stop_deg)
+                    {
+                        return;
+                    }
+                    data_slices.push([start_deg,stop_deg,name,level,data[1],data[2]]);
+                    for (var key in children)
+                    {
+                        child = children[key];
+                        var inc_deg = (stop_deg-start_deg)/total*child[count_index];
+                        var child_start_deg = current_deg;
+                        current_deg+=inc_deg;
+                        var child_stop_deg = current_deg;
+                        var span_deg = child_stop_deg-child_start_deg;
+                        process_data(child,level+1,child_start_deg,child_stop_deg);
+                    }
+                }
+                
+                process_data(data,0,0,360./180.0*Math.PI);
+
+                var ref = data_slices[0];
+                var next_ref = ref;
+                var last_refs = [];
+
+                var thickness = width/2.0/(max_level+2)*1.1;
+                    
+                var arc = d3.svg.arc()
+                .startAngle(function(d) { if(d[3]==0){return d[0];}return d[0]+0.01; })
+                .endAngle(function(d) { if(d[3]==0){return d[1];}return d[1]-0.01; })
+                .innerRadius(function(d) { return 1.1*d[3]*thickness; })
+                .outerRadius(function(d) { return (1.1*d[3]+1)*thickness; });    
+
+                var slices = svg.selectAll(".form")
+                    .data(function(d) { return data_slices; })
+                    .enter()
+                    .append("g");
+                    slices.append("path")
+                    .attr("d", arc)
+                    .attr("id",function(d,i){return element_id+i;})
+                    .style("fill", function(d) { return color(d[2]);})
+                    .on("click",animate)
+                    .on("mouseover",update_legend)
+                    .on("mouseout",remove_legend)
+                    .attr("class","form")
+                    .append("svg:title")
+                    .text(function(d) { return d[2]+","+d[3]; });
+
+                var legend = d3.select("#"+element_id+"_legend")
+                    
+                function update_legend(d)
+                {
+                    //legend.html("<h2>"+d[2]+"&nbsp;</h2><p>"+d[4]+" messages, by "+d[5]+" authors.</p>");
+                    legend.html("<h2>"+d[2]+"&nbsp;</h2><p>"+d[4]+" messages</p>");
+                    legend.transition().duration(200).style("opacity","1");
+                }
+                
+                function remove_legend(d)
+                {
+                    legend.transition().duration(1000).style("opacity","0");
+                    //legend.html("<h2>&nbsp;</h2>")
+                }
+                
+                function get_start_angle(d,ref)
+                {
+                    if (ref)
+                    {
+                        var ref_span = ref[1]-ref[0];
+                        return (d[0]-ref[0])/ref_span*Math.PI*2.0
+                    }
+                    else
+                    {
+                        return d[0];
+                    }
+                }
+                
+                function get_stop_angle(d,ref)
+                {
+                    if (ref)
+                    {
+                        var ref_span = ref[1]-ref[0];
+                        return (d[1]-ref[0])/ref_span*Math.PI*2.0
+                    }
+                    else
+                    {
+                        return d[0];
+                    }
+                }
+                
+                function get_level(d,ref)
+                {
+                    if (ref)
+                    {
+                        return d[3]-ref[3];
+                    }
+                    else
+                    {
+                        return d[3];
+                    }
+                }
+                
+                function rebaseTween(new_ref)
+                {
+                    return function(d)
+                    {
+                        var level = d3.interpolate(get_level(d,ref),get_level(d,new_ref));
+                        var start_deg = d3.interpolate(get_start_angle(d,ref),get_start_angle(d,new_ref));
+                        var stop_deg = d3.interpolate(get_stop_angle(d,ref),get_stop_angle(d,new_ref));
+                        var opacity = d3.interpolate(100,0);
+                        return function(t)
+                        {
+                            return arc([start_deg(t),stop_deg(t),d[2],level(t)]);
+                        }
+                    }
+                }
+                
+                var animating = false;
+                
+                function animate(d) {
+                    if (animating)
+                    {
+                        return;
+                    }
+                    animating = true;
+                    var revert = false;
+                    var new_ref;
+                    if (d == ref && last_refs.length > 0)
+                    {
+                        revert = true;
+                        last_ref = last_refs.pop();
+                    }
+                    if (revert)
+                    {
+                        d = last_ref;
+                        new_ref = ref;
+                        svg.selectAll(".form")
+                        .filter(
+                            function (b)
+                            {
+                                if (b[0] >= last_ref[0] && b[1] <= last_ref[1]  && b[3] >= last_ref[3])
+                                {
+                                    return true;
+                                }
+                                return false;
+                            }
+                        )
+                        .transition().duration(1000).style("opacity","1").attr("pointer-events","all");
+                    }
+                    else
+                    {
+                        new_ref = d;
+                        svg.selectAll(".form")
+                        .filter(
+                            function (b)
+                            {
+                                if (b[0] < d[0] || b[1] > d[1] || b[3] < d[3])
+                                {
+                                    return true;
+                                }
+                                return false;
+                            }
+                        )
+                        .transition().duration(1000).style("opacity","0").attr("pointer-events","none");
+                    }
+                    svg.selectAll(".form")
+                    .filter(
+                        function (b)
+                        {
+                            if (b[0] >= new_ref[0] && b[1] <= new_ref[1] && b[3] >= new_ref[3])
+                            {
+                                return true;
+                            }
+                            return false;
+                        }
+                    )
+                    .transition().duration(1000).attrTween("d",rebaseTween(d));
+                    setTimeout(function(){
+                        animating = false;
+                        if (! revert)
+                        {
+                            last_refs.push(ref);
+                            ref = d;
+                        }
+                        else
+                        {
+                            ref = d;
+                        }
+                    },1000);
+                };
+
+            }
+
+            function init_plots()
+            {
+                init_code_hierarchy_plot("code_hierarchy",code_hierarchy_data);
+            }
+
+            window.onload = init_plots;
+            window.onresize = init_plots;
+            init_plots();
         }
 
     });
