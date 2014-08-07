5e23f22c2165166f71b9a0a993db2ed82f6d17a4
Marc-Antoine Parent <maparent@acm.org>
2014-08-07 15:01:50 -0400

    dead code

--- assembl/static/js/app/objects/storage.js
+++ assembl/static/js/app/objects/storage.js
@@ -32,41 +32,6 @@ define(function (require) {
             this._store.setItem('groupItems', JSON.stringify(this.groupSpecs));
         },
 
-        createGroupItem: function(items) {
-            var data = [],
-                collection = [],
-                groups = {},
-                that = this;
-
-            //FIXME: viewId need to be uniq to delete the right storage group
-
-            if (items.length) {
-                items.forEach(function (item) {
-                    var i = {};
-                    i.type = item;
-
-                    data.push(i);
-                });
-            }
-
-            groups.panels = data;
-
-            if (!this._store.getItem('groupItems')) {
-
-                collection.push(groups);
-                this._store.setItem('groupItems', JSON.stringify(collection));
-
-            } else {
-
-                var groupOfPanels = JSON.parse(this._store.getItem('groupItems'));
-                groupOfPanels.push(groups);
-
-                this._store.removeItem('groupItems');
-                this._store.setItem('groupItems', JSON.stringify(groupOfPanels));
-            }
-
-        },
-
         getStorageGroupItem: function () {
             if (this._store.getItem('groupItems')) {
                 return JSON.parse(this._store.getItem('groupItems'));
