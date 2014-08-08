362f3358df5e2d897bbe81f4f7a8c50dbb7c7bb6
hellsingblack <hellsingblack@gmail.com>
2014-08-07 22:08:40 -0400

    improve styleguide and components, tooltip break, next step change reference to all tooltip

--- assembl/static/js/require_config.js
+++ assembl/static/js/require_config.js
@@ -1,127 +1,131 @@
 var requirejs_config = {
-  baseUrl: "/static/js/",
-  urlArgs: urlArgs,
-  waitSeconds: 20,
-  paths: {
-    'router': 'app/router',
-    'views':'app/views',
-    'models':'app/models',
-    'utils': 'app/utils',
-    'modules': 'app/modules',
-    'controllers':'app/controllers',
-    'objects':'app/objects',
+    baseUrl: "/static/js/",
+    urlArgs: urlArgs,
+    waitSeconds: 20,
+    paths: {
+        'router': 'app/router',
+        'views': 'app/views',
+        'models': 'app/models',
+        'utils': 'app/utils',
+        'modules': 'app/modules',
+        'controllers': 'app/controllers',
+        'objects': 'app/objects',
 
-    'jquery': "bower/jquery/jquery",
-    'tipsy': 'bower/tipsy/src/javascripts/jquery.tipsy',
-    'jquery-highlight': 'lib/jquery-highlight/jquery.highlight',
+        'jquery': "bower/jquery/jquery",
+        'tipsy': 'bower/tipsy/src/javascripts/jquery.tipsy',
+        'jquery-highlight': 'lib/jquery-highlight/jquery.highlight',
 
-    'backbone': 'bower/backbone/backbone',
-    'underscore': 'bower/underscore/underscore-min',
+        'backbone': 'bower/backbone/backbone',
+        'underscore': 'bower/underscore/underscore-min',
 
-    'annotator': 'lib/annotator/annotator-full.min',
+        'annotator': 'lib/annotator/annotator-full.min',
 
-    'jasmine': 'bower/jasmine/lib/jasmine-core/jasmine',
-    'jasmine-html': 'bower/jasmine/lib/jasmine-core/jasmine-html',
-    'jasmine-boot': 'bower/jasmine/lib/jasmine-core/boot',
+        'jasmine': 'bower/jasmine/lib/jasmine-core/jasmine',
+        'jasmine-html': 'bower/jasmine/lib/jasmine-core/jasmine-html',
+        'jasmine-boot': 'bower/jasmine/lib/jasmine-core/boot',
 
-    'ckeditor': 'bower/ckeditor/ckeditor',
-    'ckeditor-sharedspace': 'lib/ckeditor-sharedcontainer/plugin',
+        'ckeditor': 'bower/ckeditor/ckeditor',
+        'ckeditor-sharedspace': 'lib/ckeditor-sharedcontainer/plugin',
 
-    'moment': 'bower/momentjs/moment',
-    'moment_lang': 'bower/momentjs/lang/'+((assembl_locale=='en')?'fr':assembl_locale),
-    'zeroclipboard': 'bower/zeroclipboard/ZeroClipboard',
-    'sockjs': 'bower/sockjs/sockjs',
-    'cytoscape': 'bower/cytoscape/cytoscape',
-    'jit': 'bower/jit/Jit/jit',
-    'jed': 'bower/jed/jed',
-    'backboneModal':'lib/backbone-modal/backbone.modal',
-    'marionette':'bower/marionette/lib/backbone.marionette.min',
-    'd3':'bower/d3/d3.min'
-  },
-  shim: {
-    backbone: {
-        deps: ['underscore', 'jquery'],
-        exports: 'Backbone'
+        'moment': 'bower/momentjs/moment',
+        'moment_lang': 'bower/momentjs/lang/' + ((assembl_locale == 'en') ? 'fr' : assembl_locale),
+        'zeroclipboard': 'bower/zeroclipboard/ZeroClipboard',
+        'sockjs': 'bower/sockjs/sockjs',
+        'cytoscape': 'bower/cytoscape/cytoscape',
+        'jit': 'bower/jit/Jit/jit',
+        'jed': 'bower/jed/jed',
+        'backboneModal': 'lib/backbone-modal/backbone.modal',
+        'marionette': 'bower/marionette/lib/backbone.marionette.min',
+        'd3': 'bower/d3/d3.min',
+        'bootstrap': 'lib/bootstrap'
     },
-    'underscore': {
-        exports: '_'
-    },
-    marionette: {
-        deps: ['backbone', 'jquery'],
-        exports: 'Marionette'
-    },
-    babysitter: {
-      deps: ['backbone', 'jquery']
-    },
-    'jquery': {
-        exports: 'jQuery'
-    },
-    'jquery-highlight': {
-        deps: ['jquery'],
-        exports: 'jQuery'
-    },
-    'modules/context': {
-        deps: ['annotator', 'ckeditor', 'tipsy', 'utils/i18n', 'jquery-highlight'],
-        exports: 'context'
-    },
-    'utils/i18n': {
-        deps: ['jed'],
-        exports: 'i18n'
-    },
-    'socket': {
-        deps: ['sockjs']
-    },
-    'jasmine': {
-        exports: 'window.jasmineRequire'
-    },
-    'jasmine-html': {
-        deps: ['jasmine'],
-        exports: 'window.jasmineRequire'
-    },
-    'jasmine-boot': {
-        deps: ['jasmine', 'jasmine-html'],
-        exports: 'window.jasmineRequire'
-    },
-    'ckeditor': {
-        exports: 'CKEDITOR'
-    },
-    'ckeditor-sharedspace': {
-        deps: ['ckeditor'],
-        exports: 'CKEDITOR'
-    },
-    'tipsy': {
-        deps: ['jquery']
-    },
-    'zeroclipboard' : {
-        exports: 'ZeroClipboard'
-    },
-    'sockjs': {
-        deps: ['jquery'],
-        exports: 'SockJS'
-    },
-    'cytoscape': {
-        deps: ['jquery'],
-        exports: 'cytoscape'
-    },
-    'jit': {
-        deps: [],
-        exports: '$jit'
-    },
-    'annotator' : {
-        deps: ['jquery'],
-        exports: 'Annotator'
-    },
-    'moment_lang': {
-        deps: ['moment'],
-        exports: 'moment_lang'
-    },
-    'backboneModal': {
-        deps:['backbone'],
-        exports: 'BackboneModal'
-    },
-    'modal': {
-        deps:['backbone'],
-        exports: 'Modal'
+    shim: {
+        backbone: {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        'underscore': {
+            exports: '_'
+        },
+        marionette: {
+            deps: ['backbone', 'jquery'],
+            exports: 'Marionette'
+        },
+        babysitter: {
+            deps: ['backbone', 'jquery']
+        },
+        'jquery': {
+            exports: 'jQuery'
+        },
+        'jquery-highlight': {
+            deps: ['jquery'],
+            exports: 'jQuery'
+        },
+        'modules/context': {
+            deps: ['annotator', 'ckeditor', 'tipsy', 'utils/i18n', 'jquery-highlight'],
+            exports: 'context'
+        },
+        'utils/i18n': {
+            deps: ['jed'],
+            exports: 'i18n'
+        },
+        'socket': {
+            deps: ['sockjs']
+        },
+        'jasmine': {
+            exports: 'window.jasmineRequire'
+        },
+        'jasmine-html': {
+            deps: ['jasmine'],
+            exports: 'window.jasmineRequire'
+        },
+        'jasmine-boot': {
+            deps: ['jasmine', 'jasmine-html'],
+            exports: 'window.jasmineRequire'
+        },
+        'ckeditor': {
+            exports: 'CKEDITOR'
+        },
+        'ckeditor-sharedspace': {
+            deps: ['ckeditor'],
+            exports: 'CKEDITOR'
+        },
+        'tipsy': {
+            deps: ['jquery']
+        },
+        'zeroclipboard': {
+            exports: 'ZeroClipboard'
+        },
+        'sockjs': {
+            deps: ['jquery'],
+            exports: 'SockJS'
+        },
+        'cytoscape': {
+            deps: ['jquery'],
+            exports: 'cytoscape'
+        },
+        'jit': {
+            deps: [],
+            exports: '$jit'
+        },
+        'annotator': {
+            deps: ['jquery'],
+            exports: 'Annotator'
+        },
+        'moment_lang': {
+            deps: ['moment'],
+            exports: 'moment_lang'
+        },
+        'backboneModal': {
+            deps: ['backbone'],
+            exports: 'BackboneModal'
+        },
+        'modal': {
+            deps: ['backbone'],
+            exports: 'Modal'
+        },
+        'bootstrap': {
+            deps: ['jquery']
+        }
     }
-  }
 };
---
+++ assembl/static/js/styleguide.js
@@ -0,0 +1,14 @@
+requirejs.config(requirejs_config);
+
+require(["jquery", "bootstrap"], function ($) {
+    'use strict';
+
+    $('.popover-example').popover({
+        trigger: 'focus'
+    });
+
+    $('.tooltip-example').hover(function () {
+        $(this).tooltip('toggle');
+    })
+
+});
