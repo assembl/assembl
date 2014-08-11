cc8dab1f4f042cd8a44a50716e1f0cd27b74778f
hellsingblack <hellsingblack@gmail.com>
2014-08-11 14:39:37 -0400

    refactoring navigation

--- assembl/static/js/app/views/navigation/navigation.js
+++ assembl/static/js/app/views/navigation/navigation.js
@@ -1,34 +1,34 @@
-define(function(require) {
+define(function (require) {
 
-     var Marionette = require('marionette'),
-           IdeaList = require('views/ideaList'),
-sidebarNotification = require('views/navigation/notification'),
-            HomePanel = require('views/navigation/home'),
-       AssemblPanel = require('views/assemblPanel'),
-                  $ = require('jquery');
+    var Marionette = require('marionette'),
+        IdeaList = require('views/ideaList'),
+        sidebarNotification = require('views/navigation/notification'),
+        HomePanel = require('views/navigation/home'),
+        AssemblPanel = require('views/assemblPanel'),
+        $ = require('jquery');
 
     var navigation = AssemblPanel.extend({
         template: "#tmpl-navigation",
         className: "groupPanel navSidebar",
         regions: {
-           home:'.home',
-           debate:'.debate',
-           synthesis:'.synthesis',
-           notification:'.navNotification'
+            home: '.home',
+            debate: '.debate',
+            synthesis: '.synthesis',
+            notification: '.navNotification'
         },
         events: {
-          'click .nav': 'toggleMenu'
+            'click .nav': 'toggleMenu'
         },
-        initialize: function(options){
+        initialize: function (options) {
             var that = this;
 
             this.groupContent = options.groupContent;
 
-            $(window).resize(function(){
+            $(window).resize(function () {
                 that.initVar();
-                setTimeout(function(){
+                setTimeout(function () {
                     that.setSideBarHeight();
-                }, 1000);
+                }, 600);
             });
 
             this._window = null;
@@ -38,56 +38,56 @@ sidebarNotification = require('views/navigation/notification'),
             this.initVar();
             this.setSideBarHeight();
         },
-        onRender: function(){
-           this.setSideBarHeight();
-           this.notification.show(new sidebarNotification());
+        onRender: function () {
+            this.setSideBarHeight();
+            this.notification.show(new sidebarNotification());
         },
-        toggleMenu: function(e){
-            var elm =  $(e.target),
+        toggleMenu: function (e) {
+            var elm = $(e.target),
                 view = elm.attr('data-view');
 
             this.loadView(view);
 
             this.groupContent.setNavigationState(view);
 
-            if(!elm.next('div.second-level').is(':visible')){
+            if (!elm.next('div.second-level').is(':visible')) {
                 this.$('div.second-level').slideUp();
                 elm.next('div.second-level').css('height', this._sideBarHeight);
                 elm.next('div.second-level').slideDown();
             }
         },
-        setSideBarHeight: function(){
+        setSideBarHeight: function () {
             this.$el.find('.side-menu').css('height', this._accordion);
             this.$el.css('height', this._sideBarHeight);
         },
-        loadView: function(view){
-           switch(view){
-               case 'home':
-                   console.log('load home panel');
-                   var homePanel = new HomePanel();
-                   this.home.show(homePanel);
-                   break;
-               case 'debate':
-                   console.log('load idea table');
-                   var idealist = new IdeaList();
-                   this.debate.show(idealist);
-                   break;
-               case 'synthesis':
-                   console.log('load synthesis');
-                   break;
-               default:
-                   break
-           }
+        loadView: function (view) {
+            switch (view) {
+                case 'home':
+                    console.log('load home panel');
+                    var homePanel = new HomePanel();
+                    this.home.show(homePanel);
+                    break;
+                case 'debate':
+                    console.log('load idea table');
+                    var idealist = new IdeaList();
+                    this.debate.show(idealist);
+                    break;
+                case 'synthesis':
+                    console.log('load synthesis');
+                    break;
+                default:
+                    break
+            }
         },
 
-        initVar: function(){
+        initVar: function () {
             var _header = 60,
-                _adsBox = 150,
+                _searchBox = 43,
                 _li = 105;
 
             this._window = $(window).height() - _header;
-            this._sideBarHeight = (this._window - _adsBox) - _li;
-            this._accordion = this._window - _adsBox;
+            this._sideBarHeight = (this._window - _searchBox) - _li;
+            this._accordion = this._window - _searchBox;
         }
 
     });
