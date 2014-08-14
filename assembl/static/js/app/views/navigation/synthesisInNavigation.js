fbf0ef71faa65b7326704a970d6e9d46d201827d
Marc-Antoine Parent <maparent@acm.org>
2014-08-13 21:15:23 -0400

    show most recent synthesis by default

--- assembl/static/js/app/views/navigation/synthesisInNavigation.js
+++ assembl/static/js/app/views/navigation/synthesisInNavigation.js
@@ -3,6 +3,7 @@ define(function (require) {
     var AssemblPanel = require('views/assemblPanel'),
         CollectionManager = require('modules/collectionManager'),
         Types = require('utils/types'),
+        Ctx = require('modules/context'),
         _ = require('underscore'),
         i18n = require('utils/i18n');
 
@@ -33,11 +34,15 @@ define(function (require) {
                     var synthesisMessages = allMessageStructureCollection.where({'@type': Types.SYNTHESIS_POST}),
                         html = '';
                     if (synthesisMessages.length > 0) {
+                        _.sortBy(synthesisMessages, function (message) { return message.get('date'); });
+                        synthesisMessages.reverse();
                         _.each(synthesisMessages, function (message) {
                             var synthesis = allSynthesisCollection.get(message.get('publishes_synthesis'));
-                            html += "<li data-message-id=" + message.id + ">" + message.get('date') + " " + synthesis.get('subject') + "</li>";
+                            html += "<li data-message-id=" + message.id + ">" + i18n.gettext("Synthesis of ") + Ctx.formatDate(message.get('date')) + " " + synthesis.get('subject') + "</li>";
                         });
                         that.ui.synthesisList.html(html);
+                        that.displaySynthesis(synthesisMessages[0].id);
+                        // TODO: Show that first entry is selected.
                     }
                     else {
                         that.ui.synthesisListHeader.html(i18n.gettext("No synthesis of the discussion has been published yet"));
@@ -47,12 +52,15 @@ define(function (require) {
         },
 
         onSynthesisClick: function (ev) {
-            var messageId = ev.currentTarget.attributes['data-message-id'].value,
-                messageListView = this.groupContent.getViewByTypeName('messageList');
+            this.displaySynthesis(ev.currentTarget.attributes['data-message-id'].value);
+            // TODO: Show that entry is selected.
+        },
+        displaySynthesis: function (messageId) {
+            messageListView = this.groupContent.getViewByTypeName('messageList');
+            messageListView.currentQuery.clearAllFilters();
             messageListView.toggleFilterByPostId(messageId);
             // TODO: Make sure it's expanded
-        }
-
+        },
     });
 
 
