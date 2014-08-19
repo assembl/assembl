a40e820f8679c2d56320a2187897926c52a024a1
Swergas <swergas@yahoo.fr>
2014-08-19 19:46:29 +0200

    add i18n content to the home section of the accordion

--- assembl/static/js/app/views/navigation/home.js
+++ assembl/static/js/app/views/navigation/home.js
@@ -1,11 +1,20 @@
 define(function (require) {
 
-    var AssemblPanel = require('views/assemblPanel');
+    var Assembl = require('modules/assembl'),
+    	AssemblPanel = require('views/assemblPanel');
 
     var HomePanel = AssemblPanel.extend({
         template: '#tmpl-home',
         panelType: 'homeNavPanel',
-        className: 'homeNavPanel'
+        className: 'homeNavPanel',
+
+        events: {
+            'click .go-to-debate': 'goToDebate'
+        },
+
+        goToDebate: function(){
+        	Assembl.vent.trigger("navigation:selected", "debate");
+        }
     });
 
     return HomePanel;
