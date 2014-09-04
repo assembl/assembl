ecd9b59ff4e79d6873f7d16f3cf1ab31d671f4ff
hellsingblack <hellsingblack@gmail.com>
2014-09-04 15:58:45 +0200

    messageList change title "Messages" to "Conversations"

--- assembl/static/js/app/views/messageList.js
+++ assembl/static/js/app/views/messageList.js
@@ -57,7 +57,7 @@ define(function (require) {
             userHighlightNewViewButton: '.messageListViewStyleUserHighlightNew'
         },
         getTitle: function() {
-            return i18n.gettext('Messages');
+            return i18n.gettext('Conversation');
         },
 
         ViewStyles: {
