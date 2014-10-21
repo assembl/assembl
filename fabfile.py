b1d4aa58ab6cdde744eab734a48ebfea92b8cdc5
hellsingblack <hellsingblack@gmail.com>
2014-10-21 14:13:40 +0200

    fix fabfile

--- fabfile.py
+++ fabfile.py
@@ -579,7 +579,6 @@ def bower_install():
     bower_cmd('install', 'assembl/widget/session')
     bower_cmd('install', 'assembl/widget/video')
     bower_cmd('install', 'assembl/widget/vote')
-    bower_cmd('install', 'assembl/widget/creativity')
 
 
 @task
