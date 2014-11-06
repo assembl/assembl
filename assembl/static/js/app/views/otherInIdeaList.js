e44af82cfb45202536a2efc9c16c2e5b3ad66561
hellsingblack <hellsingblack@gmail.com>
2014-11-06 17:44:43 +0100

    fix sub title hidden ideaList

--- assembl/static/js/app/views/otherInIdeaList.js
+++ assembl/static/js/app/views/otherInIdeaList.js
@@ -2,7 +2,8 @@ define(function (require) {
     'use strict';
 
     var Ctx = require('common/context'),
-        IdeaView = require('views/idea');
+        IdeaView = require('views/idea'),
+        _ = require('underscore');
 
     var otherInIdeaList = IdeaView.extend({
         template: Ctx.loadTemplate('otherInIdeaList'),
@@ -12,14 +13,17 @@ define(function (require) {
             this.$el.addClass('idealist-item');
 
             var hasOrphanPosts = this.model.get('num_orphan_posts'),
-                hasReadPosts = this.model.get('num_read_posts'),
-                hasSynthesisPosts = this.model.get('num_synthesis_posts'),
-                hasPosts = this.model.get('num_posts');
+                hasSynthesisPosts = this.model.get('num_synthesis_posts');
 
-            if (hasOrphanPosts === 0 || hasReadPosts === 0 ||
-                hasSynthesisPosts === 0 || hasPosts === 0) {
+            var subMenu = _.find([hasOrphanPosts, hasSynthesisPosts], function (num) {
+                return num !== 0;
+            });
+
+            if (typeof subMenu === 'undefined') {
 
                 this.$el.addClass('hidden');
+            } else {
+                this.$el.removeClass('hidden');
             }
 
             this.$el.html(this.template);
