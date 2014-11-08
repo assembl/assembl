e7a6d6bf2e1f28cd5fe9f139b920c769315af805
Marc-Antoine Parent <maparent@acm.org>
2014-11-07 19:59:23 -0500

    add a periodic job to check notifications to be sent

--- assembl/tasks/notify.py
+++ assembl/tasks/notify.py
@@ -1,11 +1,22 @@
 import sys
+from datetime import timedelta
 
 from celery import Celery
 
 from . import init_task_config, config_celery_app
 
+
+CELERYBEAT_SCHEDULE = {
+    'resend-every-10-minutes': {
+        'task': 'assembl.tasks.notify.process_pending_notifications',
+        'schedule': timedelta(seconds=600),
+        'args': ()
+    },
+}
+
 # broker specified
 notify_celery_app = Celery('celery_tasks.notify')
+notify_celery_app._preconf = {"CELERYBEAT_SCHEDULE": CELERYBEAT_SCHEDULE}
 
 watcher = None
 
@@ -17,9 +28,10 @@ def notify(id):
     sys.stderr.write("notify called with "+str(id))
 
 
-@notify_celery_app.task(ignore_result=True)
-def resend_notifications():
+@notify_celery_app.task(ignore_result=False)
+def process_pending_notifications():
     init_task_config()
+    sys.stderr.write("processing...")
     # TODO: check notifications that need resending.
 
 
