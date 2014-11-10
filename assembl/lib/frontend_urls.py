917bc11676c69de42566f22a3905e11da1344307
Marc-Antoine Parent <maparent@acm.org>
2014-11-10 16:08:20 -0500

    missing component

--- assembl/lib/frontend_urls.py
+++ assembl/lib/frontend_urls.py
@@ -2,27 +2,36 @@ from ..models import Discussion
 from urlparse import urljoin
 import urllib
 
+
 class FrontendUrls():
     def __init__(self, discussion):
         assert isinstance(discussion, Discussion)
         self.discussion = discussion
-        
+
     def getDiscussionLogoUrl(self):
-        return urljoin(self.discussion.get_base_url(),'/static/img/assembl.png')
+        return urljoin(
+            self.discussion.get_base_url(), '/static/img/assembl.png')
 
     def getDiscussionUrl(self):
         from pyramid.request import Request
         req = Request.blank('/', base_url=self.discussion.get_base_url())
         #Celery didn't like this.  To revisit once we have virtual hosts
         #return req.route_url('home', discussion_slug=self.discussion.slug)
-        return urljoin(self.discussion.get_base_url(), self.discussion.slug+'/')
-    
+        return urljoin(
+            self.discussion.get_base_url(), self.discussion.slug+'/')
+
     def getUserNotificationSubscriptionsConfigurationUrl(self):
-        return urljoin(self.getDiscussionUrl(),'users/notifications')
-    
+        return urljoin(
+            self.getDiscussionUrl(), 'users/notifications')
+
     def getUserNotificationSubscriptionUnsubscribeUrl(self, subscription):
         """ TODO:  Give an actual subscription URL """
         return self.getUserNotificationSubscriptionsConfigurationUrl()
-    
+
     def getPostUrl(self, post):
-        return urljoin(self.getDiscussionUrl(),'posts/'+urllib.quote(post.uri(), ''))
+        return urljoin(
+            self.getDiscussionUrl(), 'posts/' + urllib.quote(post.uri(), ''))
+
+    def get_discussion_edition_url(self):
+        return urljoin(
+            self.getDiscussionUrl(), 'edition')
