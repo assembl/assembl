fa1494e1ad78f122347ecf1ed5884ca0ea603be9
Marc-Antoine Parent <maparent@acm.org>
2014-11-18 20:02:56 -0500

    forgotten argument. Found by integration test!

--- assembl/static/js/app/routeManager.js
+++ assembl/static/js/app/routeManager.js
@@ -125,7 +125,7 @@ define(function (require) {
                     /* Reset the context of the user view, if it's too old to be
                      usable, or if it wasn't initialized before */
                     // Find if a group exists that has a navigation panel
-                    var navigableGroupSpec = groupSpecs.find(function () {
+                    var navigableGroupSpec = groupSpecs.find(function (aGroupSpec) {
                         return aGroupSpec.findNavigationSidebarPanelSpec();
                     });
                     if (navigableGroupSpec) {
