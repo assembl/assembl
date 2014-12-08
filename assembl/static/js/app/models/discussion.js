de3c44db8daa486ef3ecdeb0f260da3f58f333cb
hellsingblack <hellsingblack@gmail.com>
2014-12-08 20:51:46 +0100

    refactoring: add missing file

---
+++ assembl/static/js/app/models/discussion.js
@@ -0,0 +1,32 @@
+'use strict';
+
+define(['models/base', 'common/context'], function (Base, Ctx) {
+
+    var discussionModel = Base.Model.extend({
+        urlRoot: '/api/v1/discussion/' + Ctx.getDiscussionId(),
+        idAttribute: "@id",
+        defaults: {
+            //'instigator': null,
+            //'introduction': null,
+            'objectives': null,
+            //'creation_date': null,
+            'topic': null,
+            //'sources': [],
+            //'@type': null,
+            //'widget_collection_url':null,
+            'slug': null
+            //'@view':null
+        }
+
+    });
+
+    var discussionCollection = Base.Collection.extend({
+        url: '/api/v1/discussion/' + Ctx.getDiscussionId(),
+        model: discussionModel
+    });
+
+    return {
+        Model: discussionModel,
+        Collection: discussionCollection
+    };
+});
