af47bb0d2beced0aa56d9d3e9acb88dc32f7ec9a
hellsingblack <hellsingblack@gmail.com>
2014-12-15 16:50:37 +0100

    refactoring: api profile

--- assembl/static/js/app/views/user/profile.js
+++ assembl/static/js/app/views/user/profile.js
@@ -6,22 +6,37 @@ define(['backbone.marionette', 'models/user', 'common/context'],
         var userProfile = Marionette.ItemView.extend({
             template: '#tmpl-profile',
             className: 'admin-profile',
+            ui: {
+                close: '.bx-alert-success .bx-close',
+                profile: '.js_saveProfile'
+            },
             initialize: function () {
-
                 this.model = new User.Model();
                 this.model.url = Ctx.getApiUrl('agents/') + Ctx.getCurrentUserId();
-                this.model.fetch();
-
+              this.model.fetch();
             },
 
             modelEvents: {
                 'sync': 'render'
             },
 
+            events: {
+                'click @ui.profile': 'saveProfile',
+                'click @ui.close': 'close'
+            },
+
             serializeData: function () {
                 return {
                     profile: this.model
                 }
+            },
+
+            saveProfile: function () {
+                //TODO: backbone sync to server
+            },
+
+            close: function () {
+                this.$('.bx-alert-success').addClass('hidden');
             }
         });
 
