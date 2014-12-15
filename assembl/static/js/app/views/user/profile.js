56b3ded88e4de92f3d70fcab2c5b4a3e029a4693
hellsingblack <hellsingblack@gmail.com>
2014-12-15 15:48:47 +0100

    refactoring: api profile

--- assembl/static/js/app/views/user/profile.js
+++ assembl/static/js/app/views/user/profile.js
@@ -3,40 +3,38 @@
 define(['backbone.marionette', 'models/user', 'common/context'],
     function (Marionette, User, Ctx) {
 
-    var userProfile = Marionette.LayoutView.extend({
-        template: '#tmpl-profile',
-        className: 'admin-profile',
-        regions: {
+        var userProfile = Marionette.LayoutView.extend({
+            template: '#tmpl-profile',
+            className: 'admin-profile',
+            regions: {
 
-        },
-        initialize: function () {
+            },
+            initialize: function () {
 
-            this.model = new User.Model();
-            this.model.url = Ctx.getApiUrl('agents/') + Ctx.getCurrentUserId();
-            this.model.fetch();
+                this.model = new User.Model();
+                this.model.url = Ctx.getApiUrl('agents/') + Ctx.getCurrentUserId();
+                this.model.fetch();
 
-        },
-        modelEvents: {
-            'sync': 'render'
-        },
+            },
+            modelEvents: {
+                'sync': 'render'
+            },
 
-        events: {
+            events: {
 
-        },
+            },
 
-        serializeData: function () {
-            return {
-                profile: this.model
+            serializeData: function () {
+                return {
+                    profile: this.model
+                }
+            },
+
+            onRender: function () {
+
             }
-        },
 
-        onRender: function () {
+        });
 
-
-            console.debug(this.model);
-        }
-
+        return userProfile;
     });
-
-    return userProfile;
-});
