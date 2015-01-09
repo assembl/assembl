16d31374363b22e8f96edfad431eac5ed70472ec
Marc-Antoine Parent <maparent@acm.org>
2015-01-09 10:37:14 -0500

    kill old quad storage contexts

---
+++ assembl/alembic/versions/236808a19c3b_delete_quad_storages.py
@@ -0,0 +1,25 @@
+"""delete quad storages
+
+Revision ID: 236808a19c3b
+Revises: 5a0ce18bf2b2
+Create Date: 2015-01-09 10:35:14.910892
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '236808a19c3b'
+down_revision = '5a0ce18bf2b2'
+
+from alembic import context
+from assembl.semantic.virtuoso_mapping import AssemblQuadStorageManager
+
+
+def upgrade(pyramid_env):
+    with context.begin_transaction():
+        aqsm = AssemblQuadStorageManager()
+        aqsm.drop_all_discussion_storages_but(-1)
+
+
+def downgrade(pyramid_env):
+    with context.begin_transaction():
+        pass
