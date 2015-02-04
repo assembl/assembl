assembl/assembl

commit b2b174b397ee5367d5b30307538f16d9dbf85fbc

Actual group separation

assembl/static/js/app/views/synthesisPanel.js
@@ -12,7 +12,7 @@ define(['views/visitors/objectTreeRenderVisitor', 'underscore', 'jquery', 'app',
              * @init
              */
             initialize: function (obj) {
-                Object.getPrototypeOf(Object.getPrototypeOf(this)).initialize(options);
+                Object.getPrototypeOf(Object.getPrototypeOf(this)).initialize.apply(this, arguments);
                 var that = this,
                     collectionManager = new CollectionManager();
 

assembl/static/js/app/common/context.js
@@ -205,24 +205,6 @@ define(['../app', 'jquery', '../utils/permissions', '../utils/roles', 'moment', 
             },
 
             /**
-             * Set the given Idea as the current one to be edited
-             * @param  {Idea} [idea]
-             */
-            DEPRECATEDsetCurrentIdea: function (idea) {
-                //console.log("DEPRECATEDsetCurrentIdea() fired", idea);
-                if (idea != this.DEPRECATEDgetCurrentIdea()) {
-                    this.currentIdea = idea;
-                    //TODO:  Remove this, it will not respect group separation of context
-                    Assembl.vent.trigger("DEPRECATEDidea:selected", idea);
-                }
-
-            },
-
-            DEPRECATEDgetCurrentIdea: function () {
-                return this.currentIdea;
-            },
-
-            /**
              * Returns a template from an script tag
              * @param {string} id The id of the script tag
              * @return {function} The Underscore.js _.template return
@@ -757,17 +739,6 @@ define(['../app', 'jquery', '../utils/permissions', '../utils/roles', 'moment', 
             },
 
             /**
-             * Format date time
-             * @param {Date|timestamp} date
-             * @param {String} [format=app.datetimeFormat] The format
-             * @return {string}
-             */
-            //FIXME: this method never use in app
-            /*formatDatetime: function(date, format){
-             return this.formatDate(date, format || this.datetimeFormat);
-             },*/
-
-            /**
              * Returns a fancy date (ex: a few seconds ago), or a formatted precise date if precise is true
              * @return {String}
              */
@@ -953,8 +924,6 @@ define(['../app', 'jquery', '../utils/permissions', '../utils/roles', 'moment', 
                         $(document.body).addClass('is-fullscreen');
                     }
                 });
-
-                this.DEPRECATEDsetCurrentIdea(null);
             },
 
             /**

assembl/static/js/app/views/allMessagesInIdeaList.js
@@ -42,12 +42,12 @@ define(['views/idea', 'common/context', 'utils/panelSpecTypes'],
              * @event
              */
             onTitleClick: function () {
-                var messageListView = this.groupContent.findViewByType(PanelSpecTypes.MESSAGE_LIST);
+                var messageListView = this._groupContent.findViewByType(PanelSpecTypes.MESSAGE_LIST);
                 messageListView.triggerMethod('messageList:clearAllFilters');
-                Ctx.DEPRECATEDsetCurrentIdea(null);
+                this._groupContent.setCurrentIdea(null);
                 //Yes, this will cause double-renders in some cases.  Will be fixed once messageList observes it's result list.
                 messageListView.render();
-                this.groupContent.resetDebateState(false);
+                this._groupContent.resetDebateState(false);
             }
         });
 

assembl/static/js/app/views/assemblPanel.js
@@ -24,7 +24,7 @@ define(['backbone.marionette', 'underscore'], function (Marionette, _) {
         minimized_size: 40,
 
         /** Subclasses need to call this first, with
-         * Object.getPrototypeOf(Object.getPrototypeOf(this)).initialize(options) */
+         * Object.getPrototypeOf(Object.getPrototypeOf(this)).initialize.apply(this, arguments) */
         initialize:  function (options) {
           this._panelWrapper = options.panelWrapper;
           if (!this._panelWrapper) {

assembl/static/js/app/views/groups/groupContent.js
@@ -36,6 +36,32 @@ define(['backbone.marionette', 'common/context', 'models/panelSpec', 'views/asse
                 };
             },
 
+            /**
+             * Set the given Idea as the current one to be edited
+             * @param  {Idea} [idea]
+             */
+            setCurrentIdea: function (idea) {
+              //console.log("setCurrentIdea() fired", idea);
+              if (idea === undefined) {
+                throw new Error("Setting the idea undefined isn't allowed.  Perhaps you meant null?");
+              }
+              if (idea != this.getCurrentIdea()) {
+                // console.log("About to set current idea on group:", this.cid);
+                this._currentIdea = idea;
+                this.trigger("idea:set", idea);
+              }
+
+            },
+
+            /**
+             * @return: undefined if not idea was set yet.  
+             * null if it was explicitely set to no idea.
+             * 
+             */
+            getCurrentIdea: function () {
+              return this._currentIdea;
+            },
+
             closeGroup: function () {
                 this.model.collection.remove(this.model);
                 this.groupContainer.resizeAllPanels();
@@ -165,6 +191,7 @@ define(['backbone.marionette', 'common/context', 'models/panelSpec', 'views/asse
             },
 
             resetDebateState: function (skip_animation) {
+                // Ensure that the simple view is in debate state, and coherent.
                 if (this.findNavigationSidebarPanelSpec()) {
                     this.groupContainer.suspendResize();
                     this.model.set('navigationState', 'debate');
@@ -239,11 +266,11 @@ define(['backbone.marionette', 'common/context', 'models/panelSpec', 'views/asse
                     ideaPanel = this.findWrapperByType(PanelSpecTypes.IDEA_PANEL);
                 this.resetMessagePanelWidth();
                 if (ideaPanel != null && !ideaPanel.model.get('locked') && (!nav || this.model.get('navigationState') == 'debate')) {
-                    if (ctx.DEPRECATEDgetCurrentIdea() == undefined) {
-                        ideaPanel.minimizePanel();
-                    } else {
-                        ideaPanel.unminimizePanel();
-                    }
+                  if (this.getCurrentIdea()) {
+                    ideaPanel.unminimizePanel();
+                  } else {
+                    ideaPanel.minimizePanel();
+                  }
                 }
             },
 

assembl/static/js/app/views/groups/panelWrapper.js
@@ -169,7 +169,7 @@ define(['backbone.marionette', 'objects/viewsFactory', 'common/context', 'views/
             unminimizePanel: function (evt) {
                 if (!this.model.get('minimized'))
                     return;
-                if (this.model.isOfType(PanelSpecTypes.IDEA_PANEL) && Ctx.DEPRECATEDgetCurrentIdea() == undefined && evt && evt.currentTarget) {
+                if (this.model.isOfType(PanelSpecTypes.IDEA_PANEL) && !this.groupContent.getCurrentIdea() && evt && evt.currentTarget) {
                     // do not accept to unminimize if no idea to show
                     var el = this.ui.minimizePanel;
                     el.attr("data-original-title", i18n.gettext('Please select an idea in the table of ideas to open the idea panel.'));

assembl/static/js/app/views/idea.js
@@ -31,12 +31,17 @@ define(['backbone', 'underscore', 'app', 'common/context', 'utils/permissions', 
             initialize: function (options, view_data) {
                 var that = this;
                 this.view_data = view_data;
-                this.groupContent = options.groupContent;
+                if(options.groupContent) {
+                  this._groupContent = options.groupContent;
+                }
+                else {
+                  throw new Error("groupContent must be passes in constructor options");
+                }
 
                 this.listenTo(this.model, 'change', this.render);
                 this.listenTo(this.model, 'replacedBy', this.onReplaced);
 
-                this.listenTo(Assembl.vent, 'DEPRECATEDidea:selected', function (idea) {
+                this.listenTo(this._groupContent, 'idea:set', function (idea) {
                     that.onIsSelectedChange(idea);
                 });
             },
@@ -73,7 +78,7 @@ define(['backbone', 'underscore', 'app', 'common/context', 'utils/permissions', 
 
                 this.$el.addClass('idealist-item');
                 Ctx.removeCurrentlyDisplayedTooltips(this.$el);
-                this.onIsSelectedChange(Ctx.DEPRECATEDgetCurrentIdea());
+                this.onIsSelectedChange(this._groupContent.getCurrentIdea());
 
                 if (data.isOpen === true) {
                     this.$el.addClass('is-open');
@@ -92,7 +97,7 @@ define(['backbone', 'underscore', 'app', 'common/context', 'utils/permissions', 
                 Ctx.initTooltips(this.$el);
                 var rendered_children = [];
                 _.each(data['children'], function (idea, i) {
-                    var ideaView = new IdeaView({model: idea, groupContent: that.groupContent}, view_data);
+                    var ideaView = new IdeaView({model: idea, groupContent: that._groupContent}, view_data);
                     rendered_children.push(ideaView.render().el);
                 });
                 this.$('.idealist-children').append(rendered_children);
@@ -133,8 +138,6 @@ define(['backbone', 'underscore', 'app', 'common/context', 'utils/permissions', 
              */
             onReplaced: function (newObject) {
                 this.model = newObject;
-                //That makes no sense, there is no way to know it's the current idea
-                //app.DEPRECATEDsetCurrentIdea(newObject);
             },
 
 
@@ -162,20 +165,21 @@ define(['backbone', 'underscore', 'app', 'common/context', 'utils/permissions', 
             onTitleClick: function (e) {
                 var messageListView;
                 e.stopPropagation();
+                console.log("idea::onTitleClick with groupcontent",this._groupContent.cid);
                 if (Ctx.getCurrentInterfaceType() === Ctx.InterfaceTypes.SIMPLE) {
-                    messageListView = this.groupContent.findViewByType(PanelSpecTypes.MESSAGE_LIST);
+                    messageListView = this._groupContent.findViewByType(PanelSpecTypes.MESSAGE_LIST);
                     messageListView.triggerMethod('messageList:clearAllFilters');
                 }
-                if (this.model === Ctx.DEPRECATEDgetCurrentIdea()) {
+                if (this.model === this._groupContent.getCurrentIdea()) {
                     // We want to avoid the "All messages" state,
                     // unless the user clicks explicitly on "All messages".
                     // TODO benoitg: Review this decision.
-                    //Ctx.DEPRECATEDsetCurrentIdea(null);
+                    //this._groupContent.setCurrentIdea(null);
                     //This is so the messageList refreshes.
                 } else {
-                    Ctx.DEPRECATEDsetCurrentIdea(this.model);
+                  this._groupContent.setCurrentIdea(this.model);
                 }
-                this.groupContent.resetDebateState(false);
+                this._groupContent.resetDebateState(false);
             },
 
             /**
@@ -186,7 +190,7 @@ define(['backbone', 'underscore', 'app', 'common/context', 'utils/permissions', 
                 e.stopPropagation();
 
                 Assembl.vent.trigger('messageList:addFilterIsRelatedToIdea', this.model, true);
-                Ctx.DEPRECATEDsetCurrentIdea(this.model);
+                this._groupContent.setCurrentIdea(this.model);
             },
 
             /**
@@ -303,7 +307,8 @@ define(['backbone', 'underscore', 'app', 'common/context', 'utils/permissions', 
                     if (isDraggedBelow) {
                         // Add as a child idea
                         var newIdea = this.model.addSegmentAsChild(segment);
-                        Ctx.DEPRECATEDsetCurrentIdea(newIdea);
+                        this._groupContent.setCurrentIdea(newIdea);
+                        this._groupContent.resetDebateState(false);
                     } else {
                         // Add to the current idea
                         this.model.addSegment(segment);
@@ -319,6 +324,7 @@ define(['backbone', 'underscore', 'app', 'common/context', 'utils/permissions', 
                         Ctx.currentAnnotationIdIdea = null;
                         Ctx.currentAnnotationNewIdeaParentIdea = this.model;
                         Ctx.saveCurrentAnnotationAsExtract();
+                        this._groupContent.resetDebateState(false);
                     } else {
                         // Add as a segment
                         Ctx.currentAnnotationIdIdea = this.model.getId();

assembl/static/js/app/views/ideaGraph.js
@@ -103,7 +103,8 @@ define(['jit', 'common/context'], function ($jit, Ctx) {
                         var idea = allIdeasCollection.get(node.id);
                         var suffix = "";
                         if (idea !== undefined) {
-                            Ctx.DEPRECATEDsetCurrentIdea(idea);
+                            //Ctx.DEPRECATEDsetCurrentIdea(idea);
+                            throw new Error("I didn't port this to the new group separation");
                             var num_posts = idea.get('num_posts'),
                                 num_read_posts = idea.get('num_read_posts');
                             if (num_read_posts == num_posts) {

commit f337636fee76fef7a56f7f1bcd6a0881522998a3

Fix synthesis publication.  Was broken in b2b174b397ee5367d5b30307538f16d9dbf85fbc, fixes https://app.asana.com/0/15264711598672/26056569613790

assembl/static/js/app/views/synthesisPanel.js
@@ -1,7 +1,7 @@
 'use strict';
 
-define(['views/visitors/objectTreeRenderVisitor', 'underscore', 'jquery', 'app', 'common/context', 'models/synthesis', 'models/idea', 'utils/permissions', 'views/ideaFamily', 'views/ideaInSynthesis', 'utils/panelSpecTypes', 'views/assemblPanel', 'utils/i18n', 'views/editableField', 'views/ckeditorField', 'common/collectionManager'],
-    function (objectTreeRenderVisitor, _, $, Assembl, Ctx, Synthesis, Idea, Permissions, IdeaFamilyView, IdeaInSynthesisView, PanelSpecTypes, AssemblPanel, i18n, EditableField, CKEditorField, CollectionManager) {
+define(['views/visitors/objectTreeRenderVisitor', 'raven', 'underscore', 'jquery', 'app', 'common/context', 'models/synthesis', 'models/idea', 'utils/permissions', 'views/ideaFamily', 'views/ideaInSynthesis', 'utils/panelSpecTypes', 'views/assemblPanel', 'utils/i18n', 'views/editableField', 'views/ckeditorField', 'common/collectionManager'],
+    function (objectTreeRenderVisitor, Raven, _, $, Assembl, Ctx, Synthesis, Idea, Permissions, IdeaFamilyView, IdeaInSynthesisView, PanelSpecTypes, AssemblPanel, i18n, EditableField, CKEditorField, CollectionManager) {
 
         var SynthesisPanel = AssemblPanel.extend({
             template: '#tmpl-synthesisPanel',
@@ -212,7 +212,7 @@ define(['views/visitors/objectTreeRenderVisitor', 'underscore', 'jquery', 'app',
                     url = Ctx.getApiUrl('posts'),
                     that = this;
 
-                var onSuccess = function (resp) {
+                var doPublish = function () {
                     var data = {
                         publishes_synthesis_id: publishes_synthesis_id,
                         subject: "Not used",
@@ -227,15 +227,24 @@ define(['views/visitors/objectTreeRenderVisitor', 'underscore', 'jquery', 'app',
                         data: JSON.stringify(data),
                         contentType: 'application/json',
                         url: url,
-                        success: function () {
+                        done: function () {
                             alert(i18n.gettext("Synthesis published!"));
-                            that.unblockPanel();
                             that.model = new Synthesis.Model({'@id': 'next_synthesis'});
                             that.model.fetch();
-                        }
+                            that.unblockPanel();
+                        },
+                        fail: function () {
+                          Raven.captureMessage('Broken!')
+                          alert(i18n.gettext("Failed publishing synthesis!"));
+                          that.model = new Synthesis.Model({'@id': 'next_synthesis'});
+                          that.model.fetch();
+                          that.unblockPanel();
+                      },
                     });
                 };
+
                 that.blockPanel();
+                doPublish();
             }
 
         });

config.rb
@@ -12,4 +12,4 @@ http_generated_images_path = '/static/img/sprites'
 
 javascripts_dir = "js"
 
-sass_options = { :debug_info => true } # activing source maps
+sass_options = { :debug_info => true } # activates source maps