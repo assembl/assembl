0860daf5ff38d5f8a1d6380cd67ac38bbd29f89a
hellsingblack <hellsingblack@gmail.com>
2015-02-11 14:53:36 -0500

    refactoring admin notification

--- assembl/static/js/app/views/admin/adminNotificationSubscriptions.js
+++ assembl/static/js/app/views/admin/adminNotificationSubscriptions.js
@@ -3,21 +3,53 @@
 define(['backbone.marionette', 'common/collectionManager', 'utils/permissions', 'common/context', 'utils/i18n'],
     function (Marionette, CollectionManager, Permissions, Ctx, i18n) {
 
-        var adminNotificationSubscriptions = Marionette.LayoutView.extend({
-            template: '#tmpl-adminNotificationSubscriptions',
-            className: 'admin-notifications',
+        var notifications = Marionette.ItemView.extend({
+            template: '#tmpl-adminNotification',
             ui: {
-                subscribeCheckbox: ".js_adminNotification"
+               subscribeCheckbox: ".js_adminNotification"
             },
+            events: {
+               'click @ui.subscribeCheckbox': 'discussionNotification'
+            },
+            serializeData: function () {
+                return {
+                    i18n: i18n,
+                    notification: this.model
+                }
+            },
+            discussionNotification: function (e) {
+                var elm = $(e.target),
+                    status = elm.is(':checked') ? 'ACTIVE' : 'UNSUBSCRIBED';
+
+                this.model.set("status", status);
+                this.model.save(null, {
+                    success: function (model, resp) {
+                    },
+                    error: function (model, resp) {
+                        console.error('ERROR: discussionNotification', resp);
+                    }
+                });
+            }
+        });
+
+        var notificationList = Marionette.CollectionView.extend({
+            childView: notifications,
             collectionEvents: {
                 "reset": "render", // equivalent to view.listenTo(view.collection, "reset", view.render, view)
                 "sync": "render"
+            }
+        });
+
+        var adminNotificationSubscriptions = Marionette.LayoutView.extend({
+            template: '#tmpl-adminNotificationSubscriptions',
+            className: 'admin-notifications',
+            regions: {
+              notification:'#notification-content'
             },
             initialize: function () {
                 var collectionManager = new CollectionManager(),
                     that = this;
 
-                this.collection = new Backbone.Collection();
                 if (!Ctx.getCurrentUser().can(Permissions.ADMIN_DISCUSSION)) {
                     // TODO ghourlier: Éviter que les gens n'ayant pas l'autorisation accèdent à cet écran.
                     alert("This is an administration screen.");
@@ -26,41 +58,16 @@ define(['backbone.marionette', 'common/collectionManager', 'utils/permissions', 
 
                 $.when(collectionManager.getNotificationsDiscussionCollectionPromise()).then(
                     function (NotificationsDiscussion) {
-                        that.collection.reset(NotificationsDiscussion.models);
+                        that.notifications = NotificationsDiscussion;
+                        that.render();
                     });
             },
-
-            events: {
-                'click @ui.subscribeCheckbox': 'discussionNotification'
-            },
-
-            serializeData: function () {
-                var discussionNotifications = _.filter(this.collection.models, function (m) {
-                    return m.get('creation_origin') === 'DISCUSSION_DEFAULT';
+            onRender: function(){
+                var notif = new notificationList({
+                    collection: this.notifications
                 });
-
-                return {
-                    i18n: i18n,
-                    DiscussionNotifications: discussionNotifications
-                }
-            },
-
-            discussionNotification: function (e) {
-                var elm = $(e.target);
-
-                var status = elm.is(':checked') ? 'ACTIVE' : 'UNSUBSCRIBED';
-
-                var notificationSubscriptionModel = this.collection.get(elm.attr('id'));
-                notificationSubscriptionModel.set("status", status);
-                notificationSubscriptionModel.save(null, {
-                    success: function (model, resp) {
-                    },
-                    error: function (model, resp) {
-                        console.error('ERROR: discussionNotification', resp);
-                    }
-                });
+                this.notification.show(notif)
             }
-
 
         });
 
