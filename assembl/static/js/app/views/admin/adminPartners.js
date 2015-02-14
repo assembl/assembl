3081a07ee89cee6ad0265eba5ec25a1efa5837e2
hellsingblack <hellsingblack@gmail.com>
2015-02-14 17:16:59 -0500

    refactoring with marionette pattern

--- assembl/static/js/app/views/admin/adminPartners.js
+++ assembl/static/js/app/views/admin/adminPartners.js
@@ -1,44 +1,54 @@
 'use strict';
 
-define(['backbone.marionette', 'jquery', 'common/collectionManager', 'common/context', 'utils/i18n'],
-    function (Marionette, $, CollectionManager, Ctx, i18n) {
+define(['backbone.marionette', 'app','jquery', 'common/collectionManager', 'common/context', 'utils/i18n', 'backbone.modal', 'backbone.marionette.modals'],
+    function (Marionette, Assembl, $, CollectionManager, Ctx, i18n, backboneModal, marionetteModal) {
 
         var Partners = Marionette.ItemView.extend({
             template: '#tmpl-partnersInAdmin',
+            className: 'gr',
             serializeData: function(){
                 return {
                     partner: this.model
                 }
+            },
+            deletePartner: function(){
+
+            },
+            editPartner: function(){
+
             }
         });
 
         var ParnerList = Marionette.CollectionView.extend({
-            childView: Partners
+            childView: Partners,
+            initialize: function(){
+                var that = this,
+                    collectionManager = new CollectionManager();
+
+                this.collection = undefined;
+
+                $.when(collectionManager.getAllPartnerOrganizationCollectionPromise()).then(
+                    function (allPartnerOrganization) {
+                        that.collection = allPartnerOrganization;
+                        that.render();
+                    });
+            }
         });
 
         var adminPartners = Marionette.LayoutView.extend({
             template: '#tmpl-adminPartners',
             className: 'admin-notifications',
             ui: {
-              partners: '.js_add-partner',
+              partners: '.js_addPartner',
               close: '.bx-alert-success .bx-close'
             },
-            regions: {
-              partners: '#partner-content'
-            },
-            initialize: function () {
-                var that = this,
-                    collectionManager = new CollectionManager();
 
-                $.when(collectionManager.getAllPartnerOrganizationCollectionPromise()).then(
-                    function (allPartnerOrganization) {
-                        that.partnerOrganization = allPartnerOrganization;
-                        that.render();
-                    });
+            regions: {
+              partner: '#partner-content'
             },
 
             events: {
-                'click @ui.partners': 'addPartner',
+                'click @ui.partners': 'addNewPartner',
                 'click @ui.close': 'close'
             },
 
@@ -51,72 +61,89 @@ define(['backbone.marionette', 'jquery', 'common/collectionManager', 'common/con
             onRender: function () {
                 Ctx.initTooltips(this.$el);
 
-                var parnerList = new ParnerList({
-                    collection: this.partnerOrganization
-                });
+                var parnerList = new ParnerList();
 
-                this.partners.show(parnerList);
+                this.partner.show(parnerList);
             },
 
             close: function () {
                 this.$('.bx-alert-success').addClass('hidden');
             },
 
-            addPartner: function (e) {
-                e.preventDefault();
+            addNewPartner: function(){
 
-                var that = this,
-                    inputs = this.$('*[required=required]'),
-                    dataPartner = this.$('#form-partner').serialize(),
-                    urlPartner = '/data/Discussion/' + Ctx.getDiscussionId() + '/partner_organizations/',
-                    regexUrl = /^(http|https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/,
-                    controls = document.querySelectorAll('#form-partner .control-group');
+                var Modal = Backbone.Modal.extend({
+                    template: _.template($('#tmpl-adminPartnerForm').html()),
+                    className: 'partner-modal popin-wrapper',
+                    cancelEl: '.close, .js_close',
+                    keyControl: false,
+                    initialize: function () {
+                        this.$('.bbm-modal').addClass('popin');
+                    },
+                    events: {
+                     'click .js_validatePartner' :'validatePartner'
+                    },
+                    validatePartner: function (e) {
 
-                inputs.each(function () {
-                    var parent = $(this).parent().parent();
+                        var that = this,
+                            inputs = this.$('*[required=required]'),
+                            dataPartner = this.$('#partner-form').serialize(),
+                            urlPartner = '/data/Discussion/' + Ctx.getDiscussionId() + '/partner_organizations/',
+                            regexUrl = /^(http|https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/,
+                            controls = document.querySelectorAll('#partner-form .control-group');
 
-                    if (!$(this).val()) {
-                        parent.addClass('error');
-                    }
-                    if ($(this).val()) {
-                        parent.removeClass('error').addClass('success');
+                        inputs.each(function () {
+                            var parent = $(this).parent().parent();
 
-                        if ($(this).hasClass('partner-homepage')) {
-                            if (!regexUrl.test($(this).val())) {
-                                parent.addClass('error').removeClass('success');
-                            } else {
-                                parent.removeClass('error').addClass('success');
+                            if (!$(this).val()) {
+                                parent.addClass('error');
                             }
-                        }
-                        if ($(this).hasClass('partner-logo')) {
-                            if (!regexUrl.test($(this).val())) {
-                                parent.addClass('error').removeClass('success');
-                            } else {
+                            if ($(this).val()) {
                                 parent.removeClass('error').addClass('success');
+
+                                if ($(this).hasClass('partner-homepage')) {
+                                    if (!regexUrl.test($(this).val())) {
+                                        parent.addClass('error').removeClass('success');
+                                    } else {
+                                        parent.removeClass('error').addClass('success');
+                                    }
+                                }
+                                if ($(this).hasClass('partner-logo')) {
+                                    if (!regexUrl.test($(this).val())) {
+                                        parent.addClass('error').removeClass('success');
+                                    } else {
+                                        parent.removeClass('error').addClass('success');
+                                    }
+                                }
                             }
+                        });
+
+                        if(!$(controls).hasClass('error')){
+                            var inputs = document.querySelectorAll('#partner-form *[required=required]');
+                            $(controls).removeClass('success');
+                            $(inputs).val('');
+
+                            $.ajax({
+                                url: urlPartner,
+                                type: "post",
+                                data: dataPartner,
+                                success: function (response, text) {
+                                    that.render();
+                                    that.$('.bx-alert-success').removeClass('hidden');
+                                },
+                                error: function (request, status, error) {
+                                    alert(status + ': ' + error);
+                                }
+                            });
+
                         }
+
                     }
                 });
 
-                if(!$(controls).hasClass('error')){
-                    var inputs = document.querySelectorAll('#form-partner *[required=required]');
-                    $(controls).removeClass('success');
-                    $(inputs).val('');
+                var modal = new Modal();
 
-                    $.ajax({
-                        url: urlPartner,
-                        type: "post",
-                        data: dataPartner,
-                        success: function (response, text) {
-                          that.render();
-                          that.$('.bx-alert-success').removeClass('hidden');
-                        },
-                        error: function (request, status, error) {
-                            alert(status + ': ' + error);
-                        }
-                    });
-
-                }
+                Assembl.slider.show(modal);
 
             }
 
