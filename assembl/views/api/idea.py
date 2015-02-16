26d8448cb04e49d9beba45c161ad278d8f853f7b
Marc-Antoine Parent <maparent@acm.org>
2015-02-15 22:39:43 -0500

    another case of null values

--- assembl/views/api/idea.py
+++ assembl/views/api/idea.py
@@ -91,6 +91,7 @@ def _get_ideas_real(discussion, view_def=None, ids=None, user_id=None):
     permissions = get_permissions(user_id, discussion.id)
     retval = [idea.generic_json(view_def, user_id, permissions)
               for idea in ideas]
+    retval = [x for x in retval if x is not None]
     widget_data = defaultdict(list)
     for widget in discussion.widgets:
         url = widget.get_ui_endpoint()
