assembl/assembl

commit 431504b93de7decbce102de4d6e8d593ef8fad81

fix bug readmore does not work when you click on.

assembl/static/js/app/views/message.js
@@ -700,11 +700,12 @@ define(['backbone.marionette','backbone', 'underscore', 'ckeditor', 'app', 'comm
             /**
              * @event
              */
-            onMessageTitleClick: function () {
-                this.toggleViewStyle();
+            onMessageTitleClick: function (e) {
+                e.stopPropagation();
                 if (this.viewStyle == this.availableMessageViewStyles.FULL_BODY) {
                     this.openReplyBox();
                 }
+                this.toggleViewStyle();
             },
 
             /**

commit 555404253633e843f83f9a4729325363533a5662

Fix message reply button (broke in 431504b93)

assembl/static/js/app/views/message.js
@@ -698,10 +698,13 @@ define(['backbone.marionette','backbone', 'underscore', 'ckeditor', 'app', 'comm
                 }
             },
             /**
+             * Note that this is also callable without an event
              * @event
              */
             onMessageTitleClick: function (e) {
-                e.stopPropagation();
+                if(e) {
+                  e.stopPropagation();
+                }
                 if (this.viewStyle == this.availableMessageViewStyles.FULL_BODY) {
                     this.openReplyBox();
                 }