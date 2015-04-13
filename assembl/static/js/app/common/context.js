56469e8f26ed5a987e3e2cf29eba4bb6c58fb577
hellsingblack <hellsingblack@gmail.com>
2015-04-13 18:05:27 +0200

    new context function

--- assembl/static/js/app/common/context.js
+++ assembl/static/js/app/common/context.js
@@ -1433,6 +1433,42 @@ define(['../app', 'jquery', 'underscore', '../utils/permissions', '../utils/role
                 } catch (e) {
                     throw new Error("Invalid json. " + e.message);
                 }
+            },
+            /**
+             * Executor of lazy code
+             * */
+            queue: {
+                _timer: null,
+                _queue: [],
+                add: function(fn, context, time) {
+                    var setTimer = function(time) {
+                        $.queue._timer = setTimeout(function() {
+                            time = $.queue.add();
+                            if ($.queue._queue.length) {
+                                setTimer(time);
+                            }
+                        }, time || 2);
+                    }
+
+                    if (fn) {
+                        $.queue._queue.push([fn, context, time]);
+                        if ($.queue._queue.length == 1) {
+                            setTimer(time);
+                        }
+                        return;
+                    }
+
+                    var next = $.queue._queue.shift();
+                    if (!next) {
+                        return 0;
+                    }
+                    next[0].call(next[1] || window);
+                    return next[2];
+                },
+                clear: function() {
+                    clearTimeout($.queue._timer);
+                    $.queue._queue = [];
+                }
             }
 
         }
