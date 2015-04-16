ae0da09c80d548ebbef34ef05dcf0bd5174006f8
Marc-Antoine Parent <maparent@acm.org>
2015-04-16 07:58:55 -0400

    allow fetch_posts on imap; allow reimport to all but disc-admin

--- assembl/views/api2/content_source.py
+++ assembl/views/api2/content_source.py
@@ -1,10 +1,10 @@
 
 from pyramid.view import view_config
 from pyramid.security import authenticated_userid
-from pyramid.httpexceptions import HTTPNotImplemented
+from pyramid.httpexceptions import HTTPUnauthorized
 
-from assembl.auth import (P_READ, P_SYSADMIN)
-from assembl.models import (ContentSource, FeedPostSource)
+from assembl.auth import (P_READ, P_ADMIN_DISC)
+from assembl.models import ContentSource
 from assembl.auth.util import get_permissions
 from ..traversal import InstanceContext
 from . import FORM_HEADER
@@ -14,19 +14,23 @@ from assembl.tasks.source_reader import wake
              ctx_instance_class=ContentSource, permission=P_READ,
              header=FORM_HEADER, renderer='json', name="fetch_posts")
 def fetch_posts(request):
-    # Temporary: Restrict to FeedPostSource
-    # until the IMAP sources are tested.
     ctx = request.context
     csource = ctx._instance
-    if not isinstance(csource, FeedPostSource):
-        # But let sysadmins still experiment with this...
+    force_restart = request.params.get('force_restart', False)
+    reimport = request.params.get('reimport', False)
+    if force_restart or reimport:
+        # Only discussion admins
         user_id = authenticated_userid(request)
         permissions = get_permissions(
             user_id, ctx.get_discussion_id())
-        if P_SYSADMIN not in permissions:
-            raise HTTPNotImplemented()
-    force_restart = request.params.get('force_restart', False)
-    reimport = request.params.get('reimport', False)
+        if P_ADMIN_DISC not in permissions:
+            requested = []
+            if reimport:
+                requested.append('reimport')
+            if force_restart:
+                requested.append('force restart')
+            raise HTTPUnauthorized("Only discussion administrator can "+'and'.join(requested))
+
     wake(csource.id, reimport, force_restart)
     return {"message":"Source notified",
             "name": csource.name}
