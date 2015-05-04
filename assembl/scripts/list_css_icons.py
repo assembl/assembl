15aaf86b2df22490b4c81baeee53ea005aa5c611
Swergas <swergas@yahoo.fr>
2015-05-04 11:42:11 +0200

    styleguide: add missing icons and a script to generate the list

---
+++ assembl/scripts/list_css_icons.py
@@ -0,0 +1,12 @@
+# to run: python3 assembl/scripts/list_css_icons.py 
+
+import json
+
+def main():
+	with open('assembl/static/css/fonts/config.json') as data_file:    
+		data = json.load(data_file)
+		l = [i["css"] for i in data["glyphs"]]
+		print(l)
+
+if __name__ == '__main__':
+	main()
