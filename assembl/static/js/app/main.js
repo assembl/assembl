eaea39bacda559ef3be214b0ea843017bad0b195
hellsingblack <hellsingblack@gmail.com>
2015-05-13 10:19:33 +0200

    add logger bluebird first step

--- assembl/static/js/app/main.js
+++ assembl/static/js/app/main.js
@@ -36,4 +36,23 @@ require(["app", "common/context", "jquery", "router", "utils/socket", "dropdown"
         window.AssemblDebug = require("debug");
         AssemblDebug.disable();
 
+        window.addEventListener("unhandledrejection", function(e) {
+            // NOTE: e.preventDefault() must be manually called to prevent the default
+            // action which is currently to log the stack trace to console.warn
+            e.preventDefault();
+            // NOTE: parameters are properties of the event detail property
+            var reason = e.detail.reason;
+            var promise = e.detail.promise;
+            // See Promise.onPossiblyUnhandledRejection for parameter documentation
+        });
+
+        window.addEventListener("rejectionhandled", function(e) {
+            // NOTE: e.preventDefault() must be manually called prevent the default
+            // action which is currently unset (but might be set to something in the future)
+            e.preventDefault();
+            // NOTE: parameters are properties of the event detail property
+            var promise = e.detail.promise;
+            // See Promise.onUnhandledRejectionHandled for parameter documentation
+        });
+
     });
