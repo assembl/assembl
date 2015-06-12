20b519de379e1c786cac8db2c7d298c1cf852e21
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2015-06-12 18:00:18 -0400

    Add IdentityProviderAccount fields to emailAccounts

--- assembl/static/js/app/models/emailAccounts.js
+++ assembl/static/js/app/models/emailAccounts.js
@@ -6,10 +6,18 @@ var Base = require('./base.js'),
 var emailAccount = Base.Model.extend({
     urlRoot: Ctx.getApiV2DiscussionUrl("/all_users/current/accounts"),
     defaults: {
+      //E-mail account specifics
       will_merge_if_validated: false,
       verified: false,
       profile: 0,
       preferred: false,
+      //IdentityProviderAccount specifics
+      provider: null,
+      username: null,
+      domain: null,
+      userid: null,
+      picture_url: null,
+      //Standards
       '@type': null,
       'email': null,
       '@id': null
