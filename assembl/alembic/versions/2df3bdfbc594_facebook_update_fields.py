ce4bbd3bc7a332cc4aa9976d51a9891bf07a3e46
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2015-06-19 14:17:16 -0400

    Rebase fix migration files

--- assembl/alembic/versions/2df3bdfbc594_facebook_update_fields.py
+++ assembl/alembic/versions/2df3bdfbc594_facebook_update_fields.py
@@ -1,14 +1,14 @@
 """facebook_update_fields
 
 Revision ID: 2df3bdfbc594
-Revises: 368a596ab4b5
+Revises: 163294913218
 Create Date: 2015-03-27 19:07:06.046621
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '2df3bdfbc594'
-down_revision = '368a596ab4b5'
+down_revision = '163294913218'
 
 from alembic import context, op
 import sqlalchemy as sa
