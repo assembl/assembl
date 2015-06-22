assembl/assembl

commit ba8847ec65f9a9d41054059ceb04528d727afb48

Uniqueness constraint on actions. Also some fixes to PUT actions

assembl/models/__init__.py
@@ -1,7 +1,6 @@
 from abc import abstractmethod, ABCMeta
 
-from sqlalchemy import (
-    Column, DateTime, Integer, UniqueConstraint, event, Table, ForeignKey)
+from sqlalchemy import and_
 from sqlalchemy.ext.declarative import DeclarativeMeta
 
 from ..lib.abc import abstractclassmethod
@@ -22,7 +21,7 @@ class DiscussionBoundBase(Base):
     @abstractmethod
     def get_discussion_id(self):
         "Get the ID of an associated discussion object, if any."
-        return self.discussion_id
+        return self.discussion_id or self.discussion.id
 
     def send_to_changes(self, connection=None, operation=UPDATE_OP):
         if not connection:
@@ -40,11 +39,9 @@ class DiscussionBoundBase(Base):
 
     def unique_query(self):
         query, usable = super(DiscussionBoundBase, self).unique_query()
-        discussion_id = self.discussion_id
-        if not discussion_id and self.discussion:
-            discussion_id = self.discussion.id
+        discussion_id = self.get_discussion_id()
         if discussion_id:
-            query = query.filter_by(discussion_id=discussion_id)
+            query.filter(and_(*self.get_discussion_conditions(discussion_id)))
         return (query, usable)
 
     def tombstone(self):

assembl/lib/sqla.py
@@ -434,6 +434,9 @@ class BaseOps(object):
         for cls in cls.mro():
             if cls.__name__ == 'Base':
                 return None
+            if not issubclass(cls, BaseOps):
+                # mixin
+                continue
             my_typename = cls.external_typename()
             local_view = view_def.get(my_typename, None)
             if local_view is False:

assembl/models/action.py
@@ -25,7 +25,7 @@ from .generic import Content
 from .discussion import Discussion
 
 
-class Action(DiscussionBoundBase, TombstonableMixin):
+class Action(TombstonableMixin, DiscussionBoundBase):
     """
     An action that can be taken by an actor.
     """
@@ -121,7 +121,19 @@ class ActionOnPost(Action):
         info={'rdf': QuadMapPatternS(None, ASSEMBL.in_conversation)})
 
 
-class ViewPost(ActionOnPost):
+class UniqueActionOnPost(ActionOnPost):
+    "An action that should be unique of its subclass for a post, user pair"
+    def unique_query(self):
+        # inheritance leads in trouble
+        query = self.db.query(self.__class__)
+        actor_id = self.actor_id or self.actor.id
+        post_id = self.post_id or self.post.id
+        return query.filter_by(
+            actor_id=actor_id, type=self.type, post_id=post_id,
+            tombstone_date=self.tombstone_date), True
+
+
+class ViewPost(UniqueActionOnPost):
     """
     A view action on a post.
     """
@@ -143,7 +155,7 @@ class ViewPost(ActionOnPost):
     verb = 'viewed'
 
 
-class LikedPost(ActionOnPost):
+class LikedPost(UniqueActionOnPost):
     """
     A like action on a post.
     """
@@ -176,7 +188,7 @@ Content.like_count = column_property(
         ).correlate_except(_actt, _lpt))
 
 
-class ExpandPost(ActionOnPost):
+class ExpandPost(UniqueActionOnPost):
     """
     An expansion action on a post.
     """
@@ -187,7 +199,7 @@ class ExpandPost(ActionOnPost):
     verb = 'expanded'
 
 
-class CollapsePost(ActionOnPost):
+class CollapsePost(UniqueActionOnPost):
     """
     A collapse action on a post.
     """

commit a3642008cfe9fbebcb4cfeb64f080c8311d47d88

bug introduced in ba8847ec65f9a9d41054059ceb04528d727afb48

assembl/models/__init__.py
@@ -41,7 +41,7 @@ class DiscussionBoundBase(Base):
         query, usable = super(DiscussionBoundBase, self).unique_query()
         discussion_id = self.get_discussion_id()
         if discussion_id:
-            query.filter(and_(*self.get_discussion_conditions(discussion_id)))
+            query = query.filter(and_(*self.get_discussion_conditions(discussion_id)))
         return (query, usable)
 
     def tombstone(self):