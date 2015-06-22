3bde48129039a4830abc005ccbd365a0da1c616a
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2015-06-22 19:27:03 -0400

    update front-end access token model

--- assembl/static/js/app/models/social.js
+++ assembl/static/js/app/models/social.js
@@ -4,26 +4,54 @@ var $ = require('../shims/jquery.js'),
     Base = require('./base.js'),
     Ctx = require('../common/context.js'),
     i18n = require('../utils/i18n.js'),
-    Agents = require('./agents.js');
+    Agents = require('./agents.js'),
+    Moment = require('moment');
 
 var FacebookAccessToken = Base.Model.extend({
-    urlRoot: Ctx.getApiV2Url('/FacebookAccessTokens'),
-
     defaults: {
-        user_id: null,
+        fb_account_id: null,
         token: null,
         expiration: null,
         token_type: null,
         object_name: null,
         '@view': null,
         '@type': null
-    }    
+    },
+
+    isExpired: function(){
+        var d = new Moment(this.expiration);
+        var now = new Moment.utc();
+        return now.isAfter(d);
+    },
+
+    isUserToken: function(){
+        return this.token_type === 'user';
+    },
+
+    isPageToken: function(){
+        return this.token_type === 'page';
+    }, 
+
+    isGroupToken: function(){
+        return this.token_type === 'group';
+    }
 });
 
 var FacebookAccessTokens = Base.Collection.extend({
     //Things to add: Promise function to get the agent model
     //represented by this model.
-    model: FacebookAccessToken
+    model: FacebookAccessToken,
+    url: function(){
+        var fbId = Ctx.getCurrentUserFacebookAccountId();
+        if (!fbId) {
+            throw new Error("There is no Facebook Account for this user");
+        }
+        else {
+            var route = Ctx.getApiV2DiscussionUrl('/all_users/current/accounts/') +
+                Ctx.extractId(fbId) + "/access_tokens"; 
+            return route;
+        }
+    }
 });
 
 module.exports = {
