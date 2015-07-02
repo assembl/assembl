ee337a1468c5ce461d27b795921daeee3bd1ddbe
hellsingblack <hellsingblack@gmail.com>
2015-07-02 18:59:17 +0200

    not logger show newest first

--- assembl/static/js/app/views/messageList.js
+++ assembl/static/js/app/views/messageList.js
@@ -1742,17 +1742,6 @@ var MessageList = AssemblPanel.extend({
           viewStyle = this.ViewStyles.NEW_MESSAGES;
       }
 
-      if (Ctx.getCurrentInterfaceType() === Ctx.InterfaceTypes.SIMPLE) {
-          if (Ctx.getCurrentUser().isUnknownUser()) {
-              //Only threaded view makes sense for annonymous users
-              viewStyle = this.ViewStyles.THREADED;
-          }
-          else if ((viewStyle !== this.ViewStyles.NEW_MESSAGES) && (viewStyle !== this.ViewStyles.REVERSE_CHRONOLOGICAL)) {//THREADED used to be allowed
-              //New messages is default view
-              viewStyle = this.ViewStyles.NEW_MESSAGES;
-          }
-      }
-
       if (viewStyle === this.ViewStyles.THREADED) {
           this.currentViewStyle = viewStyle;
           this.currentQuery.setView(this.currentQuery.availableViews.THREADED);
@@ -1772,6 +1761,19 @@ var MessageList = AssemblPanel.extend({
       else {
           throw new Error("Unsupported view style");
       }
+
+        // Do we need still need this code ?
+      if (Ctx.getCurrentInterfaceType() === Ctx.InterfaceTypes.SIMPLE) {
+        if (Ctx.getCurrentUser().isUnknownUser()) {
+            //Only threaded view makes sense for annonymous users
+            viewStyle = this.ViewStyles.REVERSE_CHRONOLOGICAL;
+        }
+        else if ((viewStyle !== this.ViewStyles.NEW_MESSAGES) && (viewStyle !== this.ViewStyles.REVERSE_CHRONOLOGICAL)) {//THREADED used to be allowed
+            //New messages is default view
+            viewStyle = this.ViewStyles.NEW_MESSAGES;
+        }
+      }
+
       if (this.storedMessageListConfig.viewStyleId != viewStyle.id) {
           this.storedMessageListConfig.viewStyleId = viewStyle.id;
           Ctx.DEPRECATEDsetMessageListConfigToStorage(this.storedMessageListConfig);
