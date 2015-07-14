1b59c8436ebab95046476688150b43275ef9008a
Marc-Antoine Parent <maparent@acm.org>
2015-07-14 11:18:30 -0400

    move the RDF migration to after alembic (& before phsell) rather than any initialization https://app.asana.com/0/41361648325456/41090174480923

--- assembl/lib/pshell_session.py
+++ assembl/lib/pshell_session.py
@@ -2,8 +2,12 @@ import sys
 
 from pyramid.paster import get_appsettings
 from sqla import get_session_maker, configure_engine
+from assembl.semantic import upgrade_semantic_mapping as _usm
+from assembl.lib.config import set_config as _set_config
 
 # an implicit session for pshell
 app_settings = get_appsettings(sys.argv[1], 'assembl')
+_set_config(app_settings)
 configure_engine(app_settings, False)
 db = get_session_maker()
+_usm()
