1f1cb547402825a67d4903e0b6f90e6c56c55313
Marc-Antoine Parent <maparent@acm.org>
2015-07-30 22:08:00 -0400

    infobar translations

---
+++ assembl/static/js/app/dummy_translations.js
@@ -0,0 +1,12 @@
+// This file will not be imported, but it will ensure that some
+// language strings from python find their way in the jed file.
+// Keep synchronized by hand until we can do better.
+
+var translations = [
+// From assembl/models/widgets.py
+gettext("A vote session is ongoing"),
+gettext("; You have until %(end_date_moment)s to vote"),
+gettext("Vote"),
+gettext("A creativity session is ongoing; You have until %(end_date_moment)s to join"),
+gettext("Join the session")
+];
