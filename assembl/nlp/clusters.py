9ad85473c4b7e1cc395e3bbdf21b2615a80459e3
Marc-Antoine Parent <maparent@acm.org>
2015-08-25 09:59:04 -0400

    user LSI instead of LDA, allows to weigh topics

--- assembl/nlp/clusters.py
+++ assembl/nlp/clusters.py
@@ -64,18 +64,18 @@ def create_dictionaries():
             stemmer.save()
 
 
-def gensimvecs_to_csr(vecs, width):
+def gensimvecs_to_csr(vecs, width, topic_intensities):
     model_matrix = lil_matrix(
         (len(vecs), width), dtype=numpy.float64)
     for nrow, row in enumerate(vecs):
         for ncol, val in row:
-            model_matrix[nrow, ncol] = val
+            model_matrix[nrow, ncol] = val * topic_intensities[ncol]
     return model_matrix.tocsr()
 
 
 def get_discussion_semantic_analysis(
-        discussion_id, num_topics=200,
-        model_cls=gmodels.ldamodel.LdaModel, **model_kwargs):
+        discussion_id, num_topics=100,
+        model_cls=gmodels.lsimodel.LsiModel, **model_kwargs):
     discussion = Discussion.get(discussion_id)
     lang = discussion.discussion_locales[0].split('_')[0]
     dirname = join(nlp_data, lang)
@@ -129,6 +129,8 @@ def identity(x):
 
 
 def parse_topic(topic, trans=identity):
+    if not topic:
+        return {}
     words = topic.split(' + ')
     words = (word.split('*') for word in words)
     return dict(((' '.join((
@@ -148,12 +150,12 @@ def post_ids_of(idea):
 
 
 def get_cluster_info(
-        idea_id, num_topics=200, passes=5,
+        idea_id, num_topics=100, passes=5,
         algorithm="DBSCAN", **algo_kwargs):
     idea = Idea.get(idea_id)
     tfidf_model, gensim_model = get_discussion_semantic_analysis(
-        idea.discussion_id, num_topics=num_topics, passes=passes)
-    # , model_cls=gmodels.lsimodel.LsiModel
+        idea.discussion_id, num_topics=num_topics,  # passes=passes)
+        model_cls=gmodels.lsimodel.LsiModel)
     if not tfidf_model or not gensim_model:
         return
     lang = idea.discussion.discussion_locales[0].split('_')[0]
@@ -174,7 +176,11 @@ def get_cluster_info(
     index_by_post_id = {post_id: n for (n, post_id) in enumerate(post_ids)}
     subcorpus = corpus.subcorpus(post_ids)
     tfidf_corpus = tfidf_model[subcorpus]
-    model_matrix = gensimvecs_to_csr(gensim_model[tfidf_corpus], num_topics)
+    if isinstance(gensim_model, gmodels.lsimodel.LsiModel):
+        topic_intensities = gensim_model.projection.s / gensim_model.projection.s[0]
+    else:
+        topic_intensities = numpy.ones((num_topics,))
+    model_matrix = gensimvecs_to_csr(gensim_model[tfidf_corpus], num_topics, topic_intensities)
     algorithm = getattr(sklearn.cluster, algorithm)
     algorithm = algorithm(**algo_kwargs)
     r = algorithm.fit(model_matrix)
@@ -202,7 +208,7 @@ def get_cluster_info(
 
         def centroid(corpus):
             clust_lda = [gensim_model[tfidf_model[c]] for c in corpus]
-            clust_lda = gensimvecs_to_csr(clust_lda, num_topics)
+            clust_lda = gensimvecs_to_csr(clust_lda, num_topics, topic_intensities)
             return clust_lda.sum(0).A1 / clust_lda.shape[0]
 
         difference_vals = centroid(cluster_corpus) - centroid(clusterneg_corpus)
@@ -261,7 +267,8 @@ def get_cluster_info(
     else:
         for post_id in children_remainder:
             ideas_of_post[post_id] = [idea_id]
-        all_idea_scores.append({idea_id:len(post_ids)})
+        for cluster in chain(post_clusters, (remainder,)):
+            all_idea_scores.append({idea_id:len(cluster)})
     post_text = dict(Content.default_db.query(Content.id, Content.body).all())
     post_info = {
         post_id:
@@ -276,7 +283,7 @@ def get_cluster_info(
              idea_scores=all_idea_scores[n])
         for (n, cluster) in enumerate(post_clusters)
     ]
-    clusters.append(dict(remainder=remainder, idea_scores=all_idea_scores[-1]))
+    clusters.append(dict(cluster=remainder, idea_scores=all_idea_scores[-1]))
     return (silhouette_score, compare_with_ideas, clusters, post_info)
 
 
@@ -290,41 +297,54 @@ def show_clusters(clusters):
             print posts[post_id].get_body_as_text()
 
 
-def show_all(db, discussion_id, eps=0.2, min_samples=4):
+def get_all_results(db, discussion_id, eps=0.2, min_samples=4):
     idea_ids = db.query(Idea.id).filter_by(
         discussion_id=discussion_id).all()
     results = {id: get_cluster_info(id, eps=eps, min_samples=min_samples)
                for (id,) in idea_ids}
     posres = {id: r for (id, r) in results.iteritems() if r is not None}
-    for id, (silhouette_score, compare_with_ideas, clusters, post_info) in posres.iteritems():
-        print id, silhouette_score, [len(x.get('cluster', ())) or len(x.get('remainder', ())) for x in clusters]
+    # for id, (silhouette_score, compare_with_ideas, clusters, post_info) in posres.iteritems():
+    #     print id, silhouette_score, [len(x['cluster']) for x in clusters]
     return posres
 
 def as_html(db, discussion_id, f=None, eps=0.2, min_samples=4):
     if not f:
         f = open('output.html', 'w')
-    results = show_all(db, discussion_id, eps=eps, min_samples=min_samples)
+    results = get_all_results(db, discussion_id, eps=eps, min_samples=min_samples)
+    results = [(silhouette_score, idea_id, compare_with_ideas, clusters, post_info)
+        for idea_id, (silhouette_score, compare_with_ideas, clusters, post_info) in results.iteritems()]
+    results.sort(reverse=True)
     with f:
-        for idea_id, (silhouette_score, compare_with_ideas, clusters, post_info) in results.iteritems():
+        for (silhouette_score, idea_id, compare_with_ideas, clusters, post_info) in results:
             idea = Idea.get(idea_id)
             f.write("<h1>Idea %d: [%f] %s</h1>\n" % (
                 idea_id, silhouette_score or 0,
                 (idea.short_title or '').encode('utf-8')))
+            if len(clusters) > 1:
+                f.write("<p><b>Cluster size: %s</b>, remainder %d</p>\n" % (
+                    ', '.join((str(len(ci['cluster'])) for ci in clusters[:-1])),
+                    len(clusters[-1]['cluster'])))
             if (compare_with_ideas):
                 f.write("<dl>\n")
                 for k, v in compare_with_ideas.iteritems():
                     f.write("<dt>%s</dt><dd>%s</dd>\n" % (k, v))
                 f.write("</dl>\n")
-            children_ids = set(chain(*(cli['idea_scores'].keys() for cli in clusters)))
+            children_ids = set(chain(*(
+                cli['idea_scores'].keys() for cli in clusters)))
             post_counts_per_idea = {
-                child_id: len([post_id for (post_id, pinfo) in post_info.iteritems() if child_id in pinfo['ideas']])
+                child_id: len([post_id for (post_id, pinfo)
+                               in post_info.iteritems()
+                               if child_id in pinfo['ideas']])
                 for child_id in children_ids}
             for n, cluster_info in enumerate(clusters):
-                is_remainder = 'remainder' in cluster_info
-                cluster = cluster_info['remainder'] if is_remainder else cluster_info['cluster']
+                is_remainder = 'features' not in cluster_info
+                cluster = cluster_info['cluster']
                 features = cluster_info.get('features', {})
                 idea_scores = cluster_info['idea_scores']
-                f.write("<h2>Cluster %d</h2>\n<ol>" % (n,))
+                if is_remainder:
+                    f.write("<h2>Remainder:</h2>\n<ol>")
+                else:
+                    f.write("<h2>Cluster %d</h2>\n<ol>" % (n,))
                 for idea_id, score in idea_scores.iteritems():
                     idea = Idea.get(idea_id)
                     f.write("<li>Idea %d: %d/%d %s</li>\n" % (
