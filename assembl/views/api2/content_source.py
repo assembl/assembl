a1f7eb34d9439306a6aacf28cbfe475e12ac15b9
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2015-10-05 10:33:11 -0400

    Pass upper/lower bounds from content_source api's to kombu and therefore, to source code

--- assembl/views/api2/content_source.py
+++ assembl/views/api2/content_source.py
@@ -1,14 +1,15 @@
-
 from pyramid.view import view_config
 from pyramid.security import authenticated_userid
-from pyramid.httpexceptions import HTTPUnauthorized, HTTPBadRequest
+from pyramid.httpexceptions import HTTPUnauthorized, HTTPError
+from dateutil.parser import parse
 
-from assembl.auth import (P_READ, P_ADMIN_DISC, P_EXPORT)
+from assembl.auth import (P_READ, P_ADMIN_DISC, P_EXPORT, Everyone)
 from assembl.models import ContentSource
 from assembl.auth.util import get_permissions
 from ..traversal import InstanceContext
 from . import FORM_HEADER, JSON_HEADER
 from assembl.tasks.source_reader import wake
+
 
 @view_config(context=InstanceContext, request_method='POST',
              ctx_instance_class=ContentSource, permission=P_READ,
@@ -18,13 +19,15 @@ def fetch_posts(request):
     csource = ctx._instance
     force_restart = request.params.get('force_restart', False)
     reimport = request.params.get('reimport', False)
-    limit = request.params.get('limit', None)
-    if limit:
-        try:
-            limit = int(limit)
-        except ValueError:
-            raise HTTPBadRequest("Non-numeric limit value: "+limit)
-    if force_restart or reimport or limit:
+    upper_bound = request.params.get('upper_limit', None)
+    lower_bound = request.params.get('lower_limit', None)
+    try:
+        p1 = parse(upper_bound)
+        p2 = parse(lower_bound)
+    except:
+        raise HTTPError(code=500)
+
+    if force_restart or reimport or upper_bound or lower_bound:
         # Only discussion admins
         user_id = authenticated_userid(request) or Everyone
         permissions = get_permissions(
@@ -35,9 +38,15 @@ def fetch_posts(request):
                 requested.append('reimport')
             if force_restart:
                 requested.append('force restart')
-            raise HTTPUnauthorized("Only discussion administrator can "+'and'.join(requested))
+            if upper_bound:
+                requested.append('upper_limit')
+            if lower_bound:
+                requested.append('lower_limit')
+            raise HTTPUnauthorized("Only discussion administrator\
+                                   can "+'and'.join(requested))
 
-    wake(csource.id, reimport, force_restart, limit=limit)
+    wake(csource.id, reimport, force_restart, upper_bound=upper_bound,
+         lower_bound=lower_bound)  # passing the string instead of datetime
     return {"message": "Source notified",
             "name": csource.name}
 
@@ -61,7 +70,8 @@ def export_post(request):
     fb_post_id = data.get('facebook_post_id', None)
     force_restart = request.params.get('force_restart', False)
     reimport = request.params.get('reimport', False)
-    limit = request.params.get('limit', None)
+    upper_bound = request.params.get('upper_limit', None)
+    lower_bound = request.params.get('lower_bound', None)
 
     if not post_id:  # or not fb_post_id:
         # post_id is necessary to establish relationship between source
@@ -69,6 +79,12 @@ def export_post(request):
         # exist in the source (eg. in case of Facebook)
         return {"error": "Could not create a content \
             sink because of improper json inputs"}
+
+    try:
+        p1 = parse(upper_bound)
+        p2 = parse(lower_bound)
+    except:
+        return {'error': 'The bound limits were not parsable'}
 
     else:
 
@@ -83,7 +99,8 @@ def export_post(request):
             db.rollback()
             return {"error": "Failed on content sink transaction"}
 
-        wake(csource.id, reimport, force_restart, limit=limit)
+        wake(csource.id, reimport, force_restart, upper_bound=upper_bound,
+             lower_bound=lower_bound)
 
         return {"message": "Source notified",
                 "name": csource.name}
