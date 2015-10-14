65401256dc002e86419eb1dcff7b79c89e66f492
Marc-Antoine Parent <maparent@acm.org>
2015-10-14 11:16:42 -0400

    de-duplicate existing documents. https://app.asana.com/0/51461630427071/52921943509398

---
+++ assembl/alembic/versions/46d8492ca1c4_deduplicate_documents.py
@@ -0,0 +1,46 @@
+"""Deduplicate documents
+
+Revision ID: 46d8492ca1c4
+Revises: 4bfc1da4d2e4
+Create Date: 2015-10-14 10:00:26.526750
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '46d8492ca1c4'
+down_revision = '4bfc1da4d2e4'
+
+from alembic import context, op
+import sqlalchemy as sa
+import transaction
+
+
+from assembl.lib import config
+
+
+def upgrade(pyramid_env):
+    with context.begin_transaction():
+        pass
+
+    # Do stuff with the app's models here.
+    from assembl import models as m
+    db = m.get_session_maker()()
+    with transaction.manager:
+        to_delete = set()
+        last_id = last_uri = None
+        for (id, uri) in db.execute(
+                'select id, uri_id from document order by uri_id'):
+            if uri == last_uri:
+                db.execute("""update attachment set document_id = %d
+                    where document_id = %d""" % (last_id, id))
+                to_delete.add(str(id))
+            else:
+                last_id = id
+                last_uri = uri
+        db.execute('delete from document where id in (' +
+            ','.join(to_delete) + ')')
+
+
+def downgrade(pyramid_env):
+    with context.begin_transaction():
+        pass
