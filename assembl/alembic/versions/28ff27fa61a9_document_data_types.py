67c9da8ce2d6a0ec268fba0ee356f722d8f052da
Marc-Antoine Parent <maparent@acm.org>
2015-10-16 06:53:02 -0400

    Rewrite migration to do in-place column modification, bypassing index nonsense.

--- assembl/alembic/versions/28ff27fa61a9_document_data_types.py
+++ assembl/alembic/versions/28ff27fa61a9_document_data_types.py
@@ -18,147 +18,88 @@ import transaction
 from assembl.lib import config
 from assembl.lib.sqla import mark_changed
 
-fields = ('id', 'uri_id', 'creation_date', 'discussion_id', 'oembed_type',
-          'mime_type', 'title', 'description', 'author_name', 'author_url',
-          'thumbnail_url', 'site_name')
 
-ascii_fields = {
-    'uri_id', 'oembed_type', 'mime_type', 'author_url', 'thumbnail_url'}
+old_types = {
+    "uri_id": sa.Unicode,
+    "oembed_type": sa.Unicode,
+    "mime_type": sa.Unicode,
+    "author_name": sa.UnicodeText,
+    "author_url": sa.UnicodeText,
+    "thumbnail_url": sa.UnicodeText,
+    "site_name": sa.UnicodeText
+}
 
-field_names = ', '.join(fields)
+new_types = {
+    "uri_id": sa.String,
+    "oembed_type": sa.String,
+    "mime_type": sa.String,
+    "author_name": sa.Unicode,
+    "author_url": sa.String,
+    "thumbnail_url": sa.String,
+    "site_name": sa.Unicode
+}
 
-cast_fields = ', '.join([
-    'cast(%s as varchar)' % (f) if f in ascii_fields else f for f in fields])
+
+with_default = {"uri_id", "oembed_type", "mime_type"}
+
 
 def upgrade(pyramid_env):
     with context.begin_transaction():
-        op.create_table(
-            'document_temp',
-            sa.Column('id', sa.Integer, primary_key=True),
-            sa.Column('uri_id', sa.String(1024), server_default="",
-                      unique=True, index=True),
-            sa.Column('creation_date',
-                      sa.DateTime,
-                      nullable=False),
-            sa.Column('discussion_id',
-                      sa.Integer,
-                      sa.ForeignKey(
-                          'discussion.id',
-                          ondelete="CASCADE",
-                          onupdate="CASCADE"),
-                      nullable=False, index=False),
-            sa.Column('oembed_type', sa.String(1024), server_default=""),
-            sa.Column('mime_type', sa.String(1024), server_default=""),
-            sa.Column('title', sa.Unicode(1024), server_default=""),
-            sa.Column('description', sa.UnicodeText),
-            sa.Column('author_name', sa.Unicode),
-            sa.Column('author_url', sa.String),
-            sa.Column('thumbnail_url', sa.String),
-            sa.Column('site_name', sa.Unicode),
-            )
-        op.execute("insert into document_temp (%s) select %s from document" % (
-            field_names, cast_fields))
+        for f, stype in new_types.iteritems():
+            op.add_column(
+                'document', sa.Column(f+'_temp', stype))
+
+        op.execute("UPDATE document SET " + ", ".join((
+            ("{f}_temp = cast({f} as varchar)"
+                if stype == sa.String else
+                "{f}_temp = {f}").format(f=f)
+            for (f, stype) in new_types.iteritems())))
         mark_changed()
     with context.begin_transaction():
-        op.drop_constraint("attachment_document_document_id_id", "attachment")
-        op.drop_table('document')
-        op.create_table(
-            'document',
-            sa.Column('id', sa.Integer, primary_key=True),
-            sa.Column('uri_id', sa.String(1024), server_default="",
-                      unique=True, index=True),
-            sa.Column('creation_date',
-                      sa.DateTime,
-                      nullable=False),
-            sa.Column('discussion_id',
-                      sa.Integer,
-                      sa.ForeignKey(
-                          'discussion.id',
-                          ondelete="CASCADE",
-                          onupdate="CASCADE"),
-                      nullable=False, index=False),
-            sa.Column('oembed_type', sa.String(1024), server_default=""),
-            sa.Column('mime_type', sa.String(1024), server_default=""),
-            sa.Column('title', sa.Unicode(1024), server_default=""),
-            sa.Column('description', sa.UnicodeText),
-            sa.Column('author_name', sa.Unicode),
-            sa.Column('author_url', sa.String),
-            sa.Column('thumbnail_url', sa.String),
-            sa.Column('site_name', sa.Unicode),
-            )
-        op.execute("insert into document (%s) select %s from document_temp" % (
-            field_names, field_names))
+        op.drop_index('ix_document_uri_id', 'document')
+        for f in new_types:
+            op.drop_column('document', f)
+        for f, stype in new_types.iteritems():
+            op.add_column('document', sa.Column(
+                f, stype,
+                server_default="" if f in with_default else None))
+        op.execute("UPDATE document SET " + ",".join((
+            "{f} = {f}_temp".format(f=f)
+            for f in new_types)))
         mark_changed()
+
     with context.begin_transaction():
-        op.execute("""
-            ALTER TABLE {schema}.{user}.attachment
-            ADD CONSTRAINT attachment_document_document_id_id FOREIGN KEY (document_id)
-            REFERENCES {schema}.{user}.document (id) ON UPDATE CASCADE ON DELETE CASCADE""".format(
-                schema=config.get('db_schema'), user=config.get('db_user')))
-        op.drop_table("document_temp")
+        op.execute(
+            "CREATE UNIQUE INDEX ix_document_uri_id ON %s.%s.document (uri_id)" % (
+                config.get('db_schema'), config.get('db_user')))
+        for f in new_types:
+            op.drop_column('document', f+"_temp")
 
 
 def downgrade(pyramid_env):
     with context.begin_transaction():
-        op.create_table(
-            'document_temp',
-            sa.Column('id', sa.Integer, primary_key=True),
-            sa.Column('uri_id', sa.Unicode(1024), server_default="", unique=False, index=True),
-            sa.Column('creation_date',
-                      sa.DateTime,
-                      nullable=False),
-            sa.Column('discussion_id',
-                       sa.Integer,
-                       sa.ForeignKey(
-                            'discussion.id',
-                             ondelete="CASCADE",
-                             onupdate="CASCADE"),
-                nullable=False,
-                index=False,),
-            sa.Column('oembed_type', sa.Unicode(1024), server_default=""),
-            sa.Column('mime_type', sa.Unicode(1024), server_default=""),
-            sa.Column('title', sa.Unicode(1024), server_default=""),
-            sa.Column('description', sa.UnicodeText),
-            sa.Column('author_name', sa.UnicodeText),
-            sa.Column('author_url', sa.UnicodeText),
-            sa.Column('thumbnail_url', sa.UnicodeText),
-            sa.Column('site_name', sa.UnicodeText))
-        op.execute("insert into document_temp ({fnames}) select {fnames} from document".format(fnames=field_names))
+        for f, stype in old_types.iteritems():
+            op.add_column(
+                'document', sa.Column(f+'_temp', stype))
+        op.execute("UPDATE document SET " + ",".join((
+            "{f}_temp = {f}".format(f=f)
+            for (f, stype) in old_types.iteritems())))
         mark_changed()
     with context.begin_transaction():
-        op.drop_constraint("attachment_document_document_id_id", "attachment")
-        op.drop_table('document')
-        op.create_table(
-            'document',
-            sa.Column('id', sa.Integer, primary_key=True),
-            sa.Column('uri_id', sa.Unicode(1024), server_default="", unique=False, index=True),
-            sa.Column('creation_date',
-                      sa.DateTime,
-                      nullable=False),
-            sa.Column('discussion_id',
-                       sa.Integer,
-                       sa.ForeignKey(
-                            'discussion.id',
-                             ondelete="CASCADE",
-                             onupdate="CASCADE"),
-                nullable=False,
-                index=False,),
-            sa.Column('oembed_type', sa.Unicode(1024), server_default=""),
-            sa.Column('mime_type', sa.Unicode(1024), server_default=""),
-            sa.Column('title', sa.Unicode(1024), server_default=""),
-            sa.Column('description', sa.UnicodeText),
-            sa.Column('author_name', sa.UnicodeText),
-            sa.Column('author_url', sa.UnicodeText),
-            sa.Column('thumbnail_url', sa.UnicodeText),
-            sa.Column('site_name', sa.UnicodeText))
-
-        op.execute("insert into document ({fnames}) select {fnames} from document_temp".format(fnames=field_names))
+        for f in old_types:
+            op.drop_column('document', f)
+        for f, stype in old_types.iteritems():
+            op.add_column('document', sa.Column(
+                f, stype,
+                server_default="" if f in with_default else None))
+        op.execute("UPDATE document SET " + ",".join((
+            "{f} = {f}_temp".format(f=f)
+            for f in old_types)))
         mark_changed()
-    with context.begin_transaction():
-        op.execute("""
-            ALTER TABLE {schema}.{user}.attachment
-            ADD CONSTRAINT attachment_document_document_id_id FOREIGN KEY (document_id)
-            REFERENCES {schema}.{user}.document (id) ON UPDATE CASCADE ON DELETE CASCADE""".format(
-              schema=config.get('db_schema'), user=config.get('db_user')))
 
-        op.drop_table("document_temp")
+    with context.begin_transaction():
+        op.execute(
+            "CREATE UNIQUE INDEX ix_document_uri_id ON %s.%s.document (uri_id)" % (
+                config.get('db_schema'), config.get('db_user')))
+        for f in old_types:
+            op.drop_column('document', f+"_temp")
