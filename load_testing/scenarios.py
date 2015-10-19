2d771255603f19cee1c02cfb60537aeac13fb50c
Marc-Antoine Parent <maparent@acm.org>
2015-10-19 15:44:32 -0400

    make more abstract

--- load_testing/scenarios.py
+++ load_testing/scenarios.py
@@ -1,20 +1,38 @@
+import re
+import json
+from urllib import quote
 from loads.case import TestCase
 
 no_gzip = {'Accept-Encoding': ''}
 
+
 class TestWebSite(TestCase):
 
     def front_page_and_json_disconnected(self):
-        self.assertEqual(self.app.get('/sandbox').status_code, 200)
-        # self.assertEqual(self.app.get('/data/Discussion/6/all_users/current/local_roles').status_code, 200)
-        # self.assertEqual(self.app.get('/api/v1/token').status_code, 200)
-        # self.assertEqual(self.app.get('/socket/info').status_code, 200)
-        self.assertEqual(self.app.get('/api/v1/discussion/6/ideas').status_code, 200)
-        self.assertEqual(self.app.get('/api/v1/discussion/6/ideas/196?view=contributors').status_code, 200)
-        self.assertEqual(self.app.get('/api/v1/discussion/6/posts?view=id_only').status_code, 200)
-        self.assertEqual(self.app.get('/api/v1/discussion/6/posts?root_idea_id=local%3AIdea%2F196&order=chronological&view=id_only').status_code, 200)
-        self.assertEqual(self.app.get('/api/v1/discussion/6/ideas/196?view=contributors').status_code, 200)
-        self.assertEqual(self.app.get('/api/v1/discussion/6/posts?root_idea_id=local%3AIdea%2F196&order=chronological&view=id_only').status_code, 200)
-        self.assertEqual(self.app.get('/data/Discussion/6/?_=1444820002494').status_code, 200)
-        self.assertEqual(self.app.get('/data/Discussion/6/idea_links?_=1444820002495').status_code, 200)
-        self.assertEqual(self.app.get('/api/v1/discussion/6/posts?ids%5B%5D=local%3AContent%2F641&ids%5B%5D=local%3AContent%2F4783&ids%5B%5D=local%3AContent%2F5091&ids%5B%5D=local%3AContent%2F4782&ids%5B%5D=local%3AContent%2F2090&ids%5B%5D=local%3AContent%2F2089&ids%5B%5D=local%3AContent%2F640&order=reverse_chronological&view=default').status_code, 200)
+        data = {'slug': "sandbox"}
+
+        def test(url):
+            result = self.app.get(url.format(**data))
+            self.assertEqual(result.status_code, 200)
+            return result._app_iter[0]
+
+        front_page = test('/{slug}')
+        match = re.search('id="discussion-id" value="([0-9]+)"', front_page)
+        assert match
+        data['disc_id'] = int(match.group(1))
+
+        # test('/data/Discussion/{disc_id}/all_users/current/local_roles')
+        # test('/api/v1/token')
+        # test('/socket/info')
+        ideas = test('/api/v1/discussion/{disc_id}/ideas')
+        ideas = json.loads(ideas)
+        data['idea'] = int(ideas[1]['@id'].split('/')[1])
+        test('/api/v1/discussion/{disc_id}/ideas/{idea}?view=contributors')
+        posts = test('/api/v1/discussion/{disc_id}/posts?view=id_only')
+        posts = json.loads(posts)['posts']
+        data['posts'] = '&'.join(['ids%5B%5D='+quote(p['@id']) for p in posts])
+        test('/api/v1/discussion/{disc_id}/posts?root_idea_id=local%3AIdea%2F{idea}&order=chronological&view=id_only')
+        test('/api/v1/discussion/{disc_id}/ideas/{idea}?view=contributors')
+        test('/data/Discussion/{disc_id}/')
+        test('/data/Discussion/{disc_id}/idea_links')
+        test('/api/v1/discussion/{disc_id}/posts?{posts}&order=reverse_chronological&view=default')
