bce66e3cb940a125afc1ec50d53a3f941ded3d51
Marc-Antoine Parent <maparent@acm.org>
2015-11-10 11:11:15 -0500

    utility function after a database reload

--- assembl/semantic/__init__.py
+++ assembl/semantic/__init__.py
@@ -12,3 +12,13 @@ def upgrade_semantic_mapping():
     if settings['sqlalchemy.url'].startswith('virtuoso:'):
         aqsm = AssemblQuadStorageManager()
         aqsm.update_all_storages()
+
+
+def reset_semantic_mapping():
+    from .virtuoso_mapping import AssemblQuadStorageManager
+    from assembl.lib.config import get_config
+    settings = get_config()
+    if settings['sqlalchemy.url'].startswith('virtuoso:'):
+        aqsm = AssemblQuadStorageManager()
+        aqsm.drop_all()
+        aqsm.update_all_storages()
