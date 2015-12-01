assembl/assembl

commit 5e78a6b16e5277e8353ed7a70824ca38cd88583c

Modularizing source views

assembl/static/js/app/views/admin/sourceEditViews.js
@@ -1,0 +1,20 @@
+var Marionette = require('../../shims/marionette.js'),
+    i18n = require('../../utils/i18n.js'),
+    CollectionManager = require('../../common/collectionManager.js'),
+    Promise = require('bluebird'),
+    SourceViewBase = require('../sourceEditView.js');
+
+//This needs to become the emailSourceEditView
+
+var EmailSourceEditView = SourceViewBase.extend({
+  template: '#tmpl-emailSource',
+
+  fetchValue: function(){
+    return 
+  }
+
+});
+
+module.exports = {
+  EmailSource: EmailSourceEditView
+};

assembl/static/js/app/models/sources.js
@@ -32,13 +32,26 @@ var Source = Base.Model.extend({
     'is_content_sink': false // Used by API V2 as flag for side-effectful POST creation only.
     // DO NOT use for any other scenario than creating facebook content_sinks
   },
+  fetchUrl: function(){
+    return this.url() + '/fetch_posts';
+  },
   doReimport: function() {
-    var url = this.url() + '/fetch_posts';
-    return $.post(url, {reimport: true});
+    return $.ajax(
+      this.fetchUrl(), {
+        method: 'POST',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: {reimport: true}
+    });
   },
   doReprocess: function() {
-    var url = this.url() + '/fetch_posts';
-    return $.post(url, {reprocess: true});
+    return $.ajax(
+      this.fetchUrl(), {
+        method: 'POST',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: {reprocess: true}
+    });
   }
 });
 

assembl/static/js/app/views/admin/emailSettings.js
@@ -1,139 +1,0 @@
-var Marionette = require('../../shims/marionette.js'),
-    i18n = require('../../utils/i18n.js'),
-    CollectionManager = require('../../common/collectionManager.js'),
-    Promise = require('bluebird');
-
-module.exports = Marionette.ItemView.extend({
-  template: '#tmpl-emailSource',
-  ui: {
-    source:'.js_saveSource',
-    reimport: '.js_reimport',
-    reprocess: '.js_reprocess'
-  },
-  events: {
-    'click @ui.source':'submitForm',
-    'click @ui.reimport':'reimportSource',
-    'click @ui.reprocess':'reprocessSource'
-  },
-  reimportSource: function(e) {
-    e.preventDefault();
-    var msg = 'This will take several minutes! You cannot leave the page in the meantime. Are you sure you want to continue?';
-    console.log('this.model is :', this.model);
-    if (confirm(i18n.gettext(msg))) {
-      return Promise.resolve(this.model.doReimport()).then(function(whatever) {
-        $.bootstrapGrowl(i18n.gettext('Reimport has begun! It can take up to 15 minutes to complete.'), {
-          ele: 'body',
-          type: 'success',
-          offset: {from: 'bottom', amount:20},
-          align: 'left',
-          delay: 4000,
-          allow_dismiss: true,
-          stackup_spacing: 10
-        });
-      }).catch(function(e) {
-        $.bootstrapGrowl(i18n.gettext('Reimport failed'), {
-          ele: 'body',
-          type: 'error',
-          offset: {from: 'bottom', amount:20},
-          align: 'left',
-          delay: 4000,
-          allow_dismiss: true,
-          stackup_spacing: 10
-        });
-      });
-    }
-    else {
-      //do nothing
-    }
-
-    return false;
-  },
-  reprocessSource: function(e) {
-    e.preventDefault();
-    var msg = 'This will take several minutes! You cannot leave the page in the meantime. Are you sure you want to continue?';
-    if (confirm(i18n.gettext(msg))) {
-      return Promise.resolve(this.model.doReprocess()).then(function(whatever) {
-        $.bootstrapGrowl(i18n.gettext('Reprocess has begun! It can take up to 15 minutes to complete.'), {
-          ele: 'body',
-          type: 'success',
-          offset: {from: 'bottom', amount:20},
-          align: 'left',
-          delay: 4000,
-          allow_dismiss: true,
-          stackup_spacing: 10
-        });
-      }).catch(function(e) {
-        $.bootstrapGrowl(i18n.gettext('Reimport failed'), {
-          ele: 'body',
-          type: 'error',
-          offset: {from: 'bottom', amount:20},
-          align: 'left',
-          delay: 4000,
-          allow_dismiss: true,
-          stackup_spacing: 10
-        });
-      });
-    }
-    else {
-      //do absolutely nothing
-    }
-
-    return false;
-  },
-  submitForm: function(e) {
-    e.preventDefault();
-    this.saveModel();
-  },
-  saveModel: function() {
-    var name = this.$('#name').val(),
-        admin_sender = this.$('#admin_sender').val(),
-        post_email_address = this.$('#post_email_address').val(),
-        host = this.$('#host').val(),
-        use_ssl = this.$('#use_ssl:checked').val(),
-        folder = this.$('#folder').val(),
-        port = parseInt(this.$('#port').val()),
-        username = this.$('#username').val(),
-        password = this.$('#password').val();
-
-    this.model.set({
-      name: name,
-      admin_sender: admin_sender,
-      post_email_address: post_email_address,
-      host: host,
-      use_ssl: use_ssl,
-      folder: folder,
-      port: port,
-      username: username,
-      password: password
-    });
-
-    this.model.save(null, {
-      success: function(model, resp) {
-
-        $.bootstrapGrowl(i18n.gettext('Your settings were saved'), {
-          ele: 'body',
-          type: 'success',
-          offset: {from: 'bottom', amount:20},
-          align: 'left',
-          delay: 4000,
-          allow_dismiss: true,
-          stackup_spacing: 10
-        });
-
-      },
-      error: function(model, resp) {
-
-        $.bootstrapGrowl(i18n.gettext('Your settings fail to update'), {
-          ele: 'body',
-          type: 'error',
-          offset: {from: 'bottom', amount:20},
-          align: 'left',
-          delay: 4000,
-          allow_dismiss: true,
-          stackup_spacing: 10
-        });
-
-      }
-    });
-  }
-});

assembl/static/js/app/views/admin/generalSource.js
@@ -1,11 +1,13 @@
 var Marionette = require('../../shims/marionette.js'),
     $ = require('../../shims/jquery.js'),
+    _ = require('../../shims/underscore.js'),
     i18n = require('../../utils/i18n.js'),
     Types = require('../../utils/types.js'),
     Ctx = require('../../common/context.js'),
+    Growl = require('../../utils/growl.js'),
     Source = require('../../models/sources.js'),
     CollectionManager = require('../../common/collectionManager.js'),
-    EmailSourceEditView = require("./emailSettings.js"),
+    SourceViews = require("./sourceEditViews.js"),
     FacebookSourceEditView = require("../facebookViews.js");
 
 function getSourceEditView(model_type) {
@@ -14,7 +16,7 @@ function getSourceEditView(model_type) {
     case Types.IMAPMAILBOX:
     case Types.MAILING_LIST:
     case Types.ABSTRACT_FILESYSTEM_MAILBOX:
-      return EmailSourceEditView;
+      return SourceViews.EmailSource;
     case Types.FACEBOOK_GENERIC_SOURCE:
     case Types.FACEBOOK_GROUP_SOURCE:
     case Types.FACEBOOK_GROUP_SOURCE_FROM_USER:
@@ -33,7 +35,9 @@ function getSourceEditView(model_type) {
 var ReadSource = Marionette.ItemView.extend({
     template: '#tmpl-adminDiscussionSettingsGeneralSourceRead',
     ui: {
-        manualStart: '.js_manualStart'
+        manualStart: '.js_manualStart',
+        reimport: '.js_reimport',
+        reprocess: '.js_reprocess',
     },
 
     modelEvents: {
@@ -41,7 +45,37 @@ var ReadSource = Marionette.ItemView.extend({
     },
 
     events: {
-        'click @ui.manualStart': 'manualStart'
+        'click @ui.manualStart': 'manualStart',
+        'click @ui.reimport': 'reimportSource',
+        'click @ui.reprocess': 'reprocessSource'
+    },
+
+    reimportSource: function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        return Promise.resolve(this.model.doReimport()).then(function(resp) {
+            if (_.has(resp, 'error')){
+                Growl.showBottomGrowl(Growl.GrowlReason.ERROR, i18n.gettext("There was a reimport error!"));
+                console.error("Source " + this.model.name + " failed to reimport due to an internal server problem with response ", resp);
+            }
+            Growl.showBottomGrowl(Growl.GrowlReason.SUCCESS, i18n.gettext('Reimport has begun! It can take up to 15 minutes to complete.'));
+        }).catch(function(e) {
+            Growl.showBottomGrowl(Growl.GrowlReason.ERROR, i18n.gettext('Reimport failed.'));
+        });
+    },
+
+    reprocessSource: function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        return Promise.resolve(this.model.doReprocess()).then(function(resp) {
+            if (_.has(resp, 'error')){
+                Growl.showBottomGrowl(Growl.GrowlReason.ERROR, i18n.gettext("There was a reprocess error!"));
+                console.error("Source " + this.model.name + " failed to reprocess due to an internal server problem with response", resp);
+            }
+            Growl.showBottomGrowl(Growl.GrowlReason.SUCCESS, i18n.gettext('Reprocess has begun! It can take up to 15 minutes to complete.'));
+        }).catch(function(e) {
+            Growl.showBottomGrowl(Growl.GrowlReason.ERROR, i18n.gettext('Reimport failed'));
+        });
     },
 
     manualStart: function(evt){

assembl/static/js/app/views/sourceEditView.js
@@ -1,0 +1,49 @@
+var Marionette = require('../shims/marionette.js'),
+    $ = require('../shims/jquery.js'),
+    i18n = require('../utils/i18n.js'),
+    Promise = require('bluebird'),
+    Growl = require('../utils/growl.js');
+
+
+/**
+ * An abstract class that defines the Marionette View
+ * to use for the editing of each source in a source list
+ * view.
+ */
+module.exports = Marionette.ItemView.extend({
+    ui: {
+        submit: '.js_saveSource',
+    },
+
+    events: {
+        'click @ui.submit': 'submitForm'
+    },
+
+    submitForm: function(e) {
+        e.preventDefault();
+        this.saveModel();
+    },
+
+    /**
+     * A function to override by sub-class to get the
+     * model changed values
+     * @return Object of values for the model to change
+     */
+    fetchValues: function(){
+        throw new Error("Cannot call fetchValues on an abstract class!");
+    },
+
+    saveModel: function(){
+        var values = this.fetchValues();
+        this.model.set(values);
+        this.model.save(null, {
+            success: function(model, resp){
+                Growl.showBottomGrowl(Growl.GrowlReason.SUCCESS, i18n.gettext('Your settings were saved!'));
+            },
+
+            error: function(model, resp){
+              Growl.showBottomGrowl(Growl.GrowlReason.ERROR, i18n.gettext('Your settings failed to update.'));  
+            }
+        });
+    }
+});

commit 1275fecdb8c4e9d130466082783bf4d8e82e01dd

Fix saving of email sources broken in https://github.com/ImaginationForPeople/assembl/commit/5e78a6b16e5277e8353ed7a70824ca38cd88583c

assembl/static/js/app/utils/growl.js
@@ -22,9 +22,9 @@ var defaultGrowlSettings = {
     stackup_spacing: 10
 };
 
-var showBottomGrowl = function(grow_reason, msg){
-    $.bootstrapGrowl(msg, _.extend(defaultGrowlSettings, {
-        type: growl_reason
+var showBottomGrowl = function(growl_reason, msg){
+  $.bootstrapGrowl(msg, _.extend(defaultGrowlSettings, {
+    type: growl_reason
     }));
 };
 

assembl/static/js/app/views/admin/sourceEditViews.js
@@ -9,8 +9,18 @@ var Marionette = require('../../shims/marionette.js'),
 var EmailSourceEditView = SourceViewBase.extend({
   template: '#tmpl-emailSource',
 
-  fetchValue: function(){
-    return 
+  fetchValues: function(){
+    return {
+      name: this.$('#name').val(),
+      admin_sender: this.$('#admin_sender').val(),
+      post_email_address: this.$('#post_email_address').val(),
+      host: this.$('#host').val(),
+      use_ssl: this.$('#use_ssl:checked').val(),
+      folder: this.$('#folder').val(),
+      port: parseInt(this.$('#port').val()),
+      username: this.$('#username').val(),
+      password: this.$('#password').val()
+    }
   }
 
 });