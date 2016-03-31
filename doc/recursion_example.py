f0411f03694c31db847f31bc32ebbf3e234735ed
Marc-Antoine Parent <maparent@acm.org>
2016-03-31 09:57:48 -0400

    level not needed

--- doc/recursion_example.py
+++ doc/recursion_example.py
@@ -19,20 +19,20 @@ def get_descendants(db, source_id):
 
     All I can say is SQLAlchemy kicks ass, and so does PostgreSQL.
     """
-    level = literal_column('ARRAY[source_id]', type_=ARRAY(Integer))
+    # level = literal_column('ARRAY[source_id]', type_=ARRAY(Integer))
     link = db.query(IdeaLink.source_id, IdeaLink.target_id) \
-                  .add_columns(level.label('level')) \
+                  # .add_columns(level.label('level')) \
                   .filter(IdeaLink.source_id == source_id) \
                   .cte(name='thread', recursive=True)
     source_alias = aliased(link, name='source')
     replies_alias = aliased(IdeaLink, name='replies')
-    cumul_level = source_alias.c.level.op('||')(replies_alias.source_id)
+    # cumul_level = source_alias.c.level.op('||')(replies_alias.source_id)
     parent_link = replies_alias.source_id == source_alias.c.target_id
-    children = db.query(replies_alias.source_id, replies_alias.target_id
-                ).add_columns(cumul_level) \
-                      .filter(parent_link)
+    children = (db.query(replies_alias.source_id, replies_alias.target_id)
+                        # add_columns(cumul_level).
+                        .filter(parent_link))
 
-    return db.query(link.union_all(children)).order_by(link.c.level)
+    return db.query(link.union_all(children))   # .order_by(link.c.level)
 
 
 
