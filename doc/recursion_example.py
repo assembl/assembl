385938cfc0ce26f4ee9a2395ded89623c0897c71
Marc-Antoine Parent <maparent@acm.org>
2016-03-31 09:10:26 -0400

    apply recursion eg to idea links

--- doc/recursion_example.py
+++ doc/recursion_example.py
@@ -1,3 +1,40 @@
+from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy import literal_column, Integer
+from sqlalchemy.orm import aliased
+from sqlalchemy.sql.functions import func
+from assembl.models import IdeaLink
+
+
+def get_descendants(db, source_id):
+    """Return a query that includes the descendants of an idea. (not idea itself yet.)
+
+    Beware: we use a recursive query via a CTE and the PostgreSQL-specific
+    ARRAY type. Blame this guy for that choice:
+    http://explainextended.com/2009/09/24/adjacency-list-vs-nested-sets-postgresql/
+
+    Also, that other guy provided insight into using CTE queries:
+    http://stackoverflow.com/questions/11994092/how-can-i-perform-this-recursive-common-table-expression-in-sqlalchemy
+
+    A literal column and an op complement nicely all this craziness.
+
+    All I can say is SQLAlchemy kicks ass, and so does PostgreSQL.
+    """
+    level = literal_column('ARRAY[source_id]', type_=ARRAY(Integer))
+    link = db.query(IdeaLink.source_id, IdeaLink.target_id) \
+                  .add_columns(level.label('level')) \
+                  .filter(IdeaLink.source_id == source_id) \
+                  .cte(name='thread', recursive=True)
+    source_alias = aliased(link, name='source')
+    replies_alias = aliased(IdeaLink, name='replies')
+    cumul_level = source_alias.c.level.op('||')(replies_alias.source_id)
+    parent_link = replies_alias.source_id == source_alias.c.target_id
+    children = db.query(replies_alias.source_id, replies_alias.target_id
+                ).add_columns(cumul_level) \
+                      .filter(parent_link)
+
+    return db.query(link.union_all(children)).order_by(link.c.level)
+
+
 
 def get_thread(self, levels=None):
     """Return a query that includes the post and its following thread.
