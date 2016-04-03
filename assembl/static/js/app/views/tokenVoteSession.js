6ae3a4e1a5106473cc6b7ffd632e5088fe71d6e7
Swergas <swergas@yahoo.fr>
2016-04-04 00:48:41 +0200

    Continuing work on Token Vote UX/UI. I finally managed to add CSS to external SVGs after a lot of pain. Also use JSON contentType instead of Form to POST votes, so that backend can handle tombstones better.

--- assembl/static/js/app/views/tokenVoteSession.js
+++ assembl/static/js/app/views/tokenVoteSession.js
@@ -35,6 +35,7 @@ function nthPermutation(atoms, index, size) {
   return dest;
 }
 
+/*
 // NOT USED YET
 // Returned array will contain alternatively one element from the beginning of the array, and one from the end (the second element of the returned array will be the last from the input array if the array size is odd, or the penultimate if the array size is pair)
 function alternateItems(atoms, size){
@@ -45,6 +46,7 @@ function alternateItems(atoms, size){
   }
   return dest;
 }
+*/
 
 // Returns the index in the array, as if the array was alternated: the array would contain alternatively one element from the beginning of the array, and one from the end (the second element of the returned array will be the last from the input array if the array size is odd, or the penultimate if the array size is pair)
 function alternatedIndex(index, size){
@@ -56,6 +58,32 @@ function alternatedIndex(index, size){
     return size - 1 + offset;
   }
 }
+
+/*
+// NOT USED YET
+// Get the linear interpolation between two value
+var lerp = function (value1, value2, amount) {
+  amount = amount < 0 ? 0 : amount;
+  amount = amount > 1 ? 1 : amount;
+  return value1 + (value2 - value1) * amount;
+}
+*/
+
+/*
+Adding CSS to an SVG which has been embedded using an <image> tag is not possible, nor with a background CSS property.
+Adding CSS to an external SVG which has been embedded using an <object> tag is possible only if the URL is on the same domain (CORS policy).
+So we have to GET the SVG file from its URL with an AJAX call, and add it inline to the DOM.
+*/
+var getSVGElementByURLPromise = function(url){
+  return $.ajax({
+    url: url,
+    dataType: 'xml'
+  }).then(function(data) {
+    var svg = $(data).find('svg');
+    svg.removeAttr('xmlns:a');
+    return svg;
+  });
+};
 
 
 var TokenIdeaAllocationView = Marionette.ItemView.extend({
@@ -124,25 +152,77 @@ var TokenIdeaAllocationView = Marionette.ItemView.extend({
       console.error("could not compte this.voteURL and this.postData");
     }
 
+    this.customTokenImageURL = this.category.get("image");
+    this.customTokenImagePromise = getSVGElementByURLPromise(this.customTokenImageURL);
   },
   onRender: function(){
     var that = this;
+
+    /*
+    var colorizeSVG = function(el, color){
+      el.find("*").attr("fill", color);
+    }
+
+    var contourOnlySVG = function(el, color){
+      el.find("*").attr("fill", "none");
+      el.find("*").attr("stroke", color);
+    }
+    */
+
     // Icon of an empty token := By clicking on it, the user sets 0 tokens on this idea
     var zeroToken = $('<a class="btn"><svg viewBox="0 0 20 20" style="width: 20px; height: 20px;"><path fill="#4691f6" d="M15.62,1.825H4.379v1.021h0.13c-0.076,0.497-0.13,1.005-0.13,1.533c0,3.998,2.246,7.276,5.11,7.629v5.145 H7.445c-0.282,0-0.511,0.229-0.511,0.512s0.229,0.511,0.511,0.511h5.109c0.281,0,0.512-0.229,0.512-0.511s-0.23-0.512-0.512-0.512 h-2.043v-5.145c2.864-0.353,5.109-3.631,5.109-7.629c0-0.528-0.054-1.036-0.129-1.533h0.129V1.825z M10,11.087 c-2.586,0-4.684-3.003-4.684-6.707c0-0.53,0.057-1.039,0.138-1.533h9.092c0.081,0.495,0.139,1.003,0.139,1.533 C14.685,8.084,12.586,11.087,10,11.087z"></path></svg></a>');
 
     // Icon of a token := There will be maximum_per_idea of them shown per votable idea. By clicking on one of them, the user sets as many tokens on the idea
     var oneToken = $('<a class="btn"><svg viewBox="0 0 20 20" style="width: 20px; height: 20px;"><path fill="#4691f6" d="M9.917,0.875c-5.086,0-9.208,4.123-9.208,9.208c0,5.086,4.123,9.208,9.208,9.208s9.208-4.122,9.208-9.208 C19.125,4.998,15.003,0.875,9.917,0.875z M9.917,18.141c-4.451,0-8.058-3.607-8.058-8.058s3.607-8.057,8.058-8.057 c4.449,0,8.057,3.607,8.057,8.057S14.366,18.141,9.917,18.141z M13.851,6.794l-5.373,5.372L5.984,9.672 c-0.219-0.219-0.575-0.219-0.795,0c-0.219,0.22-0.219,0.575,0,0.794l2.823,2.823c0.02,0.028,0.031,0.059,0.055,0.083 c0.113,0.113,0.263,0.166,0.411,0.162c0.148,0.004,0.298-0.049,0.411-0.162c0.024-0.024,0.036-0.055,0.055-0.083l5.701-5.7 c0.219-0.219,0.219-0.575,0-0.794C14.425,6.575,14.069,6.575,13.851,6.794z"></path></svg></a>');
+
+    var customToken = null;
     
 
     var container = this.$el;
     var renderClickableTokenIcon = function(number_of_tokens_represented_by_this_icon){
       var el = null;
+
+      var token_size = 50;
+      if ( that.maximum_per_idea > 0 ){
+        var maximum_total_size = 400;
+        var maximum_token_size = 60;
+        var minimum_token_size = 20;
+        var max_per_row = that.maximum_per_idea > 10 ? 10 : that.maximum_per_idea;
+        token_size = maximum_total_size / max_per_row;
+        if ( token_size < minimum_token_size ){
+          token_size = minimum_token_size;
+        }
+        if ( token_size > maximum_token_size ){
+          token_size = maximum_token_size;
+        }
+      }
+
       if ( number_of_tokens_represented_by_this_icon == 0 ){
-        el = zeroToken.clone();
+        if ( that.customTokenImageURL ){
+          el = customToken.clone();
+          //contourOnlySVG(el, "#cccccc");
+          el[0].classList.add("custom");
+        }
+        else {
+          el = zeroToken.clone();
+          el[0].classList.add("default");
+        }
+        el[0].classList.add("zero");
       }
       else {
-        el = oneToken.clone();
+        if ( that.customTokenImageURL ){
+          el = customToken.clone();
+          //contourOnlySVG(el, "#0000ff");
+          el[0].classList.add("custom");
+        }
+        else {
+          el = oneToken.clone();
+          el[0].classList.add("default");
+        }
+        el[0].classList.add("positive");
       }
+      el.css("max-width", token_size);
+      el.css("max-height", token_size);
 
       var showAsSelected = false;
       if ( number_of_tokens_represented_by_this_icon == 0 ){
@@ -153,17 +233,31 @@ var TokenIdeaAllocationView = Marionette.ItemView.extend({
         showAsSelected = true;
       }
       if ( showAsSelected ){
-        el.find("path").attr("fill", "#00ff00");
+        el[0].classList.add("selected");
+        /*
+        if ( number_of_tokens_represented_by_this_icon == 0 ){
+          colorizeSVG(el, "#cccccc");
+        }
+        else {
+          colorizeSVG(el, "#00ff00");
+        }
+        */
+      }
+      else {
+        el[0].classList.add("not-selected");
       }
 
-      el.attr("title", "set "+number_of_tokens_represented_by_this_icon+" tokens");
-      el.click(function(){
+      var link = $('<a class="btn token-icon"></a>');
+      el.appendTo(link);
+      link.attr("title", "set "+number_of_tokens_represented_by_this_icon+" tokens");
+      link.click(function(){
         console.log("set " + number_of_tokens_represented_by_this_icon + " tokens");
         that.postData["value"] = number_of_tokens_represented_by_this_icon;
         $.ajax({
           type: "POST",
+          contentType: 'application/json; charset=UTF-8',
           url: that.voteURL,
-          data: that.postData,
+          data: JSON.stringify(that.postData),
           success: function(data){
             console.log("success! data: ", data);
             that.currentValue = number_of_tokens_represented_by_this_icon;
@@ -171,14 +265,45 @@ var TokenIdeaAllocationView = Marionette.ItemView.extend({
           },
           error: function(jqXHR, textStatus, errorThrown){
             console.log("error! textStatus: ", textStatus, "; errorThrown: ", errorThrown);
+            // TODO: show error in the UI
           }
         });
       });
-      el.appendTo(container);
+      
+      link.hover(function handlerIn(){
+        el[0].classList.add("hover");
+        link.prevAll().children("svg").each(function(){
+          if ( !(this.classList.contains("zero")) ){
+            this.classList.add("hover");
+          }
+        });
+        link.nextAll().children("svg").each(function(){
+          this.classList.remove("hover");
+        });
+      }, function handlerOut(){
+        el[0].classList.remove("hover");
+        link.siblings().children("svg").each(function(){
+          this.classList.remove("hover");
+        });
+      });
+      
+      link.appendTo(container);
     };
-    for ( var i = 0; i <= this.maximum_per_idea; ++i ){
-      //console.log("i: ", i, "; maximum_per_idea: ", maximum_per_idea);
-      renderClickableTokenIcon(i);
+
+    var renderAllTokenIcons = function(){
+      for ( var i = 0; i <= that.maximum_per_idea; ++i ){
+        renderClickableTokenIcon(i);
+      }
+    };
+
+    if ( that.customTokenImageURL ){
+      $.when(that.customTokenImagePromise).then(function(svgEl){
+        customToken = svgEl;
+        renderAllTokenIcons();
+      });
+    }
+    else {
+      renderAllTokenIcons();
     }
   },
   serializeData: function(){
@@ -324,8 +449,13 @@ var TokenVoteSessionModal = Backbone.Modal.extend({
   },
 
   serializeData: function() {
+    var settings = this.widgetModel.get("settings") || {};
+    var items = "items" in settings ? settings.items : [];
+    var question_item = items.length ? items[0] : null;
     return {
-      popin_title: i18n.gettext("Token vote")
+      popin_title: i18n.gettext("Token vote"),
+      question_title: "question_title" in question_item ? question_item.question_title : "",
+      question_description: "question_description" in question_item ? question_item.question_description : ""
     };
   },
 
