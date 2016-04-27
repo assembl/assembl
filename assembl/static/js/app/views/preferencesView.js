a4fd4f931624d92df72c2ec3f2d78262746b7057
Marc-Antoine Parent <maparent@acm.org>
2016-04-27 13:25:16 -0400

    better regexp for URL

--- assembl/static/js/app/views/preferencesView.js
+++ assembl/static/js/app/views/preferencesView.js
@@ -296,7 +296,7 @@ var UrlPreferenceView = StringPreferenceView.extend({
   constructor: function UrlPreferenceView() {
     StringPreferenceView.apply(this, arguments);
   },
-  regexp: new RegExp("^https?://[^\s/$.?#].[^\s]*$", "i"),
+  regexp: new RegExp('^(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$', 'i'),
   processValue: function(value) {
     if (!this.regexp.test(value)) {
         throw i18n.gettext("This does not appear to be a URL");
