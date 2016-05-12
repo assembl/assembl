074b270bd12bdba5e2ade33f6513a255d7a4eae0
Marc-Antoine Parent <maparent@acm.org>
2016-05-12 13:57:50 -0400

    data corruption observed on a dev. machine

---
+++ assembl/alembic/versions/267b2f56d7b0_corrupt_idea_related_post_link.py
@@ -0,0 +1,39 @@
+"""corrupt idea_related_post_link
+
+Revision ID: 267b2f56d7b0
+Revises: 4097019d6357
+Create Date: 2016-05-12 10:34:46.200057
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '267b2f56d7b0'
+down_revision = '4097019d6357'
+
+from alembic import context, op
+import sqlalchemy as sa
+import transaction
+
+
+from assembl.lib import config
+
+
+def upgrade(pyramid_env):
+    # Do stuff with the app's models here.
+    from assembl import models as m
+    db = m.get_session_maker()()
+    with transaction.manager:
+        errors = list(db.execute(
+            """SELECT idea_content_link.id, post.creator_id FROM idea_content_link
+                JOIN post ON content_id = post.id
+                WHERE idea_content_link.type = 'assembl:postLinkedToIdea'
+                AND idea_content_link.creator_id=-1"""))
+        for id, contributor in errors:
+            db.execute(
+                "UPDATE idea_content_link SET contributor_id={contributor} WHERE id = {id}".format(
+                    id=id, contributor=contributor))
+
+
+def downgrade(pyramid_env):
+    with context.begin_transaction():
+        pass
