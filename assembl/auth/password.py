0a5839bae828ffe4588cf89161191fa1e7af3bed
Marc-Antoine Parent <maparent@acm.org>
2016-05-12 19:10:21 -0400

    accept null password

--- assembl/auth/password.py
+++ assembl/auth/password.py
@@ -87,8 +87,9 @@ def email_token_legacy(email):
 
 
 def password_change_token(user):
+    password = user.password.decode('iso-8859-1') if user.password else ''
     return data_token(
-        str(user.id), PASSWORD_CHANGE_TOKEN_DURATION, user.password.decode('iso-8859-1'))
+        str(user.id), PASSWORD_CHANGE_TOKEN_DURATION, password)
 
 
 def password_change_token_legacy(user):
@@ -181,7 +182,8 @@ def verify_password_change_token(token):
         user = User.get(data)
         if not user:
             return None, Validity.DATA_NOT_FOUND
-        data, valid = verify_data_token(token, user.password.decode('iso-8859-1'))
+        password = user.password.decode('iso-8859-1') if user.password else ''
+        data, valid = verify_data_token(token, password)
         return user, valid
     # Try decoding legacy
     try:
