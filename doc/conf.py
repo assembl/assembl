a8cefefa173713de2a67518d7693ea789d450852
Marc-Antoine Parent <maparent@acm.org>
2016-06-07 19:59:57 -0400

    use alabaster theme for sphinx-doc

--- doc/conf.py
+++ doc/conf.py
@@ -15,7 +15,7 @@
 import sys
 import os
 import cornice
-import sphinx_bootstrap_theme
+import alabaster
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -42,6 +42,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinxcontrib.httpdomain',
     'pyramid_autodoc',
+    'alabaster'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -112,8 +113,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme_path = [alabaster.get_path()]
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
