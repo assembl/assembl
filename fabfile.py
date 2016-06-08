3ace5ad65ac1504ffa6f0edc1b4d0bbcdb15f1f5
Marc-Antoine Parent <maparent@acm.org>
2016-06-07 21:02:05 -0400

    apidoc is in the venv

--- fabfile.py
+++ fabfile.py
@@ -1463,7 +1463,7 @@ def build_doc():
     "Build the Sphinx documentation"
     with cd(env.projectpath):
         run('rm -rf doc/autodoc')
-        run('env SPHINX_APIDOC_OPTIONS="members,show-inheritance" sphinx-apidoc -e -f -o doc/autodoc assembl')
+        venvcmd('env SPHINX_APIDOC_OPTIONS="members,show-inheritance" sphinx-apidoc -e -f -o doc/autodoc assembl')
         venvcmd('python setup.py build_sphinx')
 
 
