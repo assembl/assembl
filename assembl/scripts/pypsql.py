a17b270848c71da7c8ed602318a80f12deacf490
Marc-Antoine Parent <maparent@acm.org>
2016-06-11 10:28:13 -0400

    do not use password when running as psql

--- assembl/scripts/pypsql.py
+++ assembl/scripts/pypsql.py
@@ -10,14 +10,19 @@ def main():
     parser = argparse.ArgumentParser(description='Call postgresql.')
     parser.add_argument('--host', '-n', default="localhost")
     parser.add_argument('--user', '-u', help='database user')
-    parser.add_argument('--password', '-p', help='database user')
+    parser.add_argument('--password', '-p', help='database password',
+                        default=None)
+    parser.add_argument('--ask_password', '-P', action="store_true",
+                        help='ask for database password',)
     parser.add_argument('--database', '-d', help="database")
     parser.add_argument('--print_one', '-1', action="store_true",
                         help="print first return row (fails if none)")
     parser.add_argument('commands', help="sql commands")
     args = parser.parse_args()
     user = args.user or getpass.getuser()
-    password = args.password or getpass.getpass("password: ")
+    password = args.password
+    if args.ask_password:
+        password = getpass.getpass("password: ")
     database = args.database or "postgres"
     cx = psycopg2.connect(user=user, password=password, database=database, host=args.host)
     cur = cx.cursor()
