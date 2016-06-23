1f21bda04444581c024e75de678426176db436c3
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2016-06-23 13:33:58 -0400

    Fixes currently failing tests

--- assembl/tests/fixtures/user.py
+++ assembl/tests/fixtures/user.py
@@ -7,10 +7,10 @@ from assembl.auth import R_PARTICIPANT
 def participant1_user(request, test_session, discussion):
     """A User fixture with R_PARTICIPANT role"""
 
-    from assembl.models import User, UserRole, Role
+    from assembl.models import User, UserRole, Role, EmailAccount
     u = User(name=u"A. Barking Loon", type="user", password="password",
              verified=True)
-    # email = EmailAccount(email="abloon@gmail.com", profile=u, verified=True)
+    email = EmailAccount(email="abloon@gmail.com", profile=u, verified=True)
     test_session.add(u)
     r = Role.get_role(R_PARTICIPANT, test_session)
     ur = UserRole(user=u, role=r)
