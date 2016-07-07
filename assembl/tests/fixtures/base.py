ead4a2aad3c94e6b2bfc401ba3f28450024e737d
Marc-Antoine Parent <maparent@acm.org>
2016-07-07 08:29:47 -0400

    forgot a comment

--- assembl/tests/fixtures/base.py
+++ assembl/tests/fixtures/base.py
@@ -265,6 +265,9 @@ def base_fixture_dirname():
 
 
 def api_call_to_fname(api_call, method="GET", **args):
+    """Translate an API call to a filename containing most of the call information
+
+    Used in :js:func:`ajaxMock`"""
     import os
     import os.path
     base_fixture_dir = base_fixture_dirname()
