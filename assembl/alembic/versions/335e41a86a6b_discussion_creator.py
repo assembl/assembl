5fc2ea5b3dc1312edf870185f2366fa0312ef4be
Marc-Antoine Parent <maparent@acm.org>
2016-11-21 07:03:29 -0500

    linearize alembic history

--- assembl/alembic/versions/335e41a86a6b_discussion_creator.py
+++ assembl/alembic/versions/335e41a86a6b_discussion_creator.py
@@ -1,14 +1,14 @@
 """discussion creator
 
 Revision ID: 335e41a86a6b
-Revises: 41176a6a6758
+Revises: c6789ae35f6f
 Create Date: 2016-11-14 12:32:52.653637
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '335e41a86a6b'
-down_revision = '41176a6a6758'
+down_revision = 'c6789ae35f6f'
 
 from alembic import context, op
 import sqlalchemy as sa
