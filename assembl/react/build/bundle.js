5d156715b9898f4acbb8c8f674c41a4dc3b6ca98
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2016-12-02 20:05:36 +0200

    First structure of content in front-end

---
+++ assembl/react/build/bundle.js
@@ -0,0 +1,9 @@
+/*
+	Dummy file only used for verification of backend/frontend separation
+
+	This file instead will be autocreated by webpack and will be the compilation
+	of the React-based front-end
+*/
+
+var element = document.createElement("<p>Hello World!</p>");
+document.body.appendChild(element);
