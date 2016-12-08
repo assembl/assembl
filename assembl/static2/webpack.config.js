4e716a93c56208085538e6fd1af212e1fecf8ada
paolina92 <paolinardc@gmail.com>
2016-12-08 19:40:25 +0100

    install webpack

--- assembl/static2/webpack.config.js
+++ assembl/static2/webpack.config.js
@@ -3,30 +3,11 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: [
-    'webpack/hot/only-dev-server',
     './js/app/index.js'
   ],
   output: {
-    filename: 'build/bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loaders: ['react-hot', 'babel'],
-        include: path.join(__dirname, 'js')
-      },
-      {
-        test: /\.scss$/,
-        loaders: ['style', 'css', 'sass']
-      },
-      { 
-        test: /\.css$/, 
-        loader: "style-loader!css-loader" 
-      }
-    ]
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ]
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  }
 };
