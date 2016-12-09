456d17d46367278b917b025ef1ebb0b3579eeb46
paolina92 <paolinardc@gmail.com>
2016-12-09 10:58:35 +0100

    update gitignore + package

--- assembl/static2/js/app/index.js
+++ assembl/static2/js/app/index.js
@@ -1,1 +1,9 @@
-console.log('Hello World!!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+import '../../css/main.scss';
+
+ReactDOM.render(
+  <p>Hello World!</p>,
+  document.getElementById('root')
+);
--- assembl/static2/webpack.config.js
+++ assembl/static2/webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 
 module.exports = {
     entry: [
-        './js/app/index.js'
+    './js/app/index.js'
     ],
     output: {
         path: path.join(__dirname, 'build'),
@@ -11,10 +11,44 @@ module.exports = {
         publicPath: '/build/'
     },
     module: {
-        loaders: [{
+        loaders: [
+        {
             test: /\.js$/,
             loaders: ['babel'],
             include: path.join(__dirname, 'js')
-        }]
+        },
+        {
+            test: /\.scss$/,
+            loaders: ['style', 'css', 'sass']
+        },
+        { 
+            test: /\.css$/, 
+            loader: "style-loader!css-loader" 
+        },
+        { 
+            test: /\.png$/, 
+            loader: "url-loader?limit=100000" 
+        },
+        { 
+            test: /\.jpg$/, 
+            loader: "file-loader" 
+        },
+        {
+            test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+            loader: 'url?limit=10000&mimetype=application/font-woff'
+        },
+        {
+            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+            loader: 'url?limit=10000&mimetype=application/octet-stream'
+        },
+        {
+            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+            loader: 'file'
+        },
+        {
+            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+            loader: 'url?limit=10000&mimetype=image/svg+xml'
+        }
+        ]
     }
 };
