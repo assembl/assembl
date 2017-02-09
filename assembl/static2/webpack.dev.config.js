79c0fcd95c6026caa0800333e457ae159e9c9e5d
Vincent Fretin <vincent.fretin@gmail.com>
2017-02-09 10:43:24 +0100

    underscore was used in webpack.dev.config.js, rewrite webpack.dev.config.js to not use it

--- assembl/static2/webpack.dev.config.js
+++ assembl/static2/webpack.dev.config.js
@@ -1,26 +1,27 @@
 var path = require('path');
 var webpack = require('webpack');
-var _ = require('underscore');
-var base_config = require('./webpack.config');
+var baseConfig = require('./webpack.config');
 
-var webpack_port = parseInt(process.env.WEBPACK_URL.split(':')[2]);
+var webpackPort = parseInt(process.env.WEBPACK_URL.split(':')[2]);
 
-module.exports = _.extend(base_config, {
-    devServer: {
-        inline: true,
-        headers: {
-            "Access-Control-Allow-Origin": process.env.ASSEMBL_URL,
-            "Access-Control-Allow-Credentials":true
-        },
-        port: webpack_port,
-        host: "0.0.0.0",
+var config = Object.create(baseConfig);
+
+config.devServer = {
+    inline: true,
+    headers: {
+        "Access-Control-Allow-Origin": process.env.ASSEMBL_URL,
+        "Access-Control-Allow-Credentials":true
     },
-    entry: [
+    port: webpackPort,
+    host: "0.0.0.0",
+};
+config.entry = [
     'webpack-dev-server/client?' + process.env.WEBPACK_URL,
     'webpack/hot/only-dev-server',
     './js/app/index'
-    ],
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ],
-});
+];
+config.plugins = [
+    new webpack.HotModuleReplacementPlugin()
+];
+
+module.exports = config;
