fade0f0f8206aabf48e532616dd74924d831b984
paolina92 <paolinardc@gmail.com>
2017-02-13 13:19:02 +0100

    remove hardcoded user id

--- assembl/static2/js/app/utils/globalFunctions.js
+++ assembl/static2/js/app/utils/globalFunctions.js
@@ -19,7 +19,7 @@ class GlobalFunctions {
     return discussionId;
   }
   static getConnectedUserId() {
-    const userId = document.getElementById('user-id') ? document.getElementById('user-id').value : '1296';
+    const userId = document.getElementById('user-id') ? document.getElementById('user-id').value : null;
     return userId;
   }
 }
