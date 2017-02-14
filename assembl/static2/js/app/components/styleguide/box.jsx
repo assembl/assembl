768ad43b5c727bbc7203585c81ec63336f889237
paolina92 <paolinardc@gmail.com>
2017-02-14 23:40:06 +0100

    complete style guide with last elements

--- assembl/static2/js/app/components/styleguide/box.jsx
+++ assembl/static2/js/app/components/styleguide/box.jsx
@@ -8,6 +8,11 @@ class Box extends React.Component {
         <section>
           <div className="box-title">Box title</div>
           <div className="box">Box content</div>
+          <div className="insert-box border margin-m">
+            <h3 className="dark-title-3">Box title</h3>
+            <div className="hyphen">&nbsp;</div>
+            <div>Box content</div>
+          </div>
         </section>
         <section>
           <h3 className="dark-title-3">Code</h3>
@@ -16,6 +21,13 @@ class Box extends React.Component {
           </pre>
           <pre>
             &lt;div className="box"&gt;Box content&lt;/div&gt;
+          </pre>
+          <pre>
+            <div>&lt;div className="insert-box"&gt;</div>
+              <div style={{ marginLeft: `${20}px` }}>&lt;h3 className="dark-title-3"&gt;Box title&lt;/h3&gt;</div>
+              <div style={{ marginLeft: `${20}px` }}>&lt;div className="hyphen">&nbsp;&lt;/div&gt;</div>
+              <div style={{ marginLeft: `${20}px` }}>&lt;div>Box content&lt;/div&gt;</div>
+            <div>&lt;/div&gt;</div>
           </pre>
         </section>
       </div>
