b1fc598dd523076b073c81302395c9a4feb306c4
paolina92 <paolinardc@gmail.com>
2017-03-03 11:47:51 +0100

    get discussion preferences

--- assembl/static2/js/app/services/debateService.js
+++ assembl/static2/js/app/services/debateService.js
@@ -3,18 +3,23 @@ import HttpRequestHandler from '../utils/httpRequestHandler';
 class DebateService {
   static fetchDebateData(debateId) {
     const that = this;
-    const fetchUrl = `/data/Discussion/${debateId}`;
-    return HttpRequestHandler.request({ method: 'GET', url: fetchUrl }).then((debateData) => {
+    const url1 = `/data/Discussion/${debateId}`;
+    const url2 = `/data/Discussion/${debateId}/preferences`;
+    const request1 = HttpRequestHandler.request({ method: 'GET', url: url1 });
+    const request2 = HttpRequestHandler.request({ method: 'GET', url: url2 });
+    return Promise.all([request1, request2]).then(function(results) {
       let config;
       try {
         config = require(`../config/${debateData.slug}`);// eslint-disable-line
       } catch (e) {
         config = require('../config/default');// eslint-disable-line
       }
-      return that.buildDebateData(debateData, config);
+      const data = results[0];
+      const prefs = results[1];
+      return that.buildDebateData(data, config, prefs[0]);
     });
   }
-  static buildDebateData(debateData, config) {
+  static buildDebateData(debateData, config, prefs) {
     return {
       help_url: debateData.help_url,
       introduction: debateData.introduction,
@@ -22,6 +27,7 @@ class DebateService {
       objectives: debateData.objectives,
       slug: debateData.slug,
       topic: debateData.topic,
+      prefs: prefs,
       config: config.default
     };
   }
