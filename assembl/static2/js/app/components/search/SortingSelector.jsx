e00c57f887fd60088df08cdbb250d351d233d0a8
Vincent Fretin <vincent.fretin@gmail.com>
2017-03-03 12:40:33 +0100

    forgot to commit this file, sorry

---
+++ assembl/static2/js/app/components/search/SortingSelector.jsx
@@ -0,0 +1,32 @@
+import {
+  SortingSelector,
+  renderComponent
+} from 'searchkit';
+
+
+class FilteredSortingSelector extends SortingSelector {
+  render() {
+    const { listComponent, filterPrefix } = this.props;
+    let options = this.accessor.options.options;
+    options = options.filter((sort) => {
+      const keyPrefix = sort.key.split(':')[0];
+      return keyPrefix === filterPrefix || keyPrefix === 'common';
+    });
+    const selected = [this.accessor.getSelectedOption().key];
+    const disabled = !this.hasHits();
+
+    return renderComponent(listComponent, {
+      mod: this.props.mod,
+      className: this.props.className,
+      items: options,
+      selectedItems: selected,
+      setItems: this.setItems.bind(this),
+      toggleItem: this.toggleItem.bind(this),
+      disabled: disabled,
+      urlBuilder: (item) => { return this.accessor.urlWithState(item.key); },
+      translate: this.translate
+    });
+  }
+}
+
+export default FilteredSortingSelector;
