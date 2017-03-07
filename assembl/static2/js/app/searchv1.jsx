e0f0239ad6584fe985d20d0cdc636a1573eb20ac
Vincent Fretin <vincent.fretin@gmail.com>
2017-03-07 17:36:51 +0100

    remove false comment

--- assembl/static2/js/app/searchv1.jsx
+++ assembl/static2/js/app/searchv1.jsx
@@ -14,7 +14,6 @@ import { SearchComponent } from './components/search.jsx?v=1'; // eslint-disable
 import '../../css/views/searchv1.scss';
 /*
 searchv1.js is included in assembl/templates/index.jinja2
-(you need to change the url for production)
 The search div is included in the page from assembl/templates/views/navBar.tmpl
 and the Search react component is rendered via the onDomRefresh event in
 assembl/static/js/app/views/navBar.js
