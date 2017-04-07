856c115a1fd357997c262dfea5a242ab292ee5e2
Vincent Fretin <vincent.fretin@gmail.com>
2017-04-07 18:10:18 +0200

    graphql_request fixture now explicitly depends on fr_locale and en_locale fixtures. This fixes the case where locales was removed by fixture teardown user_language_preference_fr/en_cookie in a previous test

--- assembl/tests/fixtures/graphql.py
+++ assembl/tests/fixtures/graphql.py
@@ -2,7 +2,7 @@ import pytest
 
 
 @pytest.fixture(scope="function")
-def graphql_request(request, test_adminuser_webrequest, discussion):
+def graphql_request(request, test_adminuser_webrequest, discussion, fr_locale, en_locale):
     req = test_adminuser_webrequest
     req.matchdict = {"discussion_id": discussion.id}
     return req
