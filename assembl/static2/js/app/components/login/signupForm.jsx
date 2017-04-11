8447011351f1d668b4ccd1f4420a56a1c9ec98b2
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2017-04-11 19:12:27 +0200

    Change routes on singup form

--- assembl/static2/js/app/components/login/signupForm.jsx
+++ assembl/static2/js/app/components/login/signupForm.jsx
@@ -3,16 +3,16 @@ import { connect } from 'react-redux';
 import { Translate, I18n } from 'react-redux-i18n';
 import { form, FormGroup, FormControl, Button } from 'react-bootstrap';
 import { Link } from 'react-router';
+import { Routes } from '../../routes';
 
 class SignupForm extends React.Component {
   render() {
     const { debateData } = this.props.debate;
-    const { rootPath } = this.props.context;
     return (
       <div className="login-view">
         <div className="box-title">{debateData.topic}</div>
         <div className="box">
-          <form className="signup" method="POST" action={`${rootPath}${debateData.slug}/signup#success`}>
+          <form className="signup" method="POST" action={Routes.getContextual('signup', {slug: debateData.slug})}>
             <FormGroup className="margin-m">
               <FormControl type="text" name="name" required placeholder={I18n.t('login.fullName')} />
             </FormGroup>
@@ -33,7 +33,7 @@ class SignupForm extends React.Component {
             <FormGroup>
               <Translate value="login.alreadyAccount" />
               <span>&nbsp;</span>
-              <Link to={`${rootPath}${debateData.slug}/login`}>
+              <Link to={Routes.getContextual('login', {slug: debateData.slug})}>
                 <Translate value="login.login" />
               </Link>
             </FormGroup>
