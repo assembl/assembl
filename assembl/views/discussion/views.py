2222237473bd943f5549374675d389fed6b7c28b
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2017-05-04 19:37:44 +0200

    remove previous changes; react_login route already exists, use it in discussion list template

--- assembl/views/discussion/views.py
+++ assembl/views/discussion/views.py
@@ -326,14 +326,12 @@ def includeme(config):
     config.add_route('bare_slug', '/{discussion_slug}')
     config.add_route('auto_bare_slug', '/{discussion_slug}/')
     config.add_route('general_react_page', '/{discussion_slug}/*extra_path')
-    config.add_route('new_login', '/login')
 
     react_routes = [
                         "new_home",
                         "bare_slug",
                         "new_styleguide",
-                        "general_react_page",
-                        "new_login"
+                        "general_react_page"
                     ]
 
     register_react_views(config, react_routes)
