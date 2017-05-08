1a84b7702ec6396ded8e3aa9ba4b6d0eba1133e7
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2017-05-09 00:49:05 +0200

    change how parsing is done. Remove caching, remove use of string templates

--- assembl/static2/js/app/utils/literalStringParser.js
+++ assembl/static2/js/app/utils/literalStringParser.js
@@ -8,22 +8,15 @@
 */
 /* eslint no-useless-escape: off, no-new-func: off */
 
-const cache = {};
+function get(path, obj, fb = `$\{${path}}`) {
+  return path.split('.').reduce((res, key) => res[key] || fb, obj);
+}
 
-const createParser = (template) => {
-  const sanitized = template
-  // Replace ${expressions} (etc) with ${map.expressions}.
-  .replace(/\$\{([\s]*[^;\s\{]+[\s]*)\}/g, (_, match) => { return `\$\{map.${match.trim()}\}`; })
-  // Afterwards, replace anything that's not ${map.expressions}' (etc) with a blank string.
-  .replace(/(\$\{(?!map\.)[^}]+\})/g, '');
-
-  return new Function('map', `return \`${sanitized}\``);
-};
-
-
-const parse = (template, map) => {
-  const parser = cache[template] = cache[template] || createParser(template);
-  return map ? parser(map) : parser();
-};
+function parse(template, map, fallback) {
+  return template.replace(/\$\{.+?}/g, (match) => {
+    const path = match.substr(2, match.length - 3).trim();
+    return get(path, map, fallback);
+  });
+}
 
 export default parse;
