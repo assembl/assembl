6e716584f63c29a7e19150717b7a9818cdf619b2
Cédric Messiant <cedricmessiant@ecreall.com>
2017-05-11 09:59:21 +0200

    Don't crash if redux devtools extension is not installed

--- assembl/static2/js/app/store/configureStore.dev.js
+++ assembl/static2/js/app/store/configureStore.dev.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 
 export default function configureStore(initialState, rootReducer, middlewares) {
-  const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middlewares), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())); // eslint-disable-line no-underscore-dangle
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // eslint-disable-line no-underscore-dangle
+  const store = createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(...middlewares)));
   return store;
 }
