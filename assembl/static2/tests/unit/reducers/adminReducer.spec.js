1280d0fa297018da6418ce95d1e8b5c62b055c17
Cédric Messiant <cedricmessiant@ecreall.com>
2017-05-11 12:04:34 +0200

    Add tests for adminReducer

---
+++ assembl/static2/tests/unit/reducers/adminReducer.spec.js
@@ -0,0 +1,106 @@
+import * as reducers from '../../../js/app/reducers/adminReducer';
+
+describe('Admin reducers', () => {
+  describe('selectedLocale reducer', () => {
+    const { selectedLocale } = reducers;
+    it('should return the initial state', () => {
+      expect(selectedLocale(undefined, {})).toEqual('fr');
+    });
+
+    it('should return state by default', () => {
+      const state = 'en';
+      const expected = 'en';
+      const actual = selectedLocale(state, {});
+      expect(actual).toEqual(expected);
+    });
+
+    it('should handle UPDATE_SELECTED_LOCALE action type', () => {
+      const state = 'en';
+      const action = {
+        type: 'UPDATE_SELECTED_LOCALE',
+        newLocale: 'fr'
+      };
+      const actual = selectedLocale(state, action);
+      const expected = 'fr';
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('surveyThemes reducer', () => {
+    const { surveyThemes } = reducers;
+    it('should return the initial state', () => {
+      expect(surveyThemes(undefined, {})).toEqual([]);
+    });
+
+    it('should return state by default', () => {
+      const state = ['0', '1'];
+      const expected = ['0', '1'];
+      const actual = surveyThemes(state, {});
+      expect(actual).toEqual(expected);
+    });
+
+    it('should handle ADD_THEME_TO_SURVEY action type', () => {
+      const state = ['0', '1'];
+      const action = { type: 'ADD_THEME_TO_SURVEY', id: '42' };
+      const expected = ['0', '1', '42'];
+      const actual = surveyThemes(state, action);
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('surveyThemesById reducer', () => {
+    const { surveyThemesById } = reducers;
+    it('should return the initial state', () => {
+      expect(surveyThemesById(undefined, {})).toEqual({});
+    });
+
+    it('should return state by default', () => {
+      const state = {
+        0: {
+          title: 'Foo'
+        }
+      };
+      const expected = {
+        0: {
+          title: 'Foo'
+        }
+      };
+      const actual = surveyThemesById(state, {});
+      expect(actual).toEqual(expected);
+    });
+
+    it('should handle ADD_THEME_TO_SURVEY action type', () => {
+      const state = {
+        0: { titlesByLocale: { fr: 'Bonjour', en: 'Hello' }, image: null },
+        1: { titlesByLocale: { fr: 'Au revoir', en: 'Goodbye' }, image: null }
+      };
+      const expected = {
+        0: { titlesByLocale: { fr: 'Bonjour', en: 'Hello' }, image: null },
+        1: { titlesByLocale: { fr: 'Au revoir', en: 'Goodbye' }, image: null },
+        2: { titlesByLocale: {}, image: null }
+      };
+      const action = { type: 'ADD_THEME_TO_SURVEY', id: '2' };
+      const actual = surveyThemesById(state, action);
+      expect(actual).toEqual(expected);
+    });
+
+    it('should handle UPDATE_SURVEY_THEME_TITLE action type', () => {
+      const state = {
+        0: { titlesByLocale: { fr: 'Bonjour', en: 'Hello' }, image: null },
+        1: { titlesByLocale: { fr: 'Au revoir', en: 'Goodbye' }, image: null }
+      };
+      const expected = {
+        0: { titlesByLocale: { fr: 'Salut', en: 'Hello' }, image: null },
+        1: { titlesByLocale: { fr: 'Au revoir', en: 'Goodbye' }, image: null }
+      };
+      const action = {
+        newTitle: 'Salut',
+        locale: 'fr',
+        type: 'UPDATE_SURVEY_THEME_TITLE',
+        themeId: '0'
+      };
+      const actual = surveyThemesById(state, action);
+      expect(actual).toEqual(expected);
+    });
+  });
+});
