c8447bc2728c4972e3883ce3e17889974fb914da
Cédric Messiant <cedricmessiant@ecreall.com>
2017-05-17 14:13:58 +0200

    Update theme title on input change

--- assembl/static2/js/app/components/administration/survey/themeCreationForm.jsx
+++ assembl/static2/js/app/components/administration/survey/themeCreationForm.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Translate, I18n } from 'react-redux-i18n';
+import { gql, withApollo } from 'react-apollo';
 import { Button, FormGroup, FormControl } from 'react-bootstrap';
 
 import ImageUploader from '../../common/imageUploader';
 
-export const DumbThemeCreationForm = ({ image, index, selectedLocale, titleEntries }) => {
+export const DumbThemeCreationForm = ({ client, id, image, index, selectedLocale, titleEntries }) => {
   const trsl = I18n.t('administration.ph.title');
   const ph = `${trsl} ${selectedLocale.toUpperCase()}`;
   const num = (Number(index) + 1).toString();
@@ -14,7 +15,53 @@ export const DumbThemeCreationForm = ({ image, index, selectedLocale, titleEntri
   const title = titleEntry ? titleEntry.value : '';
   const remove = () => {}; // TODO
   const updateImage = () => {}; // TODO
-  const updateTitle = () => {}; // TODO
+  const updateTitle = (value) => {
+    let entryIndex = titleEntries.indexOf(titleEntry);
+    if (entryIndex === -1) {
+      entryIndex = titleEntries.length;
+      const res = client.readFragment({
+        id: `Thematic:${id}`,
+        fragment: gql`
+          fragment thematic on Thematic {
+            titleEntries
+          }
+        `
+      });
+      const newTitleEntries = res.titleEntries;
+      newTitleEntries.push({
+        id: `Thematic:${id}.titleEntries.${entryIndex}`,
+        type: 'id'
+      });
+      client.writeFragment({
+        id: `Thematic:${id}`,
+        fragment: gql`
+          fragment thematic on Thematic {
+            titleEntries
+          }
+        `,
+        data: {
+          titleEntries: newTitleEntries,
+          __typename: 'Thematic'
+        }
+      });
+    }
+
+    client.writeFragment({
+      id: `Thematic:${id}.titleEntries.${entryIndex}`,
+      fragment: gql`
+        fragment entry on LangStringEntry {
+          localeCode,
+          value,
+          __typename
+        }
+      `,
+      data: {
+        localeCode: selectedLocale,
+        value: value,
+        __typename: 'LangStringEntry'
+      }
+    });
+  };
 
   return (
     <div className="form-container">
@@ -53,4 +100,4 @@ DumbThemeCreationForm.defaultProps = {
   title: ''
 };
 
-export default DumbThemeCreationForm;
+export default withApollo(DumbThemeCreationForm);
