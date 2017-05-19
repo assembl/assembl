fae5ffdabc157070e591fc63f903fa7e13e45e8c
paolina92 <paolinardc@gmail.com>
2017-05-19 10:37:25 +0200

    Use parseInt with radix

--- assembl/static2/js/app/pages/surveyAdmin.jsx
+++ assembl/static2/js/app/pages/surveyAdmin.jsx
@@ -7,14 +7,15 @@ import Export from '../components/administration/survey/export';
 import Navbar from '../components/administration/navbar';
 
 const SurveyAdmin = ({ i18n, section }) => {
+  const currentStep = parseInt(section, 10);
   return (
     <div className="survey-admin">
       <Theme i18n={i18n} showSection={section === '1'} />
       <Question i18n={i18n} showSection={section === '2'} />
       <Export i18n={i18n} showSection={section === '3'} />
-      {!isNaN(Number(section)) &&
+      {!isNaN(currentStep) &&
         <Navbar
-          currentStep={Number(section)}
+          currentStep={currentStep}
           totalSteps={3}
           phaseIdentifier="survey"
         />
