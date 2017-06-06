35f6be53f477a2245884a9343cac0b25065206af
paolina92 <paolinardc@gmail.com>
2017-06-06 15:58:44 +0200

    send null if there is no image

--- assembl/static2/js/app/components/administration/survey/themeSection.jsx
+++ assembl/static2/js/app/components/administration/survey/themeSection.jsx
@@ -42,7 +42,7 @@ const ThemeSection = ({ client, i18n, selectedLocale }) => {
     thematicsData.thematics.push({
       id: newThemeId,
       titleEntries: [],
-      imgUrl: '',
+      imgUrl: null,
       questions: [],
       video: [],
       __typename: 'Thematic'
