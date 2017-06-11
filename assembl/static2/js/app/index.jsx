894c22dd0b1b75cf203db26a9c4d5800ba81d69e
Vincent Fretin <vincent.fretin@gmail.com>
2017-06-11 12:56:18 +0200

    update react-hot-reloader and fix config, now js and css are hot reloaded in less than a second

--- assembl/static2/js/app/index.jsx
+++ assembl/static2/js/app/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 import { Router, browserHistory } from 'react-router';
 import { ApolloProvider } from 'react-apollo';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -9,9 +10,27 @@ import Routes from './routes';
 
 const store = createAppStore();
 
+
 ReactDOM.render(
-  <ApolloProvider store={store} client={client}>
-    <Router history={browserHistory} routes={Routes} />
-  </ApolloProvider>,
+  <AppContainer>
+    <ApolloProvider store={store} client={client}>
+      <Router history={browserHistory} routes={Routes} />
+    </ApolloProvider>
+  </AppContainer>,
   document.getElementById('root')
 );
+
+// Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./routes', () => {
+    const NewRoutes = require('./routes').default;  // eslint-disable-line
+    ReactDOM.render(
+      <AppContainer>
+        <ApolloProvider store={store} client={client}>
+          <Router history={browserHistory} routes={NewRoutes} />
+        </ApolloProvider>
+      </AppContainer>,
+      document.getElementById('root')
+    );
+  });
+}
