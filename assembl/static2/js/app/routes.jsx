84cdca6113056ef9b23f76809b88cba75b5973bb
Vincent Fretin <vincent.fretin@gmail.com>
2017-06-12 09:30:33 +0200

    no need to have Router twice (in index.jsx and in routes.jsx), return an array of Route in routes.jsx

--- assembl/static2/js/app/routes.jsx
+++ assembl/static2/js/app/routes.jsx
@@ -56,37 +56,35 @@ const AdminChild = (props) => {
   }
 };
 
-export default (
-  <Router>
-    <Route path="/" component={Root}>
-      <Route path={routeForRouter('styleguide', false, { preSlash: true })} component={Styleguide} />
-      {/* Those login routes should be kept in synchrony with assembl.views.auth.__init__.py */}
-      <Route path={routeForRouter('login', false, { preSlash: true })} component={Login} />
-      <Route path={routeForRouter('signup', false, { preSlash: true })} component={Signup} />
-      <Route path={routeForRouter('changePassword', false, { preSlash: true })} component={ChangePassword} />
-      <Route path={routeForRouter('requestPasswordChange')} component={RequestPasswordChange} />
+export default [
+  <Route path="/" component={Root}>
+    <Route path={routeForRouter('styleguide', false, { preSlash: true })} component={Styleguide} />
+    {/* Those login routes should be kept in synchrony with assembl.views.auth.__init__.py */}
+    <Route path={routeForRouter('login', false, { preSlash: true })} component={Login} />
+    <Route path={routeForRouter('signup', false, { preSlash: true })} component={Signup} />
+    <Route path={routeForRouter('changePassword', false, { preSlash: true })} component={ChangePassword} />
+    <Route path={routeForRouter('requestPasswordChange')} component={RequestPasswordChange} />
+    <Route component={App}>
       {/* These are contextual routes for the ones above */}
       <Route path={routeForRouter('login', true)} component={Login} />
       <Route path={routeForRouter('signup', true)} component={Signup} />
       <Route path={routeForRouter('changePassword', true)} component={ChangePassword} />
       <Route path={routeForRouter('requestPasswordChange', true)} component={RequestPasswordChange} />
-      <Route component={App}>
-        <Route component={Main}>
-          <Route path={routeForRouter('home')} component={Home} />
-          <Route path={routeForRouter('homeBare')} component={Home} />
-          <Route path={routeForRouter('profile', false, { userId: ':userId' })} component={Profile} />
-          <Route path={routeForRouter('synthesis')} component={Synthesis} />
-          <Route path={routeForRouter('community')} component={Community} />
-          <Route path={routeForRouter('terms')} component={Terms} />
-          <Route path={routeForRouter('debate')} component={Debate}>
-            <Route path={routeForRouter('phase', false, { phase: ':phase', themeId: ':themeId' })} component={DebateChild} />
-          </Route>
-          <Route path={routeForRouter('administration')} component={Administration} >
-            <Route path={routeForRouter('adminPhase', false, { phase: ':phase' })} component={AdminChild} />
-          </Route>
+      <Route component={Main}>
+        <Route path={routeForRouter('home')} component={Home} />
+        <Route path={routeForRouter('homeBare')} component={Home} />
+        <Route path={routeForRouter('profile', false, { userId: ':userId' })} component={Profile} />
+        <Route path={routeForRouter('synthesis')} component={Synthesis} />
+        <Route path={routeForRouter('community')} component={Community} />
+        <Route path={routeForRouter('terms')} component={Terms} />
+        <Route path={routeForRouter('debate')} component={Debate}>
+          <Route path={routeForRouter('phase', false, { phase: ':phase', themeId: ':themeId' })} component={DebateChild} />
+        </Route>
+        <Route path={routeForRouter('administration')} component={Administration} >
+          <Route path={routeForRouter('adminPhase', false, { phase: ':phase' })} component={AdminChild} />
         </Route>
       </Route>
     </Route>
-    <Route path="*" component={NotFound} />
-  </Router>
-);
+  </Route>,
+  <Route path="*" component={NotFound} />
+];
