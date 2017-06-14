da8cb837bd872ec5ae90424a6bb61736f4d9ce90
Vincent Fretin <vincent.fretin@gmail.com>
2017-06-14 14:27:08 +0200

    enable js sourcemap for prod, and js and sass sourcemaps for dev

--- assembl/static2/webpack.dev.config.js
+++ assembl/static2/webpack.dev.config.js
@@ -29,7 +29,7 @@ function theme_entries() {
 // For css hot reload to work, don't use ExtractTextPlugin
 module.exports = {
     devServer: {
-        devtool: 'eval',
+        devtool: '#cheap-module-eval-source-map',  // http://webpack.github.io/docs/configuration.html#devtool
         inline: true,
         hot: true,
         headers: {
@@ -70,7 +70,7 @@ module.exports = {
         },
         {
             test: /\.scss$/,
-            loaders: ['style', 'css', 'sass']
+            loader: 'style!css?sourceMap=true!sass?sourceMap=true'
         },
         {
             test: /\.css$/,
