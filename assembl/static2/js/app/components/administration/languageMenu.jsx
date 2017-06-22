f89a02df26a2413bb104ed0522ef0c3d386c93a7
paolina92 <paolinardc@gmail.com>
2017-06-22 12:34:21 +0200

    Oups.. forgot a tooltip

--- assembl/static2/js/app/components/administration/languageMenu.jsx
+++ assembl/static2/js/app/components/administration/languageMenu.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Translate } from 'react-redux-i18n';
+import { Tooltip, OverlayTrigger } from 'react-bootstrap';
 
 import { updateSelectedLocale } from '../../actions/adminActions';
 import En from '../svg/flags/en';
@@ -17,24 +19,34 @@ const Flag = ({ locale }) => {
   }
 };
 
+const languageTooltip = (
+  <Tooltip id="languageTooltip">
+    <Translate value="administration.changeLanguage" />
+  </Tooltip>
+);
+
 const LanguageMenu = ({ changeLocale, selectedLocale, translations }) => {
   return (
     <div className="relative">
       <div className="language-menu">
-        {Object.keys(translations).map((key, index) => {
-          return (
-            <div
-              onClick={() => {
-                return changeLocale(key);
-              }}
-              id={key}
-              className={selectedLocale === key ? 'flag-container active' : 'flag-container'}
-              key={index}
-            >
-              <Flag locale={key} />
-            </div>
-          );
-        })}
+        <OverlayTrigger placement="top" overlay={languageTooltip}>
+          <div>
+            {Object.keys(translations).map((key, index) => {
+              return (
+                <div
+                  onClick={() => {
+                    return changeLocale(key);
+                  }}
+                  id={key}
+                  className={selectedLocale === key ? 'flag-container active' : 'flag-container'}
+                  key={index}
+                >
+                  <Flag locale={key} />
+                </div>
+              );
+            })}
+          </div>
+        </OverlayTrigger>
       </div>
     </div>
   );
