ae5c6bfd2405454d81fdc5428c0fbe524cc05f89
paolina92 <paolinardc@gmail.com>
2017-07-05 11:48:54 +0200

    hide thumbnails navbar in the phase 2

--- assembl/static2/js/app/pages/debateThread.jsx
+++ assembl/static2/js/app/pages/debateThread.jsx
@@ -1,84 +1,52 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { graphql } from 'react-apollo';
 import { Translate } from 'react-redux-i18n';
+import { get } from '../utils/routeMap';
 import Themes from '../components/debate/common/themes';
 import Timeline from '../components/debate/navigation/timeline';
-import Thumbnails from '../components/debate/navigation/thumbnails';
 import RootIdeasQuery from '../graphql/RootIdeasQuery.graphql';
 
-class DebateThread extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isThumbnailsHidden: true
-    };
-    this.showThumbnails = this.showThumbnails.bind(this);
-    this.hideThumbnails = this.hideThumbnails.bind(this);
-    this.displayThumbnails = this.displayThumbnails.bind(this);
-  }
-  showThumbnails() {
-    this.setState({ isThumbnailsHidden: false });
-  }
-  hideThumbnails() {
-    setTimeout(() => {
-      this.setState({ isThumbnailsHidden: true });
-    }, 400);
-  }
-  displayThumbnails() {
-    this.setState({ isThumbnailsHidden: !this.state.isThumbnailsHidden });
-  }
-  render() {
-    const { rootIdea } = this.props.data;
-    const thematics = rootIdea ? rootIdea.children : [];
-    const { identifier, isNavbarHidden } = this.props;
-    const isParentRoute = !this.props.params.themeId || false;
-    const themeId = this.props.params.themeId || null;
-    const children = React.Children.map(this.props.children, (child) => {
-      return React.cloneElement(child, {
-        id: themeId,
-        identifier: identifier
-      });
+const DebateThread = ({ identifier, isNavbarHidden, data, params, children, slug }) => {
+  const thematics = data.rootIdea ? data.rootIdea.children : [];
+  const isParentRoute = !params.themeId || false;
+  const themeId = params.themeId || null;
+  const childrenElm = React.Children.map(children, (child) => {
+    return React.cloneElement(child, {
+      id: themeId,
+      identifier: identifier
     });
-    return (
-      <div className="debate">
-        {thematics &&
-          <div>
-            <section
-              className={isNavbarHidden ? 'timeline-section timeline-top' : 'timeline-section timeline-shifted'}
-              id="timeline"
-            >
-              <div className="max-container">
-                {!isParentRoute &&
-                  <div className="burger-menu grey" onMouseOver={this.showThumbnails} onClick={this.displayThumbnails}>
-                    <div className="assembl-icon-thumb" />
-                    <div className="burger-menu-label">
-                      <Translate value="debate.themes" />
-                    </div>
-                  </div>}
-                <Timeline showNavigation={!isParentRoute} identifier={identifier} />
-              </div>
-            </section>
-            {isParentRoute && <Themes thematics={thematics} identifier={identifier} />}
-            {!isParentRoute &&
-              <section className="debate-section">
-                <div className={this.state.isThumbnailsHidden ? 'hiddenThumb' : 'shown'} onMouseLeave={this.hideThumbnails}>
-                  <Thumbnails
-                    showNavigation={!isParentRoute}
-                    thematics={thematics}
-                    identifier={identifier}
-                    themeId={themeId}
-                    isNavbarHidden={isNavbarHidden}
-                  />
-                </div>
-                {children}
-              </section>}
-          </div>}
-      </div>
-    );
-  }
-}
+  });
+  return (
+    <div className="debate">
+      {thematics &&
+        <div>
+          <section
+            className={isNavbarHidden ? 'timeline-section timeline-top' : 'timeline-section timeline-shifted'}
+            id="timeline"
+          >
+            <div className="max-container">
+              {!isParentRoute &&
+                <Link className="burger-menu grey" to={get('debate', { slug: slug, phase: identifier })}>
+                  <div className="assembl-icon-thumb" />
+                  <div className="burger-menu-label">
+                    <Translate value="debate.themes" />
+                  </div>
+                </Link>}
+              <Timeline showNavigation={!isParentRoute} identifier={identifier} />
+            </div>
+          </section>
+          {isParentRoute && <Themes thematics={thematics} identifier={identifier} />}
+          {!isParentRoute &&
+            <section className="debate-section">
+              {childrenElm}
+            </section>}
+        </div>}
+    </div>
+  );
+};
 
 DebateThread.propTypes = {
   data: PropTypes.shape({
@@ -92,7 +60,8 @@ const DebateWithData = graphql(RootIdeasQuery)(DebateThread);
 
 const mapStateToProps = (state) => {
   return {
-    lang: state.i18n.locale
+    lang: state.i18n.locale,
+    slug: state.debate.debateData.slug
   };
 };
 
