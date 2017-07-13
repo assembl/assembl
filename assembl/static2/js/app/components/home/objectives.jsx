f18a67f17c2a10cd8c08efae7b4f8158d1cc8309
paolina92 <paolinardc@gmail.com>
2017-07-13 16:31:10 +0200

    Use correctly get method from routeMap

--- assembl/static2/js/app/components/home/objectives.jsx
+++ assembl/static2/js/app/components/home/objectives.jsx
@@ -28,13 +28,13 @@ class Objectives extends React.Component {
       if (isSeveralPhases) {
         displayModal(null, body, true, null, button, true);
         setTimeout(() => {
-          window.location = `${get('oldDebate', slug)}`;
+          window.location = get('oldDebate', slug);
         }, 6000);
       } else {
-        window.location = `${get('oldDebate', slug)}`;
+        window.location = get('oldDebate', slug);
       }
     } else {
-      browserHistory.push(`${get('debate', slug)}`);
+      browserHistory.push(get('debate', { ...slug, phase: currentPhaseIdentifier }));
     }
   }
   render() {
