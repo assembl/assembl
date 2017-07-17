d548b53d0bbf329141101d9cdddf789deb0ab64c
Marc-Antoine Parent <maparent@acm.org>
2017-07-17 17:39:45 -0400

    reenable wheels

--- fabfile.py
+++ fabfile.py
@@ -542,6 +542,8 @@ def update_pip_requirements(force_reinstall=False):
     if force_reinstall:
         cmd = "%(venvpath)s/bin/pip install --ignore-installed -r %(projectpath)s/requirements.txt" % env
     else:
+        # Thanks to https://github.com/pypa/pip/issues/4453 disable wheel separately.
+        run("egrep '^dm.xmlsec.binding' %(projectpath)s/requirements.txt | xargs %(venvpath)s/bin/pip install --install-option='-q'" % env)
         cmd = "%(venvpath)s/bin/pip install -r %(projectpath)s/requirements.txt" % env
         run("yes w | %s" % cmd)
 
