adeb6db484d458043f2c3dd57bc15edad8347e02
paolina92 <paolinardc@gmail.com>
2017-07-18 11:55:26 +0200

    Create a separate component to add a post actions (add sentiment, share, answer...)

--- assembl/static2/js/app/components/debate/thread/post.jsx
+++ assembl/static2/js/app/components/debate/thread/post.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Translate } from 'react-redux-i18n';
-import { Row, Col, Tooltip, OverlayTrigger } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import { createSelector } from 'reselect';
 
 import { updateActiveAnswerFormId } from '../../../actions/postsActions';
 import { postSelector } from '../../../selectors';
 import { getDomElementOffset, scrollToPosition } from '../../../utils/globalFunctions';
-import Like from '../../svg/like';
-import Disagree from '../../svg/disagree';
-import DontUnderstand from '../../svg/dontUnderstand';
-import MoreInfo from '../../svg/moreInfo';
 import ProfileLine from '../../common/profileLine';
+import PostActions from './postActions';
 import { SHOW_POST_RESPONSES } from '../../../constants';
 import AnswerForm from './answerForm';
 
@@ -25,62 +22,21 @@ export const PostFolded = ({ creator }) => {
   return <Translate value="debate.thread.foldedPostLink" creatorName={creator.name} />;
 };
 
-export const answerTooltip = (
-  <Tooltip id="answerTooltip">
-    <Translate value="debate.toAnswer" />
-  </Tooltip>
-);
-
-export const shareTooltip = (
-  <Tooltip id="shareTooltip">
-    <Translate value="debate.share" />
-  </Tooltip>
-);
-
-export const likeTooltip = (
-  <Tooltip id="likeTooltip">
-    <Translate value="debate.like" />
-  </Tooltip>
-);
-
-export const disagreeTooltip = (
-  <Tooltip id="disagreeTooltip">
-    <Translate value="debate.disagree" />
-  </Tooltip>
-);
-
-export const dontUnderstandTooltip = (
-  <Tooltip id="dontUnderstandTooltip">
-    <Translate value="debate.dontUnderstand" />
-  </Tooltip>
-);
-
-export const moreInfoTooltip = (
-  <Tooltip id="moreInfoTooltip">
-    <Translate value="debate.moreInfo" />
-  </Tooltip>
-);
-
 const Post = ({
   id,
   children,
   subject,
   body,
   indirectIdeaContentLinks,
-  mySentiment,
-  sentimentCounts,
   creator,
   creationDate,
   showAnswerForm,
   activeAnswerFormId,
   ideaId,
-  refetchIdea
+  refetchIdea,
+  sentimentCounts,
+  mySentiment
 }) => {
-  let count = 0;
-  const totalSentimentsCount = sentimentCounts
-    ? sentimentCounts.like + sentimentCounts.disagree + sentimentCounts.dontUnderstand + sentimentCounts.moreInfo
-    : 0;
-
   const handleAnswerClick = () => {
     showAnswerForm(id);
     setTimeout(() => {
@@ -112,85 +68,12 @@ const Post = ({
             </div>
           </Col>
           <Col xs={12} md={1} className="post-right">
-            <div className="post-icons">
-              <div className="post-action" onClick={handleAnswerClick}>
-                <OverlayTrigger placement="right" overlay={answerTooltip}>
-                  <span className="assembl-icon-back-arrow color" />
-                </OverlayTrigger>
-              </div>
-              <div className="post-action">
-                <OverlayTrigger placement="right" overlay={shareTooltip}>
-                  <span className="assembl-icon-share color" />
-                </OverlayTrigger>
-              </div>
-              <div className="add-sentiment">
-                <OverlayTrigger placement="right" overlay={likeTooltip}>
-                  <div className={mySentiment === 'LIKE' ? 'sentiment sentiment-active' : 'sentiment'}>
-                    <Like size={25} />
-                  </div>
-                </OverlayTrigger>
-                <OverlayTrigger placement="right" overlay={disagreeTooltip}>
-                  <div className={mySentiment === 'DISAGREE' ? 'sentiment sentiment-active' : 'sentiment'}>
-                    <Disagree size={25} />
-                  </div>
-                </OverlayTrigger>
-                <OverlayTrigger placement="right" overlay={dontUnderstandTooltip}>
-                  <div className={mySentiment === 'DONT_UNDERSTAND' ? 'sentiment sentiment-active' : 'sentiment'}>
-                    <DontUnderstand size={25} />
-                  </div>
-                </OverlayTrigger>
-                <OverlayTrigger placement="right" overlay={moreInfoTooltip}>
-                  <div className={mySentiment === 'MORE_INFO' ? 'sentiment sentiment-active' : 'sentiment'}>
-                    <MoreInfo size={25} />
-                  </div>
-                </OverlayTrigger>
-              </div>
-            </div>
-            {totalSentimentsCount > 0 &&
-              <div className="sentiments-count margin-m">
-                <div>
-                  <div>
-                    {Object.keys(sentimentCounts).map((sentiment, index) => {
-                      if (sentimentCounts[sentiment] > 0 && sentiment === 'like') {
-                        return (
-                          <div className="min-sentiment" key={index} style={{ left: `${(count += 1 * 6)}px` }}>
-                            <Like size={15} />
-                          </div>
-                        );
-                      }
-                      if (sentimentCounts[sentiment] > 0 && sentiment === 'disagree') {
-                        return (
-                          <div className="min-sentiment" key={index} style={{ left: `${(count += 1 * 6)}px` }}>
-                            <Disagree size={15} />
-                          </div>
-                        );
-                      }
-                      if (sentimentCounts[sentiment] > 0 && sentiment === 'dontUnderstand') {
-                        return (
-                          <div className="min-sentiment" key={index} style={{ left: `${(count += 1 * 6)}px` }}>
-                            <DontUnderstand size={15} />
-                          </div>
-                        );
-                      }
-                      if (sentimentCounts[sentiment] > 0 && sentiment === 'moreInfo') {
-                        return (
-                          <div className="min-sentiment" key={index} style={{ left: `${(count += 1 * 6)}px` }}>
-                            <MoreInfo size={15} />
-                          </div>
-                        );
-                      }
-                      return null;
-                    })}
-                  </div>
-                </div>
-                <div className="txt">
-                  {totalSentimentsCount}
-                </div>
-              </div>}
-            <div className="answers annotation">
-              <Translate value="debate.thread.numberOfResponses" count={children ? children.length : 0} />
-            </div>
-            <div className="clear">&nbsp;</div>
+            <PostActions
+              handleAnswerClick={handleAnswerClick}
+              sentimentCounts={sentimentCounts}
+              mySentiment={mySentiment}
+              postChildren={children}
+            />
           </Col>
         </Row>
       </div>
