d5ef450b003651a87955d285d99cdd79dd0ec241
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2017-07-17 23:16:29 -0700

    Update all display views of videos

--- assembl/static2/js/app/components/home/video.jsx
+++ assembl/static2/js/app/components/home/video.jsx
@@ -36,15 +36,19 @@ class Video extends React.Component {
             <div className="content-section">
               <div className="content-margin">
                 <Row>
-                  {debateData.video.descriptionEntries[locale] &&
+                  {debateData.video.descriptionEntriesTop[locale] &&
                     <Col xs={12} md={6} className={this.state.isTextHigher ? 'col-bottom' : ''}>
-                      <div className="text" id="video-txt">{debateData.video.descriptionEntries[locale]}</div>
+                      <div className="text" id="video-txt">{debateData.video.descriptionEntriesTop[locale]}</div>
                     </Col>}
                   {debateData.video.videoUrl &&
                     <Col xs={12} md={6} className={this.state.isTextHigher ? 'col-bottom' : ''}>
                       <div className="video-container" id="video-vid">
                         <iframe src={debateData.video.videoUrl} frameBorder="0" width="560" height="315" />
                       </div>
+                    </Col>}
+                  {debateData.video.descriptionEntriesBottom[locale] &&
+                    <Col xs={12} md={6} className={this.state.isTextHigher ? 'col-bottom' : ''}>
+                      <div className="text" id="video-txt">{debateData.video.descriptionEntriesBottom[locale]}</div>
                     </Col>}
                 </Row>
               </div>
