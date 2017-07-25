0789e72f1c34b2c39b4429fc2e4d596c9eca4f1e
Vincent Fretin <vincent.fretin@gmail.com>
2017-07-25 14:06:36 +0200

    adapt and fix tests of PostFolded component

--- assembl/static2/tests/unit/components/debate/thread/post.spec.jsx
+++ assembl/static2/tests/unit/components/debate/thread/post.spec.jsx
@@ -4,14 +4,15 @@ import ShallowRenderer from 'react-test-renderer/shallow';
 import { PostFolded } from '../../../../../js/app/components/debate/thread/post';
 
 describe('PostFolded component', () => {
-  it('should render an folded post uploader', () => {
+  it('should render a folded post', () => {
     const renderer = new ShallowRenderer();
-    const props = {
-      creator: {
-        name: 'John Doe'
-      }
-    };
-    renderer.render(<PostFolded {...props} />);
+    renderer.render(<PostFolded nbPosts={1} />);
+    const result = renderer.getRenderOutput();
+    expect(result).toMatchSnapshot();
+  });
+  it('should render several folded posts', () => {
+    const renderer = new ShallowRenderer();
+    renderer.render(<PostFolded nbPosts={3} />);
     const result = renderer.getRenderOutput();
     expect(result).toMatchSnapshot();
   });
