04b04a885ba3b700bccd4faa0eb15680167b41d4
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2017-07-27 18:55:25 -0700

    Fix the issue of sharing side and top quote langstrings on migration

--- assembl/alembic/versions/9218849f9579_video_desc_extended.py
+++ assembl/alembic/versions/9218849f9579_video_desc_extended.py
@@ -18,6 +18,19 @@ from assembl.lib.sqla import mark_changed
 
 from assembl.lib import config
 
+# Do stuff with the app's models here.
+from assembl import models
+db = models.get_session_maker()()
+
+
+def put_new_langstring(thematic, attr):
+    langstring = getattr(thematic, attr)
+    new_langstring = models.LangString()
+    old_entries = langstring.entries
+    for entry in old_entries:
+        entry.clone(new_langstring, db=db)
+    return new_langstring
+
 
 def upgrade(pyramid_env):
     with context.begin_transaction():
@@ -27,7 +40,16 @@ def upgrade(pyramid_env):
         )
 
     with transaction.manager:
-        op.execute("UPDATE thematic SET video_description_side_id=video_description_top_id")
+        current_top_langstrings = [x for x
+                                   in db.query(models.Thematic).all()
+                                   if x.video_description_top is not None]
+
+        new_langstrings = map(lambda x: put_new_langstring(x,
+                              'video_description_top'),
+                              current_top_langstrings)
+        for new_langstring in new_langstrings:
+            db.add(new_langstring)
+
         mark_changed()
 
 
