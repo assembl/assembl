adf1af93ca22d3ef458a5f7094990cf386d14c48
Vincent Fretin <vincent.fretin@gmail.com>
2017-08-07 14:11:28 +0200

    fallback to English if translation not found instead of formatting the translation key

--- assembl/static2/js/app/utils/globalFunctions.js
+++ assembl/static2/js/app/utils/globalFunctions.js
@@ -1,8 +1,23 @@
+import { I18n } from 'react-redux-i18n';
+
 import deepen from './deepen';
 import Translations from './translations';
 import ja from '../../../translations/ja.json';
 
 Translations.ja = deepen(ja);
+
+const fallbackLocale = 'en';
+const myHandleMissingTranslation = function (key, replacements) {
+  // We need to use a function, not a arrow function here to be able to use 'this'.
+  let translation = '';
+  try {
+    translation = this._fetchTranslation(this._translations, `${fallbackLocale}.${key}`, replacements.count); // eslint-disable-line
+  } catch (err) {
+    return `Missing translation: ${key}`;
+  }
+  return this._replace(translation, replacements); // eslint-disable-line
+};
+I18n.setHandleMissingTranslation(myHandleMissingTranslation);
 
 export const getTranslations = () => {
   return Translations;
