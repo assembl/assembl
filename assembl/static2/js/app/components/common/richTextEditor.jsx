a0bbd9473b620bd9243ed4493f1ec8b0961d691b
Cédric Messiant <cedricmessiant@ecreall.com>
2017-08-07 10:48:42 +0200

    Add bold and italic to rich text editor

--- assembl/static2/js/app/components/common/richTextEditor.jsx
+++ assembl/static2/js/app/components/common/richTextEditor.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import { Translate } from 'react-redux-i18n';
-import { Editor } from 'draft-js';
+import { Editor, RichUtils } from 'draft-js';
 import punycode from 'punycode';
 
 export default class RichTextEditor extends React.PureComponent {
   constructor() {
     super();
     this.onChange = this.onChange.bind(this);
+    this.onBoldClick = this.onBoldClick.bind(this);
+    this.onItalicClick = this.onItalicClick.bind(this);
   }
 
   onChange(newEditorState) {
     const { updateEditorState } = this.props;
     updateEditorState(newEditorState);
+  }
+
+  onBoldClick() {
+    const { editorState } = this.props;
+    this.onChange(RichUtils.toggleInlineStyle(editorState, 'BOLD'));
+  }
+
+  onItalicClick() {
+    const { editorState } = this.props;
+    this.onChange(RichUtils.toggleInlineStyle(editorState, 'ITALIC'));
   }
 
   getCharCount(editorState) {
@@ -31,6 +44,14 @@ export default class RichTextEditor extends React.PureComponent {
     const remainingChars = maxLength - charCount;
     return (
       <div className="rich-text-editor">
+        <div className="editor-toolbar">
+          <Button onClick={this.onBoldClick}>
+            <span className="assembl-icon-text-bold" />
+          </Button>
+          <Button onClick={this.onItalicClick}>
+            <span className="assembl-icon-text-italics" />
+          </Button>
+        </div>
         <Editor editorState={editorState} onChange={this.onChange} placeholder={placeholder} />
         <div className="annotation margin-xs">
           <Translate value="debate.remaining_x_characters" nbCharacters={remainingChars < 10000 ? remainingChars : maxLength} />
