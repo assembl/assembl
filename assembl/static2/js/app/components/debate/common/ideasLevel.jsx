8a1c3905bf9a5f4b4545e43ae5dca014e801b047
Swergas <swergas@yahoo.fr>
2017-08-10 15:37:34 +0200

    first iteration on transition animation between mutliline and single line presentation of ideas

--- assembl/static2/js/app/components/debate/common/ideasLevel.jsx
+++ assembl/static2/js/app/components/debate/common/ideasLevel.jsx
@@ -11,13 +11,14 @@ class IdeasLevel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isAnimatingTowardsInline: false,
       isInline: props.isInline || false,
       selectedIdea: props.selectedIdea || null,
-      isScrollLeftButtonVisible: props.isInline || props.isAnimatingTowardsInline || false,
-      isScrollRightButtonVisible: props.isInline || props.isAnimatingTowardsInline || false
+      isScrollLeftButtonVisible: props.isInline || false,
+      isScrollRightButtonVisible: props.isInline || false
     };
     this.options = {};
+    this.setOption('selectedIdea', props.selectedIdea || null);
+
     this.initializeConstants();
 
     this.onSeeSubIdeasClick = this.onSeeSubIdeasClick.bind(this);
@@ -25,34 +26,128 @@ class IdeasLevel extends React.Component {
     this.moveAllChildren = this.moveAllChildren.bind(this);
     this.onScrollRightClick = this.onScrollRightClick.bind(this);
     this.onScrollLeftClick = this.onScrollLeftClick.bind(this);
+    this.scrollToIdeaIndexIfNecessary = this.scrollToIdeaIndexIfNecessary.bind(this);
   }
-  onSeeSubIdeasClick(ideaId, index) {
-    const wasInline = this.state.isInline || this.state.isAnimatingTowardsInline;
-    this.setState({ isAnimatingTowardsInline: true });
-    this.setState({ selectedIdea: ideaId });
+
+  onSeeSubIdeasClick(ideaId, index, step = 0) {
+    const wasInline = this.state.isInline || this.getOption('isAnimatingTowardsInline');
+
+    if (!wasInline) {
+      const goNextStep = () => {
+        this.onSeeSubIdeasClick(ideaId, index, step + 1);
+      };
+      goNextStep.bind(this);
+
+      if (!step) {
+        /*
+        Animation step 0:
+          - Animate ideas top margins from various values to 0
+          - Set row width to its current width, in anticipation of future animation
+          - Set ideas left position to current 0, in anticipation of future scroll animation
+        */
+        const row = this.getRow();
+        row.style.width = `${row.clientWidth}px`;
+        row.style.transition = 'width 0.5s';
+        row.childNodes.forEach((el) => {
+          const el2 = el;
+          el2.style.marginTop = '0';
+          el2.style.left = '0';
+        });
+
+        setTimeout(goNextStep, 500);
+        return;
+      } else if (step === 1) {
+        /*
+        Animation step 1:
+          - Set ideas positioning to a single row (set row display to inline-flex)
+          - Set necessary values consequently to inline-flex, for animation consistency
+        */
+        this.initializeConstants();
+        const row = this.getRow();
+        row.childNodes.forEach((el) => {
+          const el2 = el;
+          el2.style.flexShrink = '0';
+          el2.style.width = `${this.getOption('thematicWidth')}px`;
+        });
+        row.style.overflowX = 'hidden';
+        row.style.display = 'inline-flex';
+
+        setTimeout(goNextStep, 50);
+        return;
+      } else if (step === 2) {
+        /*
+        Animation step 2:
+          - Animate carousel from full page width to custom value
+        */
+
+        const row = this.getRow();
+        row.style.width = `${this.getOption('carouselWidth')}px`;
+
+        this.scrollToIdeaIndexIfNecessary(index);
+
+        const f = () => {
+          this.onSeeSubIdeasClick(ideaId, index, step + 1);
+          this.forceUpdate();
+        };
+        f.bind(this);
+
+        setTimeout(f, 500);
+
+        return;
+      }
+    }
+
+    if (wasInline && ideaId === this.getOption('selectedIdea')) {
+      this.goMultiline();
+      return;
+    }
+
+    this.setOption('isAnimatingTowardsInline', true);
+    this.me.classList.add('animating-towards-inline');
+
+    this.setOption('selectedIdea', ideaId);
+
     this.initializeConstants();
 
     // make sure that the clicked thematic shows fully when inline (scroll at the correct position so that it is shown)
     if (!wasInline) {
-      const thematicWidth = this.getOption('thematicWidth');
-      const carouselWidth = this.getOption('carouselWidth');
-      const ideaInitialX = index * thematicWidth;
-      if (ideaInitialX + thematicWidth > carouselWidth) {
-        const numberOfIdeasFullyShown = Math.floor(carouselWidth / thematicWidth);
-        const middleIdeaX = Math.floor(numberOfIdeasFullyShown / 2) * thematicWidth;
-        const targetScrollValue = -1 * (index * thematicWidth - middleIdeaX);
-        const row = this.me.childNodes[1];
-        this.moveAllChildren(row, targetScrollValue);
-      }
+      this.scrollToIdeaIndexIfNecessary(index);
     }
     this.updateScrollButtonsVisibility(this.state.isInline, true);
   }
+
+  onScrollRightClick() {
+    this.moveAllChildren(this.getRow(), -1 * this.getOption('animationDistance'));
+  }
+
+  onScrollLeftClick() {
+    this.moveAllChildren(this.getRow(), this.getOption('animationDistance'));
+  }
+
+  getRow() {
+    return this.me.childNodes[1];
+  }
+
   getOption(name) {
     return getValue(this, ['options', name], null);
   }
+
   setOption(name, value) {
     this.options[name] = value;
   }
+
+  scrollToIdeaIndexIfNecessary(index) {
+    const thematicWidth = this.getOption('thematicWidth');
+    const carouselWidth = this.getOption('carouselWidth');
+    const ideaInitialX = index * thematicWidth;
+    if (ideaInitialX + thematicWidth > carouselWidth) {
+      const numberOfIdeasFullyShown = Math.floor(carouselWidth / thematicWidth);
+      const middleIdeaX = Math.floor(numberOfIdeasFullyShown / 2) * thematicWidth;
+      const targetScrollValue = -1 * (index * thematicWidth - middleIdeaX);
+      this.moveAllChildren(this.getRow(), targetScrollValue, true);
+    }
+  }
+
   updateScrollButtonsVisibility(isInline, isAnimatingTowardsInline) {
     if (!(isInline || isAnimatingTowardsInline)) {
       this.scrollLeft.setState({ isVisible: false });
@@ -74,6 +169,7 @@ class IdeasLevel extends React.Component {
       }
     }
   }
+
   initializeConstants() {
     const carouselParentWidth = 1400; // TODO: read dynamically from DOM
     const scrollDisplacement = this.getOption('scrollDisplacement') || 0;
@@ -95,16 +191,15 @@ class IdeasLevel extends React.Component {
     const displacementMin = -1.0 * (len * thematicWidth - carouselTargetWidth);
     const displacementMax = 0;
 
-    this.options = {
-      scrollDisplacement: scrollDisplacement,
-      thematicWidth: thematicWidth,
-      animationDistance: thematicWidth,
-      carouselWidth: carouselTargetWidth,
-      displacementMin: displacementMin,
-      displacementMax: displacementMax
-    };
+    this.setOption('scrollDisplacement', scrollDisplacement);
+    this.setOption('thematicWidth', thematicWidth);
+    this.setOption('animationDistance', thematicWidth);
+    this.setOption('carouselWidth', carouselTargetWidth);
+    this.setOption('displacementMin', displacementMin);
+    this.setOption('displacementMax', displacementMax);
   }
-  moveAllChildren(element, distance) {
+
+  moveAllChildren(element, distance, absolute) {
     let targetValue = 0;
     let currentValue = 0;
     const displacementMin = this.getOption('displacementMin');
@@ -113,7 +208,7 @@ class IdeasLevel extends React.Component {
       const el0 = element.childNodes[0];
       currentValue = el0.style.left;
       let targetValueInt = 0;
-      if (!currentValue) {
+      if (!currentValue || absolute) {
         targetValueInt = distance;
       } else {
         targetValueInt = parseInt(currentValue, 10) + distance;
@@ -140,24 +235,17 @@ class IdeasLevel extends React.Component {
         }
       });
 
-      this.updateScrollButtonsVisibility(this.state.isInline, this.state.isAnimatingTowardsInline);
+      this.updateScrollButtonsVisibility(this.state.isInline, this.getOption('isAnimatingTowardsInline'));
     }
   }
-  onScrollRightClick(ev) {
-    const row = ev.target.parentElement.parentElement.children[1];
-    this.moveAllChildren(row, -1 * this.getOption('animationDistance'));
-  }
-  onScrollLeftClick(ev) {
-    const row = ev.target.parentElement.parentElement.children[1];
-    this.moveAllChildren(row, this.getOption('animationDistance'));
-  }
+
   render() {
     const { thematics, identifier, onSeeSubIdeasClick, level } = this.props;
     const { isScrollLeftButtonVisible, isScrollRightButtonVisible } = this.state;
-    const { selectedIdea } = this.state;
+    const selectedIdea = this.getOption('selectedIdea');
     const slug = getDiscussionSlug();
     let classNames = ['ideas-level'];
-    if (this.state.isAnimatingTowardsInline) {
+    if (this.getOption('isAnimatingTowardsInline')) {
       classNames.push('animating-towards-inline');
     }
     if (this.state.isInline) {
@@ -168,7 +256,7 @@ class IdeasLevel extends React.Component {
     classNames = classNames.join(' ');
 
     const style = {};
-    if (this.state.isInline || this.state.isAnimatingTowardsInline) {
+    if (this.state.isInline || this.getOption('isAnimatingTowardsInline')) {
       style.width = this.getOption('carouselWidth');
     }
 
