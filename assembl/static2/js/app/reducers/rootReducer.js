7da17e23ef4efe3e947ee24d5584b4b4e5653af1
Vincent Fretin <vincent.fretin@gmail.com>
2017-08-12 09:39:38 +0200

    Don't use the same redux store for apollo cache to avoid unnecessary mapStateToProps calls when a query is done. In next major apollo-client version, there is no redux integration anyway.

--- assembl/static2/js/app/reducers/rootReducer.js
+++ assembl/static2/js/app/reducers/rootReducer.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import { i18nReducer } from 'react-redux-i18n';
-import client from '../client';
 import Context from './contextReducer';
 import Debate from './debateReducer';
 import Posts from './postsReducer';
@@ -11,7 +10,6 @@ import Phase from './phaseReducer';
 import Admin from './adminReducer';
 
 export default combineReducers({
-  apollo: client.reducer(),
   i18n: i18nReducer,
   context: Context,
   debate: Debate,
