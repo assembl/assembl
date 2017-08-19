d2e76f68b066d8f5225cc6a2c3a7b0727e0341cd
Vincent Fretin <vincent.fretin@gmail.com>
2017-08-19 11:46:51 +0200

    make InfiniteSeparator a pure component

--- assembl/static2/js/app/components/common/infiniteSeparator.jsx
+++ assembl/static2/js/app/components/common/infiniteSeparator.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const InfiniteSeparator = () => {
-  return (
-    <div className="infinite-separator">
-      <span className="assembl-icon-symbol-assembl color" />
-    </div>
-  );
-};
+class InfiniteSeparator extends React.PureComponent {
+  render() {
+    return (
+      <div className="infinite-separator">
+        <span className="assembl-icon-symbol-assembl color" />
+      </div>
+    );
+  }
+}
 
 export default InfiniteSeparator;
