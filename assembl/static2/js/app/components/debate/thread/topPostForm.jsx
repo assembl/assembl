805226b078ada04ef5fc6c736073ee9e0d8aca50
Cédric Messiant <cedricmessiant@ecreall.com>
2017-08-21 10:31:01 +0200

    Use local state for top form too

--- assembl/static2/js/app/components/debate/thread/topPostForm.jsx
+++ assembl/static2/js/app/components/debate/thread/topPostForm.jsx
@@ -1,14 +1,11 @@
 // FIXME: cancel button
 import React from 'react';
-import { PropTypes } from 'prop-types';
-import { connect } from 'react-redux';
 import { graphql } from 'react-apollo';
 import { Row, Col, FormGroup, Button } from 'react-bootstrap';
 import { I18n, Translate } from 'react-redux-i18n';
 
 import createPostMutation from '../../../graphql/mutations/createPost.graphql';
-import { updateTopPostFormStatus, updateTopPostSubject, updateTopPostBody } from '../../../actions/postsActions';
-import { convertRawContentStateToHTML } from '../../../utils/draftjs';
+import { convertRawContentStateToHTML, rawContentStateIsEmpty } from '../../../utils/draftjs';
 import { displayAlert, inviteUserToLogin } from '../../../utils/utilityManager';
 import { getConnectedUserId } from '../../../utils/globalFunctions';
 import { TextInputWithRemainingChars } from '../../common/textInputWithRemainingChars';
@@ -17,19 +14,33 @@ import RichTextEditor from '../../common/richTextEditor';
 export const TEXT_INPUT_MAX_LENGTH = 140;
 export const TEXT_AREA_MAX_LENGTH = 3000;
 
-const TopPostForm = ({ ideaId, subject, updateSubject, body, updateBody, updateFormStatus, isFormActive, mutate, refetchIdea }) => {
-  const displayForm = (isActive) => {
-    return updateFormStatus(isActive);
+class TopPostForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      body: null,
+      subject: '',
+      isActive: false
+    };
+  }
+
+  displayForm = (isActive) => {
+    this.setState({
+      isActive: isActive
+    });
   };
 
-  const resetForm = () => {
-    displayForm(false);
-    updateSubject('');
-    updateBody(null);
+  resetForm = () => {
+    this.displayForm(false);
+    this.setState({ subject: '' });
+    this.setState({ body: null });
   };
 
-  const createTopPost = () => {
-    if (subject && body) {
+  createTopPost = () => {
+    const { ideaId, mutate, refetchIdea } = this.props;
+    const { body, subject } = this.state;
+    const bodyIsEmpty = !body || rawContentStateIsEmpty(body);
+    if (subject && !bodyIsEmpty) {
       const variables = {
         ideaId: ideaId,
         subject: subject,
@@ -40,109 +51,91 @@ const TopPostForm = ({ ideaId, subject, updateSubject, body, updateBody, updateF
         .then(() => {
           refetchIdea();
           displayAlert('success', I18n.t('debate.thread.postSuccess'));
-          resetForm();
+          this.resetForm();
         })
         .catch((error) => {
           displayAlert('danger', error);
         });
     } else if (!subject) {
       displayAlert('warning', I18n.t('debate.thread.fillSubject'));
-    } else if (!body) {
+    } else if (bodyIsEmpty) {
       displayAlert('warning', I18n.t('debate.thread.fillBody'));
     }
   };
 
-  const handleInputFocus = () => {
+  handleInputFocus = () => {
     const isUserConnected = getConnectedUserId(); // TO DO put isUserConnected in the store
     if (!isUserConnected) {
       inviteUserToLogin();
     } else {
-      displayForm(true);
+      this.displayForm(true);
     }
   };
 
-  const handleSubjectChange = (e) => {
-    updateSubject(e.target.value);
+  updateBody = (newValue) => {
+    this.setState({
+      body: newValue
+    });
   };
 
-  return (
-    <Row>
-      <Col xs={0} sm={1} md={2} />
-      <Col xs={12} sm={3} md={2} className="no-padding">
-        <div className="start-discussion-container">
-          <div className="start-discussion-icon">
-            <span className="assembl-icon-discussion color" />
-          </div>
-          <div className="start-discussion">
-            <h3 className="dark-title-3 no-margin">
-              <Translate value="debate.thread.startDiscussion" />
-            </h3>
-          </div>
-        </div>
-      </Col>
-      <Col xs={12} sm={7} md={6} className="no-padding">
-        <div className="form-container">
-          <FormGroup>
-            <TextInputWithRemainingChars
-              value={subject}
-              label={I18n.t('debate.subject')}
-              maxLength={TEXT_INPUT_MAX_LENGTH}
-              handleTxtChange={handleSubjectChange}
-              handleInputFocus={handleInputFocus}
-            />
-            <div className={isFormActive ? 'margin-m' : 'hidden'}>
-              <RichTextEditor
-                rawContentState={body}
-                handleInputFocus={handleInputFocus}
-                maxLength={TEXT_AREA_MAX_LENGTH}
-                placeholder={I18n.t('debate.insert')}
-                updateContentState={updateBody}
-              />
-              <Button className="button-cancel button-dark btn btn-default left margin-l" onClick={resetForm}>
-                <Translate value="cancel" />
-              </Button>
-              <Button
-                className="button-submit button-dark btn btn-default right margin-l"
-                onClick={createTopPost}
-                style={{ marginBottom: '30px' }}
-              >
-                <Translate value="debate.post" />
-              </Button>
+  handleSubjectChange = (e) => {
+    this.setState({
+      subject: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <Row>
+        <Col xs={0} sm={1} md={2} />
+        <Col xs={12} sm={3} md={2} className="no-padding">
+          <div className="start-discussion-container">
+            <div className="start-discussion-icon">
+              <span className="assembl-icon-discussion color" />
             </div>
-          </FormGroup>
-        </div>
-      </Col>
-      <Col xs={0} sm={1} md={2} />
-    </Row>
-  );
-};
+            <div className="start-discussion">
+              <h3 className="dark-title-3 no-margin">
+                <Translate value="debate.thread.startDiscussion" />
+              </h3>
+            </div>
+          </div>
+        </Col>
+        <Col xs={12} sm={7} md={6} className="no-padding">
+          <div className="form-container">
+            <FormGroup>
+              <TextInputWithRemainingChars
+                value={this.state.subject}
+                label={I18n.t('debate.subject')}
+                maxLength={TEXT_INPUT_MAX_LENGTH}
+                handleTxtChange={this.handleSubjectChange}
+                handleInputFocus={this.handleInputFocus}
+              />
+              <div className={this.state.isActive ? 'margin-m' : 'hidden'}>
+                <RichTextEditor
+                  rawContentState={this.state.body}
+                  handleInputFocus={this.handleInputFocus}
+                  maxLength={TEXT_AREA_MAX_LENGTH}
+                  placeholder={I18n.t('debate.insert')}
+                  updateContentState={this.updateBody}
+                />
+                <Button className="button-cancel button-dark btn btn-default left margin-l" onClick={this.resetForm}>
+                  <Translate value="cancel" />
+                </Button>
+                <Button
+                  className="button-submit button-dark btn btn-default right margin-l"
+                  onClick={this.createTopPost}
+                  style={{ marginBottom: '30px' }}
+                >
+                  <Translate value="debate.post" />
+                </Button>
+              </div>
+            </FormGroup>
+          </div>
+        </Col>
+        <Col xs={0} sm={1} md={2} />
+      </Row>
+    );
+  }
+}
 
-const mapStateToProps = ({ posts }) => {
-  return {
-    subject: posts.topPostSubject,
-    body: posts.topPostBody,
-    isFormActive: posts.topPostFormStatus
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    updateFormStatus: (isFormActive) => {
-      return dispatch(updateTopPostFormStatus(isFormActive));
-    },
-    updateSubject: (subject) => {
-      return dispatch(updateTopPostSubject(subject));
-    },
-    updateBody: (body) => {
-      return dispatch(updateTopPostBody(body));
-    }
-  };
-};
-
-TopPostForm.propTypes = {
-  mutate: PropTypes.func.isRequired
-};
-
-const TopPostFormWithMutation = graphql(createPostMutation)(TopPostForm);
-
-export default connect(mapStateToProps, mapDispatchToProps)(TopPostFormWithMutation);
+export default graphql(createPostMutation)(TopPostForm);
--- assembl/static2/js/app/reducers/rootReducer.js
+++ assembl/static2/js/app/reducers/rootReducer.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import { i18nReducer } from 'react-redux-i18n';
 import Context from './contextReducer';
 import Debate from './debateReducer';
-import Posts from './postsReducer';
 import Partners from './partnersReducer';
 import Synthesis from './synthesisReducer';
 import Auth from './authenticationReducer';
@@ -13,7 +12,6 @@ export default combineReducers({
   i18n: i18nReducer,
   context: Context,
   debate: Debate,
-  posts: Posts,
   partners: Partners,
   synthesis: Synthesis,
   auth: Auth,
