2503abcfec71433a24a77c5f99820c930b6e42d2
Vincent Fretin <vincent.fretin@gmail.com>
2017-08-23 18:35:18 +0200

    use smOffset and mdOffset instead of empty column

--- assembl/static2/js/app/components/debate/thread/topPostForm.jsx
+++ assembl/static2/js/app/components/debate/thread/topPostForm.jsx
@@ -92,8 +92,7 @@ class TopPostForm extends React.Component {
   render() {
     return (
       <Row>
-        <Col xs={0} sm={1} md={2} />
-        <Col xs={12} sm={3} md={2} className="no-padding">
+        <Col xs={12} sm={3} md={2} smOffset={1} mdOffset={2} className="no-padding">
           <div className="start-discussion-container">
             <div className="start-discussion-icon">
               <span className="assembl-icon-discussion color" />
