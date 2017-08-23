4d43993aaad01b36fd63140b7c58c479ce878af5
Marc-Antoine Parent <maparent@acm.org>
2017-08-23 06:31:21 -0400

    an html cleaner that does not destroy content, and does not add tags.

--- assembl/lib/clean_input.py
+++ assembl/lib/clean_input.py
@@ -1,6 +1,5 @@
 from lxml import html
 from lxml.html.clean import Cleaner
-from bs4 import BeautifulSoup
 
 VALID_TAGS = ['a',
               'b',
@@ -60,19 +59,73 @@ def _clean_html(html_value, cleaner):
             yield f
 
 
-def sanitize_html_keep(html_value, valid_tags, valid_attributes=VALID_ATTRIBUTES):
-    """Remove unwanted tags. Unlike other cleaners like bleach, keep those tag's contents."""
-    soup = BeautifulSoup(html_value)
-    for tag in soup.find_all(True):
-        if tag.name not in valid_tags:
-            tag.hidden = True
-            if tag.name in ('p', 'br'):
-                tag.append('\n')
-        else: # it might have bad attributes
-            for attr in tag.attrs.keys():
-                if attr not in valid_attributes:
-                    del tag[attr]
-    return soup.decode_contents()
+def lxml_remove_tag(tree, index):
+    # remove a tag from a tree fragment without removing its content
+    children = tree.getchildren()
+    element = children[index]
+    tree.remove(element)
+    if element.text:
+        if index:
+            text_target = children[index - 1]
+            text_target.tail = (text_target.tail or '') + element.text
+        else:
+            tree.text = (tree.text or '') + element.text
+    for sub in element.iterchildren():
+        tree.insert(index, sub)
+        index += 1
+    tail = element.tail or ''
+    if element.tag in ('p', 'br'):
+        tail += '\n'
+    if tail:
+        if index:
+            text_target = tree.getchildren()[index - 1]
+            text_target.tail = (text_target.tail or '') + tail
+        else:
+            tree.text = (tree.text or '') + tail
+    return index
+
+
+def clean_attributes(node, valid_attributes):
+    for attname in node.attrib.keys():
+        if attname not in valid_attributes:
+            del node.attrib[attname]
+
+
+def sanitize_html_rec(fragment, valid_tags, valid_attributes):
+    i = 0
+    while i < len(fragment):
+        child = fragment.getchildren()[i]
+        sanitize_html_rec(child, valid_tags, valid_attributes)
+        if child.tag in valid_tags:
+            clean_attributes(child, valid_attributes)
+            i += 1
+        else:
+            i = lxml_remove_tag(fragment, i)
+
+
+def sanitize_html_frags(html_value, valid_tags, valid_attributes):
+    fragments = html.fragments_fromstring(html_value)
+    for f in fragments:
+        if isinstance(f, html.HtmlElement):
+            sanitize_html_rec(f, valid_tags, valid_attributes)
+            if f.tag in valid_tags:
+                clean_attributes(f, valid_attributes)
+                yield html.tostring(f)
+            else:
+                if f.text:
+                    yield f.text
+                for sub in f:
+                    yield html.tostring(sub)
+                if f.tail:
+                    yield f.tail
+                if f.tag in ('p', 'br'):
+                    yield '\n'
+        else:
+            yield f
+
+
+def sanitize_html_keep(html_value, valid_tags=VALID_TAGS, valid_attributes=VALID_ATTRIBUTES):
+    return ''.join(sanitize_html_frags(html_value, valid_tags, valid_attributes))
 
 
 def sanitize_html(html_value, valid_tags=VALID_TAGS, keep_tag_content=True):
