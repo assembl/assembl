afa1ba0f1f09393d1fc4f05e8976b816996b5814
Swergas <swergas@yahoo.fr>
2017-08-24 17:52:21 +0200

    convert IdeaPreview to functional component

--- assembl/static2/js/app/components/common/ideaPreview.jsx
+++ assembl/static2/js/app/components/common/ideaPreview.jsx
@@ -3,50 +3,47 @@ import { Link } from 'react-router';
 import { Translate } from 'react-redux-i18n';
 import Statistic from './statistic';
 
-class IdeaPreview extends React.Component {
-  render() {
-    const { imgUrl, link, title, description, numPosts, numContributors, numChildren, isSelected, onSeeSubIdeasClick } = this.props;
-    const imageBoxStyle = {};
-    if (imgUrl) {
-      imageBoxStyle.backgroundImage = `url(${imgUrl})`;
-    }
-    return (
-      <div
-        className={
-          isSelected ? 'illustration illustration-box idea-preview idea-preview-selected' : 'illustration illustration-box idea-preview'
-        }
-      >
-        <div className="image-box" style={imageBoxStyle}>
-          &nbsp;
-        </div>
-        <div className="content-box" to={link}>
-          <h3 className="light-title-3 center">
-            {title}
-          </h3>
-          <div className="see-discussion">
-            <Link to={link}>
-              <Translate value="debate.thread.goToIdea" />
-            </Link>
-          </div>
-          {onSeeSubIdeasClick && numChildren
-            ? <div>
-              <div className="action-separator" />
-              <div className="see-sub-ideas" onClick={onSeeSubIdeasClick}>
-                <Translate value="debate.thread.seeSubIdeas" count={numChildren} />
-              </div>
-            </div>
-            : <div />}
-          <Statistic numPosts={numPosts} numContributors={numContributors} />
-          <div className="text-box">
-            {description}
-          </div>
-        </div>
-        <div className="color-box">&nbsp;</div>
-        <div className="box-hyphen">&nbsp;</div>
-        <div className="box-hyphen rotate-hyphen">&nbsp;</div>
-      </div>
-    );
+const IdeaPreview = ({ imgUrl, link, title, description, numPosts, numContributors, numChildren, isSelected, onSeeSubIdeasClick }) => {
+  const imageBoxStyle = {};
+  if (imgUrl) {
+    imageBoxStyle.backgroundImage = `url(${imgUrl})`;
   }
-}
+  return (
+    <div
+      className={
+        isSelected ? 'illustration illustration-box idea-preview idea-preview-selected' : 'illustration illustration-box idea-preview'
+      }
+    >
+      <div className="image-box" style={imageBoxStyle}>
+        &nbsp;
+      </div>
+      <div className="content-box" to={link}>
+        <h3 className="light-title-3 center">
+          {title}
+        </h3>
+        <div className="see-discussion">
+          <Link to={link}>
+            <Translate value="debate.thread.goToIdea" />
+          </Link>
+        </div>
+        {onSeeSubIdeasClick && numChildren
+          ? <div>
+            <div className="action-separator" />
+            <div className="see-sub-ideas" onClick={onSeeSubIdeasClick}>
+              <Translate value="debate.thread.seeSubIdeas" count={numChildren} />
+            </div>
+          </div>
+          : <div />}
+        <Statistic numPosts={numPosts} numContributors={numContributors} />
+        <div className="text-box">
+          {description}
+        </div>
+      </div>
+      <div className="color-box">&nbsp;</div>
+      <div className="box-hyphen">&nbsp;</div>
+      <div className="box-hyphen rotate-hyphen">&nbsp;</div>
+    </div>
+  );
+};
 
 export default IdeaPreview;
