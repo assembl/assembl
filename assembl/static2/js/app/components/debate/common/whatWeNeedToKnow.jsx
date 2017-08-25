906a81068c9c471bd51e406024ca6fa8a3351643
Norman Meier <norman.meier@tuta.io>
2017-08-25 16:33:17 +0200

    wip styling tests

--- assembl/static2/js/app/components/debate/common/whatWeNeedToKnow.jsx
+++ assembl/static2/js/app/components/debate/common/whatWeNeedToKnow.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 
+const style = {
+  top: '0px',
+  height: '300px',
+  width: '300px'
+};
+
+const childStyle = {
+  overflow: 'scroll',
+  height: '180px',
+  width: '100%'
+};
+
 export default ({ longTitle }) => {
   return longTitle
-    ? <div className="synthesis-container">
-      <div className="insert-box">
-        <h3 className="dark-title-4">What we need to know:</h3>
-        <div className="box-hyphen">&nbsp;</div>
-        <div className="insert-content margin-s">
-          {<p dangerouslySetInnerHTML={{ __html: longTitle }} />}
+    ? <div className="insert-box" style={style}>
+      <h3 className="dark-title-3">What we need to know:</h3>
+      <div className="box-hyphen" />
+      <div style={childStyle}>
+        {<p dangerouslySetInnerHTML={{ __html: longTitle }} />}
+          qi usdq siud uqsdi uqsi duqi usdq siud uqsdi uqsi duqi usdq siud uqsdi uqsi duqi usdq siud uqsdi uqsi duqi usdq siud uqsdi
+          uqsi duqi usdq siud uqsdi uqsi duqi usdq siud uqsdi uqsi duqi usdq siud uqsdi uqsi duqi usdq siud uqsdi uqsi duqi usdq siud
+          uqsdi uqsi duqi usdq siud uqsdi uqsi duqi usdq siud uqsdi uqsi duqi usdq siud uqsdi uqsi duqi usdq siud uqsdi uqsi duqi usdq
+          siud uqsdi uqsi duqi usdq siud uqsdi uqsi duqi usdq siud uqsdi uqsi duqi usdq siud uqsdi uqsi duqi usdq siud uqsdi uqsi duqi
+          usdq siud uqsdi uqsi duqi usdq siud uqsdi uqsi duqi usdq siud uqsdi uqsi duqi usdq siud uqsdi uqsi du
         </div>
-      </div>
     </div>
     : null;
 };
