4d608c0ef14e017d3a79ec91d2c51a0c0b7585bf
paolina92 <paolinardc@gmail.com>
2017-08-29 16:53:44 +0200

    Calculation of the idea preview width depending on the screen width

--- assembl/static2/js/app/components/common/visibilityComponent.jsx
+++ assembl/static2/js/app/components/common/visibilityComponent.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 
-// FIXME: If we transform this class component into a functional component, IdeasLevel's scroll buttons visibility becomes buggy. Why?
-class VisibilityComponent extends React.Component {
-  render() {
-    const { className, isVisible } = this.props;
-    const myClassNames = isVisible === true ? className : `${className} hidden`;
-    return (
-      <div className={myClassNames}>
-        {this.props.children}
-      </div>
-    );
-  }
-}
-
-VisibilityComponent.defaultProps = { isVisible: true };
+const VisibilityComponent = ({ className, isVisible }) => {
+  return (
+    <div className={isVisible ? className : 'hidden'}>
+      {this.props.children}
+    </div>
+  );
+};
 
 export default VisibilityComponent;
--- assembl/static2/js/app/components/debate/common/ideasLevel.jsx
+++ assembl/static2/js/app/components/debate/common/ideasLevel.jsx
@@ -3,11 +3,14 @@ import { Row, Col } from 'react-bootstrap';
 import IdeaPreview from '../../common/ideaPreview';
 import { get as getRoute } from '../../../utils/routeMap';
 import { getDiscussionSlug } from '../../../utils/globalFunctions';
+import { APP_CONTAINER_MAX_WIDTH, IDEA_PREVIEW_MAX_WIDTH, NB_IDEA_PREVIEW_TO_SHOW, APP_CONTAINER_PADDING } from '../../../constants';
 
 class IdeasLevel extends React.Component {
   constructor(props) {
     super(props);
     this.getColClassNames = this.getColClassNames.bind(this);
+    this.getColWidth = this.getColWidth.bind(this);
+    this.getSliderWidth = this.getSliderWidth.bind(this);
   }
   getColClassNames(index) {
     const { currentLevel } = this.props;
@@ -27,15 +30,40 @@ class IdeasLevel extends React.Component {
     }
     return styles;
   }
+  getColWidth() {
+    const screenWidth = window.innerWidth;
+    this.colWidth = IDEA_PREVIEW_MAX_WIDTH;
+    if (screenWidth < APP_CONTAINER_MAX_WIDTH) {
+      this.colWidth = screenWidth / NB_IDEA_PREVIEW_TO_SHOW;
+    }
+    return this.colWidth;
+  }
+  getSliderWidth() {
+    const screenWidth = window.innerWidth;
+    const halfColWidth = this.getColWidth() / 2;
+    const availableRightSpace = screenWidth - APP_CONTAINER_MAX_WIDTH;
+    let sliderWidth = APP_CONTAINER_MAX_WIDTH + halfColWidth;
+    if (availableRightSpace < halfColWidth) {
+      sliderWidth = availableRightSpace + APP_CONTAINER_MAX_WIDTH - APP_CONTAINER_PADDING;
+    }
+    return sliderWidth;
+  }
   render() {
     const { ideas, identifier, setLevelsToDisplay, currentLevel, ideaLevel } = this.props;
     const slug = getDiscussionSlug();
     return (
-      <div className="slider">
+      <div className="slider" style={currentLevel > 1 ? { width: `${this.getSliderWidth()}px` } : {}}>
         <Row className={currentLevel > 1 ? 'no-margin row-inline' : 'no-margin'}>
           {ideas.map((idea, index) => {
             return (
-              <Col xs={12} sm={6} md={3} key={index} className={this.getColClassNames(index)}>
+              <Col
+                xs={12}
+                sm={6}
+                md={3}
+                key={index}
+                className={this.getColClassNames(index)}
+                style={currentLevel > 1 ? { width: this.getColWidth() } : {}}
+              >
                 <IdeaPreview
                   imgUrl={idea.imgUrl}
                   numPosts={idea.numPosts}
