eed1e01fcc0f38cb0a6171019645c9a66d084853
Norman Meier <norman.meier@tuta.io>
2017-09-05 12:02:19 +0200

    forgot to save before commit

---
+++ assembl/static2/js/app/components/debate/common/chatbot.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class Chatbot extends React.Component {
+  componentDidMount = () => {
+    if (!('scriptURL' in this.props.args)) return;
+    const { scriptURL, cssURL, args } = this.props.args;
+    const addCss = document.createElement('link');
+    addCss.setAttribute('rel', 'stylesheet');
+    addCss.setAttribute('type', 'text/css');
+    addCss.setAttribute('href', cssURL);
+    document.body.appendChild(addCss);
+    addCss.onload = () => {
+      const addScript = document.createElement('script');
+      addScript.setAttribute('src', scriptURL);
+      document.body.appendChild(addScript);
+      addScript.onload = () => {
+        window.motionAI_Init(...args); // this function is in the script that just got loaded
+      };
+    };
+  };
+  render = () => {
+    return false;
+  };
+}
+
+const mapStateToProps = (state) => {
+  return {
+    args: state.debate.debateData.motionChatbot
+  };
+};
+
+export default connect(mapStateToProps)(Chatbot);
