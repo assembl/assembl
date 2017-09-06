b17a2027892a69b11699e4d80db2f08490852613
Norman Meier <norman.meier@tuta.io>
2017-09-06 16:50:03 +0200

    announcement section 2nd pass

--- assembl/static2/js/app/components/debate/thread/announcement.jsx
+++ assembl/static2/js/app/components/debate/thread/announcement.jsx
@@ -1,29 +1,75 @@
 import React from 'react';
 import { Translate, I18n } from 'react-redux-i18n';
+import { Col, Tooltip } from 'react-bootstrap';
+
 import Doughnut from '../../svg/doughnut';
+import { sentimentDefinitionsObject } from './sentimentDefinitions';
+import '../../../../../css/components/announcement.scss';
+import Video from '../../home/video';
+
+const createTooltip = (sentiment, count) => {
+  return (
+    <Tooltip id={`${sentiment.camelType}Tooltip`}>
+      {`${count} ${I18n.t(`debate.${sentiment.camelType}`)}`}
+    </Tooltip>
+  );
+};
 
 class Announcement extends React.Component {
   render = () => {
-    console.log('ann prop', this.props.ideaWithPostsData);
     const { idea } = this.props.ideaWithPostsData;
     const { numContributors, numPosts } = idea;
     return (
-      <div style={{ textAlign: 'center' }}>
-        <h3>
-          <Translate value="debate.thread.announcement" />
-        </h3>
-        <Doughnut
-          elements={[
-            { name: 'orange', count: 2 },
-            { name: 'purple', count: 5 },
-            { name: 'green', count: 6 },
-            { name: 'red', count: 6 }
-          ]}
-          text={`168 ${I18n.t('debate.survey.reactions')}`}
-        />
-        <section>
-          {numPosts} <span className="assembl-icon-message" /> - {numContributors} <span className="assembl-icon-profil" />
-        </section>
+      <div className="announcement">
+        <Col xs={12} md={4}>
+          <h3 className="announcement-title">
+            <Translate value="debate.thread.announcement" />
+          </h3>
+          <div className="statistics-container">
+            <div className="statistics">
+              <div className="superpose-label superpose">
+                <div>
+                  {168}
+                </div>
+                <div>
+                  <Translate value="debate.survey.reactions" />
+                </div>
+              </div>
+              <div className="superpose">
+                <Doughnut
+                  elements={[
+                    {
+                      color: sentimentDefinitionsObject.dontUnderstand.color,
+                      count: 2,
+                      Tooltip: createTooltip(sentimentDefinitionsObject.dontUnderstand, 2)
+                    },
+                    {
+                      color: sentimentDefinitionsObject.moreInfo.color,
+                      count: 5,
+                      Tooltip: createTooltip(sentimentDefinitionsObject.moreInfo, 5)
+                    },
+                    {
+                      color: sentimentDefinitionsObject.like.color,
+                      count: 6,
+                      Tooltip: createTooltip(sentimentDefinitionsObject.like, 6)
+                    },
+                    {
+                      color: sentimentDefinitionsObject.disagree.color,
+                      count: 6,
+                      Tooltip: createTooltip(sentimentDefinitionsObject.disagree, 6)
+                    }
+                  ]}
+                />
+              </div>
+            </div>
+          </div>
+          <section>
+            {numPosts} <span className="assembl-icon-message" /> - {numContributors} <span className="assembl-icon-profil" />
+          </section>
+        </Col>
+        <Col xs={12} md={8}>
+          <Video />
+        </Col>
       </div>
     );
   };
