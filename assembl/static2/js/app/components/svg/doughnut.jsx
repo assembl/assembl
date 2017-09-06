d62f2311f291e1bbc28e492d8189f88aad7e3141
Norman Meier <norman.meier@tuta.io>
2017-09-06 16:48:33 +0200

    rewrite doughnut again to allow for per arc tooltips

--- assembl/static2/js/app/components/svg/doughnut.jsx
+++ assembl/static2/js/app/components/svg/doughnut.jsx
@@ -1,15 +1,7 @@
 import React from 'react';
-import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { OverlayTrigger } from 'react-bootstrap';
 
 import { calculatePercentage } from '../../utils/globalFunctions';
-
-const createTooltip = (text) => {
-  return (
-    <Tooltip id="tooltip">
-      {text}
-    </Tooltip>
-  );
-};
 
 function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
   const angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
@@ -31,47 +23,40 @@ const describeArc = (x, y, radius, startAngle, endAngle) => {
   return d;
 };
 
-export default ({ elements, text }) => {
-  const totalCount = elements.reduce((total, element) => {
+const getColor = (element) => {
+  return 'color' in element ? element.color : 'grey';
+};
+
+const simpleCircle = (element) => {
+  return <circle className="circle" cx="5em" cy="5em" r="2.5em" stroke={getColor(element)} />;
+};
+
+const circlePaths = (elements, totalCount) => {
+  let nextStartAngle = 0;
+  return elements.map((element, index) => {
+    const normalizedSize = calculatePercentage(element.count, totalCount) / 100;
+    const startAngle = nextStartAngle;
+    const endAngle = startAngle + normalizedSize * 360;
+    nextStartAngle = endAngle;
+    const color = getColor(element);
+    return 'Tooltip' in element
+      ? <OverlayTrigger key={index} container={this} overlay={element.Tooltip}>
+        <path d={describeArc(70, 70, 35, startAngle, endAngle)} stroke={color} fill="transparent" className={'circle'} />
+      </OverlayTrigger>
+      : <path key={index} d={describeArc(70, 70, 35, startAngle, endAngle)} stroke={color} fill="transparent" className={'circle'} />;
+  });
+};
+
+export default ({ elements }) => {
+  const filteredElements = elements.filter(({ count }) => {
+    return count > 0;
+  });
+  const totalCount = filteredElements.reduce((total, element) => {
     return total + element.count;
   }, 0);
-  const radius = 2.5;
-  const circumference = 2 * radius * Math.PI;
-  const baseOffset = 0;
-  let nextStartAngle = 0;
-  return (
-    <div>
-      <svg className="doughnut" height="10em" width="10em">
-        {elements
-          .filter(({ count }) => {
-            return count > 0;
-          })
-          .map(({ name, count }, index) => {
-            const normalizedSize = calculatePercentage(count, totalCount) / 100;
-            // const offset = circumference * nextOffset;
-            const startAngle = nextStartAngle;
-            const endAngle = startAngle + normalizedSize * 360;
-            nextStartAngle = endAngle;
-            // const size = circumference * normalizedSize;
-            // const gap = circumference - size;
-            return (
-              <OverlayTrigger key={index} container={this} placement="right" overlay={createTooltip(`${count} ${name}`)}>
-                {/* <circle
-                  className={`circle radial-${name}`}
-                  r={`${radius}em`}
-                  cx={`${5}em`}
-                  cy={`${5}em`}
-                  strokeDasharray={`${0}em ${offset}em ${size}em ${gap}em`}
-                />*/}
-                <path d={describeArc(70, 70, 60, startAngle, endAngle)} stroke={name} fill="transparent" strokeWidth="10" />
-              </OverlayTrigger>
-            );
-          })}
-        {text &&
-          <text x="50%" y="50%" textAnchor="middle">
-            {text}
-          </text>}
-      </svg>
-    </div>
-  );
+  return totalCount === 0
+    ? false
+    : <svg className="doughnut" height="10em" width="10em">
+      {filteredElements.length === 1 ? simpleCircle(filteredElements[0]) : circlePaths(filteredElements, totalCount)}
+    </svg>;
 };
