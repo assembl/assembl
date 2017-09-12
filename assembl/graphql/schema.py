0f3602634219e2b3ed3a96d2286cae65d479ddba
Vincent Fretin <vincent.fretin@gmail.com>
2017-09-12 09:25:03 +0200

    better check sentimentCounts in the Post inline fragment

--- assembl/graphql/schema.py
+++ assembl/graphql/schema.py
@@ -19,6 +19,7 @@ from graphene_sqlalchemy import SQLAlchemyConnectionField
 from graphene_sqlalchemy.converter import (
     convert_column_to_string, convert_sqlalchemy_type)
 from graphene_sqlalchemy.utils import get_query, is_mapped
+from graphql.utils.ast_to_dict import ast_to_dict
 from pyramid.httpexceptions import HTTPUnauthorized
 from pyramid.security import Everyone
 from pyramid.i18n import TranslationStringFactory
@@ -92,6 +93,56 @@ class SecureObjectType(object):
 # like AgentProfile.posts_created and create dynamically the
 # object types Post, PostConnection which will conflict with those added
 # manually.
+
+
+# copied from https://github.com/graphql-python/graphene/issues/462#issuecomment-298218524
+def collect_fields(node, fragments, variables):
+    field = {}
+    selection_set = node.get('selection_set') if node else None
+    selections = selection_set.get('selections', None) if selection_set else None
+
+    if selections is not None:
+        for leaf in selections:
+            leaf_kind = leaf.get('kind')
+            leaf_name = leaf.get('name', {}).get('value')
+            leaf_directives = leaf.get('directives')
+
+            # Check if leaf should be skipped
+            # - If name is '__typename'
+            # - if @skip directive is used and evaluates to True
+            # - if @include directive is used and evaluates to False (not yet implemented!)
+            should_skip = False
+            for directive in leaf_directives:
+                if directive.get('name', {}).get('value') == 'skip':
+                    for arg in directive.get('arguments', []):
+                        arg_value = arg.get('value', {})
+                        if arg.get('name', {}).get('value') == 'if':
+                            if arg_value.get('kind') == 'Variable':
+                                var_name = arg_value.get('name', {}).get('value')
+                                should_skip = variables.get(var_name, should_skip)
+                            elif arg_value.get('kind') == 'BooleanValue':
+                                should_skip = arg_value.get('value')
+
+            if leaf_name != '__typename' and not should_skip:
+                if leaf_kind == 'Field':
+                    field.update({leaf_name: collect_fields(leaf, fragments, variables)})
+                elif leaf_kind == 'FragmentSpread':
+                    field.update(collect_fields(fragments[leaf_name], fragments, variables))
+                elif leaf_kind == 'InlineFragment':
+                    field.update(collect_fields(leaf, fragments, variables))
+    return field
+
+
+def get_fields(info):
+    """Return a nested dict of the fields requested by a graphene resolver"""
+    fragments = {}
+    node = ast_to_dict(info.field_asts[0])
+
+    for name, value in info.fragments.items():
+        fragments[name] = ast_to_dict(value)
+
+    fields = collect_fields(node, fragments, info.variable_values)
+    return fields
 
 
 def resolve_langstring(langstring, locale_code):
@@ -569,7 +620,8 @@ class Idea(SecureObjectType, SQLAlchemyObjectType):
 
         # do only one sql query to calculate sentiment_counts
         # instead of doing one query for each post
-        if 'sentimentCounts' in str(info.field_asts[0]):
+        fields = get_fields(info)
+        if 'sentimentCounts' in fields.get('edges', {}).get('node', {}):
             sentiment_counts = discussion.db.query(
                     models.Post.id, models.SentimentOfPost.type, count(models.SentimentOfPost.id)
                 ).join(models.SentimentOfPost
