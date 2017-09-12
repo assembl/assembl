c0d7995a3d7c2011875047f6e2c2ad1355ee55d8
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2017-09-12 14:18:41 +0200

    Add component and css for typical flat round switch button

---
+++ assembl/static2/js/app/components/common/switchButton.jsx
@@ -0,0 +1,82 @@
+// @flow
+import React from 'react';
+
+type Props = {
+  id: string,
+  name: string,
+  title: string,
+  label: string,
+  labelRight: string,
+  defaultChecked: boolean,
+  disabled: boolean,
+  theme: string,
+  mode: string,
+  onChange: Function
+};
+
+class SwitchButton extends React.Component<Object, Props, any> {
+  static defaultProps: Object;
+  static defaultProps = {
+    id: '',
+    name: 'switch-button',
+    title: '',
+    label: '',
+    labelRight: '',
+    disabled: false,
+    defaultChecked: false,
+    theme: 'rsbc-switch-button-flat-round',
+    mode: 'switch',
+    onChange: () => {}
+  };
+
+  render() {
+    let id;
+    let label;
+    let labelRight;
+    let mode = this.props.mode || 'switch';
+
+    if (this.props.id === '' && this.props.name !== '') {
+      id = this.props.name;
+    }
+
+    if (this.props.label !== '') {
+      label = (
+        <label htmlFor={id}>
+          {this.props.label}
+        </label>
+      );
+    }
+
+    if (this.props.labelRight !== '') {
+      labelRight = (
+        <label htmlFor={id}>
+          {this.props.labelRight}
+        </label>
+      );
+    }
+
+    if (['switch', 'select'].indexOf(mode) < -1) {
+      mode = 'switch';
+    }
+
+    const props = {
+      disabled: this.props.disabled,
+      onChange: this.props.onChange,
+      name: this.props.name,
+      type: 'checkbox',
+      value: '1',
+      id: id
+    };
+
+    return (
+      <div className={`rsbc-switch-button rsbc-mode-${mode} ${this.props.theme}${this.props.disabled ? ' disabled' : ''}`}>
+        {label}
+        <input {...props} />
+        <label htmlFor={id} />
+        {labelRight}
+      </div>
+    );
+  }
+}
+
+export default SwitchButton;
