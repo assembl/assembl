55a0e33636d90c2086d297f8ab31df5fe1bdb4cc
Norman Meier <norman.meier@tuta.io>
2017-09-19 12:13:23 +0200

    render survey video comp even is there is no video url

--- assembl/static2/js/app/components/debate/survey/video.jsx
+++ assembl/static2/js/app/components/debate/survey/video.jsx
@@ -7,54 +7,54 @@ class Video extends React.Component {
     const { title, descriptionTop, descriptionBottom, descriptionSide, htmlCode } = this.props;
     return (
       <section className="video-section relative">
-        {htmlCode &&
-          <Grid fluid className="background-light">
-            <div className="max-container">
-              <div className="video-title-section">
-                <div className="title-hyphen">&nbsp;</div>
-                <h1 className="dark-title-1">
-                  {title || I18n.t('debate.survey.titleVideo')}
-                </h1>
-              </div>
-              <div className="content-section">
-                {descriptionTop &&
-                  <Row>
-                    <Col xs={12} sm={descriptionSide ? 6 : 8} smOffset={descriptionSide ? 4 : 2}>
-                      <div
-                        className="video-description-layer video-description-top"
-                        dangerouslySetInnerHTML={{ __html: descriptionTop }}
-                      />
-                    </Col>
-                  </Row>}
+        <Grid fluid className="background-light">
+          <div className="max-container">
+            <div className="video-title-section">
+              <div className="title-hyphen">&nbsp;</div>
+              <h1 className="dark-title-1">
+                {title || I18n.t('debate.survey.titleVideo')}
+              </h1>
+            </div>
+            <div className="content-section">
+              {descriptionTop &&
                 <Row>
-                  {descriptionSide &&
-                    <Col xs={12} sm={3} smOffset={1}>
-                      <div className="video-description">
-                        <div>
-                          <span className="assembl-icon-pepite color2">&nbsp;</span>
-                        </div>
-                        <div className="description-txt" dangerouslySetInnerHTML={{ __html: descriptionSide }} />
-                        <div className="box-hyphen left">&nbsp;</div>
+                  <Col xs={12} sm={descriptionSide ? 6 : 8} smOffset={descriptionSide ? 4 : 2}>
+                    <div
+                      className="video-description-layer video-description-top"
+                      dangerouslySetInnerHTML={{ __html: descriptionTop }}
+                    />
+                  </Col>
+                </Row>}
+              <Row>
+                {descriptionSide &&
+                  <Col xs={12} sm={3} smOffset={1}>
+                    <div className="video-description">
+                      <div>
+                        <span className="assembl-icon-pepite color2">&nbsp;</span>
                       </div>
-                    </Col>}
+                      <div className="description-txt" dangerouslySetInnerHTML={{ __html: descriptionSide }} />
+                      <div className="box-hyphen left">&nbsp;</div>
+                    </div>
+                  </Col>}
+                {htmlCode &&
                   <Col xs={12} sm={descriptionSide ? 6 : 8} smOffset={descriptionSide ? 0 : 2}>
                     <div className="video-container" id="video-vid">
                       <iframe src={htmlCode} frameBorder="0" width="560" height="315" title="video" />
                     </div>
-                  </Col>
-                </Row>
-                <Row>
-                  {descriptionBottom &&
-                    <Col xs={12} sm={descriptionSide ? 6 : 8} smOffset={descriptionSide ? 4 : 2}>
-                      <div
-                        className="video-description-layer video-description-bottom"
-                        dangerouslySetInnerHTML={{ __html: descriptionBottom }}
-                      />
-                    </Col>}
-                </Row>
-              </div>
+                  </Col>}
+              </Row>
+              <Row>
+                {descriptionBottom &&
+                  <Col xs={12} sm={descriptionSide ? 6 : 8} smOffset={descriptionSide ? 4 : 2}>
+                    <div
+                      className="video-description-layer video-description-bottom"
+                      dangerouslySetInnerHTML={{ __html: descriptionBottom }}
+                    />
+                  </Col>}
+              </Row>
             </div>
-          </Grid>}
+          </div>
+        </Grid>
       </section>
     );
   }
