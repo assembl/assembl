dca0c343adddf19935ef6769a58b9748c9100569
Cédric Messiant <cedricmessiant@ecreall.com>
2017-10-02 10:00:03 +0200

    Never display the post translate switch if the language of the message is the language of the site

--- assembl/static2/js/app/components/debate/common/postTranslate.jsx
+++ assembl/static2/js/app/components/debate/common/postTranslate.jsx
@@ -80,13 +80,10 @@ class PostTranslate extends React.Component<void, PostTranslateProps, PostTransl
 
   render() {
     const { id, lang, originalLocale, translate } = this.props;
-    const userIsConnected = getConnectedUserId();
-    if (!userIsConnected) {
-      const specialLocaleCodes = ['und', 'zxx']; // locale codes that can not be translated
-      const showPostTranslate = originalLocale !== lang && specialLocaleCodes.indexOf(originalLocale) === -1;
-      if (!showPostTranslate) {
-        return null;
-      }
+    const specialLocaleCodes = ['und', 'zxx']; // locale codes that can not be translated
+    const showPostTranslate = originalLocale !== lang && specialLocaleCodes.indexOf(originalLocale) === -1;
+    if (!showPostTranslate) {
+      return null;
     }
     return (
       <div className="translate">
