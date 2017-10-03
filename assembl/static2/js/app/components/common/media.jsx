d4b1a55ca6d28bad7d9abd0f7f1af5b586c4ebd9
Norman Meier <norman.meier@tuta.io>
2017-10-03 14:49:28 +0200

    rewrite media component to be more self contained, display a large modal for picture and use bootstrap responsive components for image and embed

--- assembl/static2/js/app/components/common/media.jsx
+++ assembl/static2/js/app/components/common/media.jsx
@@ -1,79 +1,106 @@
 import React from 'react';
 import { I18n } from 'react-redux-i18n';
-import { Grid, Col } from 'react-bootstrap';
+import { Grid, Col, Button, Image, ResponsiveEmbed } from 'react-bootstrap';
 
-const isValidDescription = (description) => {
-  return !!(description && description !== '<p></p>');
-};
-
-const Title = ({ value }) => {
-  return (
-    <div className="media-title-section">
-      <div className="title-hyphen">&nbsp;</div>
-      <h1 className="dark-title-1">
-        {value || I18n.t('debate.survey.titleVideo')}
-      </h1>
-    </div>
-  );
-};
-
-const SideDescription = ({ content }) => {
-  return (
-    <div className="media-description">
-      <div>
-        <span className="assembl-icon-pepite color2">&nbsp;</span>
-      </div>
-      <div className="description-txt" dangerouslySetInnerHTML={{ __html: content }} />
-      <div className="box-hyphen left">&nbsp;</div>
-    </div>
-  );
-};
-
-const TopDescription = ({ content }) => {
-  return <div className="media-description-layer media-description-bottom" dangerouslySetInnerHTML={{ __html: content }} />;
-};
-
-const MediaContent = ({ content }) => {
-  return (
-    <div className="media-container">
-      {content[0] === '/' ? <img src={content} alt="media" /> : <object data={content} aria-label="media" />}
-    </div>
-  );
-};
-
-const BottomDescription = ({ content }) => {
-  return <div className="media-description-layer media-description-bottom" dangerouslySetInnerHTML={{ __html: content }} />;
-};
+import { displayModal } from '../../utils/utilityManager';
 
 class Media extends React.Component {
+  static isValidDescription = (description) => {
+    return !!(description && description !== '<p></p>');
+  };
+
+  static Title = ({ value }) => {
+    return (
+      <div className="media-title-section">
+        <div className="title-hyphen">&nbsp;</div>
+        <h1 className="dark-title-1">
+          {value || I18n.t('debate.survey.titleVideo')}
+        </h1>
+      </div>
+    );
+  };
+
+  static SideDescription = ({ content }) => {
+    return (
+      <div className="media-description">
+        <div className="media-description-icon">
+          <span className="assembl-icon-pepite color2">&nbsp;</span>
+        </div>
+        <div className="description-txt" dangerouslySetInnerHTML={{ __html: content }} />
+        <div className="box-hyphen left">&nbsp;</div>
+      </div>
+    );
+  };
+
+  static TopDescription = ({ content }) => {
+    return <div className="media-description-layer media-description-top" dangerouslySetInnerHTML={{ __html: content }} />;
+  };
+
+  static ImageModal = (content) => {
+    return () => {
+      displayModal(null, content, false, null, null, true, 'large');
+    };
+  };
+
+  static Content = ({ content }) => {
+    const isLocal = content[0] === '/';
+    const component = isLocal
+      ? <Image responsive src={content} />
+      : (<ResponsiveEmbed a16by9>
+        <embed src={content} />
+      </ResponsiveEmbed>);
+    return (
+      <div className="media-container">
+        {isLocal
+          ? <Button
+            onClick={Media.ImageModal(
+              <div className="media-container">
+                {component}
+              </div>
+            )}
+          >
+            {component}
+          </Button>
+          : component}
+      </div>
+    );
+  };
+
+  static BottomDescription = ({ content }) => {
+    return <div className="media-description-layer media-description-bottom" dangerouslySetInnerHTML={{ __html: content }} />;
+  };
+
   render() {
     const { title, descriptionTop, descriptionBottom, descriptionSide, htmlCode, noTitle } = this.props;
-    const validDescriptionSide = isValidDescription(descriptionSide);
-    const validDescriptionTop = isValidDescription(descriptionTop);
-    const validDescriptionBottom = isValidDescription(descriptionBottom);
+    const validDescriptionSide = Media.isValidDescription(descriptionSide);
+    const validDescriptionTop = Media.isValidDescription(descriptionTop);
+    const validDescriptionBottom = Media.isValidDescription(descriptionBottom);
 
     const validMedia = !!htmlCode;
-    const somethingOnRight = !!(validDescriptionTop || validDescriptionBottom || validMedia);
+    const somethingOnRight = !!(validDescriptionTop || validDescriptionBottom || validMedia || validDescriptionSide);
     const somethingOnLeft = validDescriptionSide;
     const something = !!(somethingOnLeft || somethingOnRight);
     const totalSize = 12;
     const leftSize = 4;
     const rightSize = totalSize - leftSize;
-    return !noTitle || validDescriptionTop || validDescriptionSide || validMedia || validDescriptionBottom
+    return !noTitle || something
       ? <section className="media-section background-light">
         <div className="max-container">
-          {!noTitle && <Title value={title} />}
+          {!noTitle && <Media.Title value={title} />}
           {something &&
           <Grid fluid>
             {somethingOnLeft &&
-            <Col xs={totalSize} sm={somethingOnRight ? leftSize : totalSize}>
-              <SideDescription content={descriptionSide} />
+            <Col sm={totalSize} md={somethingOnRight ? leftSize : totalSize}>
+              {validDescriptionSide && <Media.SideDescription content={descriptionSide} />}
             </Col>}
             {somethingOnRight &&
-            <Col xs={totalSize} sm={somethingOnLeft ? rightSize : totalSize}>
-              <TopDescription content={descriptionTop} />
-              <MediaContent content={htmlCode} />
-              <BottomDescription content={descriptionBottom} />
+            <Col sm={totalSize} md={somethingOnLeft ? rightSize : totalSize}>
+              <div className="media-right">
+                {validDescriptionTop && <Media.TopDescription content={descriptionTop} />}
+                {validMedia && <Media.Content content={htmlCode} />}
+                {validDescriptionSide && <Media.SideDescription content={descriptionSide} />}
+                {validDescriptionBottom && <Media.BottomDescription content={descriptionBottom} />}
+              </div>
             </Col>}
           </Grid>}
         </div>
