3645e7acf12680bb902de5808d210e0fe0204bda
Vincent Fretin <vincent.fretin@gmail.com>
2017-10-09 18:00:46 +0200

    The perceived load time of the idea page is now much better.
    The ideas query was blocking the other graphql queries needed to show content
    on the idea page. Now the queries are run in parallel and there is no
    loading indicator for the ideas query, we don't even need the data on
    this page.

--- assembl/static2/js/app/pages/debateThread.jsx
+++ assembl/static2/js/app/pages/debateThread.jsx
@@ -4,15 +4,14 @@ import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { compose, graphql } from 'react-apollo';
 import { Translate } from 'react-redux-i18n';
+import Loader from '../components/common/loader';
 import { get } from '../utils/routeMap';
 import Ideas from '../components/debate/common/ideas';
 import Timeline from '../components/debate/navigation/timeline';
-import withLoadingIndicator from '../components/common/withLoadingIndicator';
 import AllIdeasQuery from '../graphql/AllIdeasQuery.graphql';
 
 const DebateThread = ({ identifier, data, params, children, slug }) => {
-  const rootIdeaId = 'rootIdea' in data && 'id' in data.rootIdea ? data.rootIdea.id : null;
-  const thematics = 'ideas' in data ? data.ideas : null;
+  const { loading, ideas, rootIdea } = data;
   const isParentRoute = !params.themeId || false;
   const themeId = params.themeId || null;
   const childrenElm = React.Children.map(children, (child) => {
@@ -21,31 +20,28 @@ const DebateThread = ({ identifier, data, params, children, slug }) => {
       identifier: identifier
     });
   });
-  if (!rootIdeaId || !thematics) {
-    return <div />;
-  }
   return (
     <div className="debate">
-      {thematics &&
-        <div>
-          <section className="timeline-section" id="timeline">
-            <div className="max-container">
-              {!isParentRoute &&
-                <Link className="burger-menu grey" to={get('debate', { slug: slug, phase: identifier })}>
-                  <div className="assembl-icon-thumb" />
-                  <div className="burger-menu-label">
-                    <Translate value="debate.themes" />
-                  </div>
-                </Link>}
-              <Timeline showNavigation={!isParentRoute} identifier={identifier} />
-            </div>
-          </section>
-          {isParentRoute && <Ideas ideas={thematics} rootIdeaId={rootIdeaId} identifier={identifier} />}
-          {!isParentRoute &&
-            <section className="debate-section">
-              {childrenElm}
-            </section>}
-        </div>}
+      {loading && isParentRoute && <Loader color="black" />}
+      <div>
+        <section className="timeline-section" id="timeline">
+          <div className="max-container">
+            {!isParentRoute &&
+              <Link className="burger-menu grey" to={get('debate', { slug: slug, phase: identifier })}>
+                <div className="assembl-icon-thumb" />
+                <div className="burger-menu-label">
+                  <Translate value="debate.themes" />
+                </div>
+              </Link>}
+            <Timeline showNavigation={!isParentRoute} identifier={identifier} />
+          </div>
+        </section>
+        {ideas && isParentRoute && <Ideas ideas={ideas} rootIdeaId={rootIdea.id} identifier={identifier} />}
+        {!isParentRoute &&
+          <section className="debate-section">
+            {childrenElm}
+          </section>}
+      </div>
     </div>
   );
 };
@@ -65,4 +61,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default compose(connect(mapStateToProps), graphql(AllIdeasQuery), withLoadingIndicator())(DebateThread);
+export default compose(connect(mapStateToProps), graphql(AllIdeasQuery))(DebateThread);
