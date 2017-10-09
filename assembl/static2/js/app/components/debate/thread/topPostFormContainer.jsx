9da4d0d37af6efb9b6e53ab6af1e76a71dc6c9cd
paolina92 <paolinardc@gmail.com>
2017-10-09 16:23:51 +0200

    Improve styles on top post form's style on 2 columns

--- assembl/static2/js/app/components/debate/thread/topPostFormContainer.jsx
+++ assembl/static2/js/app/components/debate/thread/topPostFormContainer.jsx
@@ -1,4 +1,3 @@
-// @flow
 import React from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 import { Translate } from 'react-redux-i18n';
@@ -13,6 +12,11 @@ const mockMessageColumns = [
     name: 'Ajouter votre point de vue en faveur du thème'
   },
   {
+    color: '#ccc',
+    identifier: 'negative',
+    name: 'Ajouter votre point de vue en défaveur du thème'
+  },
+  {
     color: '#f9ebeb',
     identifier: 'negative',
     name: 'Ajouter votre point de vue en défaveur du thème'
@@ -20,7 +24,7 @@ const mockMessageColumns = [
 ];
 
 class topPostFormContainer extends React.Component {
-  getColumnsNumber() {
+  getMessageColumns() {
     let ideaOnColumns = [];
     if (mockMessageColumns.length === 0) {
       ideaOnColumns.push(1);
@@ -38,7 +42,12 @@ class topPostFormContainer extends React.Component {
           <Row>
             {ideaOnColumns.map((column, index) => {
               return (
-                <Col xs={12} md={12 / ideaOnColumns.length} style={{ backgroundColor: column.color }} key={index}>
+                <Col
+                  xs={12}
+                  md={12 / ideaOnColumns.length}
+                  style={{ backgroundColor: column.color }}
+                  key={`${column.identifier}-${index}`}
+                >
                   <div className="top-post-form">
                     <Row>
                       <Col
@@ -52,12 +61,16 @@ class topPostFormContainer extends React.Component {
                         <div className="start-discussion-container">
                           <div
                             className={
-                              mockMessageColumns.length > 0 ? 'start-discussion-icon' : 'start-discussion-icon onecolumn'
+                              mockMessageColumns.length > 0
+                                ? 'start-discussion-icon start-discussion-icon-2'
+                                : 'start-discussion-icon'
                             }
                           >
                             <span className="assembl-icon-discussion color" />
                           </div>
-                          <div className={mockMessageColumns.length > 0 ? 'start-discussion' : 'start-discussion onecolumn'}>
+                          <div
+                            className={mockMessageColumns.length > 0 ? 'start-discussion start-discussion-2' : 'start-discussion'}
+                          >
                             <h3 className="dark-title-3 no-margin">
                               {mockMessageColumns.length > 0 ? column.name : <Translate value="debate.thread.startDiscussion" />}
                             </h3>
