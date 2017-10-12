bde2af552254c201ea88ec255430b1e85fd7ae1b
Cédric Messiant <cedricmessiant@ecreall.com>
2017-10-12 09:59:26 +0200

    Don't render a link if it is useless in BoxWithHyphen

--- assembl/static2/js/app/components/common/boxWithHyphen.jsx
+++ assembl/static2/js/app/components/common/boxWithHyphen.jsx
@@ -3,6 +3,14 @@ import classnames from 'classnames';
 import React from 'react';
 import { Localize } from 'react-redux-i18n';
 
+type BoxProps = {
+  body: string,
+  date: string, // TODO:  date iso format type checking
+  hyphenStyle: Object,
+  subject: string,
+  title: string
+};
+
 type BoxWithHyphenProps = {
   additionalContainerClassNames: string,
   body: string,
@@ -13,31 +21,39 @@ type BoxWithHyphenProps = {
   title: string
 };
 
-const BoxWithHyphen = ({ additionalContainerClassNames, body, date, href, hyphenStyle, subject, title }: BoxWithHyphenProps) => {
+const Box = ({ body, date, hyphenStyle, subject, title }: BoxProps) => {
+  return (
+    <div className="insert-box">
+      <h3 className="dark-title-4 ellipsis">
+        <div>
+          {title}
+        </div>
+        <div className="ellipsis margin-xs">
+          {subject}
+        </div>
+      </h3>
+      <div className="box-hyphen" style={hyphenStyle}>
+        &nbsp;
+      </div>
+      <div className="date">
+        {date ? <Localize value={date} dateFormat="date.format2" /> : null}
+      </div>
+      <div className="insert-content margin-s">
+        <p dangerouslySetInnerHTML={{ __html: body }} />
+      </div>
+    </div>
+  );
+};
+
+const BoxWithHyphen = ({ additionalContainerClassNames, href, ...otherProps }: BoxWithHyphenProps) => {
   const containerClassNames = classnames([additionalContainerClassNames, 'box-with-hyphen']);
   return (
     <div className={containerClassNames}>
-      <a href={href}>
-        <div className="insert-box">
-          <h3 className="dark-title-4 ellipsis">
-            <div>
-              {title}
-            </div>
-            <div className="ellipsis margin-xs">
-              {subject}
-            </div>
-          </h3>
-          <div className="box-hyphen" style={hyphenStyle}>
-            &nbsp;
-          </div>
-          <div className="date">
-            {date ? <Localize value={date} dateFormat="date.format2" /> : null}
-          </div>
-          <div className="insert-content margin-s">
-            <p dangerouslySetInnerHTML={{ __html: body }} />
-          </div>
-        </div>
-      </a>
+      {href.length > 1
+        ? <a href={href}>
+          <Box {...otherProps} />
+        </a>
+        : <Box {...otherProps} />}
     </div>
   );
 };
--- assembl/static2/tests/unit/components/common/boxWithHyphen.spec.jsx
+++ assembl/static2/tests/unit/components/common/boxWithHyphen.spec.jsx
@@ -20,4 +20,21 @@ describe('BoxWithHyphen component', () => {
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+  it('should not render a link that wraps the box if href is empty', () => {
+    const props = {
+      additionalContainerClassNames: 'foobar',
+      body: 'Lorem ipsum dolor sit amet',
+      date: '2017-01-01T21:05:25.085948Z',
+      href: '',
+      hyphenStyle: {
+        borderColorTop: 'red'
+      },
+      subject: 'Foobar',
+      title: 'Synthesis'
+    };
+    const component = renderer.create(<BoxWithHyphen {...props} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
