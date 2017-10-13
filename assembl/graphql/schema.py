6c8fed590dacf65fb5a1ebce024e2e0681cbf0bb
Norman Meier <norman.meier@tuta.io>
2017-10-13 10:56:51 +0200

    ignore tombstoned sentiments in home sentiments count

--- assembl/graphql/schema.py
+++ assembl/graphql/schema.py
@@ -937,9 +937,10 @@ class Query(graphene.ObjectType):
     def resolve_total_sentiments(self, args, context, info):
         discussion_id = context.matchdict['discussion_id']
         discussion = models.Discussion.get(discussion_id)
-        return discussion.db.query(models.SentimentOfPost).filter(
-            models.SentimentOfPost.discussion.has(
-                id=discussion_id)
+        return discussion.db.query(models.SentimentOfPost
+            ).filter(
+                models.SentimentOfPost.discussion.has(id=discussion_id),
+                models.SentimentOfPost.tombstone_condition()
             ).count()
 
     def resolve_root_idea(self, args, context, info):
