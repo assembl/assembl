eb1a253c0c781d88a3412f1d4f150a4abccf54da
Cédric Messiant <cedricmessiant@ecreall.com>
2017-10-17 12:39:04 +0200

    Add alembic migration to create resource and resource_attachment tables

---
+++ assembl/alembic/versions/053f788ca313_resource_center.py
@@ -0,0 +1,64 @@
+"""resource_center
+
+Revision ID: 053f788ca313
+Revises: 025d088eb45a
+Create Date: 2017-10-16 18:16:11.329055
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '053f788ca313'
+down_revision = '025d088eb45a'
+
+from alembic import context, op
+import sqlalchemy as sa
+import transaction
+
+from assembl.lib import config
+
+
+def upgrade(pyramid_env):
+    with context.begin_transaction():
+        op.create_table(
+            'resource',
+            sa.Column('id', sa.Integer, primary_key=True),
+            sa.Column("title_id", sa.Integer, sa.ForeignKey("langstring.id")),
+            sa.Column("text_id", sa.Integer, sa.ForeignKey("langstring.id")),
+            sa.Column('embed_code', sa.Text())
+        )
+        op.create_table(
+            'resource_attachment',
+            sa.Column(
+                'id',
+                sa.Integer,
+                sa.ForeignKey(
+                    'attachment.id',
+                    ondelete="CASCADE",
+                    onupdate="CASCADE"
+                ),
+                primary_key=True
+            ),
+            sa.Column(
+                'resource_id',
+                sa.Integer,
+                sa.ForeignKey(
+                    'post.id',
+                    ondelete="CASCADE",
+                    onupdate="CASCADE"
+                ),
+                nullable=False,
+                index=True
+            ),
+        )
+
+    # Do stuff with the app's models here.
+    from assembl import models as m
+    db = m.get_session_maker()()
+    with transaction.manager:
+        pass
+
+
+def downgrade(pyramid_env):
+    with context.begin_transaction():
+        op.drop_table('resource')
+        op.drop_table('resource_attachment')
