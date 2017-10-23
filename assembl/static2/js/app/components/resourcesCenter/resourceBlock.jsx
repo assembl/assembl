01e168e230fc2988cbe4ee66794b73734d3935c4
Milan <milan.barande@hotmail.fr>
2017-10-23 15:23:56 +0200

    ResourcesCenter and ResourcesBlock converted to functional components

--- assembl/static2/js/app/components/resourcesCenter/resourceBlock.jsx
+++ assembl/static2/js/app/components/resourcesCenter/resourceBlock.jsx
@@ -1,40 +1,38 @@
 import React from 'react';
 
-class ResourceBlock extends React.Component {
-  render() {
-    const { index, title, description, media, doc } = this.props;
-    const { type, url } = media;
-    const isImgRight = index % 2 === 0;
-    const float = isImgRight ? 'right margin-case-left' : 'left margin-case-right';
+const ResourceBlock = (props) => {
+  const { index, title, description, media, doc } = props;
+  const { type, url } = media;
+  const isImgRight = index % 2 === 0;
+  const float = isImgRight ? 'right margin-case-left' : 'left margin-case-right';
 
-    return (
-      <div className="resource-block">
-        <div className="title-section">
-          <div className="title-hyphen" />
-          <h1 className="dark-title-1">
-            {title}
-          </h1>
-        </div>
-        <div className="resource-body">
-          {type === 'image' && <img src={url} alt="resource" className={float} />}
-          {type === 'embed' &&
-            <div className={float}>
-              <iframe title="resource-video" src={url} height={350} width={500} />
-            </div>}
-          <div className="resource-text">
-            {description}
-            {doc &&
-              <div className="resource-download-link">
-                <a href={doc} target="_blank" rel="noopener noreferrer">
-                  Download the report
-                </a>
-              </div>}
-          </div>
-          <div className="clear" />
-        </div>
+  return (
+    <div className="resource-block">
+      <div className="title-section">
+        <div className="title-hyphen" />
+        <h1 className="dark-title-1">
+          {title}
+        </h1>
       </div>
-    );
-  }
-}
+      <div className="resource-body">
+        {type === 'image' && <img src={url} alt="resource" className={float} />}
+        {type === 'embed' &&
+          <div className={float}>
+            <iframe title="resource-video" src={url} height={350} width={500} />
+          </div>}
+        <div className="resource-text">
+          {description}
+          {doc &&
+            <div className="resource-download-link">
+              <a href={doc} target="_blank" rel="noopener noreferrer">
+                Download the report
+              </a>
+            </div>}
+        </div>
+        <div className="clear" />
+      </div>
+    </div>
+  );
+};
 
 export default ResourceBlock;
