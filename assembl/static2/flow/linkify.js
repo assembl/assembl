906bd8b18bb1dedf6a85bdcd36b2337c5461d0ce
Cédric Messiant <cedricmessiant@ecreall.com>
2017-11-02 11:20:47 +0100

    Better youtube url handling in transformLinksInHtml function

---
+++ assembl/static2/flow/linkify.js
@@ -0,0 +1,4 @@
+// @flow
+declare module 'flow' {
+  declare module.exports: any;
+}
--- assembl/static2/tests/unit/utils/linkify.spec.js
+++ assembl/static2/tests/unit/utils/linkify.spec.js
@@ -10,12 +10,32 @@ describe('transformLinksInHtml function', () => {
     expect(actual).toEqual(expected);
   });
 
-  it('should turn youtube links into embedded videos', () => {
+  it('should turn www.youtube.com links into embedded videos', () => {
     const input = '<p>Foobar: https://www.youtube.com/watch?v=foobar</p>';
     const actual = transformLinksInHtml(input);
     const expected =
       '<p>Foobar: <a href="https://www.youtube.com/watch?v=foobar" class="linkified"' +
       ' target="_blank">https://www.youtube.com/watch?v=foobar</a>' +
+      '<div><iframe title="" src="https://www.youtube.com/embed/foobar" frameborder="0" allowfullscreen=""></iframe></div></p>';
+    expect(actual).toEqual(expected);
+  });
+
+  it('should turn youtube.com links into embedded videos', () => {
+    const input = '<p>Foobar: https://youtube.com/watch?v=foobar</p>';
+    const actual = transformLinksInHtml(input);
+    const expected =
+      '<p>Foobar: <a href="https://youtube.com/watch?v=foobar" class="linkified"' +
+      ' target="_blank">https://youtube.com/watch?v=foobar</a>' +
+      '<div><iframe title="" src="https://www.youtube.com/embed/foobar" frameborder="0" allowfullscreen=""></iframe></div></p>';
+    expect(actual).toEqual(expected);
+  });
+
+  it('should turn youtu.be links into embedded videos', () => {
+    const input = '<p>Foobar: https://youtu.be/foobar</p>';
+    const actual = transformLinksInHtml(input);
+    const expected =
+      '<p>Foobar: <a href="https://youtu.be/foobar" class="linkified"' +
+      ' target="_blank">https://youtu.be/foobar</a>' +
       '<div><iframe title="" src="https://www.youtube.com/embed/foobar" frameborder="0" allowfullscreen=""></iframe></div></p>';
     expect(actual).toEqual(expected);
   });
