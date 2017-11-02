8297c4697b43afd3d6c306c51b65d00333875249
Amen Souissi <amensouissi@ecreall.com>
2017-11-02 12:33:38 +0100

    fix the synthesis component test

--- assembl/static2/tests/unit/pages/synthesis.spec.jsx
+++ assembl/static2/tests/unit/pages/synthesis.spec.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import { Synthesis } from '../../../js/app/pages/synthesis';
+import { DumbSynthesis } from '../../../js/app/pages/synthesis';
 
-describe('Syntheses component', () => {
-  it('should match Syntheses snapshot', () => {
+describe('Synthesis component', () => {
+  it('should match Synthesis snapshot', () => {
     const props = {
+      routeParams: { slug: 'slugId' },
       synthesis: {
         id: 'fooId',
         subject: 'Foo',
-        imgUrl: 'http://foo.com/bar'
+        imgUrl: 'http://foo.com/foo',
+        ideas: [
+          {
+            id: 'barId',
+            title: 'Bar',
+            imgUrl: 'http://foo.com/bar',
+            synthesisTitle: 'Foo',
+            numContributors: 0,
+            numPosts: 0,
+            posts: { edges: [] },
+            phaseIdentifier: 'paseId'
+          }
+        ]
       }
     };
-    const rendered = renderer.create(<Synthesis {...props} />).toJSON();
+    const rendered = renderer.create(<DumbSynthesis {...props} />).toJSON();
     expect(rendered).toMatchSnapshot();
   });
 });
