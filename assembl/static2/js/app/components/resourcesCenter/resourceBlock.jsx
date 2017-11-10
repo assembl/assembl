3fc21b81d84c63a4d76c10c8dd62af1a3526c830
Milan <milan.barande@hotmail.fr>
2017-11-10 10:58:23 +0100

    iframe and images centered above the text instead of floating on mobiles and tablets

--- assembl/static2/js/app/components/resourcesCenter/resourceBlock.jsx
+++ assembl/static2/js/app/components/resourcesCenter/resourceBlock.jsx
@@ -1,5 +1,7 @@
 // @flow
 import React from 'react';
+import classnames from 'classnames';
+import { Grid } from 'react-bootstrap';
 
 export type Media = {
   type: string,
@@ -17,35 +19,45 @@ export type ResourceBlockProps = {
 
 const ResourceBlock = (props: ResourceBlockProps) => {
   const { index, title, text, embedCode, image, doc } = props;
-  const isImgRight = index % 2 === 0;
-  const float = isImgRight ? 'right margin-case-left' : 'left margin-case-right';
+  const isEven = index % 2 === 0;
+  const float = isEven ? 'media-right margin-case-left' : 'media-left margin-case-right';
+  const videoClassNames = classnames([float, 'resource-video']);
+  const imgClassNames = classnames([float, 'resource-img']);
+  const resourceBlockClassNames = isEven ? 'background-grey' : 'background-light';
 
   return (
-    <div className="resource-block">
-      <div className="title-section">
-        <div className="title-hyphen" />
-        <h1 className="dark-title-1">
-          {title}
-        </h1>
-      </div>
-      <div className="resource-body">
-        {image && <img src={image.externalUrl} alt="resource" className={float} />}
-        {embedCode &&
-          <div className={float}>
-            <iframe title="resource-video" src={embedCode} height={350} width={500} />
-          </div>}
-        <div className="resource-text">
-          {text}
-          {doc &&
-            <div className="resource-download-link">
-              <a href={doc.externalUrl} target="_blank" rel="noopener noreferrer">
-                Download the report
-              </a>
-            </div>}
+    <Grid fluid className={resourceBlockClassNames}>
+      <div className="max-container resource-block">
+        <div className="title-section">
+          <div className="title-hyphen" />
+          <h1 className="dark-title-1">
+            {title}
+          </h1>
         </div>
-        <div className="clear" />
+        <div className="resource-body">
+          {image &&
+            <div className="resource-img-container">
+              <img src={image.externalUrl} alt="resource" className={imgClassNames} />
+            </div>}
+          {embedCode &&
+            <div className="resource-video-container">
+              <div className={videoClassNames}>
+                <iframe title="resource-video" src={embedCode} className="resource-iframe" />
+              </div>
+            </div>}
+          <div className="resource-text">
+            <p dangerouslySetInnerHTML={{ __html: text }} />
+            {doc &&
+              <div className="resource-download-link">
+                <a href={doc.externalUrl} target="_blank" rel="noopener noreferrer">
+                  Download the report
+                </a>
+              </div>}
+          </div>
+          <div className="clear" />
+        </div>
       </div>
-    </div>
+    </Grid>
   );
 };
 
