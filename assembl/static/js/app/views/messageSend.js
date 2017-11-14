a8fff4c6d05e3a4f95105a1793634602fe126419
Swergas <swergas@yahoo.fr>
2017-11-14 17:55:56 +0100

    fix cannot access getShortTitleDisplayText property of undefined

--- assembl/static/js/app/views/messageSend.js
+++ assembl/static/js/app/views/messageSend.js
@@ -168,6 +168,7 @@ var messageSendView = Marionette.LayoutView.extend({
     }
     var show_cancel_button = ('show_cancel_button' in this.options) ? this.options.show_cancel_button : false;
     var reply_idea = ('reply_idea' in this.options) ? this.options.reply_idea : null;
+    var reply_idea_title = reply_idea ? reply_idea.getShortTitleDisplayText(this.translationData) : null;
     var reply_message_id = ('reply_message_id' in this.options) ? this.options.reply_message_id : null;
     var show_target_context_with_choice = ('show_target_context_with_choice' in this.options) ? this.options.show_target_context_with_choice : null;
     var message_send_title = this.options.message_send_title;
@@ -193,7 +194,7 @@ var messageSendView = Marionette.LayoutView.extend({
       msg_in_progress_body: this.options.msg_in_progress_body,
       msg_in_progress_title: this.options.msg_in_progress_title,
       reply_idea: reply_idea,
-      reply_idea_title: reply_idea.getShortTitleDisplayText(this.translationData),
+      reply_idea_title: reply_idea_title,
       show_cancel_button: show_cancel_button,
       reply_message_id: reply_message_id,
       show_target_context_with_choice: show_target_context_with_choice,
