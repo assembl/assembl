fdcf4fe452d3629206c2943a42b870d6b2125f15
Cédric Messiant <cedricmessiant@ecreall.com>
2017-11-16 13:48:13 +0100

    Add P_MANAGE_RESOURCE permission to R_ADMINISTRATOR and R_MODERATOR roles for all discussions

--- assembl/alembic/versions/053f788ca313_resource_center.py
+++ assembl/alembic/versions/053f788ca313_resource_center.py
@@ -18,6 +18,26 @@ from assembl.lib import config
 
 
 def upgrade(pyramid_env):
+    from assembl.auth import P_MANAGE_RESOURCE, R_MODERATOR, R_ADMINISTRATOR
+    from assembl import models as m
+
+    db = m.get_session_maker()()
+    with transaction.manager:
+        # Give the P_MANAGE_RESOURCE permission to R_ADMINISTRATOR and R_MODERATOR
+        p_manage_resource = db.query(m.Permission).filter_by(name=P_MANAGE_RESOURCE).one()
+        r_administrator = db.query(m.Role).filter_by(name=R_ADMINISTRATOR).one()
+        r_moderator = db.query(m.Role).filter_by(name=R_MODERATOR).one()
+        discussions = db.query(m.Discussion.id).all()
+        for discussion_id in discussions:
+            db.add(m.DiscussionPermission(
+                discussion_id = discussion_id,
+                role_id = r_administrator.id,
+                permission_id = p_manage_resource.id))
+            db.add(m.DiscussionPermission(
+                discussion_id = discussion_id,
+                role_id = r_moderator.id,
+                permission_id = p_manage_resource.id))
+
     with context.begin_transaction():
         op.add_column(
             'discussion', sa.Column('resources_center_title_id',
