998409a17ad0342a8053ce79348ae1e4a650eaf3
Cédric Messiant <cedricmessiant@ecreall.com>
2017-11-17 15:09:20 +0100

    Add section model

---
+++ assembl/tests/model_tests/test_section.py
@@ -0,0 +1,16 @@
+def test_add_section(admin_user, discussion, test_session):
+    from assembl.models.section import Section
+    from assembl.models import LangString
+
+    section = Section(
+        discussion_id=discussion.id,
+        title=LangString.create(u"My homepage section"),
+        url=u'http://www.example.com',
+        section_type='HOMEPAGE',
+        order=0.0
+    )
+
+    assert section.title.entries[0].value == u'My homepage section'
+    assert section.url == u'http://www.example.com'
+    assert section.order == 0.0
+    assert section.section_type == 'HOMEPAGE'
