a59ea27258d9ff63d0c87ea9466dac5fd76540c7
Cédric Messiant <cedricmessiant@ecreall.com>
2017-11-24 11:58:46 +0100

    Add flake8 to pre-commit (only on graphql module files), fix errors in graphql/view.py

--- assembl/graphql/view.py
+++ assembl/graphql/view.py
@@ -1,19 +1,11 @@
 from pyramid.view import view_config
-from pyramid.response import Response
-from pyramid.renderers import render_to_response
-from pyramid.settings import asbool
 from pyramid.httpexceptions import HTTPUnauthorized
 from pyramid.security import authenticated_userid, Everyone
-from pyramid.httpexceptions import (
-    HTTPNotFound, HTTPSeeOther)
 from graphql_wsgi import graphql_wsgi
 
 from assembl import models
 from assembl.auth import CrudPermissions
 from assembl.auth.util import get_permissions
-from assembl.auth import P_READ, P_ADD_EXTRACT
-from assembl.auth.util import user_has_permission
-from assembl.lib import config as AssemblConfig
 from assembl.graphql.schema import Schema
 
 
@@ -23,7 +15,8 @@ from assembl.graphql.schema import Schema
 def graphql_api(request):
     slug = request.matchdict['discussion_slug']
     # Check permission
-    discussion = models.Discussion.query.filter(models.Discussion.slug == slug).one()
+    discussion = models.Discussion.query.filter(
+        models.Discussion.slug == slug).one()
     if discussion is None:
         raise HTTPUnauthorized()
 
