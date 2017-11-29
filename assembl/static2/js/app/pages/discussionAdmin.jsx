1e9686037ae00579cf690ae1b0407d7fe1992aa2
Vincent Fretin <vincent.fretin@gmail.com>
2017-11-29 11:04:48 +0100

    rename languageAdmin.jsx to languageSection.jsx to be coherent with other components and so the the component name match the file name

--- assembl/static2/js/app/pages/discussionAdmin.jsx
+++ assembl/static2/js/app/pages/discussionAdmin.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Navbar from '../components/administration/navbar';
-import LanguageAdmin from '../components/administration/discussion/languageAdmin';
+import LanguageSection from '../components/administration/discussion/languageSection';
 
 const DiscussionAdmin = (props) => {
   const currentStep = parseInt(props.section, 10);
   return (
     <div className="discussion-admin">
-      {props.section === '1' && <LanguageAdmin {...props} />}
+      {props.section === '1' && <LanguageSection {...props} />}
       {!isNaN(currentStep) && <Navbar currentStep={currentStep} totalSteps={1} phaseIdentifier="discussion" />}
     </div>
   );
