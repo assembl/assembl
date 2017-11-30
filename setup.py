8673e1d1fb078ac85ba3055aa442ccca83f1661b
Vincent Fretin <vincent.fretin@gmail.com>
2017-11-30 17:12:56 +0100

    bump version to 2.6.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ tests_require = ['WebTest']
 
 
 setup(name='assembl',
-      version='2.6.1',
+      version='2.6.2',
       description='Collective Intelligence platform',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
