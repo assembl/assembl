75b9d9915d8c9f13d2ad603221f5e0de19a914e0
Vincent Fretin <vincent.fretin@gmail.com>
2017-12-09 14:56:53 +0100

    downgrade to css-loader 0.14.5 for build and rebuild performance

--- assembl/static2/webpack.dev.config.js
+++ assembl/static2/webpack.dev.config.js
@@ -26,6 +26,8 @@ function theme_entries() {
     return entries;
 }
 
+// Keep css-loader 0.14.5 for performance, see
+// https://github.com/webpack-contrib/css-loader/issues/124
 // For css hot reload to work, don't use ExtractTextPlugin
 module.exports = {
     // devtool: '#cheap-module-eval-source-map',  // http://webpack.github.io/docs/configuration.html#devtool
