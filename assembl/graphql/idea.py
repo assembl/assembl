58b6d88fe3a5a651641ef7ed9e22aa7fce35f921
Vincent Fretin <vincent.fretin@gmail.com>
2017-12-11 14:41:25 +0100

    do the filter in sql instead of Python for retrieving live descendants for ideas and questions

--- assembl/graphql/idea.py
+++ assembl/graphql/idea.py
@@ -52,14 +52,22 @@ class IdeaInterface(graphene.Interface):
 
     def resolve_num_posts(self, args, context, info):
         if isinstance(self, models.RootIdea):
-            # If this is RootIdea, do the sum of all descendants
+            # If this is RootIdea, do the sum of live descendants
             # excluding the root idea to be sure
             # we use the same counters that we see on each idea which are
             # based on countable states.
             # Don't use RootIdea.num_posts that give higher or lower count.
-            return sum([child.num_posts for child in self.get_all_descendants(
-                inclusive=False) if child.sqla_type in ('idea', 'question')
-                and child.tombstone_date is None])
+            descendants_query = models.Idea.get_descendants_query(
+                self.id, inclusive=False)
+            live_descendants = self.db.query(
+                models.Idea
+            ).filter(
+                models.Idea.id.in_(descendants_query)
+            ).filter(
+                models.Idea.sqla_type.in_(('idea', 'question'))
+            ).filter(
+                models.Idea.tombstone_date == None)  # noqa: E711
+            return sum([child.num_posts for child in live_descendants])
 
         return self.num_posts
 
