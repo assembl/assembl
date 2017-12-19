fec8ced5010f08b3b52bc8fd1675157054c314e7
Norman Meier <norman.meier@tuta.io>
2017-12-19 13:30:12 +0100

    youtube theater mode

--- assembl/static2/js/app/components/debate/common/post/postBody.jsx
+++ assembl/static2/js/app/components/debate/common/post/postBody.jsx
@@ -1,8 +1,12 @@
 // @flow
 import React from 'react';
+import get from 'lodash/get';
+import activeHtml from 'react-active-html';
+import YouTube from 'react-youtube';
 
 import PostTranslate from '../../common/translations/postTranslate';
 import { transformLinksInHtml } from '../../../../utils/linkify';
+import { youtubeRegexp } from '../../../../utils/globalFunctions';
 
 type Props = {
   body: string,
@@ -15,6 +19,77 @@ type Props = {
   originalLocale: string,
   translate: boolean,
   translationEnabled: boolean
+};
+
+class YouTubeWithTheater extends React.Component {
+  state: {
+    theaterMode?: boolean
+  };
+
+  timeout: number;
+
+  closeTheater = () => this.setState({ theaterMode: false });
+
+  openTheater = () => this.setState({ theaterMode: true });
+
+  render = () => {
+    const { videoId } = this.props;
+    const theaterMode = get(this, 'state.theaterMode', false);
+    const video = (
+      <YouTube
+        className="youtube-video"
+        videoId={videoId}
+        id={`youtube-${videoId}`} // might create problems if there is two times the same videoId
+        onPlay={() => {
+          clearTimeout(this.timeout);
+          this.openTheater();
+        }}
+        onPause={() => {
+          const skipTimeout = 200; // The timeout is there because skipping through the video fires an onPause followed by an onPlay
+          this.timeout = setTimeout(this.closeTheater, skipTimeout);
+        }}
+      />
+    );
+    return (
+      <div className={`youtube-theater ${theaterMode ? 'open' : ''}`}>
+        <div className="theater-content">
+          {video}
+          {theaterMode && <button onClick={this.closeTheater} className="close-theater-button assembl-icon-cancel" />}
+        </div>
+      </div>
+    );
+  };
+}
+
+const postBodyReplacementComponents = {
+  iframe: (attributes) => {
+    const { src } = attributes;
+    const regexpMatch = src.match(youtubeRegexp);
+    if (regexpMatch) {
+      const videoId = regexpMatch[1];
+      return <YouTubeWithTheater videoId={videoId} />;
+    }
+    return <iframe title="post-embed" {...attributes} />;
+  }
+};
+
+const Html = (props) => {
+  const { rawHtml, divRef, replacementComponents } = props;
+  /*
+   * The activeHtml() function will parse the raw html,
+   * replace specified tags with provided components
+   * and return a list of react elements
+  */
+  const nodes = activeHtml(rawHtml, replacementComponents);
+  const containerProps = { ...props };
+  delete containerProps.rawHtml;
+  delete containerProps.divRef;
+  delete containerProps.replacementComponents;
+  return (
+    <div ref={divRef} {...containerProps}>
+      {nodes}
+    </div>
+  );
 };
 
 const PostBody = ({
@@ -34,11 +109,14 @@ const PostBody = ({
       <PostTranslate contentLocale={contentLocale} id={id} lang={lang} originalLocale={originalLocale} translate={translate} />
     ) : null}
     {subject && <h3 className="dark-title-3">{subject}</h3>}
-    <div
-      className={`body ${bodyMimeType === 'text/plain' ? 'pre-wrap' : ''}`}
-      dangerouslySetInnerHTML={{ __html: transformLinksInHtml(body) }}
-      ref={bodyDivRef}
-    />
+    {body && (
+      <Html
+        rawHtml={transformLinksInHtml(body)}
+        className={`body ${bodyMimeType === 'text/plain' ? 'pre-wrap' : ''}`}
+        divRef={bodyDivRef}
+        replacementComponents={postBodyReplacementComponents}
+      />
+    )}
   </div>
 );
 
