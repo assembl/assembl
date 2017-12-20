cec4e53f36421ce2ee8c51ea5a58a1c531687de4
Cédric Messiant <cedricmessiant@ecreall.com>
2017-12-20 11:59:19 +0100

    Add className to MessagePage component title

--- assembl/static2/js/app/components/common/messagePage.jsx
+++ assembl/static2/js/app/components/common/messagePage.jsx
@@ -11,7 +11,7 @@ const MessagePage = ({ title, text }: Props) => (
   <Grid fluid>
     <div className="max-container">
       <Jumbotron>
-        {title && <h3>{title}</h3>}
+        {title && <h3 className="dark-title-3">{title}</h3>}
         <div>{text}</div>
       </Jumbotron>
     </div>
