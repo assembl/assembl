8e421c7e2ec0e71d3b96ba0013c0a15732d6b1b8
Norman Meier <norman.meier@tuta.io>
2017-12-22 16:49:30 +0100

    add langstrings_helpers and refactor VoteSession

---
+++ assembl/models/langstrings_helpers.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship, backref
+
+from .langstrings import LangString
+
+
+def langStringRelationship(Model, column_name):
+    column = getattr(Model, column_name + '_id')
+    model_name = Model.__tablename__
+    return relationship(
+        LangString,
+        lazy="joined",
+        single_parent=True,
+        primaryjoin=column == LangString.id,
+        backref=backref(model_name + "_from_" + column_name, lazy="dynamic"),
+        cascade="all, delete-orphan"
+    )
+
+
+def langStringId():
+    return Column(Integer(), ForeignKey(LangString.id))
+
+
+def declareLangStrings(Model, lang_strings_names):
+    """ Add langstrings ids and relationships properties to a model. """
+
+    for lang_string_name in lang_strings_names:
+        id_name = lang_string_name + '_id'
+        setattr(Model, id_name, property(langStringId()))
+        setattr(Model, lang_string_name, property(
+            langStringRelationship(Model, lang_string_name)))
+
+    LangString.setup_ownership_load_event(Model, lang_strings_names)
--- assembl/models/vote_session.py
+++ assembl/models/vote_session.py
@@ -1,96 +1,14 @@
-from sqlalchemy import (Column, Integer, ForeignKey)
-from sqlalchemy.orm import (relationship, backref)
-
-from .timeline import DiscussionSession
-from ..auth import (CrudPermissions, P_READ, P_ADMIN_DISC)
-from .langstrings import LangString
+from .timeline import DiscussionPhase
+from .langstrings_helpers import declareLangStrings
 
 
-class VoteSession(DiscussionSession):
+class VoteSession(DiscussionPhase):
+    """ A vote session phase.
+        Uses DiscussionPhase.description as frontend subtitle. """
     __tablename__ = "vote_session"
 
-    id = Column(Integer, primary_key=True)
 
-    crud_permissions = CrudPermissions(
-        create=P_ADMIN_DISC,
-        read=P_READ,
-        update=P_ADMIN_DISC,
-        delete=P_ADMIN_DISC,
-        read_owned=P_READ,
-        update_owned=P_ADMIN_DISC,
-        delete_owned=P_ADMIN_DISC,
-    )
-
-    title_id = Column(
-        Integer(), ForeignKey(LangString.id))
-    title = relationship(
-        LangString,
-        lazy="joined", single_parent=True,
-        primaryjoin=title_id == LangString.id,
-        backref=backref("vote_session_from_title", lazy="dynamic"),
-        cascade="all, delete-orphan")
-
-    subtitle_id = Column(
-        Integer(), ForeignKey(LangString.id))
-    subtitle = relationship(
-        LangString,
-        lazy="joined", single_parent=True,
-        primaryjoin=subtitle_id == LangString.id,
-        backref=backref("vote_session_from_subtitle", lazy="dynamic"),
-        cascade="all, delete-orphan")
-
-    instructions_section_title_id = Column(
-        Integer(), ForeignKey(LangString.id))
-    instructions_section_title = relationship(
-        LangString,
-        lazy="joined", single_parent=True,
-        primaryjoin=instructions_section_title_id == LangString.id,
-        backref=backref(
-            "vote_session_from_instructions_section_title", lazy="dynamic"
-        ),
-        cascade="all, delete-orphan")
-
-    propositions_section_title_id = Column(
-        Integer(), ForeignKey(LangString.id))
-    propositions_section_title = relationship(
-        LangString,
-        lazy="joined", single_parent=True,
-        primaryjoin=propositions_section_title_id == LangString.id,
-        backref=backref(
-            "vote_session_from_propositions_section_title", lazy="dynamic"
-        ),
-        cascade="all, delete-orphan")
-
-    discussion_id = Column(
-        Integer,
-        ForeignKey(
-            'discussion.id',
-            ondelete='CASCADE',
-            onupdate='CASCADE'
-        ),
-        nullable=False,
-        index=True,
-    )
-
-    discussion = relationship(
-        "Discussion",
-        backref=backref(
-            'vote_sessions',
-            cascade="all, delete-orphan"
-        ),
-    )
-
-    def get_discussion_id(self):
-        return self.discussion_id or self.discussion.id
-
-    @classmethod
-    def get_discussion_conditions(cls, discussion_id, alias_maker=None):
-        return (cls.discussion_id == discussion_id,)
-
-
-LangString.setup_ownership_load_event(VoteSession, [
-    'title',
-    'subtitle',
+declareLangStrings(VoteSession, [
     'instructions_section_title',
     'propositions_section_title'
 ])
