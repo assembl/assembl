0671dec1ea95635b3732990d685dab24fef7ecaf
Norman Meier <norman.n0ii.m@gmail.com>
2017-12-27 10:36:16 +0000

    test vote_session fixture validity

--- assembl/tests/model_tests/test_vote_session.py
+++ assembl/tests/model_tests/test_vote_session.py
@@ -1,2 +1,15 @@
-def test_vote_session(vote_session, test_session):
-    import pdb; pdb.set_trace();
+def en_value(lang_string):
+    return lang_string.entries[0].value
+
+def test_vote_session(vote_session):
+    assert en_value(vote_session.title)
+        == u"vote session fixture"
+    assert en_value(vote_session.description)
+        == u"vote session subtitle fixture"
+    assert en_value(vote_session.instructions_section_title)
+        == u"vote session instructions title fixture"
+    assert en_value(vote_session.instructions_section_content)
+        == u"vote session instructions fixture. Lorem ipsum dolor sit amet"
+    assert en_value(vote_session.propositions_section_title)
+        == u"vote session propositions section tile fixture"
+    assert vote_session.image_url == u"http://example.net/image.jpg"
