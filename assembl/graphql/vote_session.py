ed74e2eb7eefa6fd3f2169f7217056a07fb53424
Norman Meier <norman.meier@bluenove.com>
2018-01-02 13:08:15 +0000

    working helpers to declare graphql langstrings, used in VoteSession

--- assembl/graphql/vote_session.py
+++ assembl/graphql/vote_session.py
@@ -1,22 +1,17 @@
 import graphene
 from graphene.relay import Node
 from graphene_sqlalchemy import SQLAlchemyObjectType
-# from pyramid.httpexceptions import HTTPUnauthorized
-# from pyramid.security import Everyone
-
 from assembl import models
-# from assembl.auth import IF_OWNED, CrudPermissions
-# from assembl.auth.util import get_permissions
 
-from .langstring import (
-    LangStringEntry, LangStringEntryInput,
-    resolve_langstring, resolve_langstring_entries,
-    update_langstring_from_input_entries)
 from .types import SecureObjectType
 from .utils import abort_transaction_on_exception
+from .graphql_langstrings_helpers import (GraphQLLangstringsMixin,
+                                          update_langstrings, add_langstrings_input_attrs)
 
-resolve_prefix = "resolve_"
-entries_suffix = "_entries"
+# from assembl.auth import IF_OWNED, CrudPermissions
+# from assembl.auth.util import get_permissions
+# from pyramid.httpexceptions import HTTPUnauthorized
+# from pyramid.security import Everyone
 
 
 def add_and_flush(model):
@@ -42,78 +37,30 @@ def require_update_permission(cls, user_id, discussion_id):
 
 """
 
+langstrings_defs = {
+    "instructions_section_title": {},
+    "instructions_section_content": {},
+    "propositions_section_title": {},
+    "title": {
+        "relation_name": "discussion_phase"
+    },
+    "sub_title": {
+        "relation_name": "discussion_phase",
+        "target_name": "description"
+    }
+}
+
 
 class VoteSession(SecureObjectType, SQLAlchemyObjectType):
     class Meta:
         model = models.VoteSession
-        interfaces = (Node, )
+        interfaces = (Node, GraphQLLangstringsMixin(langstrings_defs))
         only_fields = ('id', )
 
-    title = graphene.String(lang=graphene.String())
-    title_entries = graphene.List(LangStringEntry)
-    sub_title = graphene.String(lang=graphene.String())
-    sub_title_entries = graphene.List(LangStringEntry)
     header_img_url = graphene.String()
-    instructions_section_title = graphene.String(lang=graphene.String())
-    instructions_section_title_entries = graphene.List(LangStringEntry)
-    instructions_section_content = graphene.String(lang=graphene.String())
-    instructions_section_content_entries = graphene.List(LangStringEntry)
-    propositions_section_title = graphene.String(lang=graphene.String())
-    propositions_section_title_entries = graphene.List(LangStringEntry)
-
-    def resolve_title(self, args, context, info):
-        return resolve_langstring(self.discussion_phase.title, args.get('lang'))
-
-    def resolve_title_entries(self, args, context, info):
-        return resolve_langstring_entries(self.discussion_phase, 'title')
-
-    def resolve_sub_title(self, args, context, info):
-        return resolve_langstring(self.discussion_phase.description, args.get('lang'))
-
-    def resolve_sub_title_entries(self, args, context, info):
-        return resolve_langstring_entries(self.discussion_phase, 'description')
 
     def resolve_header_img_url(self, args, context, info):
         return self.discussion_phase.image_url
-
-    def resolve_instructions_section_title(self, args, context, info):
-        return resolve_langstring(self.instructions_section_title, args.get('lang'))
-
-    def resolve_instructions_section_title_entries(self, args, context, info):
-        return resolve_langstring_entries(self, 'instructions_section_title')
-
-    def resolve_instructions_section_content(self, args, context, info):
-        return resolve_langstring(self.instructions_section_content, args.get('lang'))
-
-    def resolve_instructions_section_content_entries(self, args, context, info):
-        return resolve_langstring_entries(self, 'instructions_section_content')
-
-    def resolve_propositions_section_title(self, args, context, info):
-        return resolve_langstring(self.propositions_section_title, args.get('lang'))
-
-    def resolve_propositions_section_title_entries(self, args, context, info):
-        return resolve_langstring_entries(self, 'propositions_section_title')
-
-
-vote_session_langstrings = [
-    "instructions_section_title",
-    "instructions_section_content",
-    "propositions_section_title"
-]
-
-discussion_phase_langstrings = [
-    "title",
-    "description"
-]
-
-input_langstrings_names = ["title", "sub_title"] + vote_session_langstrings
-
-
-def attrs_dict(src, attrs_names):
-    attrs = {}
-    for attr_name in attrs_names:
-        attrs[attr_name] = getattr(src, attr_name)
-    return attrs
 
 
 class UpdateVoteSession(graphene.Mutation):
@@ -121,8 +68,7 @@ class UpdateVoteSession(graphene.Mutation):
         discussion_phase_id = graphene.Int(required=True)
         header_img_url = graphene.String()
 
-    for langstring_name in input_langstrings_names:
-        setattr(Input, langstring_name + entries_suffix, graphene.List(LangStringEntryInput))
+    add_langstrings_input_attrs(Input, langstrings_defs.keys())
 
     vote_session = graphene.Field(VoteSession)
 
@@ -131,43 +77,28 @@ class UpdateVoteSession(graphene.Mutation):
     def mutate(root, args, context, info):
         # user_id = context.authenticated_userid or Everyone
         # discussion_id = context.matchdict['discussion_id']
-
         # require_update_permission(models.DiscussionPhase, user_id, discussion_id) TODO: !!!
+        # TODO: Persmissions !!
 
         discussion_phase_id = args.get('discussion_phase_id')
         discussion_phase = models.DiscussionPhase.get(discussion_phase_id)
 
         if discussion_phase is None:
-            raise Exception("A vote session requires a discussion phase, check discussionPhaseId value")
+            raise Exception(
+                "A vote session requires a discussion phase, check discussionPhaseId value")
         if discussion_phase.identifier != "tokenVote":
-            raise Exception("A vote session can only be created or edited with a tokenVote discussion phase, check discussionPhaseId value")
+            raise Exception(
+                "A vote session can only be created or edited with a tokenVote discussion phase, check discussionPhaseId value")
 
+        # TODO: see if we can avoid this next(iter( thing with a one-to-one relationship
         vote_session = next(iter(discussion_phase.vote_session or []), None)
         if vote_session is None:
-            vote_session = models.VoteSession(discussion_phase=discussion_phase)
+            vote_session = models.VoteSession(
+                discussion_phase=discussion_phase)
 
-        for name in vote_session_langstrings:
-            update_langstring_from_input_entries(
-                vote_session,
-                name,
-                args.get(name + entries_suffix)
-            )
+        update_langstrings(vote_session, langstrings_defs, args)
         add_and_flush(vote_session)
 
-        title_entries = args.get('title' + entries_suffix)
-        if title_entries is not None:
-            update_langstring_from_input_entries(
-                discussion_phase,
-                'title',
-                title_entries
-            )
-        sub_title_entries = args.get('sub_title' + entries_suffix)
-        if sub_title_entries is not None:
-            update_langstring_from_input_entries(
-                discussion_phase,
-                'description',
-                sub_title_entries
-            )
         discussion_phase.image_url = args.get('header_img_url')
         add_and_flush(discussion_phase)
 
