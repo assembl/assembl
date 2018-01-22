8dc379ecbfb76082e8f4207013d9229bfb7a09a1
Cédric Messiant <cedricmessiant@ecreall.com>
2018-01-22 12:39:21 +0100

    Fix eslint on icons.jsx [ci skip]

--- assembl/static2/js/app/components/styleguide/icons.jsx
+++ assembl/static2/js/app/components/styleguide/icons.jsx
@@ -70,34 +70,37 @@ class Icons extends React.Component {
   render() {
     return (
       <div className="margin-xxl">
-        <h2 className="dark-title-2 underline" id="icons" style={{ borderBottom: "1px solid #ccc"}}>ICONS</h2>
+        <h2 className="dark-title-2 underline" id="icons" style={{ borderBottom: '1px solid #ccc' }}>
+          ICONS
+        </h2>
         <section>
           <h3 className="dark-title-3">complete icons list from the fontello font</h3>
-          {icons.map(icon =>
-          <div className="inline padding" key={icon} title={icon}>
-            <span className={icon}>&nbsp;</span>
-          </div>)}
+          {icons.map(icon => (
+            <div className="inline padding" key={icon} title={icon}>
+              <span className={icon}>&nbsp;</span>
+            </div>
+          ))}
         </section>
         <section>
           <h3 className="dark-title-3">icons variations</h3>
           <div className="inline padding">
-            normal<br/>
+            normal<br />
             <span className="assembl-icon-add">&nbsp;</span>
           </div>
           <div className="inline padding">
-            color<br/>
+            color<br />
             <span className="assembl-icon-add color">&nbsp;</span>
           </div>
           <div className="inline padding">
-            grey<br/>
+            grey<br />
             <span className="assembl-icon-add grey">&nbsp;</span>
           </div>
           <div className="inline padding">
-            black<br/>
+            black<br />
             <span className="assembl-icon-add black">&nbsp;</span>
           </div>
-          <div className="inline padding" style={{backgroundColor: 'gray', color: 'white'}}>
-            white<br/>
+          <div className="inline padding" style={{ backgroundColor: 'gray', color: 'white' }}>
+            white<br />
             <span className="assembl-icon-add white">&nbsp;</span>
           </div>
         </section>
@@ -123,8 +126,8 @@ class Icons extends React.Component {
               <MoreInfo size={25} />
             </div>
           </div>
-          <div style={{width: 100}}>
-            <Doughnut elements={[{color: 'green', count: 123}, {color: 'red', count: 12}]} />
+          <div style={{ width: 100 }}>
+            <Doughnut elements={[{ color: 'green', count: 123 }, { color: 'red', count: 12 }]} />
           </div>
           <div>
             <Pointer />
@@ -132,7 +135,7 @@ class Icons extends React.Component {
           <div>
             <Ellipsis />
           </div>
-          <div style={{backgroundColor:"#000", width:"200px"}}>
+          <div style={{ backgroundColor: '#000', width: '200px' }}>
             <div className="inline padding">
               <Link to="http://www.facebook.com" target="_blank">
                 <Glyphicon glyph="facebook" color="white" size={30} desc="Facebook" />
@@ -152,64 +155,62 @@ class Icons extends React.Component {
         </section>
         <section>
           <h3 className="dark-title-3">Code</h3>
-          <pre>
-            &lt;span className="assembl-icon-add"&gt;&lt;/span&gt;
-          </pre>
-          <pre>
-            &lt;span className="assembl-icon-add color"&gt;&lt;/span&gt;
-          </pre>
-          <pre>
-            &lt;span className="assembl-icon-add grey"&gt;&lt;/span&gt;
-          </pre>
-          <pre>
-            &lt;span className="assembl-icon-add black"&gt;&lt;/span&gt;
-          </pre>
-          <pre>
-            &lt;span className="assembl-icon-add white"&gt;&lt;/span&gt;
-          </pre>
+          <pre>&lt;span className="assembl-icon-add"&gt;&lt;/span&gt;</pre>
+          <pre>&lt;span className="assembl-icon-add color"&gt;&lt;/span&gt;</pre>
+          <pre>&lt;span className="assembl-icon-add grey"&gt;&lt;/span&gt;</pre>
+          <pre>&lt;span className="assembl-icon-add black"&gt;&lt;/span&gt;</pre>
+          <pre>&lt;span className="assembl-icon-add white"&gt;&lt;/span&gt;</pre>
           <pre>
             import Like from './components/svg/like'
             <div>&lt;div className="sentiment"&gt;</div>
-              <div style={{paddingLeft:`${20}px`}}>&lt;Like size=&#123;25&#125; /&gt;</div>
+            <div style={{ paddingLeft: `${20}px` }}>&lt;Like size=&#123;25&#125; /&gt;</div>
             <div>&lt;/div&gt;</div>
           </pre>
           <pre>
             import Disagree from './components/svg/disagree'
             <div>&lt;div className="sentiment"&gt;</div>
-              <div style={{paddingLeft:`${20}px`}}>&lt;Disagree size=&#123;25&#125; /&gt;</div>
+            <div style={{ paddingLeft: `${20}px` }}>&lt;Disagree size=&#123;25&#125; /&gt;</div>
             <div>&lt;/div&gt;</div>
           </pre>
           <pre>
             import DontUnderstand from './components/svg/dontUnderstand'
             <div>&lt;div className="sentiment"&gt;</div>
-              <div style={{paddingLeft:`${20}px`}}>&lt;DontUnderstand size=&#123;25&#125; /&gt;</div>
+            <div style={{ paddingLeft: `${20}px` }}>&lt;DontUnderstand size=&#123;25&#125; /&gt;</div>
             <div>&lt;/div&gt;</div>
           </pre>
           <pre>
             import MoreInfo from './components/svg/moreInfo'
             <div>&lt;div className="sentiment"&gt;</div>
-              <div style={{paddingLeft:`${20}px`}}>&lt;MoreInfo size=&#123;25&#125; /&gt;</div>
+            <div style={{ paddingLeft: `${20}px` }}>&lt;MoreInfo size=&#123;25&#125; /&gt;</div>
             <div>&lt;/div&gt;</div>
           </pre>
           <pre>
             import Doughnut from './components/svg/Doughnut'
             <div>&lt;div style=&#x7b;&#x7b;width: 100}}&gt;</div>
-              <div style={{paddingLeft:`${20}px`}}>&lt;Doughnut elements=&#x7b;[&#x7b;color: 'green', count: 123}, &#x7b;color: 'red', count: 12}]} /&gt;</div>
+            <div style={{ paddingLeft: `${20}px` }}>
+              &lt;Doughnut elements=&#x7b;[&#x7b;color: 'green', count: 123}, &#x7b;color: 'red', count: 12}]} /&gt;
+            </div>
             <div>&lt;/div&gt;</div>
           </pre>
           <pre>
             <div>&lt;Link to="http://www.facebook.com" target="_blank"&gt;</div>
-              <div style={{paddingLeft:`${20}px`}}>&lt;Glyphicon glyph="facebook" color="white" size={30} desc="Facebook"/&gt;</div>
+            <div style={{ paddingLeft: `${20}px` }}>
+              &lt;Glyphicon glyph="facebook" color="white" size={30} desc="Facebook"/&gt;
+            </div>
             <div>&lt;/Link&gt;</div>
           </pre>
           <pre>
             <div>&lt;Link to="http://www.linkedin.com" target="_blank"&gt;</div>
-              <div style={{paddingLeft:`${20}px`}}>&lt;Glyphicon glyph="linkedin" color="white" size={30} desc="Linkedin"/&gt;</div>
+            <div style={{ paddingLeft: `${20}px` }}>
+              &lt;Glyphicon glyph="linkedin" color="white" size={30} desc="Linkedin"/&gt;
+            </div>
             <div>&lt;/Link&gt;</div>
           </pre>
           <pre>
             <div>&lt;Link to="http://www.twitter.com" target="_blank"&gt;</div>
-              <div style={{paddingLeft:`${20}px`}}>&lt;Glyphicon glyph="twitter" color="white" size={30} desc="Twitter"/&gt;</div>
+            <div style={{ paddingLeft: `${20}px` }}>
+              &lt;Glyphicon glyph="twitter" color="white" size={30} desc="Twitter"/&gt;
+            </div>
             <div>&lt;/Link&gt;</div>
           </pre>
         </section>
