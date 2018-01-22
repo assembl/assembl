685c56a7008171d4db3e899ebab4a5c399499ef5
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2018-01-22 20:09:58 +0100

    Update version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ tests_require = ['WebTest']
 
 
 setup(name='assembl',
-      version='2.7.8',
+      version='2.9.0',
       description='Collective Intelligence platform',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
