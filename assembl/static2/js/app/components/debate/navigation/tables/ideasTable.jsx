0e5e198df562f6011650b103960f343b29d67d67
Amen Souissi <amensouissi@ecreall.com>
2018-01-26 12:06:19 +0100

    add the navigation tables components

---
+++ assembl/static2/js/app/components/debate/navigation/tables/ideasTable.jsx
@@ -0,0 +1,34 @@
+// @flow
+import React from 'react';
+import { connect } from 'react-redux';
+import { graphql, compose } from 'react-apollo';
+
+import AllIdeasQuery from '../../../../graphql/AllIdeasQuery.graphql';
+import MenuList, { type ItemNode } from './menuList';
+import withLoadingIndicator from '../../../common/withLoadingIndicator';
+
+type IdeasTableProps = {
+  identifier: string,
+  onMenuItemClick: Function,
+  data: {
+    loading: boolean,
+    error: Object,
+    ideas: Array<ItemNode>,
+    rootIdea: ItemNode
+  }
+};
+
+export function DumbIdeasTable(props: IdeasTableProps) {
+  const { identifier, onMenuItemClick, data } = props;
+  const { ideas, rootIdea } = data;
+  return <MenuList items={ideas} rootItem={rootIdea && rootIdea.id} identifier={identifier} onMenuItemClick={onMenuItemClick} />;
+}
+
+const IdeasTableWithData = graphql(AllIdeasQuery);
+
+const mapStateToProps = state => ({
+  lang: state.i18n.locale,
+  debate: state.debate
+});
+
+export default compose(connect(mapStateToProps), IdeasTableWithData, withLoadingIndicator())(DumbIdeasTable);
---
+++ assembl/static2/js/app/components/debate/navigation/tables/index.jsx
@@ -0,0 +1,3 @@
+// @flow
+export { default as SurveyTable } from './surveyTable';
+export { default as IdeasTable } from './ideasTable';
