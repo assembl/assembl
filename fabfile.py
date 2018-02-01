6e9bf7bac824694397e2f114c2ec55e567d84fac
mohamedzayed92 <zayed2.mohamed@gmail.com>
2018-02-01 09:35:38 +0100

    fixing comments and function name
    Fixing function name and replacing list with tuples

--- fabfile.py
+++ fabfile.py
@@ -1663,17 +1663,17 @@ def docker_compose():
 
 
 @task
-def get_ssl_certificates():
+def set_ssl_certificates():
     if env.oscp_path:
         root_certificate = run('curl https://letsencrypt.org/certs/isrgrootx1.pem.txt')
         intermediate_certificate_1 = run('curl https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem.txt')
         intermediate_certificate_2 = run('curl https://letsencrypt.org/certs/letsencryptauthorityx3.pem.txt')
         with open(env.oscp_path, 'w') as certificates_file:
-            for certificate_file in [root_certificate, intermediate_certificate_1, intermediate_certificate_2]:
+            for certificate_file in (root_certificate, intermediate_certificate_1, intermediate_certificate_2):
                 certificates_file.write(certificate_file)
                 certificates_file.write('\n')
     else:
-        print("Can't get ssl certificates, env.oscp_path does not exist")
+        print(yellow("Can't set ssl certificates, env.oscp_path is not set"))
 
 
 @task
