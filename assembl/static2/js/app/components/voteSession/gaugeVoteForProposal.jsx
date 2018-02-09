919eac5842e83d12a3f419ebd05214ec42e5320c
Swergas <swergas@yahoo.fr>
2018-02-09 17:51:21 +0100

    show gauge

--- assembl/static2/js/app/components/voteSession/gaugeVoteForProposal.jsx
+++ assembl/static2/js/app/components/voteSession/gaugeVoteForProposal.jsx
@@ -1,10 +1,33 @@
 // @flow
 import React from 'react';
+import Slider from 'rc-slider';
+
+// There is a conflict of pre-commit rules between eslint and flow about numeric object keys
+/* eslint-disable */
+const marks = {
+  '-10': '-10°C',
+  '0': '<strong>0°C</strong>',
+  '26': '26°C',
+  '37': '37°C',
+  '50': '50°C',
+  '100': {
+    style: {
+      color: 'red'
+    },
+    label: '<strong>100°C</strong>'
+  }
+};
+/* eslint-enable */
 
 type Props = {
   instructions: string
 };
 
-const GaugeVoteForProposal = ({ instructions }: Props) => <div className="gauge">{instructions}</div>;
+const GaugeVoteForProposal = ({ instructions }: Props) => (
+  <div className="gauge-vote-for-proposal">
+    <p>{instructions}</p>
+    <Slider min={-10} marks={marks} step={10} included={false} defaultValue={20} />
+  </div>
+);
 
 export default GaugeVoteForProposal;
--- assembl/static2/js/app/pages/voteSession.jsx
+++ assembl/static2/js/app/pages/voteSession.jsx
@@ -36,7 +36,7 @@ const DumbVoteSession = ({
     <Grid fluid>
       <Section title={instructionsSectionTitle}>
         <Row>
-          <Col mdOffset={3} md={8} smOffset={1} sm={10}>
+          <Col mdOffset={1} md={10} smOffset={1} sm={10}>
             <div dangerouslySetInnerHTML={{ __html: instructionsSectionContent }} className="vote-instructions" />
             {/* INSERT THE TOKENS HERE */}
           </Col>
