c20b54e5e5783d0aa01583b8439ac5a08a55c981
Cédric Messiant <cedricmessiant@ecreall.com>
2018-02-26 15:57:34 +0100

    Add tests for Proposal component

---
+++ assembl/static2/tests/unit/components/voteSession/Proposal.spec.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Proposal from '../../../../js/app/components/voteSession/proposal';
+import * as fakeData from './fakeData';
+
+describe('Proposal component', () => {
+  const voteForProposalSpy = jest.fn();
+  const voteForProposalGaugeSpy = jest.fn();
+  it('should match Proposal snapshot', () => {
+    const { remainingTokensByCategory, tokenVotes, tokenVoteModule, textGaugeModule } = fakeData;
+    const props = {
+      description: 'Try to override the CSS transmitter, maybe it will program the solid state program!',
+      id: 'my-proposal',
+      modules: [tokenVoteModule, textGaugeModule],
+      remainingTokensByCategory: remainingTokensByCategory,
+      title: 'Use the online USB transmitter, then you can reboot the cross-platform application!',
+      tokenVotes: tokenVotes,
+      voteForProposal: voteForProposalSpy,
+      voteForProposalGauge: voteForProposalGaugeSpy
+    };
+    const rendered = renderer.create(<Proposal {...props} />).toJSON();
+    expect(rendered).toMatchSnapshot();
+  });
+});
