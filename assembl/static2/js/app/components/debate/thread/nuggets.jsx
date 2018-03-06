28a8f6e3492b0ea8e0c10ac96db022f6d55aeed1
Swergas <swergas@yahoo.fr>
2018-03-06 17:33:46 +0100

    show an extract on the left differently if harvesting mode is on

--- assembl/static2/js/app/components/debate/thread/nuggets.jsx
+++ assembl/static2/js/app/components/debate/thread/nuggets.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import { getDomElementOffset, computeDomElementOffset } from '../../../utils/globalFunctions';
 
@@ -45,15 +46,59 @@ class Nuggets extends React.Component {
   }
 
   render() {
-    const { extracts } = this.props;
+    const { extracts, isHarvesting } = this.props;
     const { top } = this.state;
+    if (isHarvesting) {
+      if (Array.isArray(extracts) && extracts.length > 0) {
+        return (
+          <div
+            ref={(node) => {
+              this.node = node;
+            }}
+            className="extracts extracts--is-harvesting"
+            style={Nuggets.topToStyle(top)}
+          >
+            {extracts.map(extract => (
+              <div key={extract.id} className="extract">
+                <div className="extract-header">
+                  <div className="extract-harvester">
+                    <div className="extract-harvester-left">
+                      <div className="extract-harvester-avatar">
+                        <img
+                          src="/static2/img/icons/avatar.png?TODO"
+                          width="30"
+                          height="30"
+                          alt="Albert Boyd TODO"
+                          onError={(e) => {
+                            const target = e.target;
+                            target.src = '/static2/img/icons/avatar.png';
+                          }}
+                        />
+                      </div>
+                    </div>
+                    <div className="extract-harvester-right">
+                      <div className="extract-harvester-name">Albert Boyd TODO</div>
+                      <div className="extract-harvesting-date">2 days ago TODO</div>
+                    </div>
+                  </div>
+                  <div className="extract-header-menu" />
+                </div>
+                <div className="extract-txt">{extract.body}</div>
+              </div>
+            ))}
+          </div>
+        );
+      }
+      return null;
+    }
+
     const importantExtracts = Array.isArray(extracts) && extracts.filter(({ important }) => important);
     return importantExtracts && importantExtracts.length > 0 ? (
       <div
         ref={(node) => {
           this.node = node;
         }}
-        className="extracts"
+        className="extracts extracts--is-not-harvesting"
         style={Nuggets.topToStyle(top)}
       >
         <div className="badges">
@@ -77,4 +122,8 @@ class Nuggets extends React.Component {
 // Vertical space between overflowing extracts in px
 Nuggets.SPACER_SIZE = 100;
 
-export default Nuggets;
+const mapStateToProps = state => ({
+  isHarvesting: state.context.isHarvesting
+});
+
+export default connect(mapStateToProps)(Nuggets);
