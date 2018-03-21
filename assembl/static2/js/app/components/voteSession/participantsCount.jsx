99f851fa4fb76c4ccb2b2f6b161a79c4ea6eecc8
Milan <milan.barande@hotmail.fr>
2018-03-21 14:14:37 +0100

    correction

--- assembl/static2/js/app/components/voteSession/participantsCount.jsx
+++ assembl/static2/js/app/components/voteSession/participantsCount.jsx
@@ -4,25 +4,13 @@ import { Translate } from 'react-redux-i18n';
 
 type Props = { count: number };
 
-const ParticipantsCount = ({ count }: Props) => {
-  let msgid;
-
-  if (count === 0) {
-    msgid = 'debate.voteSession.participantsCount_0';
-  } else if (count === 1) {
-    msgid = 'debate.voteSession.participantsCount_1';
-  } else {
-    msgid = 'debate.voteSession.participantsCount_several';
-  }
-
-  return (
-    <div className="participants-count box center">
-      <div className="assembl-icon-profil black" />
-      <div className="margin-m">
-        <Translate value={msgid} count={count} />
-      </div>
+const ParticipantsCount = ({ count }: Props) => (
+  <div className="participants-count box center">
+    <div className="assembl-icon-profil black" />
+    <div className="margin-m">
+      <Translate value={'debate.voteSession.participantsCount'} count={count} />
     </div>
-  );
-};
+  </div>
+);
 
 export default ParticipantsCount;
