8a517bca6fee86d956ac02b095a296d0a3df17a0
Milan <milan.barande@hotmail.fr>
2018-03-21 13:50:07 +0100

    different sentances depending on number of voters

--- assembl/static2/js/app/components/voteSession/participantsCount.jsx
+++ assembl/static2/js/app/components/voteSession/participantsCount.jsx
@@ -4,13 +4,25 @@ import { Translate } from 'react-redux-i18n';
 
 type Props = { count: number };
 
-const ParticipantsCount = ({ count }: Props) => (
-  <div className="participants-count box center">
-    <div className="assembl-icon-profil black" />
-    <div className="margin-m">
-      <Translate value="debate.voteSession.participantsCount" count={count} />
+const ParticipantsCount = ({ count }: Props) => {
+  let msgid;
+
+  if (count === 0) {
+    msgid = 'debate.voteSession.participantsCount_0';
+  } else if (count === 1) {
+    msgid = 'debate.voteSession.participantsCount_1';
+  } else {
+    msgid = 'debate.voteSession.participantsCount_several';
+  }
+
+  return (
+    <div className="participants-count box center">
+      <div className="assembl-icon-profil black" />
+      <div className="margin-m">
+        <Translate value={msgid} count={count} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ParticipantsCount;
