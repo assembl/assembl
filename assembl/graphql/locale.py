be6ef1742419c2240e43c89459d52b8121a09317
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2018-03-21 19:11:15 +0100

    Add a resolver for label in Locale

--- assembl/graphql/locale.py
+++ assembl/graphql/locale.py
@@ -1,6 +1,15 @@
 import graphene
+from assembl import models
 
 
 class Locale(graphene.ObjectType):
     locale_code = graphene.String(required=True)
-    label = graphene.String(required=False)
+    label = graphene.String(required=False, lang=graphene.String(required=False))
+
+    def resolve_label(self, args, context, info):
+        lang = args.get('lang')
+        if not lang:
+            return None
+        target_locale = models.Locale.get_or_create(lang)
+        labels = models.LocaleLabel.names_in_locale(target_locale)
+        return labels[self.locale_code]
