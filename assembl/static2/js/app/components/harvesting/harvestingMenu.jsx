fde06a078dce964b6e37d7d9b6194f0e8f12d8d6
paolina92 <paolinardc@gmail.com>
2018-03-23 17:50:30 +0100

    use isHarvesting from the store to display the anchor

--- assembl/static2/js/app/components/harvesting/harvestingMenu.jsx
+++ assembl/static2/js/app/components/harvesting/harvestingMenu.jsx
@@ -1,20 +1,27 @@
 // @flow
 import React from 'react';
+import { connect } from 'react-redux';
 import addPostExtractMutation from '../../graphql/mutations/addPostExtract.graphql'; // eslint-disable-line
 import updateExtractMutation from '../../graphql/mutations/updateExtract.graphql'; // eslint-disable-line
 import deleteExtractMutation from '../../graphql/mutations/deleteExtract.graphql'; // eslint-disable-line
 import HarvestingAnchor from './harvestingAnchor';
 import HarvestingBox from './harvestingBox';
 
+type Props = {
+  isHarvestingMode: boolean
+};
+
 type State = {
   showHarvestingBox: boolean
 };
 
-class HarvestingMenu extends React.Component<void, *, State> {
+class HarvestingMenu extends React.Component<void, Props, State> {
+  props: Props;
+
   state: State;
 
-  constructor() {
-    super();
+  constructor(props: Props) {
+    super(props);
     this.state = {
       showHarvestingBox: false
     };
@@ -38,14 +45,19 @@ class HarvestingMenu extends React.Component<void, *, State> {
 
   render() {
     const { showHarvestingBox } = this.state;
+    const { isHarvestingMode } = this.props;
     const extract = window.getSelection().toString();
     return (
       <div>
         {showHarvestingBox && <HarvestingBox extract={extract} cancelHarvesting={this.cancelHarvesting} />}
-        <HarvestingAnchor handleClick={this.handleClick} handleMouseDown={this.handleMouseDown} />
+        {isHarvestingMode && <HarvestingAnchor handleClick={this.handleClick} handleMouseDown={this.handleMouseDown} />}
       </div>
     );
   }
 }
 
-export default HarvestingMenu;
+const mapStateToProps = ({ context }) => ({
+  isHarvestingMode: context.isHarvesting
+});
+
+export default connect(mapStateToProps)(HarvestingMenu);
