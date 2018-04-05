c044fd18ad09fcd7d1ba264b31fd4d27c3d9371d
Milan <milan.barande@hotmail.fr>
2018-04-05 14:57:48 +0200

    survey empty attachment bugfix

--- assembl/static2/js/app/pages/survey.jsx
+++ assembl/static2/js/app/pages/survey.jsx
@@ -219,7 +219,7 @@ export default compose(
 
       return {
         hasErrors: false,
-        imgUrl: img.externalUrl,
+        imgUrl: img ? img.externalUrl : '',
         loading: false,
         media: media,
         questions: questions,
