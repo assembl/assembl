5d387b88a3e6ac409ce0dd6d67a58ada64086080
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2018-04-12 16:26:41 +0200

    Remove restrictions on locale label

--- assembl/graphql/locale.py
+++ assembl/graphql/locale.py
@@ -1,15 +1,6 @@
 import graphene
-from assembl import models
 
 
 class Locale(graphene.ObjectType):
     locale_code = graphene.String(required=True)
-    label = graphene.String(required=False, lang=graphene.String(required=False))
-
-    def resolve_label(self, args, context, info):
-        lang = args.get('lang')
-        if not lang:
-            return None
-        target_locale = models.Locale.get_or_create(lang)
-        labels = models.LocaleLabel.names_in_locale(target_locale)
-        return labels[self.locale_code]
+    label = graphene.String(required=False)
