af5e6386cc782bc0eacd98985ebb27eef874123b
Milan <milan.barande@hotmail.fr>
2018-05-18 13:54:33 +0200

    wip: create page for thread and multicolumn extract

--- assembl/static2/js/app/pages/multiColumnsAdmin.jsx
+++ assembl/static2/js/app/pages/multiColumnsAdmin.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import ExportSection from '../components/administration/exportSection';
 
-class MultiColumnsAdmin extends React.Component {
+class MulticolumnAdmin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      exportLocale: null,
+      refetching: false,
+      translate: false
+    };
+  }
+
+  handleExportLocaleChange = (locale) => {
+    this.setState({ exportLocale: locale });
+  };
+
   render() {
-    return <p>Administration de la phase MultiColumnsAdmin</p>;
+    const { section, languages, debateId } = this.props;
+    const { translate } = this.state;
+    const exportLocale = this.state.exportLocale || (languages && languages[0].locale);
+    const translation = translate && exportLocale ? `?lang=${exportLocale}` : '?'; // FIXME: using '' instead of '?' does not work
+    const exportLink = '#';
+    return (
+      <div className="multicolumn-admin">
+        {section === '1' && (
+          // When the other sections of this admin will be added, this one will be the last section and not '1'
+          <ExportSection
+            withLanguageOptions
+            handleExportLocaleChange={this.handleExportLocaleChange}
+            handleTranslationChange={this.handleTranslationChange}
+            exportLink={exportLink}
+            translate={translate}
+            exportLocale={exportLocale}
+            languages={languages}
+            annotation="multicolumnAnnotation"
+          />
+        )}
+      )
+      </div>
+    );
   }
 }
 
-export default MultiColumnsAdmin;
+export default MulticolumnAdmin;
--- assembl/static2/js/app/pages/threadAdmin.jsx
+++ assembl/static2/js/app/pages/threadAdmin.jsx
@@ -1,8 +1,44 @@
 import React from 'react';
+import ExportSection from '../components/administration/exportSection';
 
 class ThreadAdmin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      exportLocale: null,
+      refetching: false,
+      translate: false
+    };
+  }
+
+  handleExportLocaleChange = (locale) => {
+    this.setState({ exportLocale: locale });
+  };
+
   render() {
-    return <p>Administration</p>;
+    const { section, languages, debateId } = this.props;
+    const { translate } = this.state;
+    const exportLocale = this.state.exportLocale || (languages && languages[0].locale);
+    const translation = translate && exportLocale ? `?lang=${exportLocale}` : '?'; // FIXME: using '' instead of '?' does not work
+    const exportLink = '#';
+    return (
+      <div className="thread-admin">
+        {section === '1' && (
+          // When the other sections of this admin will be added, this one will be the last section and not '1'
+          <ExportSection
+            withLanguageOptions
+            handleExportLocaleChange={this.handleExportLocaleChange}
+            handleTranslationChange={this.handleTranslationChange}
+            exportLink={exportLink}
+            translate={translate}
+            exportLocale={exportLocale}
+            languages={languages}
+            annotation="threadAnnotation"
+          />
+        )}
+      )
+      </div>
+    );
   }
 }
 
