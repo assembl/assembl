e26d4a3d3c630737bdd47acca69c63631b89cbc6
Swergas <swergas@yahoo.fr>
2018-05-29 09:55:26 +0200

    rebase and re-branch alembic update version

--- assembl/alembic/versions/e3f681bd70e0_debate_administration_landing_page_.py
+++ assembl/alembic/versions/e3f681bd70e0_debate_administration_landing_page_.py
@@ -1,14 +1,14 @@
 """debate administration landing page header
 
 Revision ID: e3f681bd70e0
-Revises: 2a7357d60fa5
+Revises: ce427c9d6013
 Create Date: 2018-05-25 16:59:00.488718
 
 """
 
 # revision identifiers, used by Alembic.
 revision = 'e3f681bd70e0'
-down_revision = '2a7357d60fa5'
+down_revision = 'ce427c9d6013'
 
 from alembic import context, op
 import sqlalchemy as sa
