518a1f92da563b2951d51216543de3aaf7562b51
Vincent Fretin <vincent.fretin@gmail.com>
2018-06-06 16:18:14 +0200

    modify down revision

--- assembl/alembic/versions/e3f681bd70e0_debate_administration_landing_page_.py
+++ assembl/alembic/versions/e3f681bd70e0_debate_administration_landing_page_.py
@@ -1,14 +1,14 @@
 """debate administration landing page header
 
 Revision ID: e3f681bd70e0
-Revises: 407441ce1b20
+Revises: 8d704ad414e4
 Create Date: 2018-05-25 16:59:00.488718
 
 """
 
 # revision identifiers, used by Alembic.
 revision = 'e3f681bd70e0'
-down_revision = '407441ce1b20'
+down_revision = '8d704ad414e4'
 
 from alembic import context, op
 import sqlalchemy as sa
