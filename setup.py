c4a87351ac6f11e4c3f5fd4229ee196de0112cfb
Vincent Fretin <vincent.fretin@gmail.com>
2018-06-15 11:51:44 +0200

    bump version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ tests_require = ['WebTest']
 
 
 setup(name='assembl',
-      version='2.12.11',
+      version='2.12.12',
       description='Collective Intelligence platform',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
