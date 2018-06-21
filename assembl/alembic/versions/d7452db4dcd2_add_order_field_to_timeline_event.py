aa0ec3be277ede512efc65d72e5ff6f35f7f60b2
Milan <milan.barande@hotmail.fr>
2018-06-21 11:49:05 +0200

    add order for existing timeline phases

--- assembl/alembic/versions/d7452db4dcd2_add_order_field_to_timeline_event.py
+++ assembl/alembic/versions/d7452db4dcd2_add_order_field_to_timeline_event.py
@@ -19,11 +19,18 @@ from assembl.lib import config
 
 
 def upgrade(pyramid_env):
+    from assembl import models as m
     with context.begin_transaction():
         op.add_column(
             'timeline_event',
             sa.Column('order', sa.Float, nullable=False, default=0.0, server_default='0'))
 
+    db = m.get_session_maker()()
+    with transaction.manager:
+        discussions = db.query(m.Discussion).all()
+        for discussion in discussions:
+            for n, phase in enumerate(discussion.timeline_phases):
+                phase.order = n + 1
 
 def downgrade(pyramid_env):
     with context.begin_transaction():
