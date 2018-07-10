250d7b3b35e8e479ab6a36d13f07d44699651dfc
Vincent Fretin <vincent.fretin@gmail.com>
2018-07-10 11:16:50 +0200

    fix width for new flags on Firefox

--- assembl/static2/js/app/components/svg/flags/tr.jsx
+++ assembl/static2/js/app/components/svg/flags/tr.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class TrFlag extends React.Component {
   render() {
     return (
-      <svg x="0px" y="0px" viewBox="0 0 512 512" style={{ enableBackground: 'new 0 0 512 512' }}>
+      <svg width="20px" height="20px" x="0px" y="0px" viewBox="0 0 512 512" style={{ enableBackground: 'new 0 0 512 512' }}>
         <circle style={{ fill: '#D80027' }} cx="256" cy="256" r="256" />
         <g>
           <polygon
