91c07252f695014d068bd56a8be15eaa4ec57438
mohamedzayed92 <zayed2.mohamed@gmail.com>
2018-07-20 14:30:58 +0200

    Using ids to delete the extra profile fields.

--- assembl/alembic/versions/7bf35c9261d9_delete_extra_fields_for_deleted_users.py
+++ assembl/alembic/versions/7bf35c9261d9_delete_extra_fields_for_deleted_users.py
@@ -8,7 +8,7 @@ Create Date: 2018-06-13 14:14:41.037265
 
 # revision identifiers, used by Alembic.
 revision = '7bf35c9261d9'
-down_revision = 'e3f681bd70e0'
+down_revision = 'e757aefa55e1'
 
 from alembic import context, op
 import sqlalchemy as sa
@@ -22,7 +22,11 @@ def upgrade(pyramid_env):
     from assembl import models as m
     db = m.get_session_maker()()
     with transaction.manager:
-        db.query(m.ProfileField).join(m.User).filter(m.User.is_deleted).delete()
+        ids=db.query(m.ProfileField.id).join(m.User).filter(m.User.is_deleted==True).all()
+        ids = [id for (id,) in ids]
+        for pf in db.query(m.ProfileField).all():
+			if pf.id in ids:
+				pf.delete()
 
 
 def downgrade(pyramid_env):
