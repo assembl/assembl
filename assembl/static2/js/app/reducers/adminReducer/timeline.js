5afdd3d12eebf9b34ea9295b073b88d57c959a9d
Vincent Fretin <vincent.fretin@gmail.com>
2018-07-27 20:38:39 +0200

    dynamically check hasConflictingDates, not only on save

--- assembl/static2/js/app/reducers/adminReducer/timeline.js
+++ assembl/static2/js/app/reducers/adminReducer/timeline.js
@@ -42,11 +42,25 @@ const emptyPhase = Map({
   image: defaultImage
 });
 
-const getHasConflictingDates = (phases, id, start, end) => {
-  const phaseIndex = phases.map(p => p.id).indexOf(id);
-  const previousPhase = phases[phaseIndex - 1];
-  const nextPhase = phases[phaseIndex + 1];
-  return (start && previousPhase && start.isBefore(previousPhase.end)) || (end && nextPhase && end.isAfter(nextPhase.start));
+const hasConflictingDates = (phase, phases) => {
+  const start = phase.get('start');
+  const end = phase.get('end');
+  const id = phase.get('id');
+  const orderedPhasesMap = phases.filter(p => !p.get('_toDelete')).sortBy(p => p.get('order'));
+  const phaseIndex = orderedPhasesMap.keySeq().indexOf(id);
+  const phasesArray = orderedPhasesMap.valueSeq().toJS();
+  const previousPhase = phasesArray[phaseIndex - 1];
+  const nextPhase = phasesArray[phaseIndex + 1];
+  const res = (start && previousPhase && start.isBefore(previousPhase.end)) || (end && nextPhase && end.isAfter(nextPhase.start));
+  return res || false;
+};
+
+const setHasConflictingDates = (phases) => {
+  let newState = phases;
+  newState.forEach((phase, id) => {
+    newState = newState.setIn([id, 'hasConflictingDates'], hasConflictingDates(phase, newState));
+  });
+  return newState;
 };
 
 type PhasesByIdState = Map<string, Map>;
@@ -54,9 +68,18 @@ type PhasesByIdReducer = (PhasesByIdState, ReduxAction<Action>) => PhasesByIdSta
 export const phasesById: PhasesByIdReducer = (state: PhasesByIdState = Map(), action: ReduxAction<Action>) => {
   switch (action.type) {
   case CREATE_PHASE:
-    return state.set(action.id, emptyPhase.set('id', action.id).set('order', action.order).set('identifier', 'survey'));
-  case DELETE_PHASE:
-    return state.setIn([action.id, '_toDelete'], true);
+    return state.set(
+      action.id,
+      emptyPhase
+        .set('id', action.id)
+        .set('order', action.order)
+        .set('identifier', 'survey')
+    );
+  case DELETE_PHASE: {
+    let newState = state.setIn([action.id, '_toDelete'], true);
+    newState = setHasConflictingDates(newState);
+    return newState;
+  }
   case UPDATE_PHASE_TITLE:
     return state
       .updateIn([action.id, 'titleEntries'], updateInLangstringEntries(action.locale, action.value))
@@ -71,19 +94,23 @@ export const phasesById: PhasesByIdReducer = (state: PhasesByIdState = Map(), ac
     const end = state.getIn([action.id, 'end']);
     const newStart = action.value;
     const endIsBeforeNewStart = end && end.isBefore(newStart);
-    return state
+    let newState = state
       .setIn([action.id, 'start'], newStart)
       .setIn([action.id, 'endIsBeforeStart'], endIsBeforeNewStart || false)
       .setIn([action.id, '_hasChanged'], true);
+    newState = setHasConflictingDates(newState);
+    return newState;
   }
   case UPDATE_PHASE_END: {
     const start = state.getIn([action.id, 'start']);
     const newEnd = action.value;
     const newIsBeforeStart = start && newEnd.isBefore(start);
-    return state
+    let newState = state
       .setIn([action.id, 'end'], newEnd)
       .setIn([action.id, 'endIsBeforeStart'], newIsBeforeStart || false)
       .setIn([action.id, '_hasChanged'], true);
+    newState = setHasConflictingDates(newState);
+    return newState;
   }
   case UPDATE_PHASE_IMAGE:
     return state
@@ -93,10 +120,16 @@ export const phasesById: PhasesByIdReducer = (state: PhasesByIdState = Map(), ac
       .setIn([action.id, '_hasChanged'], true);
   case UPDATE_IS_THEMATICS_TABLE:
     return state.setIn([action.id, 'isThematicsTable'], action.value).setIn([action.id, '_hasChanged'], true);
-  case MOVE_PHASE_UP:
-    return moveItemUp(state, action.id);
-  case MOVE_PHASE_DOWN:
-    return moveItemDown(state, action.id);
+  case MOVE_PHASE_UP: {
+    let newState = moveItemUp(state, action.id);
+    newState = setHasConflictingDates(newState);
+    return newState;
+  }
+  case MOVE_PHASE_DOWN: {
+    let newState = moveItemDown(state, action.id);
+    newState = setHasConflictingDates(newState);
+    return newState;
+  }
   case UPDATE_PHASES: {
     let newState = Map();
     action.phases.forEach(
@@ -112,7 +145,6 @@ export const phasesById: PhasesByIdReducer = (state: PhasesByIdState = Map(), ac
           isThematicsTable: isThematicsTable || false, // default to false until we have the interface to set a thematicstable
           id: id,
           order: order,
-          hasConflictingDates: getHasConflictingDates(action.phases, id, start, end) || false,
           endIsBeforeStart: end.isBefore(start),
           image: image ? fromJS(image) : defaultImage,
           descriptionEntries: fromJS(descriptionEntries)
@@ -121,7 +153,7 @@ export const phasesById: PhasesByIdReducer = (state: PhasesByIdState = Map(), ac
         newState = newState.set(id, phaseInfo);
       }
     );
-
+    newState = setHasConflictingDates(newState);
     return newState;
   }
   default:
