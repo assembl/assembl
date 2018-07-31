1578b9851a0684d8d814b6969fa7796df4a34621
Cédric Messiant <cedricmessiant@ecreall.com>
2018-07-31 17:27:19 +0200

    Add validationState to CheckboxFieldAdapter's FormGroup

--- assembl/static2/js/app/components/form/checkboxFieldAdapter.jsx
+++ assembl/static2/js/app/components/form/checkboxFieldAdapter.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { type FieldRenderProps } from 'react-final-form';
 import { Checkbox, FormGroup, HelpBlock } from 'react-bootstrap';
 
+import { getValidationState } from './utils';
+
 type Props = {
   label: string
 } & FieldRenderProps;
 
 const CheckboxFieldAdapter = ({ input, label, meta: { error, touched } }: Props) => (
-  <FormGroup controlId={input.name}>
+  <FormGroup controlId={input.name} validationState={getValidationState(error, touched)}>
     <Checkbox {...input}>{label}</Checkbox>
     {touched && error ? <HelpBlock>{error}</HelpBlock> : null}
   </FormGroup>
