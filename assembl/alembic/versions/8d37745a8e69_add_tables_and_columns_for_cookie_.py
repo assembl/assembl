6cce0b5faa44eec4b221e3eaf76191b139bddffb
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2018-08-03 15:35:37 +0200

    Fix migration levels

--- assembl/alembic/versions/8d37745a8e69_add_tables_and_columns_for_cookie_.py
+++ assembl/alembic/versions/8d37745a8e69_add_tables_and_columns_for_cookie_.py
@@ -1,14 +1,14 @@
 """Add tables and columns for cookie_storage
 
 Revision ID: 8d37745a8e69
-Revises: 2d0777b24f0d
+Revises: 134299d407b9
 Create Date: 2018-07-19 19:07:59.770479
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '8d37745a8e69'
-down_revision = '2d0777b24f0d'
+down_revision = '134299d407b9'
 
 from alembic import context, op
 import sqlalchemy as sa
