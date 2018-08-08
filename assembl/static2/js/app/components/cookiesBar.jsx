9e98c0e68701488e6d3c94772bad87c56b1eaba7
Milan <milan.barande@hotmail.fr>
2018-08-08 13:12:56 +0200

    use the acceptedCookies query for logged in users, browsers cookies otherwise

--- assembl/static2/js/app/components/cookiesBar.jsx
+++ assembl/static2/js/app/components/cookiesBar.jsx
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
 import { Translate } from 'react-redux-i18n';
+import { connect } from 'react-redux';
 import { compose, graphql, withApollo, type ApolloClient } from 'react-apollo';
 import { Link } from 'react-router';
 import { Button } from 'react-bootstrap';
@@ -8,6 +9,7 @@ import classnames from 'classnames';
 import { getCookieItem, setCookieItem, getDiscussionSlug, getDiscussionId } from '../utils/globalFunctions';
 import { get } from '../utils/routeMap';
 import updateAcceptedCookies from '../graphql/mutations/updateAcceptedCookies.graphql';
+import acceptedCookiesQuery from '../graphql/acceptedCookiesQuery.graphql';
 import { COOKIE_TYPES } from '../constants';
 
 type State = {
@@ -15,10 +17,13 @@ type State = {
 };
 
 type Props = {
-  client: ApolloClient
+  client: ApolloClient,
+  acceptedCookies: Array<string>
 };
 
 const discussionId = getDiscussionId();
+
+const formattedCookieNames = COOKIE_TYPES.map(cookie => discussionId && `${cookie}_${discussionId}`);
 
 export const saveAcceptedCookies = (cookies: Array<string>, client: ApolloClient) => {
   client.mutate({
@@ -26,22 +31,30 @@ export const saveAcceptedCookies = (cookies: Array<string>, client: ApolloClient
     variables: {
       actions: cookies
     }
-  });
+  })
+  // If the user is not logged in the mutation will error
+    .catch(() => { formattedCookieNames.forEach(cookie => cookie && setCookieItem(cookie, 'true')); });
 };
-
-const formattedCookieNames = COOKIE_TYPES.map(cookie => discussionId && `${cookie}_${discussionId}`);
 
 export class DumbCookiesBar extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
+    this.state = { hide: true };
+  }
 
-    const hasAcceptedEveryCookies = formattedCookieNames.every(cookie => cookie && getCookieItem(cookie) === 'true');
-    this.state = { hide: hasAcceptedEveryCookies };
+  componentWillReceiveProps({ acceptedCookies }: Props) {
+    if (acceptedCookies) {
+      // acceptedCookies from the query is only received if the user is logged in
+      const userHasConfiguredCookies = COOKIE_TYPES.some(cookie => acceptedCookies.includes(cookie));
+      this.setState({ hide: userHasConfiguredCookies });
+    } else {
+      // is the user is not logged in, we check in the browser instead of the backend
+      const hasConfiguredCookies = formattedCookieNames.some(cookie => cookie && getCookieItem(cookie) === 'true');
+      this.setState({ hide: hasConfiguredCookies });
+    }
   }
 
   acceptAllCookies = () => {
-    formattedCookieNames.forEach(cookie => cookie && setCookieItem(cookie, 'true'));
-
     const { client } = this.props;
     saveAcceptedCookies(COOKIE_TYPES, client);
     this.setState({
@@ -74,6 +87,26 @@ export class DumbCookiesBar extends React.Component<Props, State> {
   }
 }
 
-export default compose(graphql(updateAcceptedCookies, {
-  name: 'updateAcceptedCookies'
-}), withApollo)(DumbCookiesBar);
+const mapStateToProps = ({ context }) => ({
+  id: context.connectedUserIdBase64
+});
+
+export default compose(
+  connect(mapStateToProps),
+  graphql(updateAcceptedCookies, {
+    name: 'updateAcceptedCookies'
+  }),
+  graphql(acceptedCookiesQuery, {
+    props: ({ data }) => {
+      if (data.loading) {
+        return { laoding: true };
+      }
+      if (data.error) {
+        return { error: data.error };
+      }
+      return {
+        acceptedCookies: data.user.acceptedCookies
+      };
+    }
+  }),
+  withApollo)(DumbCookiesBar);
--- assembl/static2/tests/unit/components/cookiesBar.spec.jsx
+++ assembl/static2/tests/unit/components/cookiesBar.spec.jsx
@@ -4,7 +4,7 @@ import renderer from 'react-test-renderer';
 import { DumbCookiesBar } from '../../../js/app/components/cookiesBar';
 
 describe('CookiesBar component', () => {
-  it('should render a bar with 2 buttons to accept the cookies policy or read them', () => {
+  it('should render a bar with 2 buttons to accept the cookies policy or read them (bar is hidden by default)', () => {
     const component = renderer.create(<DumbCookiesBar />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
