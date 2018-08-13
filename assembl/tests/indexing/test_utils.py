f7f102fceacfb09ae42b5fdd2886a82f90ae6f4c
Cédric Messiant <cedricmessiant@ecreall.com>
2018-08-13 16:10:52 +0200

    Fix test_get_data_for_extract

--- assembl/tests/indexing/test_utils.py
+++ assembl/tests/indexing/test_utils.py
@@ -4,7 +4,7 @@ import datetime
 from assembl.indexing.utils import get_data
 
 
-def test_get_data(extract_submitted_in_post_related_to_sub_idea_1_1_1, participant2_user, post_related_to_sub_idea_1_1_1, subidea_1_1_1):
+def test_get_data_for_extract(extract_submitted_in_post_related_to_sub_idea_1_1_1, participant2_user, post_related_to_sub_idea_1_1_1, subidea_1_1_1):
     extract = extract_submitted_in_post_related_to_sub_idea_1_1_1
     uid, data = get_data(extract)
     assert uid == 'extract:{}'.format(extract.id)
@@ -15,7 +15,7 @@ def test_get_data(extract_submitted_in_post_related_to_sub_idea_1_1_1, participa
     assert data['idea_id'] == [subidea_1_1_1.id]
     assert data['phase_id'] == u'thread'
     assert data['body'] == u"Commodi maiores magni rerum. Sint natus corporis in qui in ut dignissimos cumque repellendus. Reprehenderit nihil illum."
-    assert data['extract_state'] == u'PUBLISHED'
+    assert data['extract_state'] == u'taxonomy_state.PUBLISHED'
     assert data['creation_date'].date() == datetime.date(2018, 8, 7)
     assert data['extract_nature'] == 'taxonomy_nature.actionable_solution'
     assert data['extract_action'] == 'taxonomy_action.give_examples'
