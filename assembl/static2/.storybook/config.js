4951a14afe16677b54ab3208057221f431dd4aad
Cédric Messiant <cedricmessiant@ecreall.com>
2018-08-14 16:59:08 +0200

    Fix i18n in storybook

--- assembl/static2/.storybook/config.js
+++ assembl/static2/.storybook/config.js
@@ -1,6 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '../css/themes/default/assembl_web.scss';
+
+import { I18n } from 'react-i18nify';
 import { configure } from '@storybook/react';
+
+import messages from '../js/app/utils/translations';
+
+I18n.setTranslations(messages);
+I18n.setLocale('fr');
 
 function loadStories() {
   require('../js/app/stories/components/common/common.jsx');
