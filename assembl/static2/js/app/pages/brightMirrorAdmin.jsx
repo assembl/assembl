7cd37b127e8446672aef9b25f6477a7314ef6cf2
Amen Souissi <amensouissi@ecreall.com>
2018-08-20 10:52:03 +0200

    add phaseId to the BM admin section

--- assembl/static2/js/app/pages/brightMirrorAdmin.jsx
+++ assembl/static2/js/app/pages/brightMirrorAdmin.jsx
@@ -5,13 +5,17 @@ import { I18n } from 'react-redux-i18n';
 import SectionTitle from '../components/administration/sectionTitle';
 import BrightMirrorAdminForm from '../components/administration/brightMirror/index';
 
-const BrightMirrorAdmin = () => (
+type Props = {
+  phaseId: string
+};
+
+const BrightMirrorAdmin = (props: Props) => (
   <div className="bright-mirror-admin admin-box admin-content">
     <SectionTitle
       title={I18n.t('administration.brightMirrorSection.configureTitle')}
       annotation={I18n.t('administration.timelineAdmin.annotation')}
     />
-    <BrightMirrorAdminForm />
+    <BrightMirrorAdminForm {...props} />
   </div>
 );
 
--- assembl/static2/js/app/routes.jsx
+++ assembl/static2/js/app/routes.jsx
@@ -119,7 +119,7 @@ const AdminChild = (props) => {
       />
     );
   case 'brightMirror':
-    return <BrightMirrorAdmin {...props} />;
+    return <BrightMirrorAdmin {...props} phaseId={props.location.query.phaseId} />;
   case 'thread':
     return <ThreadAdmin {...props} section={props.location.query.section} />;
   case 'multiColumns':
