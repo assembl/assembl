be283a3b543a07e135a28987b502929d006936be
Pascal Huynh <pascal@beyowi.com>
2018-08-24 12:12:34 +0200

    create fictionHeader components

--- assembl/static2/js/app/stories/components/common/circleAvatar.jsx
+++ assembl/static2/js/app/stories/components/common/circleAvatar.jsx
@@ -8,24 +8,27 @@ import { withKnobs, text, select } from '@storybook/addon-knobs';
 
 import CircleAvatar from '../../../components/common/circleAvatar';
 
-const customCircleAvatar = {
+export const customCircleAvatar = {
   src: 'https://loremflickr.com/300/300'
 };
 
 const playgroundButton = {
   size: '34',
   username: 'bright-mirror-author',
-  src: ['https://loremflickr.com/300/300', '/static2/img/icons/avatar.png']
+  src: ['/static2/img/icons/avatar.png', 'https://loremflickr.com/300/300']
 };
 
 storiesOf('CircleAvatar', module)
   .addDecorator(withKnobs)
   .add('default', withInfo()(() => <CircleAvatar />))
   .add('custom image', withInfo()(() => <CircleAvatar src={customCircleAvatar.src} />))
-  .add('playground', withInfo()(() => (
-    <CircleAvatar
-      size={text('size', playgroundButton.size)}
-      username={text('username', playgroundButton.username)}
-      src={select('src', playgroundButton.src)}
-    />
-  )));
+  .add(
+    'playground',
+    withInfo()(() => (
+      <CircleAvatar
+        size={text('size', playgroundButton.size)}
+        username={text('username', playgroundButton.username)}
+        src={select('src', playgroundButton.src)}
+      />
+    ))
+  );
