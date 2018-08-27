bcaa14dc7d80f19c80a897f3af0596255a6339c8
Pascal Huynh <pascal@beyowi.com>
2018-08-27 14:24:57 +0200

    fix FictionHeader display bug in BrightMirrorFiction integration page

--- assembl/static2/js/app/integration/brightMirror/pages/brightMirrorFiction.jsx
+++ assembl/static2/js/app/integration/brightMirror/pages/brightMirrorFiction.jsx
@@ -1,13 +1,23 @@
+// @flow
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 import FictionHeader from '../../../components/debate/brightMirror/fictionHeader';
 import FictionBody from '../../../components/debate/brightMirror/fictionBody';
+import type { FictionHeaderType } from '../../../components/debate/brightMirror/fictionHeader';
 
 // import existing storybook data
-import { defaultFictionHeader } from '../../../stories/components/debate/brightMirror/fictionHeader.stories';
+import { defaultCircleAvatar } from '../../../stories/components/debate/brightMirror/circleAvatar.stories';
 import { defaultFictionBody } from '../../../stories/components/debate/brightMirror/fictionBody.stories';
 
-class BrightMirrorFiction extends Component {
+const defaultFictionHeader: FictionHeaderType = {
+  authorFullname: 'Helen Aguilar',
+  publishedDate: new Date('2018-07-09'),
+  circleAvatar: { ...defaultCircleAvatar }
+};
+
+type BrightMirrorFictionType = {};
+
+class BrightMirrorFiction extends Component<BrightMirrorFictionType> {
   render() {
     return (
       <Grid fluid className="bright-mirror background-fiction-default">
