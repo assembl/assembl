ea4d02a3e5ae14406c7b1b57188f27a3c981fd53
Pascal Huynh <pascal@beyowi.com>
2018-08-27 18:34:54 +0200

    update style on images in fiction content (after fetching data)

--- assembl/static2/js/app/stories/components/debate/brightMirror/fictionBody.stories.jsx
+++ assembl/static2/js/app/stories/components/debate/brightMirror/fictionBody.stories.jsx
@@ -12,25 +12,32 @@ import type { FictionBodyType } from '../../../../components/debate/brightMirror
 export const defaultFictionBody: FictionBodyType = {
   title: 'Les émotifs',
   content: `
-    <p>15 janvier 2050 : je suis assis paisiblement chez moi en train de lire un épisode de la série « Ceux qui restent »
-    en version émotive. L’histoire me rend triste mais je la lis parce que j’y entrevois la profondeur des sentiments.
-    Avant l’arrivée de la tablette sensorielle, quand les films et les séries n’avaient pas d’émo-description, je
-    préférais les histoires écrites. Au moins dans les livres, les sentiments sont détaillés. Je les comprends. Je
-    suis là, dans l’histoire, dans la vie des personnages, dans leur tête, dans leur âme. J’entre en introspection
-    // pour sonder puis ranger dans les tiroirs de mon esprit les nouvelles émotions que je découvre.</p>
-
-    <div class="fiction-image" style="background-image: url(&quot;https://loremflickr.com/1200/800/paris&quot;);"></div>
-
-    <p>Maintenant, avec les sous-titres c’est plus facile pour moi de regarder une vidéo : « Confusion indice 5 + Honte
-    indice 1 » ; ou « Joie indice 3 + Nostalgie indice 2 » ; ou encore « Colère indice 8 + Amour indice 9 + Regret
-    indice 4 » Ces idéogrammes qui apparaissent à chaque scène que je parcours du doigt, je sais les lire couramment
-    maintenant. C’est comme du braille digital, mais pour les gens que le monde des interactions déroute. C’est généré
-    par une IA qui analyse le format facial des inférences émotionnelles pour les traduire en émo-signes qui se
-    dessinent et se déploient comme des fleurs sur l’écran. En lisant, je prends en moi la palette des émotions qui
-    existent. Et j’apprends à lier le signe au sentiment. Cela me donne une sensation d’universalité qui se manifeste
-    presque de façon physique. Comme un courant électrique qui me traverse. Très intime, personnel. Mais tellement
-    collectif : je me sens comme tout le monde. J’appartiens moi aussi. Je peux entrer en interférence émotionnelle
-    avec un univers de sensations, toucher le monde des impressions et des consciences humaines.</p>
+    <p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+    been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+    scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
+    electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
+    Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+    PageMaker including versions of Lorem Ipsum.</p><p></p><p></p><div class="atomic-block" data-blocktype="atomic">
+    <img class="attachment-image" src="http://localhost:6543/data/Discussion/2/documents/2417/data" alt=""
+    title="one-piece-unlimited-cruise-sp-banner.jpg" data-id="2417" data-mimetype="image/jpeg"></div><p></p><p>
+    <strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+    the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled
+    it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem
+    Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem
+    Ipsum.</p><p></p><p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem
+    Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+    and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+    typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+    Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem
+    Ipsum.</p><p></p><p></p><div class="atomic-block" data-blocktype="atomic"><img class="attachment-image"
+    src="http://localhost:6543/data/Discussion/2/documents/2418/data" alt="" title="one-piece-unlimited-cruise-sp-banner.jpg"
+    data-id="2418" data-mimetype="image/jpeg"></div><p></p><p><strong>Lorem Ipsum</strong> is simply dummy text of the printing
+    and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+    printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but
+    also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
+    of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
+    including versions of Lorem Ipsum.</p>
   `
 };
 
