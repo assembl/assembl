07826c1d8edb146ad8fb9137fcfe6836b398d857
Victor Neuret <victor.neuret@bluenove.com>
2018-08-28 15:30:03 +0200

    Remove useless comment in caching.py

--- assembl/lib/caching.py
+++ assembl/lib/caching.py
@@ -1,10 +1,8 @@
 from dogpile.cache import make_region
 from .config import get_config
 
-# visit_analytics_region = make_region()
 
 def create_analytics_region():
-    # global visit_analytics_region
     visit_analytics_region = make_region().configure(
         'dogpile.cache.redis',
         arguments={
@@ -15,8 +13,8 @@ def create_analytics_region():
     )
     return visit_analytics_region
 
-# def get_analytics_region():
-#     return visit_analytics_region
+create_analytics_region.count = 0
+
 
 def includeme(config):
     create_analytics_region()
