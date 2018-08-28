3a18747f26b9a93c74fe82c107dd4f9f422c03eb
Victor Neuret <victor.neuret@bluenove.com>
2018-08-28 14:36:30 +0200

    Move test_dogpile_cache to new file

--- assembl/tests/views_tests/test_graphql.py
+++ assembl/tests/views_tests/test_graphql.py
@@ -9,8 +9,6 @@ from assembl import models
 from assembl.graphql.schema import Schema as schema
 from assembl.graphql.utils import create_root_thematic
 
-from assembl.lib.caching import create_analytics_region
-
 
 def test_get_locales(graphql_request):
     res = schema.execute(
@@ -2624,20 +2622,3 @@ def test_get_all_posts(graphql_request, proposition_id):
     first_post = res.data['posts']['edges'][0]['node']
     assert proposition_id == first_post['id']
     return res
-
-
-def test_dogpile_cache():
-    visit_analytics_region = create_analytics_region()
-    # mock object
-    mock_o = mock.MagicMock(return_value=1)
-    # wrap in function, because dogpile really wants functions
-    mock_f = lambda x: mock_o(x)
-    # wrap in dogpile
-    dogMock = visit_analytics_region.cache_on_arguments(expiration_time=10)(mock_f)
-    # invalidate from previous tests
-    dogMock.invalidate(1)
-    # call twice
-    dogMock(1)
-    dogMock(1)
-    # underlying mock should be called once
-    assert mock_o.call_count == 1
