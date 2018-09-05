7da4b8d7b9021abd4cda53781cf0cd2e81fda458
Amen Souissi <amensouissi@ecreall.com>
2018-09-05 17:26:12 +0200

    add opened menu storybook

--- assembl/static2/js/app/integration/101/components/menu/menu.stories.jsx
+++ assembl/static2/js/app/integration/101/components/menu/menu.stories.jsx
@@ -4,10 +4,12 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withInfo } from '@storybook/addon-info';
-import { withKnobs, text, boolean, select } from '@storybook/addon-knobs';
+import { withKnobs, object } from '@storybook/addon-knobs';
 /* eslint-enable */
 
 import { Menu, MenuItem } from '../../../../components/common/menu';
+
+const openedPath = [0, 2, 1];
 
 storiesOf('Menu', module)
   .addDecorator(withKnobs)
@@ -18,6 +20,27 @@ storiesOf('Menu', module)
         <MenuItem title="Foo">
           <Menu>
             <MenuItem title="Sub Foo" />
+          </Menu>
+        </MenuItem>
+        <MenuItem title="Bar" />
+      </Menu>
+    ))
+  )
+  .add(
+    'opened',
+    withInfo()(() => (
+      <Menu openedPath={object('openedPath', openedPath)}>
+        <MenuItem title="Foo">
+          <Menu>
+            <MenuItem title="Foo 1" />
+            <MenuItem title="Foo 2" />
+            <MenuItem title="Foo 3">
+              <Menu>
+                <MenuItem title="Foo 1.1" />
+                <MenuItem title="Foo 1.2" />
+                <MenuItem title="Foo 1.3" />
+              </Menu>
+            </MenuItem>
           </Menu>
         </MenuItem>
         <MenuItem title="Bar" />
