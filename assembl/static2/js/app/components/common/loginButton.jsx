1190224011779514a411d14683b6a316a6f7f04d
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2018-09-05 12:21:19 +0200

    Create smart button for login. Use it for inviteUserToLogin utility method

---
+++ assembl/static2/js/app/components/common/loginButton.jsx
@@ -0,0 +1,54 @@
+// @flow
+import * as React from 'react';
+import { Link } from 'react-router';
+import { Button } from 'react-bootstrap';
+import { compose, graphql } from 'react-apollo';
+import { getCurrentView, getContextual } from '../../utils/routeMap';
+import { getDiscussionSlug } from '../../utils/globalFunctions';
+import withoutLoadingIndicator from './withoutLoadingIndicator';
+import DiscussionQuery from '../../graphql/DiscussionQuery.graphql';
+
+type ButtonProps = {
+  data: {
+    discussion: {
+      loginData: {
+        url: string,
+        local: boolean
+      }
+    }
+  },
+  label: React.Node | string
+};
+
+const DumbButton = ({ data: { discussion: { loginData: { url, local } } }, label }: ButtonProps): React.Node => {
+  const slug = getDiscussionSlug();
+  const next = getCurrentView();
+  let link = `${getContextual('login', { slug: slug })}?next=${next}`;
+  if (url && !local) {
+    link = (url.includes('?')) ? `${url}&next=${next}` : `${url}?next=${next}`;
+  }
+  const handleButtonClick = () => {
+    window.location = link;
+  };
+  return (
+    <div>
+      {local ? (
+        <Link to={link} className="button-link button-dark">
+          {label}
+        </Link>
+      ) : (
+        <Button
+          onClick={handleButtonClick}
+          className="button-submit button-dark"
+        >
+          {label}
+        </Button>
+      )}
+    </div>
+  );
+};
+
+export default compose(
+  graphql(DiscussionQuery),
+  withoutLoadingIndicator()
+)(DumbButton);
