023b531077db5426b6453c4669d0a47e3d72218b
Milan <milan.barande@hotmail.fr>
2018-09-06 17:34:30 +0200

    remove validation on legalContents form as the fields are not required

--- assembl/static2/js/app/components/administration/legalContents/validate.js
+++ assembl/static2/js/app/components/administration/legalContents/validate.js
@@ -1,14 +1,5 @@
 // @flow
-import { I18n } from 'react-redux-i18n';
-import { i18nValueIsEmpty } from '../../form/utils';
-import type { LegalContentsFormValues } from './types.flow';
 
-const validate = (values: LegalContentsFormValues) => ({
-  legalNotice: i18nValueIsEmpty(values.legalNotice) ? I18n.t('error.required') : undefined,
-  termsAndConditions: i18nValueIsEmpty(values.termsAndConditions) ? I18n.t('error.required') : undefined,
-  cookiesPolicy: i18nValueIsEmpty(values.cookiesPolicy) ? I18n.t('error.required') : undefined,
-  privacyPolicy: i18nValueIsEmpty(values.privacyPolicy) ? I18n.t('error.required') : undefined,
-  userGuidelines: i18nValueIsEmpty(values.userGuidelines) ? I18n.t('error.required') : undefined
-});
+const validate = () => {};
 
 export default validate;
