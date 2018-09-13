d364b2b89682c7019ab6ba437237dfa43c28ec5d
Cédric Messiant <cedricmessiant@ecreall.com>
2018-09-13 15:01:22 +0200

    Remove warning from RichTextEditor tests

--- assembl/static2/tests/unit/components/common/richTextEditor/index.spec.jsx
+++ assembl/static2/tests/unit/components/common/richTextEditor/index.spec.jsx
@@ -12,12 +12,12 @@ describe('RichTextEditor component', () => {
   it.skip('should render a rich text editor', () => {
     const contentState = ContentState.createFromText('foobar');
     const editorState = EditorState.createWithContent(contentState);
-    const updateEditorStateSpy = jest.fn(newState => newState);
+    const onChangeSpy = jest.fn();
     const props = {
       editorState: editorState,
       maxLength: 2000,
-      placeholder: 'Write here',
-      updateEditorState: updateEditorStateSpy
+      onChange: onChangeSpy,
+      placeholder: 'Write here'
     };
 
     const shallowRenderer = new ShallowRenderer();
@@ -28,23 +28,26 @@ describe('RichTextEditor component', () => {
 
   describe('shouldHidePlaceholder method', () => {
     it('should return true if the content is empty and the block type is not unstyled', () => {
+      const onChangeSpy = jest.fn();
       let editorState = EditorState.createWithContent(ContentState.createFromText(''));
       editorState = RichUtils.toggleBlockType(editorState, 'unordered-list-item');
-      const wrapper = mount(<RichTextEditor editorState={editorState} />);
+      const wrapper = mount(<RichTextEditor editorState={editorState} onChange={onChangeSpy} />);
       const actual = wrapper.instance().shouldHidePlaceholder();
       expect(actual).toBeTruthy();
     });
 
     it('should return false if the content is not empty', () => {
+      const onChangeSpy = jest.fn();
       const editorState = EditorState.createWithContent(ContentState.createFromText('Hello world'));
-      const wrapper = mount(<RichTextEditor editorState={editorState} />);
+      const wrapper = mount(<RichTextEditor editorState={editorState} onChange={onChangeSpy} />);
       const actual = wrapper.instance().shouldHidePlaceholder();
       expect(actual).toBeFalsy();
     });
 
     it('should return false if the block type is unstyled', () => {
+      const onChangeSpy = jest.fn();
       const editorState = EditorState.createWithContent(ContentState.createFromText(''));
-      const wrapper = mount(<RichTextEditor editorState={editorState} />);
+      const wrapper = mount(<RichTextEditor editorState={editorState} onChange={onChangeSpy} />);
       const actual = wrapper.instance().shouldHidePlaceholder();
       expect(actual).toBeFalsy();
     });
