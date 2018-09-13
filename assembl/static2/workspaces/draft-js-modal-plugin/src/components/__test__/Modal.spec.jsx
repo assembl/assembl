808bbcb2382244da99994a3560d1fe2d502b8ac0
Cédric Messiant <cedricmessiant@ecreall.com>
2018-09-13 16:20:18 +0200

    Add draft-js-modal-plugin workspace

---
+++ assembl/static2/workspaces/draft-js-modal-plugin/src/components/__test__/Modal.spec.jsx
@@ -0,0 +1,46 @@
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Modal from '../Modal';
+
+configure({ adapter: new Adapter() });
+
+describe('Modal component', () => {
+  let state;
+  let jsx;
+  let store;
+
+  beforeEach(() => {
+    state = {
+      content: {
+        title: 'Modal title',
+        body: <div>My modal body</div>
+      }
+    };
+    store = {
+      subscribeToItem: jest.fn(),
+      unsubscribeFromItem: jest.fn(),
+      getItem: name => state[name]
+    };
+    const props = {
+      close: jest.fn(),
+      store: store
+    };
+    jsx = <Modal {...props} />;
+  });
+
+  it('should subscribe/unsubscribe to content', () => {
+    const wrapper = mount(jsx);
+    expect(store.subscribeToItem).toHaveBeenCalledWith('content', expect.any(Function));
+    wrapper.unmount();
+    expect(store.unsubscribeFromItem).toHaveBeenCalledWith('content', expect.any(Function));
+  });
+
+  it('should render a form to add a link', () => {
+    const component = renderer.create(jsx);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
