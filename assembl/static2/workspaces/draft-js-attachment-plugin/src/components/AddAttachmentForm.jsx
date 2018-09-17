8f3c783cc378870478a860b1690a689f10682508
Cédric Messiant <cedricmessiant@ecreall.com>
2018-09-17 14:25:59 +0200

    Remove comments in AddAttachmentForm, make it a functional component

--- assembl/static2/workspaces/draft-js-attachment-plugin/src/components/AddAttachmentForm.jsx
+++ assembl/static2/workspaces/draft-js-attachment-plugin/src/components/AddAttachmentForm.jsx
@@ -14,77 +14,29 @@ type Props = {
   onSubmit: Function
 };
 
-type State = {
-  file: File | null
-};
-
-class AddAttachmentForm extends React.Component<Props, State> {
-  // fileInput: HTMLInputElement;
-
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     file: null
-  //   };
-  // }
-
-  // handleSubmit = (): void => {
-  //   this.props.onSubmit(this.state.file);
-  // };
-
-  // handleFileChange = (file: File): void => {
-  //   this.setState({
-  //     file: file
-  //   });
-  // };
-
-  // render() {
-  //   let isImage;
-  //   const file = this.state.file;
-  //   if (file !== null) {
-  //     isImage = file.type.includes('image');
-  //   }
-
-  //   return (
-  //     <div className={isImage ? 'attach-file with-preview' : 'attach-file'}>
-  //       <div htmlFor="attachment" className="attachment-label">
-  //         <Translate value="common.attachFileForm.label" />
-  //       </div>
-
-  //       <FileUploader handleChange={this.handleFileChange} fileOrUrl={this.state.file} withPreview />
-  //       <Button className="button-submit button-dark btn btn-default" onClick={this.handleSubmit}>
-  //         <Translate value="common.attachFileForm.submit" />
-  //       </Button>
-  //     </div>
-  //   );
-  // }
-  render() {
-    const { onSubmit } = this.props;
-    return (
-      <Form
-        onSubmit={onSubmit}
-        render={({ handleSubmit }) => (
-          <BootstrapForm componentClass="div" horizontal>
-            <Field name="file" component={FileUploaderFieldAdapter} />
-            <FormGroup>
-              <Col smOffset={2} sm={10}>
-                <Button
-                  type="submit"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    handleSubmit();
-                  }}
-                >
-                  <Translate value="common.editor.linkPlugin.submit" />
-                </Button>
-              </Col>
-            </FormGroup>
-          </BootstrapForm>
-        )}
-      />
-    );
-  }
-}
+const AddAttachmentForm = ({ onSubmit }: Props) => (
+  <Form
+    onSubmit={onSubmit}
+    render={({ handleSubmit }) => (
+      <BootstrapForm componentClass="div" horizontal>
+        <Field name="file" component={FileUploaderFieldAdapter} />
+        <FormGroup>
+          <Col smOffset={2} sm={10}>
+            <Button
+              type="submit"
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                handleSubmit();
+              }}
+            >
+              <Translate value="common.editor.linkPlugin.submit" />
+            </Button>
+          </Col>
+        </FormGroup>
+      </BootstrapForm>
+    )}
+  />
+);
 
 export default AddAttachmentForm;
