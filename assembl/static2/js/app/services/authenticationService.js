45088e69e3199217da203ca48a3e22633e104df3
Felix Saint-Genis <saintgenisfelix@gmail.com>
2018-09-19 15:08:51 +0200

    sign up fix

--- assembl/static2/js/app/services/authenticationService.js
+++ assembl/static2/js/app/services/authenticationService.js
@@ -19,6 +19,7 @@ export const signUp = (payload) => {
   const {
     privacyPolicyIsChecked,
     termsAndConditionsIsChecked,
+    userGuidelinesIsChecked,
     discussionSlug,
     email,
     fullname,
