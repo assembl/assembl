158b81befa5167f4638973bc10bf59f4f929b90e
Milan <milan.barande@hotmail.fr>
2018-10-10 09:10:31 +0200

    flow annotations: common/alert.jsx

--- assembl/static2/js/app/components/common/alert.jsx
+++ assembl/static2/js/app/components/common/alert.jsx
@@ -1,15 +1,28 @@
+// @flow
 import React from 'react';
 import { Alert } from 'react-bootstrap';
 
-class AssemblAlert extends React.Component {
-  constructor(props) {
+type Props = {
+  isBase: boolean
+};
+
+type State = {
+  showAlert?: boolean,
+  topPosition?: boolean,
+  alertMsg?: string,
+  base: boolean,
+  alertStyle?: string
+};
+
+class AssemblAlert extends React.Component<Props, State> {
+  constructor(props: Props) {
     super(props);
     this.state = {
       base: true
     };
   }
 
-  componentWillReceiveProps(nextProps) {
+  componentWillReceiveProps(nextProps: Props) {
     if (nextProps.isBase) {
       this.setState({
         base: true
@@ -18,8 +31,8 @@ class AssemblAlert extends React.Component {
   }
 
   render() {
-    const { showAlert, alertStyle, alertMsg, topPosition } = this.state;
-    if (this.state.base) {
+    const { showAlert, alertStyle, alertMsg, topPosition, base } = this.state;
+    if (base) {
       return <Alert className="hideAlert" />;
     }
     return (
