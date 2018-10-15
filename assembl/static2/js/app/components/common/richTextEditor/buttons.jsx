c7401224c8f161abb662876642f4b3817c021686
Cédric Messiant <cedricmessiant@ecreall.com>
2018-10-15 14:31:58 +0200

    Fix title translation on rich text buttons

--- assembl/static2/js/app/components/common/richTextEditor/buttons.jsx
+++ assembl/static2/js/app/components/common/richTextEditor/buttons.jsx
@@ -3,17 +3,26 @@ import React from 'react';
 import { I18n } from 'react-redux-i18n';
 import { createBlockStyleButton, createInlineStyleButton } from 'draft-js-buttons';
 
+// we need to create this intermediate component for title attribute to be translated
+const DumbBoldButton = () => <span className="assembl-icon-text-bold" title={I18n.t('common.editor.bold')} />;
+
 export const BoldButton = createInlineStyleButton({
   style: 'BOLD',
-  children: <span className="assembl-icon-text-bold" title={I18n.t('common.editor.bold')} />
+  children: <DumbBoldButton />
 });
+
+// we need to create this intermediate component for title attribute to be translated
+const DumbItalicButton = () => <span className="assembl-icon-text-italics" title={I18n.t('common.editor.italic')} />;
 
 export const ItalicButton = createInlineStyleButton({
   style: 'ITALIC',
-  children: <span className="assembl-icon-text-italics" title={I18n.t('common.editor.italic')} />
+  children: <DumbItalicButton />
 });
+
+// we need to create this intermediate component for title attribute to be translated
+const DumbUnorderedListButton = () => <span className="assembl-icon-text-bullets" title={I18n.t('common.editor.bulletList')} />;
 
 export const UnorderedListButton = createBlockStyleButton({
   blockType: 'unordered-list-item',
-  children: <span className="assembl-icon-text-bullets" title={I18n.t('common.editor.bulletList')} />
+  children: <DumbUnorderedListButton />
 });
