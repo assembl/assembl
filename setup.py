5a4bcaae11a0b69a22df1f0414b0249a96190ae4
Cédric Messiant <cedricmessiant@ecreall.com>
2018-10-19 09:19:37 +0200

    Fix pip imports when we are in a gitlab runner

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from __future__ import print_function
 import os
 
-from pip.download import PipSession
-from pip.req import parse_requirements
 from setuptools import setup, find_packages
+try:
+    from pip._internal.req import parse_requirements
+    from pip._internal.download import PipSession
+except ImportError:
+    from pip.req import parse_requirements
+    from pip.download import PipSession
+
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.md')).read()
