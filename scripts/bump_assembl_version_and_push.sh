4c06d1b316eccf3f9b74adebd421416a88f459bd
Cédric Messiant <cedricmessiant@ecreall.com>
2018-10-25 11:09:20 +0200

    ssh-agent outside of the script

--- scripts/bump_assembl_version_and_push.sh
+++ scripts/bump_assembl_version_and_push.sh
@@ -1,19 +1,12 @@
 #!/bin/sh
-echo '.git/config'
-cat .git/config
-echo 'known_hosts'
-cat ~/.ssh/known_hosts
-echo 'remotes'
-git remote -v
-ssh assembl_user@dev-assembl.bluenove.com ls
-ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+cd ${CI_PROJECT_DIR}/
 git fetch github
 git checkout -b bumpversion
 bumpversion minor
-git checkout -B ${MASTER_BRANCH} bumpversion
+git checkout -B $MASTER_BRANCH bumpversion
 git branch -d bumpversion
-git pull --rebase github ${MASTER_BRANCH}
-git push github ${MASTER_BRANCH}
-git checkout -B ${DEVELOP_BRANCH} --track github/${DEVELOP_BRANCH}
-git merge ${MASTER_BRANCH} --no-edit -m "Merge new version"
-git push github ${DEVELOP_BRANCH}
+git pull --rebase github $MASTER_BRANCH
+git push github $MASTER_BRANCH
+git checkout -B $DEVELOP_BRANCH --track github/$DEVELOP_BRANCH
+git merge $MASTER_BRANCH --no-edit -m "Merge new version"
+git push github $DEVELOP_BRANCH
--- scripts/configure_ssh.sh
+++ scripts/configure_ssh.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y)
-eval $(ssh-agent -s)
 echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
