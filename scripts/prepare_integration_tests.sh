cd7aff9473c4785179f5a45ef73cdaa041e3db50
Cédric Messiant <cedricmessiant@ecreall.com>
2018-10-25 14:10:01 +0200

    Create a script to prepare integration and ui tests

---
+++ scripts/prepare_integration_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+apt-get update -qq
+# install chrome dependencies
+apt-get install -yq gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+git clone https://github.com/bluenove/assembl-tests
+cd assembl-tests
+yarn
+# fetch data required for e2e tests
+scp $UI_TESTS_DATA_PATH data.js
