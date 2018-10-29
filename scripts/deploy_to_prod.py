55718437056458dec0ff57d0959df1465fd35293
Milan <milan.barande@hotmail.fr>
2018-10-29 15:42:21 +0100

    add $ at the beginning of variable name and make deploy_to_prod automatic for testing

--- scripts/deploy_to_prod.py
+++ scripts/deploy_to_prod.py
@@ -3,7 +3,7 @@ import json
 
 # prod_clients_json = '{"prod_clients": [{"client_name": "sncf","hostname": "preprod-assembl.bluenove.com"},{"client_name": "edf","hostname": "preprod-assembl.bluenove.com"}]}'
 
-prod_clients_json = os.environ.get('PROD_CLIENTS')
+prod_clients_json = os.environ.get('$PROD_CLIENTS')
 
 prod_clients = json.loads(prod_clients_json)
 
