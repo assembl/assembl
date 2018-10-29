9ebbc98b05df3db7b5e9a5eb45f172e49602d82f
Milan <milan.barande@hotmail.fr>
2018-10-29 17:24:28 +0100

    restore normal state of gitlab ci (uncomment lines and set deploy_to_prod as manual)

--- scripts/deploy_to_prod.py
+++ scripts/deploy_to_prod.py
@@ -6,5 +6,6 @@ prod_clients_json = os.environ['PROD_CLIENTS']
 prod_clients = json.loads(prod_clients_json)
 
 for client in prod_clients["prod_clients"]:
+    # We just print a fake list of hostnames for now
     print(client["hostname"])
-    # Here we want to launch the deployment for each server using the required arguments for each of them
+    # Eventually, we want to launch the deployment for each server using the required arguments for each of them
