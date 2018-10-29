53506446e6d8d514062d3387b9dcc54937529bd7
Milan <milan.barande@hotmail.fr>
2018-10-29 13:08:47 +0100

    test commiting script modifications to be taken account of in gitlab runner

--- scripts/deploy_to_prod.py
+++ scripts/deploy_to_prod.py
@@ -1,7 +1,9 @@
 import os
 import json
 
-prod_clients_json = os.environ.get['PROD_CLIENTS']
+prod_clients_json = os.environ.get('PROD_CLIENTS')
+
+print(prod_clients_json)
 
 prod_clients = json.load(prod_clients_json)
 for client in prod_clients.prod_clients:
