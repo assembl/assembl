8401c35084269e652bdc2269902b9f8651d4a9d3
Milan <milan.barande@hotmail.fr>
2018-10-29 16:34:58 +0100

    clean script
    add comment to explain point of the loop

--- scripts/deploy_to_prod.py
+++ scripts/deploy_to_prod.py
@@ -1,11 +1,10 @@
 import os
 import json
 
-# prod_clients_json = '{"prod_clients": [{"client_name": "sncf","hostname": "preprod-assembl.bluenove.com"},{"client_name": "edf","hostname": "preprod-assembl.bluenove.com"}]}'
-
-prod_clients_json = os.environ.get('$PROD_CLIENTS')
+prod_clients_json = os.environ['PROD_CLIENTS']
 
 prod_clients = json.loads(prod_clients_json)
 
 for client in prod_clients["prod_clients"]:
     print(client["hostname"])
+    # Here we want to launch the deployment for each server using the required arguments for each of them
