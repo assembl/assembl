1c7a6ab2f38a7d2138ddc3bdd911e202f15c07f1
Milan <milan.barande@hotmail.fr>
2018-10-29 15:12:16 +0100

    test on gitlab

--- scripts/deploy_to_prod.py
+++ scripts/deploy_to_prod.py
@@ -1,14 +1,11 @@
 import os
 import json
 
+# prod_clients_json = '{"prod_clients": [{"client_name": "sncf","hostname": "preprod-assembl.bluenove.com"},{"client_name": "edf","hostname": "preprod-assembl.bluenove.com"}]}'
+
 prod_clients_json = os.environ.get('PROD_CLIENTS')
 
-print('HELLO WORLD')
+prod_clients = json.loads(prod_clients_json)
 
-print(prod_clients_json)
-
-prod_clients = json.load(prod_clients_json)
-
-print(prod_clients)
-# for client in prod_clients.prod_clients:
-#     print(client.hostname)
+for client in prod_clients["prod_clients"]:
+    print(client["hostname"])
