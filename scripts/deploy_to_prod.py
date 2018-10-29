1975e8c7dfd81e582ba0a598ff8ab9b1492adb9c
Milan <milan.barande@hotmail.fr>
2018-10-29 13:16:22 +0100

    test

--- scripts/deploy_to_prod.py
+++ scripts/deploy_to_prod.py
@@ -6,5 +6,7 @@ prod_clients_json = os.environ.get('PROD_CLIENTS')
 print(prod_clients_json)
 
 prod_clients = json.load(prod_clients_json)
-for client in prod_clients.prod_clients:
-    print(client.hostname)
+
+print(prod_clients)
+# for client in prod_clients.prod_clients:
+#     print(client.hostname)
