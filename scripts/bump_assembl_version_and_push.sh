a56ebee9d72c359fdb543540a95f3203ed8f8518
Cédric Messiant <cedricmessiant@ecreall.com>
2018-11-05 09:58:31 +0100

    develop already exists so we have to use another name: github_develop

--- scripts/bump_assembl_version_and_push.sh
+++ scripts/bump_assembl_version_and_push.sh
@@ -9,6 +9,6 @@ git checkout -b $MASTER_BRANCH github/$MASTER_BRANCH
 git merge --no-ff bumpversion $MASTER_BRANCH
 git branch -d bumpversion
 git push github $MASTER_BRANCH
-git checkout -b $DEVELOP_BRANCH github/$DEVELOP_BRANCH
+git checkout -b github_$DEVELOP_BRANCH github/$DEVELOP_BRANCH
 git merge $MASTER_BRANCH --no-edit -m "Merge new version"
 git push github $DEVELOP_BRANCH
