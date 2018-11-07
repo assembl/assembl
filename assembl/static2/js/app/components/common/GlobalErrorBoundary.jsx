e35afa247261da22a5a512e14cfb0bfb4ab28dd2
Cédric Messiant <cedricmessiant@ecreall.com>
2018-11-07 13:14:21 +0100

    Add sentry integration for js (static2) code

--- assembl/static2/js/app/components/common/GlobalErrorBoundary.jsx
+++ assembl/static2/js/app/components/common/GlobalErrorBoundary.jsx
@@ -2,6 +2,9 @@
 import * as React from 'react';
 import { Translate } from 'react-redux-i18n';
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as Sentry from '@sentry/browser';
+
 type Props = {
   children: React.Node
 };
@@ -15,7 +18,13 @@ export default class GlobalErrorBoundary extends React.Component<Props, State> {
 
   componentDidCatch(error: Error, info: { componentStack: string }) {
     this.setState({ error: error });
-    // TODO: log the error to sentry
+    Sentry.withScope((scope) => {
+      Object.keys(info).forEach((key) => {
+        scope.setExtra(key, info[key]);
+      });
+      Sentry.captureException(error);
+    });
+
     if (process.env.NODE_ENV === 'development') {
       console.error(error, info.componentStack); // eslint-disable-line no-console
     }
