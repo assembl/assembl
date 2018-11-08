69a021d3e9ab4374597cda06c6d36ccdfc1d3840
Victor Neuret <victor.neuret@bluenove.com>
2018-11-08 17:30:41 +0100

    Add aws rds create-db-instance

--- ci_cd_scripts/create_ec2_instance.sh
+++ ci_cd_scripts/create_ec2_instance.sh
@@ -9,3 +9,27 @@ aws ec2 run-instances   \
     --security-group-ids "sg-0b6c0251567043d03" \
     --associate-public-ip-address   \
     --user-data file://~/assembl/ci_cd_scripts/user-data.txt
+
+aws rds create-db-instance  \
+    --engine postgres   \
+    --db-instance-class db.t2.micro \
+    --no-multi-az   \
+    --storage-type gp2  \
+    --allocated-storage 20  \
+    --db-instance-identifier test-instance \
+    --master-username master    \
+    --master-user-password secret99 \
+    --db-subnet-group-name default  \
+    --no-publicly-accessible    \
+    --availability-zone eu-west-1a  \
+    --vpc-security-group-ids "sg-0b6c0251567043d03" \
+    --db-name rd_test   \
+    --port 5432 \
+    --db-parameter-group-name default.postgres10    \
+    --option-group-name default:postgres-10 \
+    --enable-iam-database-authentication    \
+    --backup-retention-period 7 \
+    --preferred-backup-window 02:00-03:00   \
+    --monitoring-interval 0 \
+    --preferred-maintenance-window Mon:03:00-Mon:04:00  \
+    --no-deletion-protection
