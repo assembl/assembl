953a9359bc91f7cdcd66a1b44ba5cd387b45e380
Milan <milan.barande@hotmail.fr>
2018-11-19 10:49:14 +0100

    adding flow on statistic.jsx and slight refactoring

--- assembl/static2/js/app/components/home/header/statistic.jsx
+++ assembl/static2/js/app/components/home/header/statistic.jsx
@@ -1,24 +1,39 @@
-// @noflow
+// @flow
 import * as React from 'react';
 import { compose, graphql } from 'react-apollo';
 import { I18n } from 'react-redux-i18n';
 import moment from 'moment';
 import 'moment-duration-format'; // needed for momentDuration.format()
-import RootIdeaStats from '../../../graphql/RootIdeaStats.graphql';
+import RootIdeaStatsQuery from '../../../graphql/RootIdeaStats.graphql';
 import manageErrorAndLoading from '../../../components/common/manageErrorAndLoading';
 import HeaderStatistics, { statMessages, statParticipants, statSentiments } from '../../../components/common/headerStatistics';
 
-class Statistic extends React.Component<$FlowFixMeProps> {
+export type VisitsAnalytics = {
+  nbPageviews: ?number,
+  npUniqPageviews: ?number,
+  sumVisitsLength: ?number,
+  __typename?: 'VisitsAnalytics'
+};
+
+type Props = {
+  rootIdea: Idea,
+  numParticipants: number,
+  totalSentiments: number,
+  visitsAnalytics: VisitsAnalytics
+};
+
+class Statistic extends React.PureComponent<Props> {
   render() {
-    const { rootIdea, numParticipants, totalSentiments, visitsAnalytics } = this.props.data;
+    const { rootIdea, numParticipants, totalSentiments, visitsAnalytics } = this.props;
+    const { sumVisitsLength, nbPageviews } = visitsAnalytics;
     const statElements = [statSentiments(totalSentiments), statParticipants(numParticipants)];
     if (rootIdea) {
       statElements.push(statMessages(rootIdea.numPosts));
     }
 
     if (visitsAnalytics) {
-      if ('sumVisitsLength' in visitsAnalytics && visitsAnalytics.sumVisitsLength > 0) {
-        const totalSeconds = visitsAnalytics.sumVisitsLength;
+      if (sumVisitsLength && sumVisitsLength > 0) {
+        const totalSeconds = sumVisitsLength;
         // Second parameter of parseInt() is mandatory, see
         // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
         const secondsAsNumber = parseInt(totalSeconds, 10);
@@ -31,10 +46,10 @@ class Statistic extends React.Component<$FlowFixMeProps> {
           metricNameTranslateKey: 'home.sumVisitsLength'
         });
       }
-      if ('nbPageviews' in visitsAnalytics && visitsAnalytics.nbPageviews > 0) {
+      if (nbPageviews && nbPageviews > 0) {
         statElements.push({
           iconName: 'double-page',
-          metricValue: visitsAnalytics.nbPageviews,
+          metricValue: nbPageviews,
           metricNameTranslateKey: 'home.pageViews'
         });
       }
@@ -43,4 +58,22 @@ class Statistic extends React.Component<$FlowFixMeProps> {
   }
 }
 
-export default compose(graphql(RootIdeaStats), manageErrorAndLoading({ textHidden: true, color: 'white' }))(Statistic);
+export default compose(
+  graphql(RootIdeaStatsQuery, {
+    props: ({ data: { rootIdea, numParticipants, totalSentiments, visitsAnalytics, loading, error } }) => {
+      if (error || loading) {
+        return {
+          error: error,
+          loading: loading
+        };
+      }
+      return {
+        rootIdea: rootIdea,
+        numParticipants: numParticipants,
+        totalSentiments: totalSentiments,
+        visitsAnalytics: visitsAnalytics
+      };
+    }
+  }),
+  manageErrorAndLoading({ displayLoader: false, textHidden: true, color: 'white' })
+)(Statistic);
