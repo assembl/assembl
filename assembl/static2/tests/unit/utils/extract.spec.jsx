d8a80136dc2bb41819d23b0667065d556bdfffab
Cédric Messiant <cedricmessiant@ecreall.com>
2018-11-20 11:16:45 +0100

    Fix flow for extract.spec.jsx

--- assembl/static2/tests/unit/utils/extract.spec.jsx
+++ assembl/static2/tests/unit/utils/extract.spec.jsx
@@ -4,42 +4,42 @@ import * as extractUtils from '../../../js/app/utils/extract';
 describe('getExtractColor function', () => {
   const { getExtractColor } = extractUtils;
   it('should return the default color if the extract is not categorized by nature', () => {
-    const actual = getExtractColor('');
+    const actual = getExtractColor('', 'PUBLISHED', false);
     expect(actual).toEqual('#7ed321');
   });
 
   it('should return the color for actionable solution nature', () => {
-    const actual = getExtractColor('Enum.actionable_solution');
+    const actual = getExtractColor('Enum.actionable_solution', 'PUBLISHED', false);
     expect(actual).toEqual('#35C646');
   });
 
   it('should return the color for issue nature', () => {
-    const actual = getExtractColor('Enum.issue');
+    const actual = getExtractColor('Enum.issue', 'PUBLISHED', false);
     expect(actual).toEqual('#FF001F');
   });
 
   it('should return the color for knowledge nature', () => {
-    const actual = getExtractColor('Enum.knowledge');
+    const actual = getExtractColor('Enum.knowledge', 'PUBLISHED', false);
     expect(actual).toEqual('#BD10E0');
   });
 
   it('should return the color for example nature', () => {
-    const actual = getExtractColor('Enum.example');
+    const actual = getExtractColor('Enum.example', 'PUBLISHED', false);
     expect(actual).toEqual('#FF9F00');
   });
 
   it('should return the color for concept nature', () => {
-    const actual = getExtractColor('Enum.concept');
+    const actual = getExtractColor('Enum.concept', 'PUBLISHED', false);
     expect(actual).toEqual('#00B6FF');
   });
 
   it('should return the color for argument nature', () => {
-    const actual = getExtractColor('Enum.argument');
+    const actual = getExtractColor('Enum.argument', 'PUBLISHED', false);
     expect(actual).toEqual('#FFEC00');
   });
 
   it('should return the color for cognitive bias nature', () => {
-    const actual = getExtractColor('Enum.cognitive_bias');
+    const actual = getExtractColor('Enum.cognitive_bias', 'PUBLISHED', false);
     expect(actual).toEqual('#000000');
   });
 
