2f023251f7d34eb68a96cf1479e2a743a225a274
Marc-Antoine Parent <maparent@acm.org>
2018-11-21 15:00:11 -0500

    fix migration

--- assembl/alembic/versions/d813ab110e00_discussion_start_end_dates.py
+++ assembl/alembic/versions/d813ab110e00_discussion_start_end_dates.py
@@ -23,7 +23,7 @@ from assembl.lib.sqla import mark_changed
 def upgrade(pyramid_env):
     with context.begin_transaction():
         op.add_column('discussion', sa.Column('active_start_date', sa.DateTime))
-        op.add_column('discussion', sa.Column('active_stop_date', sa.DateTime))
+        op.add_column('discussion', sa.Column('active_end_date', sa.DateTime))
 
     # Do stuff with the app's models here.
     from assembl import models as m
@@ -34,41 +34,38 @@ def upgrade(pyramid_env):
         for d_id, slug, pref_id in discussion_ids:
             pref = db.query(m.Preferences).filter_by(id=pref_id).first()
             extra = pref['extra_json']
+            success = False
             if 'dates' in extra:
                 dates = extra['dates']
                 print("Checking if both start and end dates from Discussion %s exist" % slug)
                 if 'startDate' in dates and 'endDate' in dates:
                     print("Migrating Discussion %s's dates to model from extra_json" % slug)
                     try:
-                        start_date = datetime.strptime(dates.startDate, "%Y-%m-%d")
-                        stop_date = datetime.strptime(dates.startDate, "%Y-%m-%d")
+                        start_date = datetime.strptime(dates['startDate'], "%Y-%m-%d")
+                        stop_date = datetime.strptime(dates['startDate'], "%Y-%m-%d")
                         if start_date and stop_date:
                             d = m.Discussion.__table__
-                            db.execute(d.update().where(d.c.id == d_id).values(active_start_date=start_date))
-                            db.execute(d.update().where(d.c.id == d_id).values(active_stop_date=stop_date))
-                            db.flush()
+                            db.execute(d.update().where(d.c.id == d_id).values(
+                                active_start_date=start_date, active_end_date=stop_date))
+                            success = True
                     except:
                         print("Failed to convert Discussion %s's dates to model" % slug)
                 else:
                     print("Not both start and end dates from Discussion %s are set! Skipped..." % slug)
-            else:
+            if not success:
                 # Try to use phases
                 print("Migrating Discussion %s's dates to model from phases" % slug)
-                phases = db.query(m.TimelineEvent).filter_by(discussion_id=d_id).order_by(m.TimelineEvent.start.desc()).all()
-                if phases:
-                    try:
-                        start_date = phases[0].start
-                        end_date = phases[-1].end
-                        if start_date and end_date and end_date < start_date:
-                            raise Exception("End date %s is earlier than the start date %s!" % end_date, start_date)
-                        if start_date:
-                            d = m.Discussion.__table__
-                            db.execute(d.update().where(d.c.id == d_id).values(active_start_date=start_date))
-                        if end_date:
-                            d = m.Discussion.__table__
-                            db.execute(d.update().where(d.c.id == d_id).values(active_start_date=start_date))
-                    except:
-                        print("Failed to convert Discussion %s's dates to model" % slug)
+                dates = db.query(sa.sql.func.min(m.TimelineEvent.start),
+                                 sa.sql.func.max(m.TimelineEvent.end)
+                                ).filter_by(discussion_id=d_id).first()
+                if dates:
+                    (start_date, end_date) = dates
+                    if (start_date and end_date and start_date < end_date):
+                        db.execute(d.update().where(d.c.id == d_id).values(
+                            active_start_date=start_date, active_end_date=end_date))
+                        success = True
+                    else:
+                        print("Not both start and end dates from Discussion %s are set in phases! Skipped..." % slug)
                 else:
                     print("There are no set phases for Discussion %s to continue. Skipped..." % slug)
         mark_changed(db)
@@ -77,5 +74,5 @@ def upgrade(pyramid_env):
 def downgrade(pyramid_env):
     # Only drop the columsn, as the calculation of these columns does not allow reversion
     with context.begin_transaction():
-        op.drop_column('discussion', 'active_stop_date')
+        op.drop_column('discussion', 'active_end_date')
         op.drop_column('discussion', 'active_start_date')
