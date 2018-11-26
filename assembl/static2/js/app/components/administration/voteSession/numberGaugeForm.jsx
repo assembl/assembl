54cf110513d852f28e5f45f948031f86ba150461
Milan <milan.barande@hotmail.fr>
2018-11-26 12:00:46 +0100

    refactoring value check for numbergaugeform

--- assembl/static2/js/app/components/administration/voteSession/numberGaugeForm.jsx
+++ assembl/static2/js/app/components/administration/voteSession/numberGaugeForm.jsx
@@ -20,44 +20,32 @@ const DumbNumberGaugeForm = ({
   handleMinChange,
   handleMaxChange,
   handleUnitChange
-}: NumberGaugeFormProps) => {
-  const onMinChange = (value: number) => {
-    if (value > 0) {
-      handleMinChange(value);
-    }
-  };
-  const onMaxChange = (value: number) => {
-    if (value > 0) {
-      handleMaxChange(value);
-    }
-  };
-  return (
-    <div>
-      <FormGroup>
-        <FormControlWithLabel
-          onChange={e => onMinChange(parseInt(e.target.value, 10))}
-          value={minimum ? minimum.toString() : ''}
-          label={I18n.t('administration.minValue')}
-          required
-          type="number"
-        />
-        <FormControlWithLabel
-          onChange={e => onMaxChange(parseInt(e.target.value, 10))}
-          value={maximum ? maximum.toString() : ''}
-          label={I18n.t('administration.maxValue')}
-          required
-          type="number"
-        />
-        <FormControlWithLabel
-          onChange={e => handleUnitChange(e.target.value)}
-          value={unit}
-          label={I18n.t('administration.unit')}
-          required
-          type="text"
-        />
-      </FormGroup>
-    </div>
-  );
-};
+}: NumberGaugeFormProps) => (
+  <React.Fragment>
+    <FormGroup>
+      <FormControlWithLabel
+        onChange={e => handleMinChange(parseInt(e.target.value, 10))}
+        value={minimum ? minimum.toString() : ''}
+        label={I18n.t('administration.minValue')}
+        required
+        type="number"
+      />
+      <FormControlWithLabel
+        onChange={e => handleMaxChange(parseInt(e.target.value, 10))}
+        value={maximum ? maximum.toString() : ''}
+        label={I18n.t('administration.maxValue')}
+        required
+        type="number"
+      />
+      <FormControlWithLabel
+        onChange={e => handleUnitChange(e.target.value)}
+        value={unit}
+        label={I18n.t('administration.unit')}
+        required
+        type="text"
+      />
+    </FormGroup>
+  </React.Fragment>
+);
 
 export default DumbNumberGaugeForm;
