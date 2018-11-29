a7ac361b4dff0cada5aeea82f2218e4d1efe8ac7
Pascal Huynh <pascal@beyowi.com>
2018-11-29 12:23:12 +0100

    DEVAS-BM-helper: add sharePostButton story

--- assembl/static2/.storybook/config.js
+++ assembl/static2/.storybook/config.js
@@ -35,6 +35,7 @@ function loadStories() {
 
   require('../js/app/stories/components/debate/common/toggleCommentButton.stories.jsx');
   require('../js/app/stories/components/debate/common/replyToCommentButton.stories.jsx');
+  require('../js/app/stories/components/debate/common/sharePostButton.stories.jsx');
 
   require('../js/app/stories/components/debate/brightMirror/fictionPreview.stories.jsx');
   require('../js/app/stories/components/debate/brightMirror/fictionsList.stories.jsx');
