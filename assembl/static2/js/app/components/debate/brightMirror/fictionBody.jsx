fd9ca7a703c7ec61fdf957e1063fe87a9aa018c5
cedrtang <cedric@beyowi.com>
2018-11-29 16:18:12 -0800

    BH49-80-81-89: filter extracts by lang, size up for emojis, comment badge and next/prev comment btn

--- assembl/static2/js/app/components/debate/brightMirror/fictionBody.jsx
+++ assembl/static2/js/app/components/debate/brightMirror/fictionBody.jsx
@@ -195,13 +195,16 @@ class FictionBody extends React.Component<Props, State> {
       commentSentimentFixedPosition
     } = this.state;
 
+    // Filter extract by lang
+    const filteredExtracts = extracts && extracts.filter(extract => extract.lang === contentLocale);
+
     return (
       <div ref={this.fictionBodyView}>
         <SideCommentMenu
           postId={postId}
           ideaId={ideaId}
           lang={contentLocale}
-          extracts={extracts}
+          extracts={filteredExtracts}
           commentAnchorPosition={commentAnchorPosition}
           badgeDynamicPosition={commentBadgeDynamicPosition}
           badgeFixedPosition={commentBadgeFixedPosition}
@@ -227,7 +230,7 @@ class FictionBody extends React.Component<Props, State> {
           handleMouseUpWhileHarvesting={this.handleMouseUpWhileHarvesting}
           body={content || noContentMessage}
           dbId={dbId}
-          extracts={extracts}
+          extracts={filteredExtracts}
           bodyMimeType={bodyMimeType}
           contentLocale={contentLocale}
           id={postId}
