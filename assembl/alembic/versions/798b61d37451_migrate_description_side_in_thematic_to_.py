933734802ca340d1c144817b77fa70bb7996f07f
mohamedzayed92 <zayed2.mohamed@gmail.com>
2018-12-11 11:49:51 +0100

    changing alembic migration again

--- assembl/alembic/versions/798b61d37451_migrate_description_side_in_thematic_to_.py
+++ assembl/alembic/versions/798b61d37451_migrate_description_side_in_thematic_to_.py
@@ -20,35 +20,34 @@ from assembl.lib import config
 
 def upgrade(pyramid_env):
     with context.begin_transaction():
-    	# op.drop_table('thematic')
-        # op.drop_column('thematic', "video_description_side_id")
+	# op.drop_table('thematic')
+		# op.drop_column('thematic', "video_description_side_id")
         op.add_column('announce', sa.Column('quote_id', sa.Integer, sa.ForeignKey('langstring.id')))
-        op.drop_column("vote_session", "title")
-        op.drop_column("vote_session", "sub_title")
-        op.drop_column("vote_session", "instructions_section_title")
-        op.drop_column("vote_session", "instructions_section_content")
-        op.drop_column("vote_session", "discussion_phase_id")
-        op.add_column("vote_session", sa.Column('idea_id', sa.Integer, sa.ForeignKey(m.Idea.id), nullable=False))
+	op.drop_column("vote_session", "title")
+	op.drop_column("vote_session", "sub_title")
+	op.drop_column("vote_session", "instructions_section_title")
+	op.drop_column("vote_session", "instructions_section_content")
+	op.drop_column("vote_session", "discussion_phase_id")
+	op.add_column("vote_session", sa.Column('idea_id', sa.Integer, sa.ForeignKey(m.Idea.id), nullable=False))
 
     # Do stuff with the app's models here.
-    # from assembl import models as m
-    # db = m.get_session_maker()()
-    # with transaction.manager:
-    #     thematics = db.query(m.Thematic).all()
-    #     announcements = db.query(m.IdeaAnnouncement)
-    #     for thematic in thematics:
-    #     	thematic.sqla_type = 'idea'
-        	# thematic_announcement = announcements.filter(m.IdeaAnnouncement.idea_id==thematic.id).first()
-        	# if thematic_announcement:
-        	# 	thematic_announcement.quote_id = thematic.video_description_side_id
-
+	from assembl import models as m
+    db = m.get_session_maker()()
+    with transaction.manager:
+        thematics = db.query(m.Thematic).all()
+	announcements = db.query(m.IdeaAnnouncement)
+	for thematic in thematics:
+            thematic.sqla_type = 'idea'
+	    thematic_announcement = announcements.filter(m.IdeaAnnouncement.idea_id==thematic.id).first()
+	    if thematic_announcement:
+                thematic_announcement.quote_id = thematic.video_description_side_id
 
 def downgrade(pyramid_env):
-	from assembl import models as m
+    from assembl import models as m
     with context.begin_transaction():
-        op.drop_column('announce', 'quote_id')
-        op.add_column('vote_session', sa.Column('discussion_phase_id', sa.Integer, sa.ForeignKey('discussion_phase.id')))
-        op.add_column('vote_session', sa.Column('title_id', sa.Integer, sa.ForeignKey(m.LangString.id), nullable=True))
-        op.add_column('vote_session', sa.Column('sub_title_id', sa.Integer, sa.ForeignKey(m.LangString.id), nullable=True))
-        op.add_column('vote_session', sa.Column('instructions_section_title_id', sa.Integer, sa.ForeignKey(m.LangString.id), nullable=True))
-        op.add_column('vote_session', sa.Column('instructions_section_content', sa.Integer, sa.ForeignKey(m.LangString.id), nullable=True))
+        op.drop_column('announce', quote_id)
+	op.add_column('vote_session', sa.Column('discussion_phase_id', sa.Integer, sa.ForeignKey('discussion_phase.id')))
+	op.add_column('vote_session', sa.Column('title_id', sa.Integer, sa.ForeignKey(m.LangString.id), nullable=True))
+	op.add_column('vote_session', sa.Column('sub_title_id', sa.Integer, sa.ForeignKey(m.LangString.id), nullable=True))
+	op.add_column('vote_session', sa.Column('instructions_section_title_id', sa.Integer, sa.ForeignKey(m.LangString.id), nullable=True))
+	op.add_column('vote_session', sa.Column('instructions_section_content', sa.Integer, sa.ForeignKey(m.LangString.id), nullable=True))
