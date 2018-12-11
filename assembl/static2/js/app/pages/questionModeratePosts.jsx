3970c95815a89647f2a88ba4709fd175c458e662
Cédric Messiant <cedricmessiant@ecreall.com>
2018-12-11 11:34:11 +0100

    Deny access to pending posts via url in survey to non admin users

--- assembl/static2/js/app/pages/questionModeratePosts.jsx
+++ assembl/static2/js/app/pages/questionModeratePosts.jsx
@@ -2,7 +2,24 @@
 import React from 'react';
 
 import Question, { type Props } from './question';
+import { connectedUserIsAdmin } from '../utils/permissions';
+import { get, goTo } from '../utils/routeMap';
 
-const QuestionModeratePosts = (props: Props) => <Question {...props} isModerating />;
+class QuestionModeratePosts extends React.Component<Props> {
+  componentDidMount() {
+    if (!connectedUserIsAdmin()) {
+      const { params: { slug } } = this.props;
+      goTo(
+        get('unauthorizedAdministration', {
+          slug: slug
+        })
+      );
+    }
+  }
+
+  render() {
+    return <Question {...this.props} isModerating />;
+  }
+}
 
 export default QuestionModeratePosts;
