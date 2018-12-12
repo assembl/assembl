9be3bd06cce2a0298d1dd3643047d464ef97d042
Vincent Fretin <vincent.fretin@gmail.com>
2018-12-12 11:44:43 +0100

    freeze time

--- assembl/static2/tests/setupTests.js
+++ assembl/static2/tests/setupTests.js
@@ -5,3 +5,5 @@ Object.defineProperty(document, 'cookie', {
   value: 'assembl_session=1234; _LOCALE_=fr; _pk_id.abcd1234=1234;',
   writable: true
 });
+
+jest.spyOn(Date, 'now').mockImplementation(() => new Date('2018-09-27T12:54:59.218Z').getTime());
