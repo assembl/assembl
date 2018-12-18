90d2bb63c20d52e9137ef327828ca7424e32ac51
Milan <milan.barande@hotmail.fr>
2018-12-18 17:47:04 +0100

    update enzyme-adapter-react to 16.3

--- assembl/static2/workspaces/draft-js-link-plugin/src/components/__test__/LinkButton.spec.jsx
+++ assembl/static2/workspaces/draft-js-link-plugin/src/components/__test__/LinkButton.spec.jsx
@@ -1,5 +1,5 @@
 import { configure, mount, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-16.3'; // eslint-disable-line
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { EditorState } from 'draft-js';
