63a1d114f0809cd5d1f5289aef5f38b53417fe89
Cedric Tang <cedric@beyowi.com>
2019-01-16 11:43:38 +0100

    add style to toolbarSlider and toolbarSliderIcon

--- assembl/static2/js/app/integration/semanticAnalysis/pages/semanticAnalysis.jsx
+++ assembl/static2/js/app/integration/semanticAnalysis/pages/semanticAnalysis.jsx
@@ -2,11 +2,13 @@
 import React from 'react';
 // Component imports
 import Loader, { LOADER_TYPE } from '../../../components/common/loader/loader';
+import ToolbarSlider from '../../../components/common/toolbarSlider/toolbarSlider';
 
 const SemanticAnalysis = () => (
   <div className="semantic-analysis">
     <Loader type={LOADER_TYPE.LOADING} />
     <Loader type={LOADER_TYPE.ERROR} />
+    <ToolbarSlider defaultValue={50} onSliderChange={() => {}} />
   </div>
 );
 
