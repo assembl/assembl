0004b64f20f06848fc3f38f97e9e61b2390cf6c5
louise dupas <louise@beyowi.com>
2019-01-17 17:33:51 +0100

    add test file

---
+++ assembl/static2/js/app/components/common/resultInformation/resultInformation.spec.jsx
@@ -0,0 +1,39 @@
+// @flow
+import React from 'react';
+/* eslint-disable import/no-extraneous-dependencies */
+import initStoryshots from '@storybook/addon-storyshots';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16.3';
+/* eslint-enable */
+
+import ResultInformation from './resultInformation';
+import type { Props as ResultInformationProps } from './resultInformation';
+
+// Separate the snapshots in directories next to each component
+// Name should match with the story name
+initStoryshots({
+  storyKindRegex: /^ResultInformation$/
+});
+
+configure({ adapter: new Adapter() });
+
+describe('<ResultInformation /> - with shallow', () => {
+  let wrapper;
+  const resultInformationProps: ResultInformationProps = {
+    wordsNumber: 23
+  };
+
+  describe('when is loading', () => {
+    beforeEach(() => {
+      wrapper = shallow(<ResultInformation {...resultInformationProps} />);
+    });
+
+    it('should use description component', () => {
+      expect(wrapper.find('Description')).toHaveLength(1);
+    });
+
+    it('should use translate component', () => {
+      expect(wrapper.find('Translate')).toHaveLength(1);
+    });
+  });
+});
