ecf4b6efd60843ef201638d18963726b571e0779
Vincent Fretin <vincent.fretin@gmail.com>
2019-01-18 17:34:23 +0100

    redirect to the thematic if there is only one

--- assembl/static2/js/app/pages/debateThread.jsx
+++ assembl/static2/js/app/pages/debateThread.jsx
@@ -5,6 +5,7 @@ import { compose, graphql } from 'react-apollo';
 import Loader from '../components/common/loader';
 import Ideas from '../components/debate/tableOfIdeas/ideas';
 import AllIdeasQuery from '../graphql/AllIdeasQuery.graphql';
+import { get, goTo } from '../utils/routeMap';
 
 type Props = {
   phaseId: string,
@@ -16,51 +17,69 @@ type Props = {
     ideas: Array<Idea>
   },
   params: {
-    phaseId: string,
+    slug: string,
+    phase: string,
     themeId?: string,
     questionId?: string
   },
   children: React.Node
 };
 
-const DebateThread = ({ phaseId, identifier, data, params, children }: Props) => {
-  if (!data) {
+class DebateThread extends React.Component<Props> {
+  static defaultProps = {
+    data: null
+  };
+
+  componentDidUpdate() {
+    const { data, params } = this.props;
+    if (data && data.loading) {
+      return;
+    }
+    const { ideas } = data;
+    const themeId = params.themeId || null;
+    const questionId = params.questionId || null;
+    const isParentRoute = !(themeId || questionId) || false;
+    // After fetching thematics, redirect to the thematic if there is only one
+    if (isParentRoute && ideas.length === 1) {
+      goTo(get('idea', { slug: params.slug, phase: params.phase, themeId: ideas[0].id }));
+    }
+  }
+
+  render() {
+    const { phaseId, identifier, data, params, children } = this.props;
+    if (!data) {
+      return (
+        <div className="debate">
+          <Loader color="black" />
+        </div>
+      );
+    }
+    const { loading, ideas, rootIdea } = data;
+    const themeId = params.themeId || null;
+    const questionId = params.questionId || null;
+    const isParentRoute = !(themeId || questionId) || false;
+    const childrenElm = React.Children.map(children, child =>
+      React.cloneElement(child, {
+        id: themeId || questionId,
+        identifier: identifier,
+        phaseId: phaseId
+      })
+    );
+
     return (
       <div className="debate">
-        <Loader color="black" />
+        {loading && isParentRoute && <Loader color="black" />}
+        {!loading && ideas && isParentRoute && rootIdea ? (
+          <Ideas key={phaseId} ideas={ideas} rootIdeaId={rootIdea.id} identifier={identifier} phaseId={phaseId} />
+        ) : null}
+        {!isParentRoute && <section className="debate-section">{childrenElm}</section>}
       </div>
     );
   }
-  const { loading, ideas, rootIdea } = data;
-  const themeId = params.themeId || null;
-  const questionId = params.questionId || null;
-  const isParentRoute = !(themeId || questionId) || false;
-  const childrenElm = React.Children.map(children, child =>
-    React.cloneElement(child, {
-      id: themeId || questionId,
-      identifier: identifier,
-      phaseId: phaseId
-    })
-  );
-
-  return (
-    <div className="debate">
-      {loading && isParentRoute && <Loader color="black" />}
-      {!loading && ideas && isParentRoute && rootIdea ? (
-        <Ideas key={phaseId} ideas={ideas} rootIdeaId={rootIdea.id} identifier={identifier} phaseId={phaseId} />
-      ) : null}
-      {!isParentRoute && <section className="debate-section">{childrenElm}</section>}
-    </div>
-  );
-};
-
-DebateThread.defaultProps = {
-  data: null
-};
+}
 
 const mapStateToProps = state => ({
-  lang: state.i18n.locale,
-  slug: state.debate.debateData.slug
+  lang: state.i18n.locale
 });
 
 export default compose(
