ae8cec45bd1061e1261f3bca01c590c6618379b2
Milan <milan.barande@hotmail.fr>
2019-01-18 14:33:44 +0100

    redirect to the new url of the debate after modifying the slug

--- assembl/static2/js/app/components/administration/discussion/preferences/save.js
+++ assembl/static2/js/app/components/administration/discussion/preferences/save.js
@@ -3,6 +3,8 @@ import type { ApolloClient } from 'react-apollo';
 import UpdateDiscussionPreference from '../../../../graphql/mutations/updateDiscussionPreference.graphql';
 import type { DiscussionPreferencesFormValues } from './types.flow';
 import { createSave, convertCheckboxListValueToVariable } from '../../../form/utils';
+import { browserHistory } from '../../../../router';
+import { get } from '../../../../utils/routeMap';
 
 const getVariables = async (client: ApolloClient, values: DiscussionPreferencesFormValues) => ({
   languages: convertCheckboxListValueToVariable(values.languages),
@@ -10,15 +12,25 @@ const getVariables = async (client: ApolloClient, values: DiscussionPreferencesF
   slug: values.slug
 });
 
-export const createMutationsPromises = (client: ApolloClient) => (values: DiscussionPreferencesFormValues) => [
+export const createMutationsPromises = (client: ApolloClient) => (
+  values: DiscussionPreferencesFormValues,
+  initialValues: DiscussionPreferencesFormValues
+) => [
   () =>
     getVariables(client, values).then(variables =>
-      client.mutate({
-        mutation: UpdateDiscussionPreference,
-        variables: {
-          ...variables
-        }
-      })
+      client
+        .mutate({
+          mutation: UpdateDiscussionPreference,
+          variables: {
+            ...variables
+          }
+        })
+        .then(() => {
+          if (values.slug !== initialValues.slug) {
+            // When the slug is changed we need to redirect the user to the updated url with the new slug
+            browserHistory.push(get('discussionPreferencesAdmin', { slug: values.slug }));
+          }
+        })
     )
 ];
 
