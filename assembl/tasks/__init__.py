5d8075ea0faffc5fdb8f46ee17a70bb5344d473b
Marc-Antoine Parent <maparent@acm.org>
2019-01-22 16:20:07 +0100

    rename, functional compose

--- assembl/tasks/__init__.py
+++ assembl/tasks/__init__.py
@@ -1,8 +1,8 @@
 from invoke import Collection
 
 import sudoer
-import assembl
+import deploy
 
 ns = Collection()
 ns.add_collection(Collection.from_module(sudoer))
-ns.add_collection(Collection.from_module(assembl))
+ns.add_collection(Collection.from_module(deploy))
