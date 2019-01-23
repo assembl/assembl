0e421d387786525c3006402e814f2109dfa154e5
Marc-Antoine Parent <maparent@acm.org>
2019-01-23 09:01:55 +0100

    explicitly add invoke. Create lambda zip.

---
+++ assembl/scripts/lambda_create_db_aws_user.py
@@ -0,0 +1,37 @@
+import pg8000
+import boto3
+import os
+import json
+
+
+def lambda_handler(input, ctx):
+    sm = boto3.client('secretsmanager')
+    response = sm.get_secret_value(SecretId='db_password')
+    if 'SecretString' in response:
+        info = response['SecretString']
+    else:
+        info = base64.b64decode(response['SecretBinary'])
+    info = json.loads(info)
+    database = input.get('database', os.environ.get('db_database', 'assembl'))
+    cnx = pg8000.core.Connection(
+        info['username'],
+        info['host'], None,
+        info['port'],
+        database,
+        info['password'],
+        False, 1000, 'assembl', 100)
+    cur = cnx.cursor()
+    try:
+        cur.execute("SELECT 1 FROM pg_user WHERE usename = 'assembl_user'")
+        if len(list(cur)):
+            return "exists"
+        else:
+            cur.execute("CREATE USER assembl_user with login createdb")
+            cnx.commit()
+            cur.execute("GRANT rds_iam TO assembl_user")
+            cnx.commit()
+            cur.execute("ALTER DATABASE %s OWNER TO assembl_user" % database)
+            cnx.commit()
+            return "created"
+    finally:
+        cnx.close()
