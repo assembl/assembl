cdc473ef63df8d4f1e91f7b3ae3967be6199e0f1
Marc-Antoine Parent <maparent@acm.org>
2019-01-25 14:32:13 +0100

    Multiple secret ids.

--- assembl/tasks/common.py
+++ assembl/tasks/common.py
@@ -69,17 +69,20 @@ def setup_ctx(c):
         target = data.get('_extends', None)
         current = rec_update(data, current)
 
-    aws_secret_id = current.get('aws_secrets_id', None)
-    if aws_secret_id:
+    aws_secret_ids = current.get('aws_secrets_ids', None)
+    if aws_secret_ids:
         import boto3
         sm = boto3.client('secretsmanager')
-        response = sm.get_secret_value(SecretId=aws_secret_id)
-        if 'SecretString' in response:
-            info = response['SecretString']
-        else:
-            info = base64.b64decode(response['SecretBinary'])
-        info = json.loads(info)
-        current = rec_update(current, info)
+        if not isinstance(aws_secret_ids, list):
+            aws_secret_ids = aws_secret_ids.split()
+        for aws_secret_id in aws_secret_ids:
+            response = sm.get_secret_value(SecretId=aws_secret_id)
+            if 'SecretString' in response:
+                info = response['SecretString']
+            else:
+                info = base64.b64decode(response['SecretBinary'])
+            info = json.loads(info)
+            current = rec_update(current, info)
 
     if current is not c.config._project:
         c.config._project = current
