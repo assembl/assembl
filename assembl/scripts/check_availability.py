f8dfcf742d4b6e16794d071d6638b4e08c51aed8
Marc-Antoine Parent <maparent@acm.org>
2019-01-28 10:01:18 +0100

    bootstrap db schema and data as part of check_availability

--- assembl/scripts/check_availability.py
+++ assembl/scripts/check_availability.py
@@ -1,8 +1,11 @@
 #!env python
 import argparse
+
 import requests
+from sqlalchemy.exc import ProgrammingError
 
 from ..lib.config import get
+from ..lib.migration import bootstrap_db, bootstrap_db_data
 from . import boostrap_configuration
 
 
@@ -10,7 +13,17 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("configuration", help="configuration file")
     args = parser.parse_args()
-    db = boostrap_configuration(args.configuration, False)
+    try:
+        db = boostrap_configuration(args.configuration, False)
+        assert list(db.execute('select count(id) from role'))[0][0] > 1
+    except ProgrammingError as e:
+        db.rollback()
+        expected_message = 'relation "role" does not exist'
+        if expected_message in e.orig.message:
+            db = bootstrap_db(args.configuration)
+            bootstrap_db_data(db)
+        else:
+            raise e
     assert list(db.execute('select count(id) from role'))[0][0] > 1
     elasticsearch_host = get('elasticsearch_host')
     elasticsearch_port = get('elasticsearch_port')
