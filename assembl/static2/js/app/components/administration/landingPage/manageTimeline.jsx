aaf007359f7f7c800dec7239fe80c2949a1dbde4
felixsaintgenis <saintgenisfelix@gmail.com>
2019-02-06 10:29:34 +0100

    delete unuse code for the title and subtitle in manageTimeline.jsx

--- assembl/static2/js/app/components/administration/landingPage/manageTimeline.jsx
+++ assembl/static2/js/app/components/administration/landingPage/manageTimeline.jsx
@@ -3,35 +3,19 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Translate, I18n } from 'react-redux-i18n';
 import { Link } from 'react-router';
-import { getEntryValueForLocale } from '../../../utils/i18n';
 import SectionTitle from '../../administration/sectionTitle';
-import FormControlWithLabel from '../../common/formControlWithLabel';
 import PhaseForm from './phaseForm';
 import Helper from '../../common/helper';
-import { updateLandingPageModuleTitle, updateLandingPageModuleSubtitle } from '../../../actions/adminActions/landingPage';
 import { getDiscussionSlug } from '../../../utils/globalFunctions';
 import { get } from '../../../utils/routeMap';
 
 type Props = {
   timelineModuleId: string, // eslint-disable-line react/no-unused-prop-types
   discussionPhaseIds: Array<string>,
-  editLocale: string,
-  sectionTitle: string,
-  sectionSubtitle: string,
-  handleTitleChange: Function,
-  handleSubtitleChange: Function
+  editLocale: string
 };
 
-const DumbManageTimeline = ({
-  discussionPhaseIds,
-  editLocale,
-  sectionTitle,
-  sectionSubtitle,
-  handleTitleChange,
-  handleSubtitleChange
-}: Props) => {
-  const sectionTitlePh = I18n.t('administration.ph.propositionSectionTitle');
-  const subtitleSectionPh = I18n.t('administration.ph.propositionSectionSubtitle');
+const DumbManageTimeline = ({ discussionPhaseIds, editLocale }: Props) => {
   const slug = { slug: getDiscussionSlug() };
   return (
     <div className="admin-box">
@@ -41,20 +25,6 @@ const DumbManageTimeline = ({
       />
       <div className="admin-content">
         <div className="form-container">
-          <Helper
-            label={I18n.t('administration.landingPage.timeline.sectionTitle')}
-            helperUrl="/static2/img/helpers/landing_page_admin/timeline_title.png"
-            helperText={I18n.t('administration.helpers.timelineTitle')}
-            classname="title"
-          />
-          <FormControlWithLabel label={sectionTitlePh} onChange={handleTitleChange} required type="text" value={sectionTitle} />
-          <FormControlWithLabel
-            label={subtitleSectionPh}
-            onChange={handleSubtitleChange}
-            required
-            type="text"
-            value={sectionSubtitle}
-          />
           <div className="margin-l">
             <Helper
               label={I18n.t('administration.landingPage.timeline.phaseSection')}
@@ -79,13 +49,9 @@ const DumbManageTimeline = ({
   );
 };
 
-const mapStateToProps = (state, { editLocale, timelineModuleId }) => {
+const mapStateToProps = (state, { editLocale }) => {
   const { phasesById } = state.admin.timeline;
-  const { modulesById } = state.admin.landingPage;
-  const timelineModule = modulesById.get(timelineModuleId);
   return {
-    sectionTitle: timelineModule ? getEntryValueForLocale(timelineModule.get('titleEntries'), editLocale, '') : null,
-    sectionSubtitle: timelineModule ? getEntryValueForLocale(timelineModule.get('subtitleEntries'), editLocale, '') : null,
     discussionPhaseIds: phasesById
       .filter(phase => !phase.get('_toDelete'))
       .keySeq()
@@ -94,9 +60,4 @@ const mapStateToProps = (state, { editLocale, timelineModuleId }) => {
   };
 };
 
-const mapDispatchToProps = (dispatch, { editLocale, timelineModuleId }) => ({
-  handleTitleChange: e => dispatch(updateLandingPageModuleTitle(timelineModuleId, editLocale, e.target.value)),
-  handleSubtitleChange: e => dispatch(updateLandingPageModuleSubtitle(timelineModuleId, editLocale, e.target.value))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(DumbManageTimeline);
+export default connect(mapStateToProps)(DumbManageTimeline);
