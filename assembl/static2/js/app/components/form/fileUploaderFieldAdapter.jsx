45c1770da4c9db3d4d77b87c10e206b678ed73d3
felixsaintgenis <saintgenisfelix@gmail.com>
2019-02-07 11:01:45 +0100

    add a confirmation modal to the file uploader adapter

--- assembl/static2/js/app/components/form/fileUploaderFieldAdapter.jsx
+++ assembl/static2/js/app/components/form/fileUploaderFieldAdapter.jsx
@@ -1,8 +1,10 @@
 // @flow
 import * as React from 'react';
 import { type FieldRenderProps } from 'react-final-form';
-import { ControlLabel, FormGroup, Tooltip } from 'react-bootstrap';
+import { ControlLabel, FormGroup, Tooltip, Button } from 'react-bootstrap';
+import { Translate } from 'react-redux-i18n';
 
+import { displayModal, closeModal } from '../../utils/utilityManager';
 import FileUploader from '../common/fileUploader';
 import Error from './error';
 import { getValidationState } from './utils';
@@ -13,6 +15,27 @@ type Props = {
 } & FieldRenderProps;
 
 const FileUploaderFieldAdapter = ({ deleteTooltip, input, label, meta: { error, touched } }: Props) => {
+  const confirmDeletionTitle = <Translate value="deleteConfirmation.confirmDeletionTitle" />;
+  const confirmDeletionBody = <Translate value="deleteConfirmation.confirmDeletionBody" />;
+  const confirmDeletionModal = () => {
+    const footer = [
+      <Button key="cancel" onClick={closeModal} className="button-cancel button-dark">
+        <Translate value="cancel" />
+      </Button>,
+      <Button
+        key="delete"
+        onClick={() => {
+          input.onChange('');
+          closeModal();
+        }}
+        className="button-submit button-dark"
+      >
+        <Translate value="delete" />
+      </Button>
+    ];
+    return displayModal(confirmDeletionTitle, confirmDeletionBody, true, footer);
+  };
+
   const onChange = (value) => {
     input.onChange({
       externalUrl: value,
@@ -32,7 +55,7 @@ const FileUploaderFieldAdapter = ({ deleteTooltip, input, label, meta: { error, 
         mimeType={input.value && input.value.mimeType}
         name={input.name}
         isAdminUploader
-        onDeleteClick={() => input.onChange('')}
+        onDeleteClick={() => confirmDeletionModal()}
       />
       <Error name={input.name} />
     </FormGroup>
