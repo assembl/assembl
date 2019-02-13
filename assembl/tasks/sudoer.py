6b3be72ab8ea8e89fc3a095762fb62276f625b93
Marc-Antoine Parent <maparent@acm.org>
2019-02-13 09:21:22 +0100

    repair after cleanup, bis

--- assembl/tasks/sudoer.py
+++ assembl/tasks/sudoer.py
@@ -279,6 +279,7 @@ def install_chrome_dependencies(c):
     """
     Installs the requirements for chrome + pupeteer to be run. Assumes to be run on Debian based machines
     """
+    # assumes apt up-to-date from previously.
     deps = ['gconf-service', 'libasound2', 'libatk1.0-0', 'libc6', 'libcairo2', 'libcups2', 'libdbus-1-3', 'libexpat1',
             'libfontconfig1', 'libgcc1', 'libgconf-2-4', 'libgdk-pixbuf2.0-0', 'libglib2.0-0', 'libgtk-3-0', 'libnspr4',
             'libpango-1.0-0', 'libpangocairo-1.0-0', 'libstdc++6', 'libx11-6', 'libx11-xcb1', 'libxcb1', 'libxcomposite1',
