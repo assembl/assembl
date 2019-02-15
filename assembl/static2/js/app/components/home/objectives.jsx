68e45582cbfaa710851a8d4ee9b28f2184bb26af
Milan <milan.barande@hotmail.fr>
2019-02-15 16:49:19 +0100

    turn Objectives component into function

--- assembl/static2/js/app/components/home/objectives.jsx
+++ assembl/static2/js/app/components/home/objectives.jsx
@@ -1,3 +1,4 @@
+// @flow
 import React from 'react';
 import { connect } from 'react-redux';
 import { Grid, Row, Col } from 'react-bootstrap';
@@ -8,85 +9,82 @@ import { getCurrentPhaseData } from '../../utils/timeline';
 import { browserHistory } from '../../router';
 import ParticipateButton from '../common/participateButton';
 
-class Objectives extends React.Component {
-  constructor(props) {
-    super(props);
-    this.displayPhase = this.displayPhase.bind(this);
-  }
+type Props = {
+  debate: DebateData,
+  timeline: Timeline,
+  i18n: Object
+};
 
-  displayPhase() {
+const Objectives = ({ debate, timeline, i18n }: Props) => {
+  const displayPhase = () => {
     const slug = { slug: getDiscussionSlug() };
-    const { timeline } = this.props;
     const { currentPhaseIdentifier } = getCurrentPhaseData(timeline);
     browserHistory.push(get('debate', { ...slug, phase: currentPhaseIdentifier }));
-  }
+  };
 
-  render() {
-    const { debateData } = this.props.debate;
-    const { timeline } = this.props;
-    let { locale } = this.props.i18n;
-    if (locale === 'zh-CN') {
-      locale = 'zh_CN';
-    }
-    return (
-      <section className="home-section objectives-section">
-        <Grid fluid>
-          <div className="max-container">
-            <div className="title-section">
-              <div className="title-hyphen">&nbsp;</div>
-              <h1 className="dark-title-1">{debateData.objectives.titleEntries[locale]}</h1>
-            </div>
-            <div className="content-section">
-              <div className="content-margin">
-                <Row>
-                  <Col
-                    xs={12}
-                    sm={12}
-                    md={debateData.objectives.images.img1Url && debateData.objectives.images.img2Url ? 6 : 9}
-                    className="objectives"
+  const { debateData } = debate;
+  let { locale } = i18n;
+  if (locale === 'zh-CN') {
+    locale = 'zh_CN';
+  }
+  return (
+    <section className="home-section objectives-section">
+      <Grid fluid>
+        <div className="max-container">
+          <div className="title-section">
+            <div className="title-hyphen">&nbsp;</div>
+            <h1 className="dark-title-1">{debateData.objectives.titleEntries[locale]}</h1>
+          </div>
+          <div className="content-section">
+            <div className="content-margin">
+              <Row>
+                <Col
+                  xs={12}
+                  sm={12}
+                  md={debateData.objectives.images.img1Url && debateData.objectives.images.img2Url ? 6 : 9}
+                  className="objectives"
+                >
+                  <div
+                    className={
+                      debateData.objectives.images.img1Url && debateData.objectives.images.img2Url
+                        ? 'text-column-2'
+                        : 'text-column-3'
+                    }
                   >
-                    <div
-                      className={
-                        debateData.objectives.images.img1Url && debateData.objectives.images.img2Url
-                          ? 'text-column-2'
-                          : 'text-column-3'
-                      }
-                    >
-                      <span>{debateData.objectives.descriptionEntries[locale]}</span>
-                    </div>
-                  </Col>
-                  {debateData.objectives.images && (
-                    <div className="container-objectives-img">
-                      {debateData.objectives.images.img1Url && (
-                        <Col xs={12} sm={6} md={3} className="objectives">
-                          <div
-                            className="objectives-img"
-                            style={{ backgroundImage: `url(${debateData.objectives.images.img1Url})` }}
-                          />
-                        </Col>
-                      )}
-                      {debateData.objectives.images.img2Url && (
-                        <Col xs={12} sm={6} md={3} className="objectives">
-                          <div
-                            className="objectives-img"
-                            style={{ backgroundImage: `url(${debateData.objectives.images.img2Url})` }}
-                          />
-                        </Col>
-                      )}
-                    </div>
-                  )}
-                </Row>
-              </div>
-              <div className="center inline full-size margin-xxl">
-                <ParticipateButton displayPhase={this.displayPhase} timeline={timeline} btnClass="dark" />
-              </div>
+                    <span>{debateData.objectives.descriptionEntries[locale]}</span>
+                  </div>
+                </Col>
+                {debateData.objectives.images && (
+                  <div className="container-objectives-img">
+                    {debateData.objectives.images.img1Url && (
+                      <Col xs={12} sm={6} md={3} className="objectives">
+                        <div
+                          className="objectives-img"
+                          style={{ backgroundImage: `url(${debateData.objectives.images.img1Url})` }}
+                        />
+                      </Col>
+                    )}
+                    {debateData.objectives.images.img2Url && (
+                      <Col xs={12} sm={6} md={3} className="objectives">
+                        <div
+                          className="objectives-img"
+                          style={{ backgroundImage: `url(${debateData.objectives.images.img2Url})` }}
+                        />
+                      </Col>
+                    )}
+                  </div>
+                )}
+              </Row>
+            </div>
+            <div className="center inline full-size margin-xxl">
+              <ParticipateButton displayPhase={displayPhase} timeline={timeline} btnClass="dark" />
             </div>
           </div>
-        </Grid>
-      </section>
-    );
-  }
-}
+        </div>
+      </Grid>
+    </section>
+  );
+};
 
 const mapStateToProps = state => ({
   debate: state.debate,
