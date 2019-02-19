5b67b93637bfe4b612642d64f11369dc320ee135
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2019-02-19 19:33:20 +0100

    Move migration of semantic analysis ahead of attachements - prod is already on 243942b0a23d

--- assembl/alembic/versions/4f529b59026b_add_semantic_analysis_to_section_type.py
+++ assembl/alembic/versions/4f529b59026b_add_semantic_analysis_to_section_type.py
@@ -1,14 +1,14 @@
 """Add semantic analysis to section type
 
 Revision ID: 4f529b59026b
-Revises: 4227dfe5456c
+Revises: 243942b0a23d
 Create Date: 2019-01-17 16:47:22.932933
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '4f529b59026b'
-down_revision = '4227dfe5456c'
+down_revision = '243942b0a23d'
 
 def upgrade(pyramid_env):
     # Do stuff with the app's models here.
