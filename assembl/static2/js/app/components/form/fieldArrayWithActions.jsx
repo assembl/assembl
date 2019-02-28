6cf9942139a77a95e49b2fa105b7d0aad9e459d1
felixsaintgenis <saintgenisfelix@gmail.com>
2019-02-28 17:13:04 +0100

    undo the fix that made the delete unstable (wip)

--- assembl/static2/js/app/components/form/fieldArrayWithActions.jsx
+++ assembl/static2/js/app/components/form/fieldArrayWithActions.jsx
@@ -107,14 +107,14 @@ export class Fields extends React.PureComponent<FieldsProps> {
     fields.swap(index, index - 1);
   };
 
-  removedIndex: ?number = null;
+  // removedIndex: ?number = null;
 
   remove = (index: number) => {
     const { fields, onRemove, isTree, subFieldName } = this.props;
     const fieldValue = fields.value[index];
     const children = subFieldName && isTree && fieldValue[subFieldName];
     if (!children || children.length === 0) {
-      this.removedIndex = index;
+      // this.removedIndex = index;
       if (onRemove) {
         onRemove(fieldValue.id);
       }
@@ -151,17 +151,17 @@ export class Fields extends React.PureComponent<FieldsProps> {
       onUp,
       onDown
     } = this.props;
-    /* Hack to fix issue with richtext field:
-    when clicking on delete action, the value of richtext field becomes '' and so
-    new EditorState.createEmpty() is created triggering onChange event on first render of the field)
-    preventing the whole block to be removed */
-    // console.log(fields.length);
-    const removedIndex = this.removedIndex;
-    if (this.removedIndex) {
-      setTimeout(() => {
-        this.removedIndex = null;
-      }, 2000);
-    }
+    // /* Hack to fix issue with richtext field:
+    // when clicking on delete action, the value of richtext field becomes '' and so
+    // new EditorState.createEmpty() is created triggering onChange event on first render of the field)
+    // preventing the whole block to be removed */
+    // // console.log(fields.length);
+    // const removedIndex = this.removedIndex;
+    // if (this.removedIndex) {
+    //   setTimeout(() => {
+    //     this.removedIndex = null;
+    //   }, 2000);
+    // }
     const isRoot = level === 0;
     const className = level > 0 ? 'form-branch' : 'form-tree';
     const addBtnTop = isTree && !isRoot;
@@ -177,9 +177,9 @@ export class Fields extends React.PureComponent<FieldsProps> {
       <div className={classNames({ [className]: isTree })}>
         {addBtnTop ? addBtn : null}
         {fields.map((fieldname, idx) => {
-          if (idx === removedIndex) {
-            return null;
-          }
+          // if (idx === removedIndex) {
+          //   return null;
+          // }
           const fieldValue = fields.value[idx];
           const hasChildren = fieldValue.children && fieldValue.children.length;
           const enableDeleteBtn = fields.length > minItems && ((isTree && !hasChildren) || !isTree);
