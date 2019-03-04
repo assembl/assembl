4ea42de7904ca79bd4629d6063f56567756e7dce
louise dupas <louise@beyowi.com>
2019-03-04 17:34:29 +0100

    add two icons: addkeyword, cross

---
+++ assembl/static2/js/app/components/common/icons/addkeywordIcon/addkeywordIcon.spec.jsx
@@ -0,0 +1,29 @@
+// @flow
+import React from 'react';
+/* eslint-disable import/no-extraneous-dependencies */
+import initStoryshots from '@storybook/addon-storyshots';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16.3';
+/* eslint-enable */
+
+import AddkeywordIcon from './addkeywordIcon';
+
+// Separate the snapshots in directories next to each component
+// Name should match with the story name
+initStoryshots({
+  storyKindRegex: /^AddkeywordIcon$/
+});
+
+configure({ adapter: new Adapter() });
+
+describe('<AddkeywordIcon /> - with shallow', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<AddkeywordIcon />);
+  });
+
+  it('should render a svg with addkeyword-icon class', () => {
+    expect(wrapper.find('svg [className="addkeyword-icon"]')).toHaveLength(1);
+  });
+});
---
+++ assembl/static2/js/app/components/common/icons/crossIcon/crossIcon.spec.jsx
@@ -0,0 +1,29 @@
+// @flow
+import React from 'react';
+/* eslint-disable import/no-extraneous-dependencies */
+import initStoryshots from '@storybook/addon-storyshots';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16.3';
+/* eslint-enable */
+
+import CrossIcon from './crossIcon';
+
+// Separate the snapshots in directories next to each component
+// Name should match with the story name
+initStoryshots({
+  storyKindRegex: /^CrossIcon$/
+});
+
+configure({ adapter: new Adapter() });
+
+describe('<CrossIcon /> - with shallow', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<CrossIcon />);
+  });
+
+  it('should render a svg with cross-icon class', () => {
+    expect(wrapper.find('svg [className="cross-icon"]')).toHaveLength(1);
+  });
+});
