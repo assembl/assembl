1cee8af89e26d950c0742f607d338822260219e2
Vincent Fretin <vincent.fretin@gmail.com>
2019-03-12 19:44:13 +0100

    better dates validation

--- assembl/static2/js/app/components/administration/discussion/timelineConfiguration/validate.js
+++ assembl/static2/js/app/components/administration/discussion/timelineConfiguration/validate.js
@@ -9,17 +9,28 @@ type Errors = {
   end?: string
 };
 
-const validatePhase = (phase: PhaseValue): Errors => ({
-  title: i18nValueIsEmpty(phase.title) ? I18n.t('error.required') : undefined,
-  start:
-    phase.start && phase.end && validStartDate(phase.start.time, phase.end.time)
+const validatePhase = (phase: PhaseValue): Errors => {
+  const errors = {
+    title: i18nValueIsEmpty(phase.title) ? I18n.t('error.required') : undefined,
+    start: undefined,
+    end: undefined
+  };
+  if (!phase.start || !phase.start.time) {
+    errors.start = I18n.t('error.required');
+  } else if (phase.start && phase.start.time && phase.end && phase.end.time) {
+    errors.start = validStartDate(phase.start.time, phase.end.time)
       ? undefined
-      : I18n.t('administration.landingPage.header.startDateError'),
-  end:
-    phase.start && phase.end && validEndDate(phase.start.time, phase.end.time)
+      : I18n.t('administration.landingPage.header.startDateError');
+  }
+  if (!phase.end || !phase.end.time) {
+    errors.end = I18n.t('error.required');
+  } else if (phase.start && phase.start.time && phase.end && phase.end.time) {
+    errors.end = validEndDate(phase.start.time, phase.end.time)
       ? undefined
-      : I18n.t('administration.landingPage.header.endDateError')
-});
+      : I18n.t('administration.landingPage.header.endDateError');
+  }
+  return errors;
+};
 
 export default function validate(values: PhasesValues): { phases: Array<Errors> } {
   const phases = values.phases.map(validatePhase);
