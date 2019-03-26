96513291940408385607481d739be8fdd2da1439
Mohamed Zayed <zayed2.mohamed@gmail.com>
2019-03-26 11:48:26 +0100

    modify function get_sentiments to return a specific sentiment count

--- assembl/models/idea.py
+++ assembl/models/idea.py
@@ -1183,7 +1183,7 @@ class Idea(HistoryMixin, DiscussionBoundBase):
         return [Widget.uri_generic(l.widget_id)
                 for l in self.active_showing_widget_links]
 
-    def get_total_sentiments(self):
+    def get_total_sentiments(self, sentiment_type=None):
         from .action import SentimentOfPost
         from .post import Post, countable_publication_states
         from .generic import Content
@@ -1203,6 +1203,8 @@ class Idea(HistoryMixin, DiscussionBoundBase):
             *SentimentOfPost.get_discussion_conditions(discussion_id)
         )
         query = query.filter(Post.id.in_(post_ids))
+        if sentiment_type is not None:
+            return query.filter(SentimentOfPost.type == 'sentiment:{}'.format(sentiment_type)).count()
         return query.count()
 
     def get_vote_proposals(self):
