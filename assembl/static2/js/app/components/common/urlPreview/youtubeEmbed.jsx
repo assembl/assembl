5b3ee1528901843df461fabd542fa01867d4fa31
felixsaintgenis <saintgenisfelix@gmail.com>
2019-04-26 17:17:32 +0200

    bugherd-155 resizing of the youtube embed video

--- assembl/static2/js/app/components/common/urlPreview/youtubeEmbed.jsx
+++ assembl/static2/js/app/components/common/urlPreview/youtubeEmbed.jsx
@@ -6,16 +6,18 @@ type Props = {
 };
 
 const YoutubeVideo = ({ id }: Props) => (
-  <div className="video-container">
-    <iframe
-      title="YouTube video"
-      id="ytplayer"
-      type="text/html"
-      width="640"
-      height="360"
-      src={`https://www.youtube.com/embed/${id}?autoplay=0`}
-      frameBorder="0"
-    />
+  <div className="video-wrapper">
+    <div className="video-container">
+      <iframe
+        title="YouTube video"
+        id="ytplayer"
+        type="text/html"
+        width="640"
+        height="360"
+        src={`https://www.youtube.com/embed/${id}?autoplay=0`}
+        frameBorder="0"
+      />
+    </div>
   </div>
 );
 
