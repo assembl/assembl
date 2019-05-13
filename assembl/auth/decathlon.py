7cc46d29b408d66c5fc897e5db73bfc77db2d533
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2019-05-13 17:49:01 +0200

    switch to authorization code flow

--- assembl/auth/decathlon.py
+++ assembl/auth/decathlon.py
@@ -1,5 +1,4 @@
 from social_core.backends.oauth import BaseOAuth2
-from social_core.utils import handle_http_errors
 from urlparse import urljoin
 
 from assembl.lib.config import get
@@ -10,13 +9,16 @@ log = getLogger('assembl')
 
 class DecathlonOAuth(BaseOAuth2):
     """
-    Decathlon OAuth authentication backend.
+    Decathlon OAuth authentication backend. Authorization Code flow.
     """
     name = 'decathlon'
     BASE_AS_URL = get('SOCIAL_AUTH_DECATHLON_BASE_AS_URI')
     AUTHORIZATION_URL = urljoin(BASE_AS_URL, '/as/authorization.oauth2')
+    ACCESS_TOKEN_URL = urljoin(BASE_AS_URL, '/as/token.oauth2')
+    REFRESH_TOKEN_URL = urljoin(BASE_AS_URL, '/token')
 
-    RESPONSE_TYPE = 'token'
+    RESPONSE_TYPE = 'code'
+    ACCESS_TOKEN_METHOD = 'POST'
     SCOPE_SEPARATOR = ' '
     ID_KEY = 'uid'
     REDIRECT_STATE = False
@@ -25,9 +27,18 @@ class DecathlonOAuth(BaseOAuth2):
     def get_scope_argument(self):
         return {'scope': 'profile openid'}
 
+    def auth_headers(self):
+        headers = super(DecathlonOAuth, self).auth_headers()
+        headers.update({'cache-control': 'no-cache'})
+        log.info("[Decathlon][auth_headers][headers] %s" % headers)
+        return headers
+
+    def auth_complete_credentials(self):
+        return self.get_key_and_secret()
+
     def get_user_details(self, response):
         """Return user details from Decathlon account"""
-        print "[Decathlon][get_user_details][response] %s" % response
+        log.info("[Decathlon][get_user_details][response] %s" % response)
         details = {
             'email': response.get('mail'),
             'first_name': response.get('givenName'),
@@ -46,14 +57,20 @@ class DecathlonOAuth(BaseOAuth2):
         except ValueError:
             return None
 
-    @handle_http_errors
-    def auth_complete(self, *args, **kwargs):
-        """Completes login process, must return user instance"""
-        log.info("[Decathlon][auth_complete][args] %s" % ' '.join(args))
-        log.info("[Decathlon][auth_complete][kwargs] %s" % kwargs)
-        log.info("[Decathlon][auth_complete][data] %s" % self.data)
-        log.info("[Decathlon][auth_complete][request_path] %s" % self.strategy.request_url())
-        self.process_error(self.data)
-        # Implicit flow doesn't make API call
-        token = self.data.get('access_token', '')
-        return self.do_auth(token, *args, **kwargs)
+    def refresh_token_params(self, token, *args, **kwargs):
+        return {
+            'grant_type': 'refresh_token',
+            'refresh_token': token
+        }
+
+    def refresh_token(self, token, *args, **kwargs):
+        params = self.refresh_token_params(token, *args, **kwargs)
+        url = self.refresh_token_url()
+        method = self.REFRESH_TOKEN_METHOD
+        key = 'params' if method == 'GET' else 'data'
+        request_args = {'headers': self.auth_headers(),
+                        'method': method,
+                        'auth': self.auth_complete_credentials(),
+                        key: params}
+        request = self.request(url, **request_args)
+        return self.process_refresh_token_response(request, *args, **kwargs)
