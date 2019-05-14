3f6fc4d2b02a138083149a013de675c3e63b41ae
felixsaintgenis <saintgenisfelix@gmail.com>
2019-05-14 11:32:31 +0200

    Revert "bugherd-223: hide numerotation when only one suestion in survey"
    This reverts commit 8fa78e5856dbe88b94c4cbe50d9b3b7f3c72aeac.

--- assembl/static2/js/app/pages/question.jsx
+++ assembl/static2/js/app/pages/question.jsx
@@ -81,7 +81,7 @@ export function DumbQuestion(props: Props) {
               </div>
               <div className="center">
                 <h3 className="collapsed-title">
-                  {numPosts > 1 ? <span>{`${params.questionIndex}/ ${title}`}</span> : <span>{`${title}`}</span>}
+                  <span>{`${params.questionIndex}/ ${title}`}</span>
                 </h3>
                 <Posts
                   isModerating={isModerating}
