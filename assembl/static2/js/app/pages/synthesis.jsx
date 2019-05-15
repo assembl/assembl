1fedbdc5d01991d17249f709634c12f737e49424
Vincent Fretin <vincent.fretin@gmail.com>
2019-05-15 14:43:00 +0200

    put back intro and conclusion for structured synthesis

--- assembl/static2/js/app/pages/synthesis.jsx
+++ assembl/static2/js/app/pages/synthesis.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { compose, graphql } from 'react-apollo';
 import { Col, Grid, Row } from 'react-bootstrap';
+import debounce from 'lodash/debounce';
 
 import Header from '../components/common/header';
 import Section from '../components/common/section';
@@ -11,6 +12,7 @@ import manageErrorAndLoading from '../components/common/manageErrorAndLoading';
 import IdeaSynthesisTree from '../components/synthesis/IdeaSynthesisTree';
 import { getChildren, getPartialTree } from '../utils/tree';
 import SideMenu from '../components/synthesis/sideMenu';
+import { getDomElementOffset } from '../utils/globalFunctions';
 import type { Synthesis } from '../components/synthesis/types.flow';
 
 type SynthesisProps = {
@@ -20,6 +22,8 @@ type SynthesisProps = {
 };
 
 type SynthesisState = {
+  introBlock: ?HTMLElement,
+  conclusionBlock: ?HTMLElement,
   bodyBlock: ?HTMLElement,
   sideMenuNode: ?HTMLElement,
   sideMenuIsHidden: boolean,
@@ -35,6 +39,8 @@ export class DumbSynthesis extends React.Component<SynthesisProps, SynthesisStat
   constructor(props: SynthesisProps) {
     super(props);
     this.state = {
+      introBlock: null,
+      conclusionBlock: null,
       bodyBlock: null,
       sideMenuNode: null,
       sideMenuIsHidden: true,
@@ -44,14 +50,21 @@ export class DumbSynthesis extends React.Component<SynthesisProps, SynthesisStat
     };
   }
 
-  //
-  // componentWillMount() {
-  //   window.addEventListener('scroll', this.updateSideMenuVisibility);
-  // }
-  //
-  // componentWillUnmount() {
-  //   window.removeEventListener('scroll', this.updateSideMenuVisibility);
-  // }
+  componentWillMount() {
+    window.addEventListener('scroll', this.updateSideMenuVisibility);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.updateSideMenuVisibility);
+  }
+
+  updateIntroBlock = (node: HTMLElement) => {
+    this.setState({ introBlock: node });
+  };
+
+  updateConclusionBlock = (node: HTMLElement) => {
+    this.setState({ conclusionBlock: node });
+  };
 
   updateBodyBlock = (node: HTMLElement) => {
     this.setState({ bodyBlock: node });
@@ -61,38 +74,37 @@ export class DumbSynthesis extends React.Component<SynthesisProps, SynthesisStat
     this.setState({ sideMenuNode: node });
   };
 
-  //
-  // updateSideMenuVisibility = debounce(() => {
-  //   const { bodyBlock, sideMenuNode } = this.state;
-  //   if (introBlock && conclusionBlock) {
-  //     const introNodeTop = getDomElementOffset(introBlock).top;
-  //     const introNodeHeight = introBlock.getBoundingClientRect().height;
-  //     const sideMenuHeight = sideMenuNode && sideMenuNode.getBoundingClientRect().height;
-  //     if (sideMenuHeight && sideMenuHeight > 0) {
-  //       this.setState({ sideMenuHeight: sideMenuHeight });
-  //     }
-  //     const conclusionBlockTopOffset = getDomElementOffset(conclusionBlock).top;
-  //     const firstIdeaTopOffset = introNodeTop + introNodeHeight;
-  //     const scroll = window.pageYOffset;
-  //     const { innerHeight, innerWidth } = window;
-  //     const sideMenuTopOffset = innerHeight * sideMenuTopPercentage / 100;
-  //     // this value is the gap between the top of sideMenu and the top of the window set by the css 'top' property
-  //     if (sideMenuHeight && sideMenuHeight + sideMenuTopOffset > innerHeight) {
-  //       this.setState({ sideMenuOverflow: 'scroll', setSideMenuHeight: '80%' });
-  //     }
-  //
-  //     const hasScrollReachedSynthesis = scroll + 60 > firstIdeaTopOffset;
-  //     // 60 corresponds to the gap between the first Idea and the introduction block (synthesis-tree's padding)
-  //     const isBottomReached = scroll + sideMenuTopOffset + this.state.sideMenuHeight + 90 >= conclusionBlockTopOffset;
-  //     // 90 corresponds to the gap between the top of the conclusion block and the top of its div (padding)
-  //
-  //     if (isBottomReached || !hasScrollReachedSynthesis || innerWidth < 1000) {
-  //       this.setState({ sideMenuIsHidden: true });
-  //     } else {
-  //       this.setState({ sideMenuIsHidden: false });
-  //     }
-  //   }
-  // }, 100);
+  updateSideMenuVisibility = debounce(() => {
+    const { introBlock, conclusionBlock, sideMenuNode } = this.state;
+    if (introBlock && conclusionBlock) {
+      const introNodeTop = getDomElementOffset(introBlock).top;
+      const introNodeHeight = introBlock.getBoundingClientRect().height;
+      const sideMenuHeight = sideMenuNode && sideMenuNode.getBoundingClientRect().height;
+      if (sideMenuHeight && sideMenuHeight > 0) {
+        this.setState({ sideMenuHeight: sideMenuHeight });
+      }
+      const conclusionBlockTopOffset = getDomElementOffset(conclusionBlock).top;
+      const firstIdeaTopOffset = introNodeTop + introNodeHeight;
+      const scroll = window.pageYOffset;
+      const { innerHeight, innerWidth } = window;
+      const sideMenuTopOffset = innerHeight * sideMenuTopPercentage / 100;
+      // this value is the gap between the top of sideMenu and the top of the window set by the css 'top' property
+      if (sideMenuHeight && sideMenuHeight + sideMenuTopOffset > innerHeight) {
+        this.setState({ sideMenuOverflow: 'scroll', setSideMenuHeight: '80%' });
+      }
+
+      const hasScrollReachedSynthesis = scroll + 60 > firstIdeaTopOffset;
+      // 60 corresponds to the gap between the first Idea and the introduction block (synthesis-tree's padding)
+      const isBottomReached = scroll + sideMenuTopOffset + this.state.sideMenuHeight + 90 >= conclusionBlockTopOffset;
+      // 90 corresponds to the gap between the top of the conclusion block and the top of its div (padding)
+
+      if (isBottomReached || !hasScrollReachedSynthesis || innerWidth < 1000) {
+        this.setState({ sideMenuIsHidden: true });
+      } else {
+        this.setState({ sideMenuIsHidden: false });
+      }
+    }
+  }, 100);
 
   handleScrollForMenu = (node: HTMLElement) => {
     this.setState({ ideaOnScroll: node.id });
@@ -101,7 +113,7 @@ export class DumbSynthesis extends React.Component<SynthesisProps, SynthesisStat
   render() {
     const { synthesis, routeParams, synthesisPostId } = this.props;
     const { sideMenuIsHidden, ideaOnScroll, sideMenuOverflow, setSideMenuHeight } = this.state;
-    const { body, ideas, subject } = synthesis;
+    const { introduction, conclusion, body, ideas, subject } = synthesis;
     const sortedIdeas = [...ideas].sort((a, b) => {
       if (a.live.order < b.live.order) {
         return -1;
@@ -118,13 +130,24 @@ export class DumbSynthesis extends React.Component<SynthesisProps, SynthesisStat
         <div className="background-light">
           <Header title={subject} imgUrl={synthesis.img ? synthesis.img.externalUrl : ''} type="synthesis" />
           <Grid fluid>
-            <Section title={''} className="synthesis-block" innerRef={this.updateBodyBlock}>
-              <Row>
-                <Col mdOffset={3} md={8} smOffset={1} sm={10}>
-                  <div dangerouslySetInnerHTML={{ __html: body }} />
-                </Col>
-              </Row>
-            </Section>
+            {introduction && (
+              <Section title="introduction" translate className="synthesis-block" innerRef={this.updateIntroBlock}>
+                <Row>
+                  <Col mdOffset={3} md={8} smOffset={1} sm={10}>
+                    <div dangerouslySetInnerHTML={{ __html: introduction }} />
+                  </Col>
+                </Row>
+              </Section>
+            )}
+            {body && (
+              <Section title="" className="synthesis-block" innerRef={this.updateBodyBlock}>
+                <Row>
+                  <Col mdOffset={3} md={8} smOffset={1} sm={10}>
+                    <div dangerouslySetInnerHTML={{ __html: body }} />
+                  </Col>
+                </Row>
+              </Section>
+            )}
             <Row className="background-grey synthesis-tree">
               <Col
                 md={3}
@@ -159,6 +182,21 @@ export class DumbSynthesis extends React.Component<SynthesisProps, SynthesisStat
                 ))}
               </Col>
             </Row>
+            {conclusion && (
+              <Section
+                title="conclusion"
+                translate
+                className="synthesis-block"
+                id="conclusion"
+                innerRef={this.updateConclusionBlock}
+              >
+                <Row>
+                  <Col mdOffset={3} md={8} smOffset={1} sm={10}>
+                    <div dangerouslySetInnerHTML={{ __html: conclusion }} />
+                  </Col>
+                </Row>
+              </Section>
+            )}
           </Grid>
         </div>
       </div>
