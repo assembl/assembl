417b44dec52f7498584c82728499ed056bf8920a
MathieuVDP <mathieu.vander-plaetse@bluenove.com>
2019-06-03 14:11:04 +0200

    Add list of generic mimetypes (such as image/*) and handling it

--- assembl/graphql/document.py
+++ assembl/graphql/document.py
@@ -3,6 +3,7 @@ from graphene.relay import Node
 import ntpath
 from pyramid.i18n import TranslationStringFactory
 from pyramid.httpexceptions import HTTPUnauthorized
+from pyramid.settings import aslist
 import magic
 
 from graphene_sqlalchemy import SQLAlchemyObjectType
