f713fc3e0505c016190aac0fc4dabc2768497965
felixsaintgenis <saintgenisfelix@gmail.com>
2019-06-05 14:30:45 +0200

    delete the subject prefix for a post

--- assembl/static2/js/app/components/debate/common/post/index.jsx
+++ assembl/static2/js/app/components/debate/common/post/index.jsx
@@ -182,12 +182,7 @@ export class DumbPost extends React.PureComponent<Props, State> {
     const translate = contentLocale !== originalLocale;
     const { body, subject, originalBody, originalSubject } = this.getBodyAndSubject(translate);
 
-    const modifiedSubject = (
-      <span>
-        {getSubjectPrefixString(fullLevel)}
-        {subject ? subject.replace('Re: ', '') : ''}
-      </span>
-    );
+    const modifiedSubject = <span>{subject ? subject.replace('Re: ', '') : ''}</span>;
 
     const editPostareaRef = (el: ?HTMLTextAreaElement) => {
       this.editPostarea = el;
