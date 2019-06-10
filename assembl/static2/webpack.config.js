21bd0a01c3d9a0c6ed34808575852f690dae70aa
Vincent Fretin <vincent.fretin@gmail.com>
2019-06-10 09:09:25 +0200

    remove uglifyjs-webpack-plugin, latest webpack now use terser by default with production mode for minification

--- assembl/static2/webpack.config.js
+++ assembl/static2/webpack.config.js
@@ -5,11 +5,9 @@ Once you have made changes to this file, you have to run `supervisorctl restart 
 var path = require('path');
 var webpack = require('webpack');
 var MiniCssExtractPlugin = require("mini-css-extract-plugin");
-var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var glob = require('glob');
 var _ = require('lodash');
-
 
 function theme_entries() {
     var entries = {},
@@ -123,11 +121,6 @@ module.exports = {
         },
     },
     mode: 'production',
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({ sourceMap: true, parallel: true, cache: true })
-        ]
-    },
     plugins: [
         new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
         new HtmlWebpackPlugin({ title: 'Caching', filename: 'resources.html'}),
