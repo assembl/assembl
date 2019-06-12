57a4ea812b6f318540523cbfe5a516e0644c63b2
Gary McDowell <gary.mcdowell@bluenove.com>
2019-06-12 12:37:03 +0200

    PR comments

--- assembl/static2/js/app/utils/csrf.js
+++ assembl/static2/js/app/utils/csrf.js
@@ -1,11 +1,12 @@
-import request from 'superagent';
 import urljoin from 'url-join';
 
 import { basePath } from './server';
 
 export async function getCSRFToken() {
   // Get CSRF token, note the response contains nothing as the cookie is set directly!!
-  const responseToken = await request.get(urljoin(basePath(), '/v1/generateToken')).withCredentials();
+  const responseToken = await fetch.get(urljoin(basePath(), '/v1/generateToken'), {
+    credentials: 'include'
+  });
 
   return responseToken;
 }
