1c4c2c07f8c6bfd79ec1ec5979a1686c2c2b4257
Vincent Fretin <vincent.fretin@gmail.com>
2019-06-12 10:20:02 +0200

    remove unused Card and CardList components

--- assembl/static2/js/app/stories/components/common/common.jsx
+++ assembl/static2/js/app/stories/components/common/common.jsx
@@ -3,31 +3,10 @@
 */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Link } from 'react-router';
 
-import Card from '../../../components/common/card';
 import Loader from '../../../components/common/loader';
 import SwitchButton from '../../../components/common/switchButton';
 import TabbedContent from '../../../components/common/tabbedContent';
-
-const cardContent = (
-  <Link className="content-box">
-    <div className="title-container center">
-      <h3 className="light-title-3">Sunt quas ratione culpa</h3>
-      <h4 className="light-title-4">
-        Commodi architecto fugiat maxime quod. Dignissimos aut atque. Qui autem qui et sed voluptas iure suscipit. Est sit alias
-        voluptatibus. Aliquam aperiam dolores dolores sint.
-      </h4>
-    </div>
-  </Link>
-);
-
-const imgUrl = 'https://images.unsplash.com/uploads/141155339325423394b24/03982423';
-
-storiesOf('Card', module)
-  .add('default', () => <Card>{cardContent}</Card>)
-  .add('with className (margin)', () => <Card className="margin-xxl">{cardContent}</Card>)
-  .add('with imgUrl', () => <Card imgUrl={imgUrl}>{cardContent}</Card>);
 
 storiesOf('Loader', module)
   .add('with text', () => <Loader textHidden={false} />)
