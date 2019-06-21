04c0c17f39344a6e58374f90b76f2d11c5f9e79b
Gary McDowell <gary.mcdowell@bluenove.com>
2019-06-21 10:58:08 +0200

    Change stargate port to be soft-configured

--- assembl/static2/js/app/utils/server.js
+++ assembl/static2/js/app/utils/server.js
@@ -5,11 +5,12 @@
  * */
 export const basePath = () => {
   const useStargate = document.getElementById('useStargate') ? document.getElementById('useStargate').value : 'false';
+  const stargatePort = document.getElementById('stargatePort') ? document.getElementById('stargatePort').value : '3000';
 
   let path = `${window.location.protocol}//${window.location.host}`;
 
   if (useStargate === 'true') {
-    path = `${window.location.protocol}//${window.location.hostname}:3000`; // move to config ?
+    path = `${window.location.protocol}//${window.location.hostname}:${stargatePort}`;
   }
 
   return path;
