e12235b0613c6148e33649d770088cf1ed8ebf98
Gary McDowell <gary.mcdowell@bluenove.com>
2019-06-21 13:30:42 +0200

    Wrong syntax for fetch

--- assembl/static2/js/app/utils/csrf.js
+++ assembl/static2/js/app/utils/csrf.js
@@ -4,7 +4,7 @@ import { basePath } from './server';
 
 export async function getCSRFToken() {
   // Get CSRF token, note the response contains nothing as the cookie is set directly!!
-  const responseToken = await fetch.get(urljoin(basePath(), '/v1/generateToken'), {
+  const responseToken = await fetch(urljoin(basePath(), '/v1/generateToken'), {
     credentials: 'include'
   });
 
--- assembl/static2/js/app/utils/server.js
+++ assembl/static2/js/app/utils/server.js
@@ -1,8 +1,8 @@
-/** *
+/**
  * Use the standard assembl server location for API calls or Stargate
  *
  * @return String path - the path to use for the API call
- * */
+ */
 export const basePath = () => {
   const useStargate = document.getElementById('useStargate') ? document.getElementById('useStargate').value : 'false';
   const stargatePort = document.getElementById('stargatePort') ? document.getElementById('stargatePort').value : '3000';
