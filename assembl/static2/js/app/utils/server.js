f7bc227ca687faa5512f6b9f65f90590d0a5db9b
Gary McDowell <gary.mcdowell@bluenove.com>
2019-06-26 09:45:07 +0200

    Check header for URL

--- assembl/static2/js/app/utils/server.js
+++ assembl/static2/js/app/utils/server.js
@@ -12,7 +12,13 @@ export const basePath = (obj) => {
   let path = `${window.location.protocol}//${window.location.host}`;
 
   // Only pass json requests
-  if (obj.headers['Content-Type'] !== 'application/x-www-form-urlencoded') {
+  if (obj && obj.headers['Content-Type'] !== 'application/x-www-form-urlencoded') {
+    if (useStargate === 'true') {
+      path = `${window.location.protocol}//${window.location.hostname}:${stargatePort}`;
+    }
+  }
+
+  if (!obj) {
     if (useStargate === 'true') {
       path = `${window.location.protocol}//${window.location.hostname}:${stargatePort}`;
     }
