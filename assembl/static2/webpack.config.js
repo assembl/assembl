3e187e49d09d505fd7f580222e1cc2087b93a6ac
Gary McDowell <gary.mcdowell@bluenove.com>
2019-07-04 13:33:45 +0200

    Patch master with dnd-core transpile

--- assembl/static2/webpack.config.js
+++ assembl/static2/webpack.config.js
@@ -75,6 +75,7 @@ module.exports = {
               path.join(__dirname, 'css'),
               path.join(__dirname, 'workspaces'),
               path.join(__dirname, 'node_modules/rc-slider')
+              path.join(__dirname, 'node_modules/dnd-core')
             ]
         },
         {
