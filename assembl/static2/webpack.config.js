7776453c9d90965864f56b3f9d1558f44df49332
Gary McDowell <gary.mcdowell@bluenove.com>
2019-07-05 10:46:37 +0200

    Remove dnd-core, breaking things, this will break IE as well

--- assembl/static2/webpack.config.js
+++ assembl/static2/webpack.config.js
@@ -74,8 +74,7 @@ module.exports = {
               path.join(__dirname, 'js'),
               path.join(__dirname, 'css'),
               path.join(__dirname, 'workspaces'),
-              path.join(__dirname, 'node_modules/rc-slider'),
-              path.join(__dirname, 'node_modules/dnd-core')
+              path.join(__dirname, 'node_modules/rc-slider')
             ]
         },
         {
