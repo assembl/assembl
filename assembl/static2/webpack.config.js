32012f1ee5ee61b0bd434da311a92b16986367b2
Gary McDowell <gary.mcdowell@bluenove.com>
2019-07-23 09:10:49 +0200

    Revert change

--- assembl/static2/webpack.config.js
+++ assembl/static2/webpack.config.js
@@ -75,7 +75,7 @@ module.exports = {
               path.join(__dirname, 'js'),
               path.join(__dirname, 'css'),
               path.join(__dirname, 'workspaces'),
-              path.join(__dirname, 'node_modules')
+              path.join(__dirname, 'node_modules/rc-slider')
             ]
         },
         {
