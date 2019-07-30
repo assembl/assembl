3fdad37007c724aa157363154a832d63c8bd1bc2
Thomas Desvenain <thomas.desvenain@gmail.com>
2019-07-30 16:36:01 +0200

    [Thread filter] test filter my posts and answers

--- assembl/tests/views_tests/test_graphql_ideas_with_post.py
+++ assembl/tests/views_tests/test_graphql_ideas_with_post.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from graphql_relay import from_global_id
 
 from assembl.graphql.schema import Schema as schema
 
@@ -78,3 +79,41 @@ def test_idea_with_posts_only_my_posts_participant1(graphql_request, graphql_reg
     assert len(posts) == 1
     assert posts[0]['node']['creator']['displayName'] == u"A. Barking Loon"
     assert posts[0]['node']['subjectEntries'][0]['value'] == u"Post de participant1"
+
+
+def test_idea_with_posts_answers_to_my_posts_show_my_posts(graphql_request, graphql_registry, idea_in_thread_phase,
+                                             top_post_in_thread_phase, participant1_post_in_thread_phase,
+                                             participant1_user):
+    graphql_request.authenticated_userid = participant1_user.id
+    res = schema.execute(
+        graphql_registry['IdeaWithPostsQuery'],
+        context_value=graphql_request,
+        variable_values={"id": idea_in_thread_phase, "lang": "en", "additionalFields": True,
+                         "onlyMyPosts": False, "myPostsAndAnswers": True},
+    )
+    assert res.errors is None
+    posts = res.data['idea']['posts']['edges']
+    assert len(posts) == 1
+    assert posts[0]['node']['creator']['displayName'] == u"A. Barking Loon"
+    assert posts[0]['node']['subjectEntries'][0]['value'] == u"Post de participant1"
+
+
+def test_idea_with_posts_answers_to_my_posts_show_answers(graphql_request, graphql_registry, idea_in_thread_phase,
+                                             top_post_in_thread_phase, participant1_post_in_thread_phase,
+                                             third_post_in_thread_phase, participant1_user):
+    from assembl.models import Post
+    # top_post_in_thread_phase admin's post is an answer of participant1_post_in_thread_phase
+    participant_instance_id = int(from_global_id(participant1_post_in_thread_phase)[1])
+    admin_instance_id = int(from_global_id(top_post_in_thread_phase)[1])
+    Post.get(admin_instance_id).parent_id = participant_instance_id
+
+    graphql_request.authenticated_userid = participant1_user.id
+    res = schema.execute(
+        graphql_registry['IdeaWithPostsQuery'],
+        context_value=graphql_request,
+        variable_values={"id": idea_in_thread_phase, "lang": "en", "additionalFields": True,
+                         "onlyMyPosts": False, "myPostsAndAnswers": True},
+    )
+    assert res.errors is None
+    posts = res.data['idea']['posts']['edges']
+    assert len(posts) == 2
