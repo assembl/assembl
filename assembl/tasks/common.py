a9042e45ab87c0e4983a8953fdcf07e8e80f3f02
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2019-08-22 17:09:26 +0200

    Revert "get_secrets function should be a callable task"
    This reverts commit 0a9b0238d8280472bfe673b57986fcda5d0cc01a.

--- assembl/tasks/common.py
+++ assembl/tasks/common.py
@@ -114,7 +114,6 @@ def get_cached_secrets(c):
     return base
 
 
-@task()
 def get_secrets(c, aws_secret_ids, cache=True, reset_cache=False):
     if exists(c, "secrets.yaml") and not reset_cache:
         return get_cached_secrets(c)
