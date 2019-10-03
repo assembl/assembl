dfdceaf2937a06e0e06e34dbcc5f79dad032e3e5
Thomas Desvenain <thomas.desvenain@gmail.com>
2019-10-03 17:14:41 +0200

    [Hashtags] search idea posts with hashtags

--- assembl/tests/views_tests/test_graphql_ideas_with_post.py
+++ assembl/tests/views_tests/test_graphql_ideas_with_post.py
@@ -49,7 +49,7 @@ def test_idea_with_posts_chronological(graphql_request, graphql_registry, idea_i
     assert posts[0]['node']['subjectEntries'][0]['value'] == u"Manger des choux à la crème"
     assert posts[2]['node']['subjectEntries'][0]['value'] == u"Troisième post"
 
-# FIXME: give permission to participant1 so he can add a post
+
 def test_idea_with_posts_only_my_posts_admin(admin_user, graphql_request, graphql_registry, idea_in_thread_phase,
                                              top_post_in_thread_phase, participant1_post_in_thread_phase,
                                              ):
@@ -117,3 +117,57 @@ def test_idea_with_posts_answers_to_my_posts_show_answers(graphql_request, graph
     assert res.errors is None
     posts = res.data['idea']['posts']['edges']
     assert len(posts) == 2
+
+
+def test_idea_with_posts_hashtags_one(graphql_request, graphql_registry, idea_in_thread_phase, top_post_in_thread_phase,
+                                      second_post_in_thread_phase, third_post_in_thread_phase):
+    from assembl.models import Post
+    # top_post_in_thread_phase admin's post is an answer of participant1_post_in_thread_phase
+    top_post_instance_id = int(from_global_id(top_post_in_thread_phase)[1])
+    post = Post.get(top_post_instance_id)
+    post.body.entries[0].value = "The quick brown #fox jumps over the lazy #dog."
+    post.db.flush()
+
+    second_post_instance_id = int(from_global_id(second_post_in_thread_phase)[1])
+    post2 = Post.get(second_post_instance_id)
+    post2.body.entries[0].value = "The quick brown #fox jumps over the lazy #mouse."
+
+    res = schema.execute(
+        graphql_registry['IdeaWithPostsQuery'],
+        context_value=graphql_request,
+        variable_values={
+            "id": idea_in_thread_phase,
+            "additionalFields": True, "lang": "en", "hashtags": ['fox']
+        },
+    )
+    assert res.errors is None
+    posts = res.data['idea']['posts']['edges']
+    assert len(posts) == 2
+
+def test_idea_with_posts_hashtags_several(
+    graphql_request,
+    graphql_registry, idea_in_thread_phase, top_post_in_thread_phase,
+    second_post_in_thread_phase, third_post_in_thread_phase):
+    from assembl.models import Post
+    # top_post_in_thread_phase admin's post is an answer of participant1_post_in_thread_phase
+    top_post_instance_id = int(from_global_id(top_post_in_thread_phase)[1])
+    post = Post.get(top_post_instance_id)
+    post.body.entries[0].value = "The quick brown #fox jumps over the lazy #dog."
+
+    second_post_instance_id = int(from_global_id(second_post_in_thread_phase)[1])
+    post2 = Post.get(second_post_instance_id)
+    post2.body.entries[0].value = "The quick brown #fox jumps over the lazy #mouse."
+
+    post.db.flush()
+
+    res = schema.execute(
+        graphql_registry['IdeaWithPostsQuery'],
+        context_value=graphql_request,
+        variable_values={
+            "id": idea_in_thread_phase,
+            "additionalFields": True, "lang": "en", "hashtags": ['dog', 'fox']
+        },
+    )
+    assert res.errors is None
+    posts = res.data['idea']['posts']['edges']
+    assert len(posts) == 1
