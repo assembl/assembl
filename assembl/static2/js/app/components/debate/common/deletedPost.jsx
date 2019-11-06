c8b6255a36a11dc8911df5a4e42961adb1f7151d
felixsaintgenis <saintgenisfelix@gmail.com>
2019-11-06 11:00:30 +0100

    delete subjerct when post is deleted

--- assembl/static2/js/app/components/debate/common/deletedPost.jsx
+++ assembl/static2/js/app/components/debate/common/deletedPost.jsx
@@ -7,16 +7,17 @@ type DeletedByType = 'user' | 'admin';
 
 type DeletedPostProps = {
   id: string,
-  subject: React.Node,
   deletedBy: DeletedByType
 };
 
-const DeletedPost = ({ id, subject, deletedBy }: DeletedPostProps): React.Element<any> => (
+const DeletedPost = ({ id, deletedBy }: DeletedPostProps): React.Element<any> => (
   <div className="posts deleted-post" id={id}>
     <div className="box">
       <Row className="post-row">
         <Col xs={12} md={12} className="post-left">
-          <h3 className="dark-title-3">{subject}</h3>
+          <h3 className="dark-title-3">
+            <Translate value="debate.thread.postDeleted" />
+          </h3>
           <div className="body">
             <Translate value={deletedBy === 'user' ? 'debate.thread.postDeletedByUser' : 'debate.thread.postDeletedByAdmin'} />
           </div>
